2d33fbce62f5527dbc26f7eb1eac4fd7bb772004
Nelis <seregon888@gmail.com>
2012-11-09 16:57:50 +0000

    Added MyGUI libraries

--- src/ogre/BasicTutorial6.h
+++ src/ogre/BasicTutorial6.h
@@ -1,35 +1,56 @@
-#include<OgreRoot.h>
-
+/*
+-----------------------------------------------------------------------------
+Filename:    BasicTutorial6.h
+-----------------------------------------------------------------------------
+ 
+This source file is part of the
+   ___                 __    __ _ _    _ 
+  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+/ \_// (_| | | |  __/  \  /\  /| |   <| |
+\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+      |___/                              
+      Tutorial Framework
+      http://www.ogre3d.org/tikiwiki/
+-----------------------------------------------------------------------------
+*/
+#ifndef __BasicTutorial6_h_
+#define __BasicTutorial6_h_
+ 
+#include <OgreRoot.h>
 #include <OISEvents.h>
 #include <OISInputManager.h>
 #include <OISKeyboard.h>
 #include <OISMouse.h>
-
 #include <OgreWindowEventUtilities.h>
-
+ 
 class BasicTutorial6 : public Ogre::WindowEventListener, public Ogre::FrameListener
 {
-private:
-    Ogre::Root* mRoot;
-    Ogre::String mPluginsCfg;
-    Ogre::String mResourcesCfg;
-    Ogre::RenderWindow* mWindow;
-    
-    Ogre::SceneManager* mSceneMgr;
-    Ogre::Camera* mCamera;
-    
-    // OIS Input devices
-    OIS::InputManager* mInputManager;
-    OIS::Mouse*    mMouse;
-    OIS::Keyboard* mKeyboard;
-protected:
-    /** testing 2 */
-    virtual void windowResized(Ogre::RenderWindow* rw);
-    virtual void windowClosed(Ogre::RenderWindow* rw);
-    // FrameListener
-    virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
 public:
     BasicTutorial6(void);
     virtual ~BasicTutorial6(void);
     bool go(void);
+ 
+protected:
+    // Ogre::WindowEventListener
+    virtual void windowResized(Ogre::RenderWindow* rw);
+    virtual void windowClosed(Ogre::RenderWindow* rw);
+ 
+    // Ogre::FrameListener
+    virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
+ 
+private:
+    Ogre::Root* mRoot;
+    Ogre::String mResourcesCfg;
+    Ogre::String mPluginsCfg;
+    Ogre::RenderWindow* mWindow;
+    Ogre::SceneManager* mSceneMgr;
+    Ogre::Camera* mCamera;
+ 
+    // OIS Input devices
+    OIS::InputManager* mInputManager;
+    OIS::Mouse*    mMouse;
+    OIS::Keyboard* mKeyboard;
 };
+ 
+#endif // #ifndef __BasicTutorial6_h_
