b233a0f295ccb0ba4f6c895be7c2066d8f9f1651
Nelis <seregon888@gmail.com>
2013-01-28 02:03:54 +0000

    Basic setup with background plane (using skyplane)

--- src/ogre/Main.h
+++ src/ogre/Main.h
@@ -1,26 +1,8 @@
-/*
------------------------------------------------------------------------------
-Filename:    BasicTutorial6.h
------------------------------------------------------------------------------
- 
-This source file is part of the
-   ___                 __    __ _ _    _ 
-  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
- //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
-/ \_// (_| | | |  __/  \  /\  /| |   <| |
-\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
-      |___/                              
-      Tutorial Framework
-      http://www.ogre3d.org/tikiwiki/
------------------------------------------------------------------------------
-*/
 #ifndef __BasicTutorial6_h_
 #define __BasicTutorial6_h_
 
 #include "CellStage/World.h"
  
-#include <memory>
-
 #include <OgreRoot.h>
 #include <OISEvents.h>
 #include <OISInputManager.h>
@@ -29,7 +11,6 @@ This source file is part of the
 #include <OgreWindowEventUtilities.h>
  
 class Main : public Ogre::WindowEventListener, public Ogre::FrameListener
-        //, public OIS::MouseListener, public OIS::KeyListener
 {
 public:
     Main(void);
@@ -44,12 +25,6 @@ protected:
     // Ogre::FrameListener
     virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
  
-    // OIS Listeners
-//    bool mouseMoved( const OIS::MouseEvent &arg );
-//    bool mousePressed( const OIS::MouseEvent &arg, OIS::MouseButtonID id );
-//    bool mouseReleased( const OIS::MouseEvent &arg, OIS::MouseButtonID id );
-//    bool keyPressed( const OIS::KeyEvent &arg );
-//    bool keyReleased( const OIS::KeyEvent &arg );
 private:
     Ogre::Root*         mRoot;
     Ogre::String        mResourcesCfg;
