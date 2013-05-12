a30d476ffb5a0da72ec6e3a3ddd348912ff7a919
Benjamin Kloster <benjamin.kloster@videlco.eu>
2013-05-12 14:54:44 +0200

    Refactored EntityFilter to be easier to document

--- src/engine/entity_filter.h
+++ src/engine/entity_filter.h
@@ -21,12 +21,8 @@ namespace thrive {
 template<typename ComponentType>
 struct Optional {};
 
+namespace detail {
 
-/**
-* @brief Extracts the Component class from a filter argument
-*
-* @tparam ComponentType A bare Component class
-*/
 template<typename ComponentType>
 struct ExtractComponentType {
 
@@ -36,13 +32,6 @@ struct ExtractComponentType {
 
 };
 
-
-/**
-* @brief Extracts the Component class from a filter argument
-* wrapped by the Optional template
-*
-* @tparam ComponentType A Component class wrapped by Op
-*/
 template<typename ComponentType>
 struct ExtractComponentType<Optional<ComponentType>> {
 
@@ -52,265 +41,99 @@ struct ExtractComponentType<Optional<ComponentType>> {
 
 };
 
-
-template<typename ComponentType>
-struct IsRequired {
-    
-    static const bool value = true;
-
-};
+} // namespace detail
 
 
-template<typename ComponentType>
-struct IsRequired<Optional<ComponentType>> {
-
-    static const bool value = false;
-
-};
-
-
-template<size_t index, typename... ComponentTypes>
-struct ComponentGroupBuilder {
-
-    using ComponentGroup = std::tuple<
-        typename ExtractComponentType<ComponentTypes>::PointerType...
-    >;
-
-    static bool 
-    build(
-        Engine* engine,
-        EntityId entityId,
-        ComponentGroup& group
-    ) {
-        using ComponentType = typename std::tuple_element<index, std::tuple<ComponentTypes...>>::type;
-        using RawType = typename ExtractComponentType<ComponentType>::Type;
-        bool isRequired = IsRequired<ComponentType>::value;
-        RawType* component = engine->getComponent<RawType>(entityId);
-        if (isRequired and not component) {
-            return false;
-        }
-        std::get<index>(group) = component;
-        return ComponentGroupBuilder<index-1, ComponentTypes...>::build(engine, entityId, group);
-    }
-        
-};
-
-
-template<typename... ComponentTypes>
-struct ComponentGroupBuilder<0, ComponentTypes...> {
-
-    static bool 
-    build(
-        Engine* engine,
-        EntityId entityId,
-        std::tuple<typename ExtractComponentType<ComponentTypes>::PointerType...>& group
-    ) {
-        using ComponentType = typename std::tuple_element<0, std::tuple<ComponentTypes...>>::type;
-        using RawType = typename ExtractComponentType<ComponentType>::Type;
-        bool isRequired = IsRequired<ComponentType>::value;
-        RawType* component = engine->getComponent<RawType>(entityId);
-        if (isRequired and not component) {
-            return false;
-        }
-        std::get<0>(group) = component;
-        return true;
-    }
-        
-};
-
-template<size_t tupleIndex>
-struct RegisterNextCallback {
-    
-    template<typename Filter>
-    static void registerNextCallback(
-        Filter& filter
-    ) {
-        // May the programming gods have mercy for the poor souls
-        // who will have to read this.
-        filter.template registerCallback<tupleIndex-1>();
-    }
-};
-
-template<>
-struct RegisterNextCallback<0> {
-
-    template<typename Filter>
-    static void registerNextCallback(Filter&) {}
-};
-
-
+/**
+* @brief Filters for entities that contain specific components
+*
+* An entity filter helps a system in finding the entities that have exactly
+* the right components to be relevant for the system. 
+*
+* @tparam ComponentTypes
+*   The component classes to watch for. You can wrap a class with the 
+*   Optional template if you want to know if it's there, but it's not
+*   required.
+*
+* Usage example:
+* \code
+* class MySystem : public System {
+*
+* private:
+*   
+*   EntityFilter<
+*       MyComponent,
+*       Optional<SomeOtherComponent>
+*   >
+*   m_entities;
+*
+* public:
+*
+*   void init(Engine* engine) override {
+*       System::init(engine);
+*       m_entities.setEngine(engine);
+*   }
+*
+*   void update(int milliseconds) override {
+*       for (auto& value : m_entities) {
+*           EntityId entity = value.first;
+*           MyComponent* myComponent = std::get<0>(value.second);
+*           // Do something with myComponent
+*           SomeOtherComponent* someOtherComponent = std::get<1>(value.second);
+*           if (someOtherComponent) {
+*               // Do something with someOtherComponent
+*           }
+*       }
+*   }
+*
+*   void shutdown() overrde {
+*       m_entities.setEngine(nullptr);
+*       System::shutdown();
+*   }
+* };
+* \endcode
+*/
 template<typename... ComponentTypes>
 class EntityFilter {
 
 public:
 
     using ComponentGroup = std::tuple<
-        typename ExtractComponentType<ComponentTypes>::PointerType...
+        typename detail::ExtractComponentType<ComponentTypes>::PointerType...
     >;
 
     using EntityMap = std::unordered_map<EntityId, ComponentGroup>;
 
     EntityFilter(
         bool recordChanges = false
-    ) : m_recordChanges(recordChanges)
-    {
-    }
+    );
+
+    ~EntityFilter() {}
 
     std::unordered_set<EntityId>&
-    addedEntities() {
-        assert(m_recordChanges && "Added entities are not recorded by this filter");
-        return m_addedEntities;
-    }
+    addedEntities();
+
+    typename EntityMap::const_iterator
+    begin() const;
+
+    typename EntityMap::const_iterator
+    end() const;
 
     const EntityMap&
-    entities() const {
-        return m_entities;
-    };
+    entities() const;
 
     std::unordered_set<EntityId>&
-    removedEntities() {
-        assert(m_recordChanges && "Removed entities are not recorded by this filter");
-        return m_removedEntities;
-    }
+    removedEntities();
 
     void
     setEngine(
         Engine* engine
-    ) {
-        this->unregisterCallbacks();
-        m_entities.clear();
-        m_addedEntities.clear();
-        m_removedEntities.clear();
-        m_engine = engine;
-        if (engine) {
-            this->registerCallback<sizeof...(ComponentTypes)-1>();
-            this->initEntities();
-        }
-    }
+    );
 
 private:
 
-    template<size_t> friend class RegisterNextCallback;
-
-    void
-    initEntities() {
-        for (EntityId id : m_engine->entities()) {
-            this->initEntity(id);
-        }
-    }
-
-    void
-    initEntity(
-        EntityId id
-    ) {
-        ComponentGroup group;
-        bool isComplete = ComponentGroupBuilder<sizeof...(ComponentTypes) - 1, ComponentTypes...>::build(
-            m_engine,
-            id,
-            group
-        );
-        if (isComplete) {
-            m_entities[id] = group;
-            if (m_recordChanges) {
-                m_addedEntities.insert(id);
-            }
-        }
-    }
-
-    void
-    onComponentAdded(
-        EntityId entityId
-    ) {
-        this->initEntity(entityId);
-    }
-
-    template<int tupleIndex>
-    void
-    onOptionalComponentRemoved(
-        EntityId entityId
-    ) {
-        auto iter = m_entities.find(entityId);
-        if (iter != m_entities.end()) {
-            std::get<tupleIndex>(iter->second) = nullptr;
-            if (iter->second == ComponentGroup()) {
-                m_entities.erase(entityId);
-                if (m_recordChanges) {
-                    m_removedEntities.insert(entityId);
-                }
-            }
-        }
-    }
-
-    void
-    onRequiredComponentRemoved(
-        EntityId entityId
-    ) {
-        m_entities.erase(entityId);
-        if (m_recordChanges) {
-            m_removedEntities.insert(entityId);
-        }
-    }
-
-    template<int tupleIndex>
-    void
-    registerCallback() {
-        using ComponentType = typename std::tuple_element<
-            tupleIndex, 
-            std::tuple<ComponentTypes...>
-        >::type;
-        using RawType = typename ExtractComponentType<ComponentType>::Type;
-        bool isRequired = IsRequired<ComponentType>::value;
-        auto& collection = m_engine->getComponentCollection(
-            RawType::TYPE_ID()
-        );
-        // Callbacks
-        auto onAdded = [this] (EntityId id, Component&) {
-            this->onComponentAdded(id);
-        };
-        ComponentCollection::ChangeCallback onRemoved;
-        if (isRequired) {
-            onRemoved = [this] (EntityId id, Component&) {
-                this->onRequiredComponentRemoved(id);
-            };
-        }
-        else {
-            onRemoved = [this] (EntityId id, Component&) {
-                this->onOptionalComponentRemoved<tupleIndex>(id);
-            };
-        }
-        unsigned int id = collection.registerChangeCallbacks(
-            onAdded,
-            onRemoved
-        );
-        m_registeredCallbacks.push_front(
-            std::make_pair(std::ref(collection), id)
-        );
-        RegisterNextCallback<tupleIndex>::registerNextCallback(*this);
-    }
-
-    void
-    unregisterCallbacks() {
-        for(auto& pair : m_registeredCallbacks) {
-            pair.first.get().unregisterChangeCallbacks(pair.second);
-        }
-        m_registeredCallbacks.clear();
-    }
-
-    std::unordered_set<EntityId> m_addedEntities;
-
-    Engine* m_engine = nullptr;
-
-    EntityMap m_entities;
-
-    bool m_recordChanges;
-
-    std::forward_list<std::pair<
-        std::reference_wrapper<ComponentCollection>, 
-        unsigned int
-    >> m_registeredCallbacks;
-
-    std::unordered_set<EntityId> m_removedEntities;
-
+    struct Implementation;
+    std::unique_ptr<Implementation> m_impl;
 };
 
 template<>
@@ -318,3 +141,5 @@ class EntityFilter<> {
 };
 
 }
+
+#include "engine/entity_filter.cpp"
