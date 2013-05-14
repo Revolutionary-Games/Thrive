fedf902fd8c2d249f873c953207dcb374697d8aa
Dani Ferri Tomas <daniferrito2@hotmail.com>
2013-05-14 13:46:39 +0200

    Added missing files

---
+++ cmake_modules/FindirrKlang.cmake
@@ -0,0 +1,51 @@
+if (IRRKLANG_LIBRARIES AND IRRKLANG_INCLUDE_DIRS)
+	  # in cache already
+	  set(IRRKLANG_FOUND TRUE)
+	else (IRRKLANG_LIBRARIES AND IRRKLANG_INCLUDE_DIRS)
+	  find_path(IRRKLANG_INCLUDE_DIR
+	    NAMES
+	      irrKlang.h
+	    PATHS
+		${OGRE_SDK}/../irrKlang/include
+	  )
+	
+	  find_library(IRRKLANG_LIBRARY
+	    NAMES
+	      IrrKlang
+	    PATHS
+	      ${OGRE_SDK}/../irrKlang/lib/Win32-gcc
+	  )
+	
+	  if (IRRKLANG_LIBRARY)
+	    set(IRRKLANG_FOUND TRUE)
+	  endif (IRRKLANG_LIBRARY)
+	
+	  set(IRRKLANG_INCLUDE_DIRS
+	    ${IRRKLANG_INCLUDE_DIR}
+	  )
+	
+	  if (IRRKLANG_FOUND)
+	    set(IRRKLANG_LIBRARIES
+	      ${IRRKLANG_LIBRARIES}
+	      ${IRRKLANG_LIBRARY}
+	    )
+	  endif (IRRKLANG_FOUND)
+	
+	  if (IRRKLANG_INCLUDE_DIRS AND IRRKLANG_LIBRARIES)
+	     set(IRRKLANG_FOUND TRUE)
+	  endif (IRRKLANG_INCLUDE_DIRS AND IRRKLANG_LIBRARIES)
+	
+	  if (IRRKLANG_FOUND)
+	    if (NOT IRRKLANG_FIND_QUIETLY)
+	      message(STATUS "Found irrKlang: ${IRRKLANG_LIBRARIES}")
+	    endif (NOT IRRKLANG_FIND_QUIETLY)
+	  else (IRRKLANG_FOUND)
+	    if (IRRKLANG_FIND_REQUIRED)
+	      message(FATAL_ERROR "Could not find irrKlang")
+	    endif (IRRKLANG_FIND_REQUIRED)
+	  endif (IRRKLANG_FOUND)
+	
+	  # show the IRRKLANG_INCLUDE_DIRS and IRRKLANG_LIBRARIES variables only in the advanced view
+	  mark_as_advanced(IRRKLANG_INCLUDE_DIRS IRRKLANG_LIBRARIES)
+	
+	endif (IRRKLANG_LIBRARIES AND IRRKLANG_INCLUDE_DIRS) 
