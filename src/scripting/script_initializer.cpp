81fcc0963fc74609c678ff8367c4861873c5a73e
Benjamin Kloster <benjamin.kloster@videlco.eu>
2013-06-22 16:11:19 +0200

    Split script initializer
    Should help a bit in compilation times

--- src/scripting/script_initializer.cpp
+++ src/scripting/script_initializer.cpp
@@ -1,22 +1,13 @@
 #include "scripting/script_initializer.h"
 
-#include "engine/component.h"
+#include "bullet/script_bindings.h"
 #include "engine/engine.h"
-#include "engine/entity.h"
+#include "engine/script_bindings.h"
 #include "game.h"
-#include "ogre/camera_system.h"
-#include "ogre/entity_system.h"
 #include "ogre/keyboard_system.h"
-#include "ogre/light_system.h"
-#include "ogre/on_key.h"
-#include "ogre/scene_node_system.h"
 #include "ogre/script_bindings.h"
-#include "ogre/sky_system.h"
-#include "ogre/viewport_system.h"
 #include "scripting/luabind.h"
-#include "scripting/on_update.h"
-#include "bullet/bullet_lua_bindings.h"
-#include "bullet/rigid_body_system.h"
+#include "scripting/script_bindings.h"
 
 #include <forward_list>
 #include <iostream>
@@ -35,25 +26,10 @@ thrive::initializeLua(
     luabind::open(L);
     luabind::module(L) [
         luabind::def("debug", debug),
-        // Base classes
-        Component::luaBindings(),
-        Entity::luaBindings(),
-        // Script Components
-        OnUpdateComponent::luaBindings(),
-        // Ogre Components
+        EngineBindings::luaBindings(),
         OgreBindings::luaBindings(),
-        KeyboardSystem::luaBindings(),
-        OnKeyComponent::luaBindings(),
-        OgreCameraComponent::luaBindings(),
-        OgreEntityComponent::luaBindings(),
-        OgreLightComponent::luaBindings(),
-        OgreSceneNodeComponent::luaBindings(),
-        SkyPlaneComponent::luaBindings(),
-        OgreViewport::luaBindings(),
-        OgreViewportSystem::luaBindings(),
-        // Physics Components
         BulletBindings::luaBindings(),
-        RigidBodyComponent::luaBindings()
+        ScriptBindings::luaBindings()
     ];
     luabind::object globals = luabind::globals(L);
     globals["Keyboard"] = &(Game::instance().engine().keyboardSystem());
