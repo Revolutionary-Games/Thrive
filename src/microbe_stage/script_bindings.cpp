5edb011cfda2793e47bd9f9ed9069e00371c20f3
Benjamin Kloster <benjamin.kloster@videlco.eu>
2013-08-16 15:37:37 +0200

    Fix microbe stage script bindings

--- src/microbe_stage/script_bindings.cpp
+++ src/microbe_stage/script_bindings.cpp
@@ -1,12 +1,12 @@
 #include "microbe_stage/script_bindings.h"
 
 #include "scripting/luabind.h"
-#include "microbe_stage/movement.h"
+#include "microbe_stage/agent.h"
 
 luabind::scope
 thrive::MicrobeBindings::luaBindings() {
     return (
-        MicrobeMovementComponent::luaBindings()
+        AgentEmitterComponent::luaBindings()
     );
 }
 
