51823d235c5969f3673d7e5eea606423c1a18215
Benjamin Kloster <benjamin.kloster@videlco.eu>
2013-08-22 19:20:48 +0200

    Make MovementOrganelle require energy for movement

--- res/scripts/microbe_stage/movement_organelle.lua
+++ res/scripts/microbe_stage/movement_organelle.lua
@@ -2,35 +2,52 @@ class 'MovementOrganelle' (Organelle)
 
 function MovementOrganelle:__init(force, torque)
     Organelle.__init(self)
+    self.energyMultiplier = 0.025
     self.force = force
     self.torque = torque
 end
 
-local function moveMicrobe(microbe, force, milliseconds)
-    local direction = microbe.movementDirection
+local function sign(x)
+    if x < 0 then
+        return -1
+    elseif x > 0 then
+        return 1
+    else
+        return 0
+    end
+end
+
+function MovementOrganelle:_moveMicrobe(milliseconds)
+    local direction = self.microbe.movementDirection
     if direction:isZeroLength() then
         return
     end 
-    if force:isZeroLength() then
+    if self.force:isZeroLength() then
         return
     end
-    local forceMagnitude = force:dotProduct(direction)
+    local forceMagnitude = self.force:dotProduct(direction)
+    local energy = math.abs(self.energyMultiplier * forceMagnitude * milliseconds / 1000)
+    local availableEnergy = self.microbe:takeAgent(1, energy)
+    debug("Available energy: " .. tostring(energy) .. "  " .. tostring(availableEnergy))
+    if availableEnergy < energy then
+        forceMagnitude = sign(forceMagnitude) * availableEnergy * 1000 / milliseconds
+    end
     if forceMagnitude > 0 then
         local impulseMagnitude = milliseconds * forceMagnitude / 1000
         local impulse = impulseMagnitude * direction
-        microbe.rigidBody:applyCentralImpulse(
-            microbe.sceneNode.transform.orientation * impulse
+        self.microbe.rigidBody:applyCentralImpulse(
+            self.microbe.sceneNode.transform.orientation * impulse
         )
     end
 end
 
 
-local function turnMicrobe(microbe, torque)
-    if torque == 0 then
+function MovementOrganelle:_turnMicrobe()
+    if self.torque == 0 then
         return
     end
-    local transform = microbe.sceneNode.transform
-    local targetDirection = microbe.facingTargetPoint - transform.position
+    local transform = self.microbe.sceneNode.transform
+    local targetDirection = self.microbe.facingTargetPoint - transform.position
     local localTargetDirection = transform.orientation:Inverse() * targetDirection
     assert(localTargetDirection.z < 0.01, "Microbes should only move in the 2D plane with z = 0")
     local alpha = math.atan2(
@@ -38,15 +55,15 @@ local function turnMicrobe(microbe, torque)
         localTargetDirection.y
     )
     if math.abs(math.deg(alpha)) > 1 then
-        microbe.rigidBody:applyTorque(
-            Vector3(0, 0, torque * alpha)
+        self.microbe.rigidBody:applyTorque(
+            Vector3(0, 0, self.torque * alpha)
         )
     end
 end
 
 
 function MovementOrganelle:update(microbe, milliseconds)
-    turnMicrobe(microbe, self.torque)
-    moveMicrobe(microbe, self.force, milliseconds)
+    self:_turnMicrobe()
+    self:_moveMicrobe(milliseconds)
 end
 
