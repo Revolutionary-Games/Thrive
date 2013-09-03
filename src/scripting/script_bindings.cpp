30a27cbae4b061424440b45552248d92bfa2596f
Benjamin Kloster <benjamin.kloster@videlco.eu>
2013-09-03 21:45:10 +0200

    Move ScriptComponent up into Component
    No sense in having separate classes for this

--- src/scripting/script_bindings.cpp
+++ src/scripting/script_bindings.cpp
@@ -2,14 +2,12 @@
 
 #include "scripting/luabind.h"
 #include "scripting/on_update.h"
-#include "scripting/script_component.h"
 #include "scripting/script_entity_filter.h"
 
 luabind::scope
 thrive::ScriptBindings::luaBindings() {
     return (
         OnUpdateComponent::luaBindings(),
-        ScriptComponent::luaBindings(),
         ScriptEntityFilter::luaBindings()
     );
 }
