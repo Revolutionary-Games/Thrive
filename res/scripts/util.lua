7de08359d284d3342cc7b5af9bef5b854adc6f31
Benjamin Kloster <benjamin.kloster@videlco.eu>
2013-09-13 13:06:52 +0200

    Add Entity:getOrCreate utility function in Lua

--- res/scripts/util.lua
+++ res/scripts/util.lua
@@ -9,3 +9,12 @@ function REGISTER_COMPONENT(name, cls)
         cls
     )
 end
+
+function Entity:getOrCreate(componentCls)
+    component = self:getComponent(componentCls.TYPE_ID)
+    if component == nil then
+        component = componentCls()
+        self:addComponent(component)
+    end
+    return component
+end
