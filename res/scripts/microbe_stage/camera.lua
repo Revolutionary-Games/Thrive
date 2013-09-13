909e4366c1a039f2602fba2dd113cc487929a0cb
Benjamin Kloster <benjamin.kloster@videlco.eu>
2013-09-13 13:07:31 +0200

    Do not cache entities in microbe camera system
    The cached entities are invalidated after loading

--- res/scripts/microbe_stage/camera.lua
+++ res/scripts/microbe_stage/camera.lua
@@ -4,14 +4,14 @@ local OFFSET = Vector3(0, 0, 30)
 
 function MicrobeCameraSystem:__init()
     System.__init(self)
-    self.camera = Entity(CAMERA_NAME)
-    self.player = Entity(PLAYER_NAME)
 end
 
 
 function MicrobeCameraSystem:update(milliseconds)
-    local playerNode = self.player:getComponent(OgreSceneNodeComponent.TYPE_ID)
-    local cameraNode = self.camera:getComponent(OgreSceneNodeComponent.TYPE_ID)
+    local camera = Entity(CAMERA_NAME)
+    local player = Entity(PLAYER_NAME)
+    local playerNode = player:getComponent(OgreSceneNodeComponent.TYPE_ID)
+    local cameraNode = camera:getComponent(OgreSceneNodeComponent.TYPE_ID)
     cameraNode.transform.position = playerNode.transform.position + OFFSET
     cameraNode.transform:touch()
 end
