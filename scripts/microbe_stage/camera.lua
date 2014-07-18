14d1a84ab8be8ec807e95610e70af1388d90b858
Jacob Jensen <jjonj@hotmail.com>
2014-07-18 19:31:57 +0200

    Simply improvement to how skyboxes tiles in the microbe stage

--- scripts/microbe_stage/camera.lua
+++ scripts/microbe_stage/camera.lua
@@ -3,7 +3,6 @@ class 'MicrobeCameraSystem' (System)
 
 function MicrobeCameraSystem:__init()
     System.__init(self)
-    -- The offset from player microbe to camera
     self.camera = nil
     self.cameraScenenode = nil
 end
@@ -11,6 +10,7 @@ end
 function MicrobeCameraSystem:activate()
     local camera = Entity(CAMERA_NAME)
     self.camera = camera:getComponent(OgreCameraComponent.TYPE_ID)
+    -- The offset from player microbe to camera
     self.camera.properties.offset = Vector3(0, 0, 30)
     self.camera.properties:touch()
     self.cameraScenenode = camera:getComponent(OgreSceneNodeComponent.TYPE_ID)
