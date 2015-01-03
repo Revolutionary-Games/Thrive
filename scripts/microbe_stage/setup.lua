26e21db382e3a42eed612ddd780948c30bbd377e
Jacob Jensen <jjonj@hotmail.com>
2015-01-04 00:21:40 +0100

    Some fixes!

--- scripts/microbe_stage/setup.lua
+++ scripts/microbe_stage/setup.lua
@@ -392,7 +392,7 @@ local function setupSound()
     local soundSource = SoundSourceComponent()
     soundSource.ambientSoundSource = true
     soundSource.autoLoop = true
-    soundSource.volumeMultiplier = 0.1
+    soundSource.volumeMultiplier = 0.12
     ambientEntity:addComponent(soundSource)
     -- Sound
     soundSource:addSound("microbe-theme-1", "microbe-theme-1.ogg")
@@ -403,7 +403,7 @@ local function setupSound()
     soundSource:addSound("microbe-theme-7", "microbe-theme-7.ogg")   
     local ambientEntity2 = Entity("ambience2")
     local soundSource = SoundSourceComponent()
-     soundSource.volumeMultiplier = 0.4
+    soundSource.volumeMultiplier = 0.4
     soundSource.ambientSoundSource = true
     ambientSound = soundSource:addSound("microbe-ambient", "soundeffects/microbe-ambience.ogg")
     soundSource.autoLoop = true
