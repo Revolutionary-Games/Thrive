8cb250da83895ca539b491dcc6f2e6923eb5043f
Filip Burlacu <filburli@gmail.com>
2015-02-22 12:24:53 -0500

    A large refactoring project, adding proper species handling, and a model for handling populations.
    Also refactored:
    *Spawning functions heavily simplified
    * Editor organelle placement heavily simplified
    
    Fixes #191

--- scripts/microbe_stage/microbe_replacement.lua
+++ scripts/microbe_stage/microbe_replacement.lua
@@ -5,22 +5,37 @@ class 'MicrobeReplacementSystem' (System)
 global_newEditorMicrobe = false
 
 function MicrobeReplacementSystem:__init()
+    self.globalSpeciesNameCounter = 1
     System.__init(self)
 end
 
 function MicrobeReplacementSystem:activate()
     activeCreatureId = Engine:playerData():activeCreature()
     if Engine:playerData():isBoolSet("edited_microbe") then
-        Engine:playerData():setBool("edited_microbe", false);
+        Engine:playerData():setBool("edited_microbe", false)
+
         workingMicrobe = Microbe(Entity(activeCreatureId, GameState.MICROBE_EDITOR))
-        if workingMicrobe:getCompoundAmount(CompoundRegistry.getCompoundId("atp")) == 0 then
-            workingMicrobe:storeCompound(CompoundRegistry.getCompoundId("atp"), 10)
+
+        speciesEntity = Entity(workingMicrobe.microbe.speciesName, GameState.MICROBE)
+        species = SpeciesComponent(workingMicrobe.microbe.speciesName)
+        speciesEntity:addComponent(species)
+        self.globalSpeciesNameCounter = self.globalSpeciesNameCounter + 1
+        species:fromMicrobe(workingMicrobe)
+
+        workingMicrobe.entity:destroy()
+
+        newMicrobe = Microbe.createMicrobeEntity(PLAYER_NAME, false, workingMicrobe.microbe.speciesName)
+        species:template(newMicrobe)
+
+        if newMicrobe:getCompoundAmount(CompoundRegistry.getCompoundId("atp")) < 10 then
+            newMicrobe:storeCompound(CompoundRegistry.getCompoundId("atp"), 10)
         end
-        newMicrobeEntity = workingMicrobe.entity
-        newPlayerMicrobe = newMicrobeEntity:transfer(GameState.MICROBE)
-        newPlayerMicrobe:stealName(PLAYER_NAME)
+
+        newMicrobe.collisionHandler:addCollisionGroup("powerupable")
+        newMicrobeEntity = newMicrobe.entity:transfer(GameState.MICROBE)
+        newMicrobeEntity:stealName(PLAYER_NAME)
         global_newEditorMicrobe = false
-        Engine:playerData():setActiveCreature(newPlayerMicrobe.id, GameState.MICROBE)
+        Engine:playerData():setActiveCreature(newMicrobeEntity.id, GameState.MICROBE)
     end
    
 end
