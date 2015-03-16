9174995deaec8f6fcb97a5a09225e448c0ec4bdd
Filip Burlacu <filburli@gmail.com>
2015-03-16 00:12:27 -0400

    Added remaining starter microbes, started on compound registry lua-loader

--- scripts/microbe_stage/configs.lua
+++ scripts/microbe_stage/configs.lua
@@ -42,7 +42,7 @@ compounds = {
         weight = 1,
         mesh = "co2.mesh",
         size = 0.16
-    }
+    },
 }
 
 agents = {
@@ -52,7 +52,7 @@ agents = {
         size = 0.3,
         effect = "oxytoxyEffect" 
         -- we'll have to be careful with this referencing
-    }
+    },
 }
 
 processes = {
@@ -79,7 +79,7 @@ processes = {
         },
         outputs = {
             reproductase = 5
-        }
+        },
     },
     AminoAcidSynthesis = {
         speedFactor = 1,
@@ -92,7 +92,7 @@ processes = {
             co2 = 1,
             atp = 1,
             aminoacids = 1
-        }
+        },
     },
     OxyToxySynthesis = {
         speedFactor = 0.1,
@@ -102,7 +102,7 @@ processes = {
         },
         outputs = {
             oxytoxy = 1
-        }
+        },
     },
     Photosynthesis = {
         speedFactor = 0.03,
@@ -113,16 +113,25 @@ processes = {
         outputs = {
             glucose = 1,
             oxygen = 6
-        }
-    }
+        },
+    },
 }
+
+--[[
+Placing organelles can get downright annoying if you don't
+map them out. To make it easier, download a few sheets of hexgrid 
+off the internet. Before you print them though, set up the axes
+properly. See http://i.imgur.com/kTxHFMC.png for how. When you're
+drawing out your microbe, keep in mind that it faces forward along
+the +r direction.
+]]
 
 starter_microbes = {
     Default = {
         compounds = {
             atp = {priority=10,amount=40},
             glucose = {amount = 5},
-            reproductase = {priority = 8}
+            reproductase = {priority = 8},
         },
         organelles = {
             {name="nucleus",q=0,r=0},
@@ -134,8 +143,8 @@ starter_microbes = {
             {name="flagellum",q=1,r=0},
             {name="flagellum",q=0,r=-2},
             {name="flagellum",q=-1,r=-1},
-            {name="flagellum",q=1,r=-2}
-        }
+            {name="flagellum",q=1,r=-2},
+        },
     },
     Teeny = {
         compounds = {atp = {amount = 60}},
@@ -143,8 +152,21 @@ starter_microbes = {
             {name="nucleus",q=0,r=0},
             {name="vacuole",q=-1,r=0},
             {name="mitochondrion",q=1,r=-1},
-            {name="flagellum",q=0,r=-1}
-        }
+            {name="flagellum",q=0,r=-1},
+        },
+    },
+    Speedy = {
+        compounds = {apt = {amount = 15}},
+        organelles = {
+            {name="nucleus", q=0, r=0},
+            {name="mitochondrion", q=0, r=-1},
+            {name="vacuole", q=0, r=-2},
+            {name="flagellum", q=0, r=-3},
+            {name="flagellum", q=-1, r=-2},
+            {name="flagellum", q=0, r=-3},
+            {name="flagellum", q=-1, r=-3},
+            {name="flagellum", q=1, r=-4},
+        },
     },
     Plankton = {
         compounds = {atp = {amount = 60}},
@@ -177,8 +199,8 @@ starter_microbes = {
             {name="chloroplast", q=-1, r=3},
             {name="chloroplast", q=3,r=-2},
             {name="chloroplast", q=1,r=-3},
-            {name="chloroplast", q=-3, r=2}
-        }
+            {name="chloroplast", q=-3, r=2},
+        },
     },
     Algae = {
         compounds = {atp = {amount = 20}},
@@ -189,10 +211,10 @@ starter_microbes = {
             {name="chloroplast", q=1, r=0},
             {name="chloroplast", q=-1, r=0},
             {name="chloroplast", q=1, r=-1},
-            {name="chloroplast", q=-1, r=1}
-        }
+            {name="chloroplast", q=-1, r=1},
+        },
     },
-    Posionous = {
+    Poisonous = {
         compounds = {
             atp = {amount = 30},
             oxytoxy = {amount = 15}
@@ -212,7 +234,23 @@ starter_microbes = {
             {name="flagellum", q=2, r=-1},
             {name="flagellum", q=-2, r=1},
             {name="flagellum", q=0, r=4},
-            {name="flagellum", q=0, r=-4}
-        }
+            {name="flagellum", q=0, r=-4},
+        },
+    },
+    Predator = {
+        compounds = {
+            atp = {amount = 30},
+            oxytoxy = {amount = 20},
+        },
+        organelles = {
+            {name="nucleus", q=0, r=0},
+            {name="vacuole", q=-1, r=0},
+            {name="vacuole", q=1, r=-1},
+            {name="mitochondrion", q=1, r=0},
+            {name="mitochondrion", q=1, r=-1},
+            {name="flagellum", q=0, r=-1},
+            {name="flagellum", q=0, r=-2},
+            {name="oxytoxy", q=0, r=1},
+        },
     },
 }
