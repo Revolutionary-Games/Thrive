d8b414a36ca445413f05a21d5d1e34a1a800c7d4
Filip Burlacu <filburli@gmail.com>
2015-03-17 00:09:33 -0400

    Microbe spawning and sparter species now completely bypass species registry

--- scripts/microbe_stage/configs.lua
+++ scripts/microbe_stage/configs.lua
@@ -1,47 +1,57 @@
 -- Holds config file contents, translated into lua table form
 
+--[[
+Style tips:
+- Always indent, only with spaces, 4 spaces to an indent.
+- Always leave trailing commas on the last elements of multiline tables
+    then, the next time something gets added, you don't need to change the line above
+    just keeps things cleaner.
+- Always leave a trailing newline at the end of the file. 
+    Google it, it is the right way to end text files.
+]]
+
 compounds = {
     atp = {
         name = "ATP",
         weight = 1,
         mesh = "ATP.mesh",
-        size = 0.1
+        size = 0.1,
     },
     oxygen = {
         name = "Oxygen",
         weight = 1,
         mesh = "molecule.mesh",
-        size = 0.3
+        size = 0.3,
     },
     reproductase = {
         name = "Reproductase",
         weight = 1,
         mesh = "reproductase.mesh",
-        size = 0.09
+        size = 0.09,
     },
     aminoacids = {
         name = "Amino Acids",
         weight = 1,
         mesh = "aminoacid.mesh",
-        size = 0.2
+        size = 0.2,
     },
     ammonia = {
         name = "Ammonia",
         weight = 1,
         mesh = "ammonia.mesh",
-        size = 0.16
+        size = 0.16,
     },
     glucose = {
         name = "Glucose",
         weight = 1,
         mesh = "glucose.mesh",
-        size = 0.3
+        size = 0.3,
     },
     co2 = {
         name = "CO2",
         weight = 1,
         mesh = "co2.mesh",
-        size = 0.16
+        size = 0.16,
     },
 }
 
@@ -50,8 +60,12 @@ agents = {
         name = "OxyToxy NT",
         mesh = "oxytoxy.mesh",
         size = 0.3,
-        effect = "oxytoxyEffect" 
-        -- we'll have to be careful with this referencing
+        effect = "oxytoxyEffect",
+        --[[
+        we'll have to be careful with this referencing. 
+        should we link the function directly? Or just name? 
+        link should be cleaner to use, but may be more difficult to set up?
+        ]]
     },
 }
 
@@ -61,11 +75,11 @@ processes = {
         energyCost = 0,
         inputs = {
             glucose = 1,
-            oxygen = 6
+            oxygen = 6,
         },
         outputs = {
             co2 = 6,
-            atp = 36
+            atp = 36,
         },
     },
     ReproductaseSynthesis = {
@@ -75,10 +89,10 @@ processes = {
             aminoacids = 6,
             glucose = 6,
             oxygen = 6,
-            atp = 6
+            atp = 6,
         },
         outputs = {
-            reproductase = 5
+            reproductase = 5,
         },
     },
     AminoAcidSynthesis = {
@@ -86,33 +100,33 @@ processes = {
         energyCost = 0,
         inputs = {
             glucose = 1,
-            ammonia = 1
+            ammonia = 1,
         },
         outputs = {
             co2 = 1,
             atp = 1,
-            aminoacids = 1
+            aminoacids = 1,
         },
     },
     OxyToxySynthesis = {
         speedFactor = 0.1,
         energyCost = 1,
         inputs = {
-            oxygen = 3
+            oxygen = 3,
         },
         outputs = {
-            oxytoxy = 1
+            oxytoxy = 1,
         },
     },
     Photosynthesis = {
         speedFactor = 0.03,
         energyCost = 0,
         inputs = {
-            co2 = 6
+            co2 = 6,
         },
         outputs = {
             glucose = 1,
-            oxygen = 6
+            oxygen = 6,
         },
     },
 }
@@ -128,6 +142,7 @@ the +r direction.
 
 starter_microbes = {
     Default = {
+        spawnDensity = 1/12000,
         compounds = {
             atp = {priority=10,amount=40},
             glucose = {amount = 5},
@@ -147,6 +162,7 @@ starter_microbes = {
         },
     },
     Teeny = {
+        spawnDensity = 1/6000,
         compounds = {atp = {amount = 60}},
         organelles = {
             {name="nucleus",q=0,r=0},
@@ -156,12 +172,13 @@ starter_microbes = {
         },
     },
     Speedy = {
-        compounds = {apt = {amount = 15}},
+        spawnDensity = 1/12000,
+        compounds = {atp = {amount = 30}},
         organelles = {
             {name="nucleus", q=0, r=0},
             {name="mitochondrion", q=0, r=-1},
             {name="vacuole", q=0, r=-2},
-            {name="flagellum", q=0, r=-3},
+            {name="flagellum", q=1, r=-3},
             {name="flagellum", q=-1, r=-2},
             {name="flagellum", q=0, r=-3},
             {name="flagellum", q=-1, r=-3},
@@ -169,6 +186,7 @@ starter_microbes = {
         },
     },
     Plankton = {
+        spawnDensity = 1/32000,
         compounds = {atp = {amount = 60}},
         organelles = {
             {name="nucleus", q=0, r=0},
@@ -203,6 +221,7 @@ starter_microbes = {
         },
     },
     Algae = {
+        spawnDensity = 1/8000,
         compounds = {atp = {amount = 20}},
         organelles = {
             {name="nucleus", q=0, r=0},
@@ -215,9 +234,10 @@ starter_microbes = {
         },
     },
     Poisonous = {
+        spawnDensity = 1/32000,
         compounds = {
             atp = {amount = 30},
-            oxytoxy = {amount = 15}
+            oxytoxy = {amount = 15},
         },
         organelles = {
             {name="nucleus", q=0, r=0},
@@ -238,6 +258,7 @@ starter_microbes = {
         },
     },
     Predator = {
+        spawnDensity = 1/15000,
         compounds = {
             atp = {amount = 30},
             oxytoxy = {amount = 20},
@@ -247,7 +268,7 @@ starter_microbes = {
             {name="vacuole", q=-1, r=0},
             {name="vacuole", q=1, r=-1},
             {name="mitochondrion", q=1, r=0},
-            {name="mitochondrion", q=1, r=-1},
+            {name="mitochondrion", q=-1, r=1},
             {name="flagellum", q=0, r=-1},
             {name="flagellum", q=0, r=-2},
             {name="oxytoxy", q=0, r=1},
