6dcdf42fbefa1c2b491d48105e743e177020ff9e
Filip Burlacu <filburli@gmail.com>
2016-02-28 15:19:29 -0500

    Some cmake tweaks for linux builds

--- cmake_modules/FindOgreFFMPEG.cmake
+++ cmake_modules/FindOgreFFMPEG.cmake
@@ -9,6 +9,7 @@ SET( OgreFFMPEG_INCLUDE_SEARCH_DIR
        ${MINGW_ENV}/install/include
        ${MINGW_ENV}/install/include/ogre-ffmpeg
        /usr/local/include
+       /usr/local/include/ogre-ffmpeg
        /usr/include
        /sw/include # Fink
        /opt/local/include # DarwinPorts
@@ -39,7 +40,7 @@ NAMES "libogre-ffmpeg-videoplayer.a"
 PATHS ${OgreFFMPEG_LIBRARY_SEARCH_DIR})
 
 
-# handle the QUIETLY and REQUIRED arguments and set TINYXML_FOUND to TRUE if
+# handle the QUIETLY and REQUIRED arguments and set OgreFFMPEG_FOUND to TRUE if
 # all listed variables are TRUE
 INCLUDE( "FindPackageHandleStandardArgs" )
 FIND_PACKAGE_HANDLE_STANDARD_ARGS( "OgreFFMPEG" DEFAULT_MSG OgreFFMPEG_INCLUDE_DIR OgreFFMPEG_LIBRARIES)
