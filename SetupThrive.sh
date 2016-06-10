9c26a86cd54bf472a90195b8708a82926453204a
Anthorx <pascal.dengler@elyxon.de>
2016-06-10 22:40:17 +0200

    add function calls to make the script more readable and more modular

--- SetupThrive.sh
+++ SetupThrive.sh
@@ -1,368 +1,419 @@
 #!/bin/sh
 
-# detect if running from thrive folder
-InsideFolder=${PWD##*/}
-OriginalFolder=$(pwd)
-
-# Define colors for output
-ERROR="\033[1;31m"
-GOOD="\033[1;32m"
-INFO="\033[1;36m"
-NC="\033[0m" # No Color
-
-if [ -f ./thriveversion.ver -o "$InsideFolder" = "thrive" ]; then
-
-    # Running from thrive folder
-    StartingDirectory=$(dirname "$(pwd)")
-    
-else
-    
-    StartingDirectory=$(pwd)
-fi
-
-
-# Variable setup
-
-OS=$(cat /etc/*-release | grep ID | cut -c 4- | sed -e "s/\b\(.\)/\u\1/g")
-
-
-THREADS=$(nproc)
-DEBUG=0
-VERBOSE=0
-
-THRIVE_BRANCH="master"
-
-# Parse arguments
-getopt --test > /dev/null
-if [ $? != 4 ]; then
-    echo -e "$ERROR I’m sorry, 'getopt --test' failed in this environment. $NC"
-    exit 1
-fi
-
-SHORT=dfj:t:v
-LONG=debug,force,threads:,verbose
-
-PARSED=$(getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@")
-if [ $? != 0 ]; then
-    exit 2
-fi
-eval set -- "$PARSED"
-
-while true; do
-    case "$1" in
-        -d|--debug)
-            DEBUG=1
-            shift
-            ;;
-        -v|--verbose)
-            VERBOSE=1
-            shift
-            ;;
-        -j|-t|--threads)
-            THREADS="$2"
-            shift 2
-            ;;
-        --)
-            shift
-            break
-            ;;
-        *)
-            echo -e "$ERROR Programming error $NC"
-            exit 3
-            ;;
-    esac
-done
+function init() {
+	# detect if running from thrive folder
+	InsideFolder=${PWD##*/}
+	OriginalFolder=$(pwd)
+
+	# Define colors for output
+	ERROR="\033[1;31m"
+	GOOD="\033[1;32m"
+	INFO="\033[1;36m"
+	NC="\033[0m" # No Color
+
+	if [ -f ./thriveversion.ver -o "$InsideFolder" = "thrive" ]; then
+
+		# Running from thrive folder
+		StartingDirectory=$(dirname "$(pwd)")
+		
+	else
+		
+		StartingDirectory=$(pwd)
+	fi
+
+	# Variable setup
+	OS=$(cat /etc/*-release | grep ID | cut -c 4- | sed -e "s/\b\(.\)/\u\1/g")
+
+	THREADS=$(nproc)
+	DEBUG=0
+	VERBOSE=0
+
+	THRIVE_BRANCH="master"
+
+	# Parse arguments
+	getopt --test > /dev/null
+	if [ $? != 4 ]; then
+	    echo -e "$ERROR I’m sorry, 'getopt --test' failed in this environment. $NC"
+	    exit 1
+	fi
+
+	SHORT=dfj:t:v
+	LONG=debug,force,threads:,verbose
+
+	PARSED=$(getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@")
+	if [ $? != 0 ]; then
+	    exit 2
+	fi
+	eval set -- "$PARSED"
+
+	while true; do
+	    case "$1" in
+		-d|--debug)
+		    DEBUG=1
+		    shift
+		    ;;
+		-v|--verbose)
+		    VERBOSE=1
+		    shift
+		    ;;
+		-j|-t|--threads)
+		    THREADS="$2"
+		    shift 2
+		    ;;
+		--)
+		    shift
+		    break
+		    ;;
+		*)
+		    echo -e "$ERROR Programming error $NC"
+		    exit 3
+		    ;;
+	    esac
+	done
+
+	echo -e -e "$INFO Using $THREADS threads to compile $NC"
+	echo -e "$INFO Running in folder $StartingDirectory $NC"
+	cd "$StartingDirectory"
+
+	MakeArgs="-j $THREADS"
+
+	if [ $DEBUG = 1 -o $VERBOSE = 1 ]; then
+
+	    MakeArgs="$MakeArgs -d"
+	    
+	fi
+}
+
+function install_Packages() {
+	PackageManager="dnf install -y "
+	PackagesToInstall="bullet-devel boost gcc-c++ libXaw-devel freetype-devel freeimage-devel \
+			   zziplib-devel boost-devel ois-devel tinyxml-devel glm-devel ffmpeg-devel ffmpeg-libs openal-soft-devel"
+	CommonPackages="cmake make git mercurial svn"
+
+	if [ "$OS" = "Fedora" ]; then
+
+		echo -e "$INFO Creating CEGUI project folder for $OS $NC"
+	   
+	elif [ "$OS" = "Ubuntu" ]; then
+
+		PackageManager="apt-get install -y "
+		 
+		PackagesToInstall="bullet-dev boost-dev build-essential automake libtool libfreetype6-dev \
+				   libfreeimage-dev libzzip-dev libxrandr-dev libxaw7-dev freeglut3-dev libgl1-mesa-dev \
+				   libglu1-mesa-dev libois-dev libboost-thread-dev tinyxml-dev glm-dev ffmpeg-dev libavutil-dev libopenal-dev"
 
-echo -e -e "$INFO Using $THREADS threads to compile $NC"
-echo -e "$INFO Running in folder $StartingDirectory $NC"
-cd "$StartingDirectory"
+	elif [ "$OS" = "Arch" ]; then
+		PackageManager="pacman -S --noconfirm --color auto"
+		PackagesToInstall="bullet boost base-devel automake libtool freetype2 \
+				   freeimage zziplib libxrandr libxaw freeglut mesa-libgl \
+				   ois tinyxml glm ffmpeg openal"
 
-MakeArgs="-j $THREADS"
+	else
+		 
+		echo -e "$ERROR Unkown linux OS \"$OS\" $NC"
+		exit 2
+	fi
 
-if [ $DEBUG = 1 -o $VERBOSE = 1 ]; then
+	PackagesToInstall="$PackagesToInstall $CommonPackages"
 
-    MakeArgs="$MakeArgs -d"
-    
-fi
+	echo -e "$INFO Installing prerequisite libraries, be prepared to type password for sudo $NC"
+	eval "sudo $PackageManager $PackagesToInstall"
 
-PackageManager="dnf install -y "
-PackagesToInstall="bullet-devel boost gcc-c++ libXaw-devel freetype-devel freeimage-devel \
-                   zziplib-devel boost-devel ois-devel tinyxml-devel glm-devel ffmpeg-devel ffmpeg-libs openal-soft-devel"
-CommonPackages="cmake make git mercurial svn"
+	if [ $? -eq 0 ]; then
+		echo -e "$GOOD Prerequisites installed successfully $NC"
+	else
+		echo -e "$ERROR Package manager failed to install required packages, install \"$PackagesToInstall\" manually $NC"
+		exit 1
+	fi
+}
 
-if [ "$OS" = "Fedora" ]; then
+function prepare_Deps() {
+	echo -e "$INFO Cloning repositories and creating build files... $NC"
+	prepare_Ogre
+	prepare_CEGUI
+	prepare_OgreFFMPEG
+	prepare_cAudio
+}
 
-    echo -e "$INFO Creating CEGUI project folder for $OS $NC"
-   
-elif [ "$OS" = "Ubuntu" ]; then
+function prepare_Ogre() {
+	echo "Ogre..."
 
-    PackageManager="apt-get install -y "
-         
-    PackagesToInstall="bullet-dev boost-dev build-essential automake libtool libfreetype6-dev \
-                       libfreeimage-dev libzzip-dev libxrandr-dev libxaw7-dev freeglut3-dev libgl1-mesa-dev \
-                       libglu1-mesa-dev libois-dev libboost-thread-dev tinyxml-dev glm-dev ffmpeg-dev libavutil-dev libopenal-dev"
+	mkdir -p ogreBuild
+	cd ogreBuild
 
-elif [ "$OS" = "Arch" ]; then
-    PackageManager="pacman -S --noconfirm --color auto"
-    PackagesToInstall="bullet boost base-devel automake libtool freetype2 \
-                       freeimage zziplib libxrandr libxaw freeglut mesa-libgl \
-                       ois tinyxml glm ffmpeg openal"
+	# Main repo
+	if [ -d ogre ]; then
 
-else
-         
-    echo -e "$ERROR Unkown linux OS \"$OS\" $NC"
-    exit 2
-fi
+		cd ogre
+		hg pull
+			
+	else
+		hg clone https://bitbucket.org/sinbad/ogre ogre
+		cd ogre
+	fi
 
-PackagesToInstall="$PackagesToInstall $CommonPackages"
+	hg update v2-0
 
-echo -e "$INFO Installing prerequisite libraries, be prepared to type password for sudo $NC"
-eval "sudo $PackageManager $PackagesToInstall"
+	cd ..
 
-if [ $? -eq 0 ]; then
-    echo -e "$GOOD Prerequisites installed successfully $NC"
-else
-    echo -e "$ERROR Package manager failed to install required packages, install \"$PackagesToInstall\" manually $NC"
-    exit 1
-fi
+	# TODO: allow using this if wanted
+	# Dependencies repo
 
-echo -e "$INFO Cloning repositories and creating build files... $NC"
 
-echo -e "$INFO Ogre... $NC"
+	# Build file
+	mkdir -p build
 
-mkdir -p ogreBuild
-cd ogreBuild
+	echo "cmake_minimum_required(VERSION 2.8.11)
+	# Let's try forcing static FreeImage
+	set(FreeImage_USE_STATIC_LIBS TRUE) 
+	# depencies must be first 
+	#add_subdirectory(ogredeps)
+	# actual ogre
+	add_subdirectory(ogre)" > CMakeLists.txt
 
-# Main repo
-if [ -d ogre ]; then
 
-    cd ogre
-    hg pull
-    
-else
-    hg clone https://bitbucket.org/sinbad/ogre ogre
-    cd ogre
-fi
+	# Run cmake
+	cd build
+		cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DOGRE_BUILD_RENDERSYSTEM_GL3PLUS=ON -DOGRE_BUILD_COMPONENT_OVERLAY=OFF -DOGRE_BUILD_COMPONENT_PAGING=OFF -DOGRE_BUILD_COMPONENT_PROPERTY=OFF -DOGRE_BUILD_COMPONENT_TERRAIN=OFF -DOGRE_BUILD_COMPONENT_VOLUME=OFF -DOGRE_BUILD_PLUGIN_BSP=OFF -DOGRE_BUILD_PLUGIN_CG=OFF -DOGRE_BUILD_PLUGIN_OCTREE=OFF -DOGRE_BUILD_PLUGIN_PCZ=OFF -DOGRE_BUILD_SAMPLES=OFF
 
-hg update v2-0
+		cd "$StartingDirectory"
+		echo "Done"
+}
 
-cd ..
+function prepare_CEGUI() {
+	echo -e "$INFO CEGUI... $NC"
 
-# TODO: allow using this if wanted
-# Dependencies repo
+	if [ -d cegui ]; then
 
+	cd cegui
+	hg pull
 
-# Build file
-mkdir -p build
+	else
+	hg clone https://bitbucket.org/cegui/cegui cegui
+	cd cegui
+	fi
 
-echo -e "cmake_minimum_required(VERSION 2.8.11)
+	hg update default
 
-# Let's try forcing static FreeImage
+	mkdir build
+	cd build
+	cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCEGUI_BUILD_APPLICATION_TEMPLATES=OFF -DCEGUI_BUILD_PYTHON_MODULES=OFF -DCEGUI_SAMPLES_ENABLED=OFF
 
-set(FreeImage_USE_STATIC_LIBS TRUE) 
+	cd "$StartingDirectory"
 
-# depencies must be first 
-#add_subdirectory(ogredeps)
+	echo -e "$GOOD Done $NC"
+}
 
-# actual ogre
-add_subdirectory(ogre)" > CMakeLists.txt
+function prepare_OgreFFMPEG() {
+	echo "OgreFFMPEG"
 
+	if [ -d ogre-ffmpeg-videoplayer ]; then
 
-# Run cmake
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DOGRE_BUILD_RENDERSYSTEM_GL3PLUS=ON -DOGRE_BUILD_COMPONENT_OVERLAY=OFF -DOGRE_BUILD_COMPONENT_PAGING=OFF -DOGRE_BUILD_COMPONENT_PROPERTY=OFF -DOGRE_BUILD_COMPONENT_TERRAIN=OFF -DOGRE_BUILD_COMPONENT_VOLUME=OFF -DOGRE_BUILD_PLUGIN_BSP=OFF -DOGRE_BUILD_PLUGIN_CG=OFF -DOGRE_BUILD_PLUGIN_OCTREE=OFF -DOGRE_BUILD_PLUGIN_PCZ=OFF -DOGRE_BUILD_SAMPLES=OFF
+		cd ogre-ffmpeg-videoplayer
+		git checkout master
+		git pull origin master
+		
+	else
 
-cd "$StartingDirectory"
-echo -e "$GOOD Done $NC"
+		#Official repo
+		#git clone https://github.com/scrawl/ogre-ffmpeg-videoplayer.git ogre-ffmpeg-videoplayer
+		# Currently working hhyyrylainen's fork
+		git clone https://github.com/hhyyrylainen/ogre-ffmpeg-videoplayer.git ogre-ffmpeg-videoplayer
+		cd ogre-ffmpeg-videoplayer
+	fi
 
-echo -e "$INFO CEGUI... $NC"
 
-if [ -d cegui ]; then
+	mkdir -p build
+	cd build
+	cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_VIDEOPLAYER_DEMO=OFF
+	cd "$StartingDirectory"
 
-    cd cegui
-    hg pull
-    
-else
-    hg clone https://bitbucket.org/cegui/cegui cegui
-    cd cegui
-fi
+	echo "Done"
+}
 
-hg update default
+function prepare_cAudio() {
+	echo "cAudio"
 
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCEGUI_BUILD_APPLICATION_TEMPLATES=OFF -DCEGUI_BUILD_PYTHON_MODULES=OFF -DCEGUI_SAMPLES_ENABLED=OFF
+	if [ -d cAudio ]; then
 
-cd "$StartingDirectory"
+		cd cAudio
+		#Workaround for broken latest version
+		#git checkout master
+		#git pull origin master
 
-echo -e "$GOOD Done $NC"
+		
+	else
 
-echo -e "$INFO OgreFFMPEG $NC"
+		git clone https://github.com/wildicv/cAudio.git
+		cd cAudio
+	fi
 
-if [ -d ogre-ffmpeg-videoplayer ]; then
+	#Workaround for broken build with latest version
+	git checkout 22ff1a97a9a820c72726463708590adfae77008c
 
-    cd ogre-ffmpeg-videoplayer
-    git checkout master
-    git pull origin master
-    
-else
+	mkdir -p build
+	cd build
+	cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
 
-    #Official repo
-    #git clone https://github.com/scrawl/ogre-ffmpeg-videoplayer.git ogre-ffmpeg-videoplayer
-    # Currently working hhyyrylainen's fork
-    git clone https://github.com/hhyyrylainen/ogre-ffmpeg-videoplayer.git ogre-ffmpeg-videoplayer
-    cd ogre-ffmpeg-videoplayer
-fi
 
+	echo "Done"
+}
 
-mkdir -p build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_VIDEOPLAYER_DEMO=OFF
-cd "$StartingDirectory"
+function build_Deps() {
+	echo "Compiling. This may take a long time!"
+	build_Ogre
+	build_CEGUI
+	build_OgreFFMPEG
+	build_cAudio
+}
 
-echo -e "$GOOD Done $NC"
+function build_Ogre() {
+	echo "Ogre..."
 
+	cd "$StartingDirectory/ogreBuild/build"
+	eval "make $MakeArgs"
 
-echo -e "$INFO cAudio $NC"
+	
+	echo "Done"
+}
 
-if [ -d cAudio ]; then
+function build_CEGUI() {
+	echo "CEGUI..."
 
-    cd cAudio
-    #Workaround for broken latest version
-    #git checkout master
-    #git pull origin master
+	cd "$StartingDirectory/cegui/build"
+	eval "make $MakeArgs"
 
-    
-else
+	
+	echo "Done"
+}
 
-    git clone https://github.com/wildicv/cAudio.git
-    cd cAudio
-fi
+function build_OgreFFMPEG() {
+	echo "OgreFFMPEG..."
 
-#Workaround for broken build with latest version
-git checkout 22ff1a97a9a820c72726463708590adfae77008c
+	cd "$StartingDirectory/ogre-ffmpeg-videoplayer/build"
+	eval "make $MakeArgs"
 
-mkdir -p build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
+	
+	echo "Done"
+}
 
+function build_cAudio() {
+	echo "cAudio..."
 
-echo -e "$GOOD Done $NC"
+	cd "$StartingDirectory/cAudio/build"
+	eval "make $MakeArgs"
 
 
-echo -e "$INFO Compiling. This may take a long time! $NC"
+	echo "Done"
+}
 
-echo -e "$INFO Ogre... $NC"
+function install_Deps() {
+	echo "Installing dependencies"
+	install_Ogre
+    install_CEGUI
+    install_OgreFFMPEG
+    install_cAudio
+	echo "Done"
+}
 
-cd "$StartingDirectory/ogreBuild/build"
-eval "make $MakeArgs"
+function install_Ogre() {
+	echo "Installing Ogre, prepare for sudo password"
+	cd "$StartingDirectory/ogreBuild/build"
+	sudo make install
+}
 
+function install_CEGUI() {
+	echo "Installing CEGUI, prepare for sudo password"
+	cd "$StartingDirectory/cegui/build"
+	sudo make install
+}
 
-echo -e "$GOOD Done $NC"
+function install_OgreFFMPEG() {
+	echo "Installing OgreFFMPEG, prepare for sudo password"
+	cd "$StartingDirectory/ogre-ffmpeg-videoplayer/build"
+	sudo make install
+}
 
-echo -e "$INFO CEGUI... $NC"
+function install_cAudio() {
+	echo "Installing cAudio, prepare for sudo password"
+	cd "$StartingDirectory/cAudio/build"
+	sudo make install
+}
 
-cd "$StartingDirectory/cegui/build"
-eval "make $MakeArgs"
+function setup_Thrive() {
+	echo "Setting up Thrive"
+	cd "$StartingDirectory"
 
-echo -e "$GOOD Done $NC"
+	echo "Getting code"
 
-echo -e "$INFO OgreFFMPEG... $NC"
+	if [ -d thrive ]; then
 
-cd "$StartingDirectory/ogre-ffmpeg-videoplayer/build"
-eval "make $MakeArgs"
+		cd thrive
+		
+	else
 
-echo -e "$GOOD Done $NC"
+		git clone https://github.com/Revolutionary-Games/Thrive.git thrive
+		cd thrive
+		git submodule update --init --recursive
+	fi
 
-echo -e "$INFO cAudio... $NC"
+	git checkout $THRIVE_BRANCH
+	git pull --recurse-submodules origin $THRIVE_BRANCH
+	git submodule update --recursive
 
-cd "$StartingDirectory/cAudio/build"
-eval "make $MakeArgs"
+	echo "Getting assets"
 
-echo -e "$GOOD Done $NC"
+	if [ -d assets ]; then
+		(
+			cd assets
+			svn up
+		)   
+	else
 
+		svn checkout http://assets.revolutionarygamesstudio.com/ assets
+	fi
 
-echo -e "$INFO Installing dependencies $NC"
+	echo "Making all the links"
+	ln -sf assets/cegui_examples cegui_examples
+	ln -sf assets/definitions definitions
+	ln -sf assets/fonts fonts
+	ln -sf assets/gui gui
+	ln -sf assets/materials materials
+	ln -sf assets/models models
+	ln -sf assets/sounds sounds
 
-echo -e "$INFO Installing Ogre, prepare for sudo password $NC"
-cd "$StartingDirectory/ogreBuild/build"
-sudo make install
+	echo "Copying Ogre resources file"
+	cp ogre_cfg/resources.cfg build/resources.cfg
 
-echo -e "$INFO Installing CEGUI, prepare for sudo password $NC"
-cd "$StartingDirectory/cegui/build"
-sudo make install
+	echo "Copying complelety pointless Ogre files"
 
-echo -e "$INFO Installing OgreFFMPEG, prepare for sudo password $NC"
-cd "$StartingDirectory/ogre-ffmpeg-videoplayer/build"
-sudo make install
+	cp /usr/local/share/OGRE/plugins.cfg build/plugins.cfg
+}
 
-echo -e "$INFO Installing cAudio, prepare for sudo password $NC"
-cd "$StartingDirectory/cAudio/build"
-sudo make install
+function build_Thrive() {
+	echo "Compiling Thrive"
+	mkdir -p build
+	cd build
+	cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+	eval "make $MakeArgs"
+}
 
-echo -e "$GOOD Done $NC"
+function print_final_message() {
+	echo -e "$INFO ."
+	echo -e "$INFO ."
+	echo -e "$INFO ."
 
+	cd "$StartingDirectory"
 
-echo -e "$INFO Setting up Thrive $NC"
-cd "$StartingDirectory"
+	echo -e "$GOOD Done, run the game with '$StartingDirectory/thrive/build/Thrive' $NC"
+	cd "$OriginalFolder"
+}
 
-echo -e "$INFO Getting code $NC"
-
-if [ -d thrive ]; then
-
-    cd thrive
-    
-else
-
-    git clone https://github.com/Revolutionary-Games/Thrive.git thrive
-    cd thrive
-    git submodule update --init --recursive
-fi
-
-git checkout $THRIVE_BRANCH
-git pull --recurse-submodules origin $THRIVE_BRANCH
-git submodule update --recursive
-
-echo -e "$INFO Getting assets $NC"
-
-if [ -d assets ]; then
-    (
-        cd assets
-        svn up
-    )   
-else
-    svn checkout http://assets.revolutionarygamesstudio.com/ assets
-fi
-
-echo -e "$INFO Making all the links $NC"
-ln -sf assets/cegui_examples cegui_examples
-ln -sf assets/definitions definitions
-ln -sf assets/fonts fonts
-ln -sf assets/gui gui
-ln -sf assets/materials materials
-ln -sf assets/models models
-ln -sf assets/sounds sounds
-
-echo -e "$INFO Copying Ogre resources file $NC"
-cp ogre_cfg/resources.cfg build/resources.cfg
-
-echo -e "$INFO Copying complelety pointless Ogre files $NC"
-
-cp /usr/local/share/OGRE/plugins.cfg build/plugins.cfg
-
-
-
-echo -e "$INFO Compiling Thrive $NC"
-mkdir -p build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-eval "make $MakeArgs"
-
-echo -e "$INFO ."
-echo -e "$INFO ."
-echo -e "$INFO ."
-
-cd "$StartingDirectory"
-
-echo -e "$GOOD Done, run the game with '$StartingDirectory/thrive/build/Thrive' $NC"
-cd "$OriginalFolder"
+init
+install_Packages
+prepare_Deps
+build_Deps
+install_Deps
+setup_Thrive
+build_Thrive
+print_final_message
