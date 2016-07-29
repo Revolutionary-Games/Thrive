2d06cad3d473f0d1439f35595716cb0747966fbe
Filip Burlacu <filburli@gmail.com>
2016-07-29 18:20:53 -0400

    Species System bugfix (#439)
    Editor-created microbes now have proper species set up, and create copies of themselves on reproduction

--- scripts/microbe_stage/microbe_replacement.lua
+++ scripts/microbe_stage/microbe_replacement.lua
@@ -14,39 +14,31 @@ function MicrobeReplacementSystem:init()
 end
 
 function MicrobeReplacementSystem:activate()
-    activeCreatureId = Engine:playerData():activeCreature()
     if Engine:playerData():isBoolSet("edited_microbe") then
         Engine:playerData():setBool("edited_microbe", false)
 
-        workingMicrobe = Microbe(Entity(activeCreatureId, GameState.MICROBE_EDITOR), true)
+        activeCreatureId = Engine:playerData():activeCreature()
+        local workingMicrobe = Microbe(Entity(activeCreatureId, GameState.MICROBE_EDITOR), true)
 
-        new_species_name = workingMicrobe.microbe.speciesName .. global_speciesNameCounter
+        local new_species_name = workingMicrobe.microbe.speciesName .. global_speciesNameCounter
         global_speciesNameCounter = global_speciesNameCounter + 1
-        print("NEW SPECIES: "..new_species_name)
 
-        speciesEntity = Entity(new_species_name, GameState.MICROBE)
-        species = SpeciesComponent(new_species_name)
+        local speciesEntity = Entity(new_species_name)
+        local species = SpeciesComponent(new_species_name)
         speciesEntity:addComponent(species)
-        processorComponent = ProcessorComponent()
-        speciesEntity:addComponent(processorComponent)
-        for compoundID in CompoundRegistry.getCompoundList() do
-            processorComponent:setThreshold(compoundID, 10, 50, 100) -- we currently just generate a new processor for the new species
-        end
-        species:fromMicrobe(workingMicrobe)
-        -- below folded into :fromMicrobe()
-        -- species.colour = workingMicrobe:getComponent(MembraneComponent.TYPE_ID):getColour()
 
-        newMicrobe = Microbe.createMicrobeEntity(nil, false, new_species_name)
-        print(": "..newMicrobe.microbe.speciesName)
+        SpeciesSystem.fromMicrobe(workingMicrobe, species)
         workingMicrobe.entity:destroy()
 
-        -- species:template(newMicrobe)
-        -- newMicrobe.compoundBag:setProcessor(Entity(workingMicrobe.microbe.speciesName):getComponent(ProcessorComponent.TYPE_ID))
-        -- newMicrobe.compoundBag:setProcessor(processorComponent)
+        species.avgCompoundAmounts = {}
+        species.avgCompoundAmounts[CompoundRegistry.getCompoundId("atp")] = 10
+        species.avgCompoundAmounts[CompoundRegistry.getCompoundId("glucose")] = 20
+        species.avgCompoundAmounts[CompoundRegistry.getCompoundId("oxygen")] = 30
 
-        if newMicrobe:getCompoundAmount(CompoundRegistry.getCompoundId("atp")) < 10 then
-            newMicrobe:storeCompound(CompoundRegistry.getCompoundId("atp"), 10)
-        end
+        SpeciesSystem.initProcessorComponent(speciesEntity, species)
+
+        local newMicrobe = Microbe.createMicrobeEntity(nil, false, new_species_name)
+        print(": "..newMicrobe.microbe.speciesName)
 
         newMicrobe.collisionHandler:addCollisionGroup("powerupable")
         newMicrobeEntity = newMicrobe.entity:transfer(GameState.MICROBE)
@@ -54,7 +46,6 @@ function MicrobeReplacementSystem:activate()
         global_newEditorMicrobe = false
         Engine:playerData():setActiveCreature(newMicrobeEntity.id, GameState.MICROBE)
     end
-   
 end
 
 function MicrobeReplacementSystem:update(renderTime, logicTime)
