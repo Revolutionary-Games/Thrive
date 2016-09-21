ab78a2eb4003ec927caf27847f0d6e36a64f306e
MontySpud <montanaspud@gmail.com>
2016-09-20 18:37:50 -0600

    Fixed it up so it could be cleaner

--- scripts/microbe_stage/microbe_replacement.lua
+++ scripts/microbe_stage/microbe_replacement.lua
@@ -35,9 +35,6 @@ function MicrobeReplacementSystem:activate()
 		global_speciesGenName = (global_speciesNamePrefix[math.random(#global_speciesNamePrefix)]) .. (global_speciesNameCofix[math.random(#global_speciesNameCofix)]) .. (global_speciesNameSuffix[math.random(#global_speciesNameSuffix)])
         local new_species_name = global_Genus_Name .. global_speciesGenName
 		global_speciesPreviousName = global_speciesNamePrefix
-        global_speciesNamePrefix = {' Ce', ' Ar',' Sp', ' Th',' Co', ' So', ' Pu', ' Cr', ' Cy', ' Gr', ' Re', ' Ty', ' Tr', ' Ac' }
-        global_speciesNameCofix = { 'nan', 'mo', 'na', 'yt', 'yn', 'il', 'li','le', 'op', 'un', 'rive','ec', 'ro','lar' }
-        global_speciesNameSuffix = { 'pien', 'olera', 'rius', 'nien', 'ster', 'ilia', 'canus', 'tus', 'cys','ium'}
         
         local speciesEntity = Entity(new_species_name)
         local species = SpeciesComponent(new_species_name)
