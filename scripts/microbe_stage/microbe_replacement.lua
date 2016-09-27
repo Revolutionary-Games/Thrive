171fb22bd2e61952f3d317e07df4afbe71510864
Untrustedlife <bountybayonlineroblox@gmail.com>
2016-09-26 20:29:54 -0500

    465-Genus tweaking, rename improvements etc. (#466)
    Tweaked the genus system to be cleaner, with numerous user interface improvements, and more variation.
    
    Fixes #317 
    Fixes #465

--- scripts/microbe_stage/microbe_replacement.lua
+++ scripts/microbe_stage/microbe_replacement.lua
@@ -3,15 +3,16 @@ class 'MicrobeReplacementSystem' (System)
 
 -- Global boolean for whether a new microbe is avaliable in the microbe editor.
 global_newEditorMicrobe = false
---global_speciesNameCounter = 1  SERVES NO PURPOSE--
-global_speciesNamePrefix = { ' Co', ' So', ' Pu', ' Cr', ' Cy', ' Gr', ' Re', ' Ty', ' Tr' }
-global_speciesNameCofix = { 'nan', 'mo', 'na', 'yt', 'yn', 'il', 'li', 'op', 'un' }
-global_speciesNameSuffix = { 'pien', 'olera', 'rius', 'nien', 'ster', 'ilia', 'canus', 'tus', 'cys'}
-global_Genus_Picked = 0
+--set it up so the game knows whether or not to replace the genus.
+global_genusPicked = false
 
 
 function MicrobeReplacementSystem:__init()
     System.__init(self)
+    --prefix,cofix,suffix list
+    self.speciesNamePrefix = {' Ce', ' Ar',' Sp', ' Th',' Co', ' So', ' Pu', ' Cr', ' Cy', ' Gr', ' Re', ' Ty', ' Tr', ' Ac',' Pr' }
+    self.speciesNameCofix = { 'nan', 'mo', 'na', 'yt', 'yn', 'il', 'li','le', 'op', 'un', 'rive','ec', 'ro','lar','im' }
+    self.speciesNameSuffix = { 'pien', 'olera', 'rius', 'nien', 'ster', 'ilia', 'canus', 'tus', 'cys','ium','um'} 
 end
 
 function MicrobeReplacementSystem:init()
@@ -25,22 +26,16 @@ function MicrobeReplacementSystem:activate()
 
         activeCreatureId = Engine:playerData():activeCreature()
         local workingMicrobe = Microbe(Entity(activeCreatureId, GameState.MICROBE_EDITOR), true)
-		
-		if global_Genus_Picked == 0 then
-			global_Genus_Name = workingMicrobe.microbe.speciesName
-			global_Genus_Picked = 1
-		end
-			
-        math.randomseed(os.time())
-		global_speciesGenName = (global_speciesNamePrefix[math.random(9)]) .. (global_speciesNameCofix[math.random(9)]) .. (global_speciesNameSuffix[math.random(9)])
-        local new_species_name = global_Genus_Name .. global_speciesGenName
-		global_speciesPreviousName = global_speciesNamePrefix
-		global_speciesNamePrefix = { ' Co', ' So', ' Pu', ' Cr', ' Cy', ' Gr', ' Re', ' Ty', ' Tr' }
-		global_speciesNameCofix = { 'nan', 'mo', 'na', 'yt', 'yn', 'il', 'li', 'op', 'un' }
-		global_speciesNameSuffix = { 'pien', 'olera', 'rius', 'nien', 'ster', 'ilia', 'canus', 'tus', 'cys'}
+ 
         
-        local speciesEntity = Entity(new_species_name)
-        local species = SpeciesComponent(new_species_name)
+        if not global_genusPicked  then
+            global_genusPicked = true;
+            global_genusName = workingMicrobe.microbe.speciesName
+        end
+			
+        newSpeciesName = self:generateSpeciesName();
+        local speciesEntity = Entity(newSpeciesName)
+        local species = SpeciesComponent(newSpeciesName)
         speciesEntity:addComponent(species)
 
         SpeciesSystem.fromMicrobe(workingMicrobe, species)
@@ -53,7 +48,7 @@ function MicrobeReplacementSystem:activate()
 
         SpeciesSystem.initProcessorComponent(speciesEntity, species)
 
-        local newMicrobe = Microbe.createMicrobeEntity(nil, false, new_species_name)
+        local newMicrobe = Microbe.createMicrobeEntity(nil, false, newSpeciesName)
         print(": "..newMicrobe.microbe.speciesName)
 
         newMicrobe.collisionHandler:addCollisionGroup("powerupable")
@@ -62,6 +57,14 @@ function MicrobeReplacementSystem:activate()
         global_newEditorMicrobe = false
         Engine:playerData():setActiveCreature(newMicrobeEntity.id, GameState.MICROBE)
     end
+end
+
+--Faux-latin name generation routine (Move to own file eventually?)
+function MicrobeReplacementSystem:generateSpeciesName()
+    --Generate random seed
+    math.randomseed(os.time())    
+    local speciesGenName = (self.speciesNamePrefix[math.random(#self.speciesNamePrefix)]) .. (self.speciesNameCofix[math.random(#self.speciesNameCofix)]) .. (self.speciesNameSuffix[math.random(#self.speciesNameSuffix)])
+    return global_genusName .. speciesGenName;
 end
 
 function MicrobeReplacementSystem:update(renderTime, logicTime)
