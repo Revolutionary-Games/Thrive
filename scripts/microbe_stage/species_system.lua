5dec9df7e12adb64180b62161f9e68f7796e2cd6
crodnu <crodnu@gmail.com>
2016-12-25 17:13:31 -0300

    Fixed auto-evo spawn system
    Fixed the bug where extinguishing a species from the auto-evo system
    wouldn't remove it's spawn.
    Also removed minor bug where the game would start paused after entering
    a second time.

--- scripts/microbe_stage/species_system.lua
+++ scripts/microbe_stage/species_system.lua
@@ -36,11 +36,10 @@ local DEFAULT_INITIAL_COMPOUNDS =
 
 --sets up the spawn of the species
 function Species:setupSpawn()
-    local name = self.name --this line is important, otherwise the game only spawns the las microbe generated
     self.id = currentSpawnSystem:addSpawnType
     (
         function(pos)
-            return microbeSpawnFunctionGeneric(pos, name, true, nil)
+            return microbeSpawnFunctionGeneric(pos, self.name, true, nil)
         end, 
         DEFAULT_SPAWN_DENSITY, --spawnDensity should depend on population
         DEFAULT_SPAWN_RADIUS
@@ -118,12 +117,12 @@ function createSpeciesTemplate(name, organelles, colour, compounds, speciesThres
     return speciesEntity
 end
 
-function Species:init()
+function Species:__init()
     self.population = INITIAL_POPULATION
     self.name = "Species_" .. tostring(math.random()) --gotta use the latin names
     
     local stringSize = math.random(MIN_INITIAL_LENGTH, MAX_INITIAL_LENGTH)
-    self.stringCode = "NY" --it should always have a nucleus and a cytoplasm.
+    self.stringCode = organelleTable.nucleus.gene .. organelleTable.cytoplasm.gene --it should always have a nucleus and a cytoplasm.
     for i = 1, stringSize do
         self.stringCode = self.stringCode .. getRandomLetter()
     end
@@ -151,8 +150,8 @@ end
 
 --delete a species
 function Species:extinguish()
-    self.template:destroy()
     currentSpawnSystem:removeSpawnType(self.id)
+    --self.template:destroy() --game crashes if i do that.
 end
 
 local function mutate(stringCode)
@@ -312,7 +311,6 @@ function SpeciesSystem:update(_, milliseconds)
     gSpeciesSystem = self --so hacky :c
     self.timeSinceLastCycle = self.timeSinceLastCycle + milliseconds
     while self.timeSinceLastCycle > SPECIES_SIM_INTERVAL do
-        -- do mutation-management here
         --update population numbers and split/extinct species as needed
         local numberOfSpecies = self.number_of_species
         for i = 0, numberOfSpecies - 1 do
@@ -344,7 +342,7 @@ function SpeciesSystem:update(_, milliseconds)
         --new species
         if self.number_of_species < MIN_SPECIES then
             for i = self.number_of_species, INITIAL_SPECIES - 1 do
-                newSpecies = Species:init()
+                newSpecies = Species()
                 table.insert(self.species, newSpecies)
                 self.number_of_species = self.number_of_species + 1
             end
