9082110238031b879b28790f01389d635005f206
Oliveriver <oliver.lugg@tesco.net>
2017-01-06 19:38:08 +0000

    Update microbe_stage_hud.lua

--- scripts/microbe_stage/microbe_stage_hud.lua
+++ scripts/microbe_stage/microbe_stage_hud.lua
@@ -25,6 +25,8 @@ function HudSystem:activate()
     end
     self.helpOpen = true
     self.menuOpen = true
+    self.compoundsOpen = true
+    Engine:resumeGame()
     self:updateLoadButton()
     -- Always start the game without being able to reproduce.
     
@@ -37,29 +39,37 @@ function HudSystem:init(gameState)
     self.compoundListBox = self.rootGUIWindow:getChild("CompoundsOpen")
     self.hitpointsBar = self.rootGUIWindow:getChild("HealthPanel"):getChild("LifeBar")
     self.hitpointsCountLabel = self.hitpointsBar:getChild("NumberLabel")
-    self.nameLabel = self.rootGUIWindow:getChild("SpeciesNamePanel"):getChild("SpeciesNameLabel")
     local menuButton = self.rootGUIWindow:getChild("MenuButton")
-    local saveButton = self.rootGUIWindow:getChild("SaveGameButton") 
-    local loadButton = self.rootGUIWindow:getChild("LoadGameButton")	
+    local saveButton = self.rootGUIWindow:getChild("PauseMenu"):getChild("SaveGameButton") 
+    local loadButton = self.rootGUIWindow:getChild("PauseMenu"):getChild("LoadGameButton")
+    local resumeButton = self.rootGUIWindow:getChild("PauseMenu"):getChild("ResumeButton")
+    local closeHelpButton = self.rootGUIWindow:getChild("PauseMenu"):getChild("CloseHelpButton")
     --local collapseButton = self.rootGUIWindow:getChild() collapseButtonClicked
-    local helpButton = self.rootGUIWindow:getChild("HelpButton")
-    local helpPanel = self.rootGUIWindow:getChild("HelpPanel")
+    local helpButton = self.rootGUIWindow:getChild("PauseMenu"):getChild("HelpButton")
+    local helpPanel = self.rootGUIWindow:getChild("PauseMenu"):getChild("HelpPanel")
     self.editorButton = self.rootGUIWindow:getChild("EditorButton")
+    local editorInfoOpen = self.rootGUIWindow:getChild("EditorInfoOpen")
+    local editorInfoClosed = self.rootGUIWindow:getChild("EditorInfoClosed")
+    local suicideButton = self.rootGUIWindow:getChild("SuicideButton")
     --local returnButton = self.rootGUIWindow:getChild("MenuButton")
-    local compoundButton = self.rootGUIWindow:getChild("CompoundsClosed")
+    local compoundButton = self.rootGUIWindow:getChild("CompoundExpandButton")
     --local compoundPanel = self.rootGUIWindow:getChild("CompoundsOpen")
-    local quitButton = self.rootGUIWindow:getChild("QuitButton")
+    local quitButton = self.rootGUIWindow:getChild("PauseMenu"):getChild("QuitButton")
     saveButton:registerEventHandler("Clicked", function() self:saveButtonClicked() end)
     loadButton:registerEventHandler("Clicked", function() self:loadButtonClicked() end)
     menuButton:registerEventHandler("Clicked", function() self:menuButtonClicked() end)
+    resumeButton:registerEventHandler("Clicked", function() self:resumeButtonClicked() end)
+    closeHelpButton:registerEventHandler("Clicked", function() self:closeHelpButtonClicked() end)
     helpButton:registerEventHandler("Clicked", function() self:helpButtonClicked() end)
-    helpPanel:registerEventHandler("Clicked", function() self:helpButtonClicked() end)
+    editorInfoOpen:registerEventHandler("Clicked", function() self:editorInfoOpenClicked() end)
+    editorInfoClosed:registerEventHandler("Clicked", function() self:editorInfoClosedClicked() end)
+    suicideButton:registerEventHandler("Clicked", function() self:suicideButtonClicked() end)
     self.editorButton:registerEventHandler("Clicked", function() self:editorButtonClicked() end)
     --returnButton:registerEventHandler("Clicked", returnButtonClicked)
     compoundButton:registerEventHandler("Clicked", function() self:openCompoundPanel() end)
     --compoundPanel:registerEventHandler("Clicked", function() self:closeCompoundPanel() end)
     quitButton:registerEventHandler("Clicked", quitButtonClicked)
-    self.rootGUIWindow:getChild("MainMenuButton"):registerEventHandler("Clicked", menuMainMenuClicked) -- in microbe_editor_hud.lua
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("MainMenuButton"):registerEventHandler("Clicked", function() self:menuMainMenuClicked() end)
     self:updateLoadButton();
 end
 
@@ -67,12 +77,6 @@ end
 function HudSystem:update(renderTime)
     local player = Entity("player")
     local playerMicrobe = Microbe(player)
-    local name = playerMicrobe.microbe.speciesName
-    if string.len(name) > 18 then
-        name = string.sub(playerMicrobe.microbe.speciesName, 1, 15)
-        name = name .. "..."
-    end
-    self.nameLabel:setText(name)
 
     self.hitpointsBar:progressbarSetProgress(playerMicrobe.microbe.hitpoints/playerMicrobe.microbe.maxHitpoints)
     self.hitpointsCountLabel:setText("".. math.floor(playerMicrobe.microbe.hitpoints))
@@ -172,9 +176,9 @@ end
 
 function HudSystem:updateLoadButton()
     if Engine:fileExists("quick.sav") then
-        self.rootGUIWindow:getChild("LoadGameButton"):enable();
+        self.rootGUIWindow:getChild("PauseMenu"):getChild("LoadGameButton"):enable();
     else
-        self.rootGUIWindow:getChild("LoadGameButton"):disable();
+        self.rootGUIWindow:getChild("PauseMenu"):getChild("LoadGameButton"):disable();
     end
 end
 
@@ -185,7 +189,7 @@ function HudSystem:saveButtonClicked()
     Engine:save("quick.sav")
     print("Game Saved");
 	--Because using update load button here doesn't seem to work unless you press save twice
-    self.rootGUIWindow:getChild("LoadGameButton"):enable();
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("LoadGameButton"):enable();
 end
 function HudSystem:loadButtonClicked()
     local guiSoundEntity = Entity("gui_sounds")
@@ -198,56 +202,79 @@ function HudSystem:menuButtonClicked()
     local guiSoundEntity = Entity("gui_sounds")
     guiSoundEntity:getComponent(SoundSourceComponent.TYPE_ID):playSound("button-hover-click")
     print("played sound")
-    if not self.menuOpen then
-        self.rootGUIWindow:getChild("StatsButton"):playAnimation("MoveToStatsButton");
-        self.rootGUIWindow:getChild("HelpButton"):playAnimation("MoveToHelpButton");
-        self.rootGUIWindow:getChild("OptionsButton"):playAnimation("MoveToOptionsButton");
-        self.rootGUIWindow:getChild("LoadGameButton"):playAnimation("MoveToLoadGameButton");
-        self.rootGUIWindow:getChild("SaveGameButton"):playAnimation("MoveToSaveGameButton");
-        self:updateLoadButton();
-        self.menuOpen = true
-    else
-        self.rootGUIWindow:getChild("StatsButton"):playAnimation("MoveToMenuButtonD0");
-        self.rootGUIWindow:getChild("HelpButton"):playAnimation("MoveToMenuButtonD2");
-        self.rootGUIWindow:getChild("OptionsButton"):playAnimation("MoveToMenuButtonD1");
-        self.rootGUIWindow:getChild("LoadGameButton"):playAnimation("MoveToMenuButtonD4");
-        self.rootGUIWindow:getChild("SaveGameButton"):playAnimation("MoveToMenuButtonD3");
-        self:updateLoadButton();
-        self.menuOpen = false
+    self.rootGUIWindow:getChild("PauseMenu"):show()
+    self.rootGUIWindow:getChild("PauseMenu"):moveToFront()
+    self:updateLoadButton();
+    Engine:pauseGame()
+    self.menuOpen = true
     end
 end
 
-
-function HudSystem:openCompoundPanel()
+function HudSystem:resumeButtonClicked()
     local guiSoundEntity = Entity("gui_sounds")
     guiSoundEntity:getComponent(SoundSourceComponent.TYPE_ID):playSound("button-hover-click")
-    self.rootGUIWindow:getChild("CompoundsOpen"):show()
-    self.rootGUIWindow:getChild("CompoundsClosed"):hide()
+    print("played sound")
+    self.rootGUIWindow:getChild("PauseMenu"):hide()
+    self:updateLoadButton();
+    Engine:resumeGame()
+    self.menuOpen = false
 end
 
-function HudSystem:closeCompoundPanel()
+function HudSystem:toggleCompoundPanel()
     local guiSoundEntity = Entity("gui_sounds")
     guiSoundEntity:getComponent(SoundSourceComponent.TYPE_ID):playSound("button-hover-click")
-    self.rootGUIWindow:getChild("CompoundsOpen"):hide()
-    self.rootGUIWindow:getChild("CompoundsClosed"):show()
+    if self.compoundsOpen then
+    self.rootGUIWindow:getChild("CompoundPanel"):hide()
+    self.rootGUIWindow:getChild("CompoundExpandButton"):getChild("CompoundExpandIcon"):hide()
+    self.rootGUIWindow:getChild("CompoundExpandButton"):getChild("CompoundContractIcon"):show()
+    self.compoundsOpen = false
+    else
+    self.rootGUIWindow:getChild("CompoundPanel"):show()
+    self.rootGUIWindow:getChild("CompoundExpandButton"):getChild("CompoundExpandIcon"):show()
+    self.rootGUIWindow:getChild("CompoundExpandButton"):getChild("CompoundContractIcon"):hide()
+    self.compoundsOpen = true
+    end
 end
 
 function HudSystem:helpButtonClicked()
     local guiSoundEntity = Entity("gui_sounds")
     guiSoundEntity:getComponent(SoundSourceComponent.TYPE_ID):playSound("button-hover-click")
-    --Engine:currentGameState():rootGUIWindow():getChild("MenuPanel"):hide()
-    if Engine:currentGameState():name() == "microbe" then
-        if self.helpOpen then
-            Engine:resumeGame()
-            self.rootGUIWindow:getChild("HelpPanel"):hide()
-        else
-            Engine:pauseGame()
-            self.rootGUIWindow:getChild("HelpPanel"):show()
-        end
-        self.helpOpen = not self.helpOpen
-    end
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("HelpPanel"):show()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("CloseHelpButton"):show()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("ResumeButton"):hide()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("QuicksaveButton"):hide()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("SaveGameButton"):hide()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("LoadGameButton"):hide()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("StatsButton"):hide()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("HelpButton"):hide()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("OptionsButton"):hide()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("MainMenuButton"):hide()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("QuitButton"):hide()
+    self.helpOpen = not self.helpOpen
 end
 
+function HudSystem:closeHelpButtonClicked()
+    local guiSoundEntity = Entity("gui_sounds")
+    guiSoundEntity:getComponent(SoundSourceComponent.TYPE_ID):playSound("button-hover-click")
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("HelpPanel"):hide()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("CloseHelpButton"):hide()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("ResumeButton"):show()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("QuicksaveButton"):show()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("SaveGameButton"):show()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("LoadGameButton"):show()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("StatsButton"):show()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("HelpButton"):show()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("OptionsButton"):show()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("MainMenuButton"):show()
+    self.rootGUIWindow:getChild("PauseMenu"):getChild("QuitButton"):show()
+    self.helpOpen = not self.helpOpen
+end
+
+function HudSystem:menuMainMenuClicked()
+    local guiSoundEntity = Entity("gui_sounds")
+    guiSoundEntity:getComponent(SoundSourceComponent.TYPE_ID):playSound("button-hover-click")
+    Engine:setCurrentGameState(GameState.MAIN_MENU)
+end
 
 function HudSystem:editorButtonClicked()
     local player = Entity("player")
@@ -259,6 +286,20 @@ function HudSystem:editorButtonClicked()
     guiSoundEntity:getComponent(SoundSourceComponent.TYPE_ID):playSound("button-hover-click")
     self.editorButton:disable()
     Engine:setCurrentGameState(GameState.MICROBE_EDITOR)        
+end
+
+function HudSystem:editorInfoOpenClicked()
+    local guiSoundEntity = Entity("gui_sounds")
+    guiSoundEntity:getComponent(SoundSourceComponent.TYPE_ID):playSound("button-hover-click")
+    self.rootGUIWindow:getChild("EditorInfoClosed"):show()
+    self.rootGUIWindow:getChild("EditorInfoOpen"):hide()
+end
+
+function HudSystem:editorInfoClosedClicked()
+    local guiSoundEntity = Entity("gui_sounds")
+    guiSoundEntity:getComponent(SoundSourceComponent.TYPE_ID):playSound("button-hover-click")
+    self.rootGUIWindow:getChild("EditorInfoClosed"):hide()
+    self.rootGUIWindow:getChild("EditorInfoOpen"):show()
 end
 
 --[[
