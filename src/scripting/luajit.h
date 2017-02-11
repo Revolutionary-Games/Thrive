0cbe15b1049af0cce5b3115a6dc462cf566b5250
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2017-02-11 13:39:01 +0200

    Now ignoring a bunch of warnings so that sol.hpp can be included

--- src/scripting/luajit.h
+++ src/scripting/luajit.h
@@ -7,5 +7,20 @@
 // }
 
 #include "luajit/src/lua.hpp"
+
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wredundant-decls"
+#pragma GCC diagnostic ignored "-Wfloat-equal"
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wctor-dtor-privacy"
+#endif
+
+
 #include "sol.hpp"
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
 
