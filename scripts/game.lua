fa93c7551e99e33645a50c6a32136b068665dd53
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2017-03-05 01:05:22 +0200

    Made FPS counter actually accurate

--- scripts/game.lua
+++ scripts/game.lua
@@ -19,39 +19,39 @@ function enterLuaMain(cppGame)
 
    local fpsCount = 0
 
-   local fpsTime = 0
-
    local lastUpdate = Game.now()
+
+   -- For more accurate FPS counting
+   local lastSecond = Game.now()
    
    while cppGame.shouldQuit == false do
 
-      local now = Game.now();
+      local now = Game.now()
       
       local milliseconds = Game.asMS(Game.delta(now, lastUpdate))
       
-      lastUpdate = now;
+      lastUpdate = now
 
       -- Update engine stuff and GameStates
       g_luaEngine:update(milliseconds)
 
-      local frameDuration = Game.delta(Game.now(), now);
+      local frameDuration = Game.delta(Game.now(), now)
 
       -- sleep if we are going too fast
       cppGame:sleepIfNeeded(frameDuration)
       
       -- update fps counter
-      fpsCount = fpsCount + 1;
-      fpsTime = fpsTime + Game.asMS(frameDuration)
-      
+      fpsCount = fpsCount + 1
+
+      local fpsTime = Game.asMS(Game.delta(now, lastSecond))
       if fpsTime >= 1000 then
          
          local fps = 1000 * (fpsCount / fpsTime)
          
          print("FPS: " .. fps)
-         
+
+         lastSecond = now
          fpsCount = 0
-         fpsTime = 0
-         
       end
    end
 end
