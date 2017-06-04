a92e7df2fd5901e0318a0a821043cc1fc6875f74
crodnu <crodnu@gmail.com>
2017-06-04 12:12:30 -0300

    Reverting cloud colors
    lol

--- scripts/microbe_stage/compound_table.lua
+++ scripts/microbe_stage/compound_table.lua
@@ -26,9 +26,9 @@ compoundTable = {
         isCloud = true,
 
         colour = {
-            r = 100,
-            g = 230,
-            b = 230
+            r = 60,
+            g = 160,
+            b = 180
         }
     },
 
@@ -63,9 +63,9 @@ compoundTable = {
         isCloud = true,
 
         colour = {
-            r = 220,
-            g = 220,
-            b = 220
+            r = 150,
+            g = 170,
+            b = 180
         }
     },
 
