67ac98a74193bf3b93b2b767d55b7e354ce7e041
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2017-07-25 00:22:47 +0300

    Basic cmake configure now runs successfully

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,86 +1,153 @@
-#include <boost/thread.hpp>
-#include <cstdio>
-#include <fstream>
-#include "game.h"
-#include <iostream>
-#include <OgreRoot.h>
-#include <string>
+#include "thrive_include.h"
+#include "ThriveGame.h"
+#include "resource.h"
 
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-#define WIN32_LEAN_AND_MEAN
-#include "windows.h"
-#endif
+#include "Define.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-    /**
-    *
-    * Init function. Anything that has to happen first goes here
-    *
-    * @return: 0 for success, 1 for failure
-    **/
-    int
-    initThrive();
-
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-    INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
+// Breakpad is used to detect and report crashes
+#ifdef LEVIATHAN_USING_BREAKPAD
+#ifdef __linux
+#include "client/linux/handler/exception_handler.h"
+#elif defined(_WIN32)
+#include "client/windows/handler/exception_handler.h"
 #else
-    int main(int argc, char *argv[])
+#error no breakpad on platform
 #endif
-    {
-        if (initThrive() != 0){
-            return -1;
-        }
-        using namespace thrive;
-        Game& game = Game::instance();
-        game.run();
-        return 0;
-    }
+#endif //USE_BREAKPAD
 
-    int
-    initThrive(){
-        std::ifstream versionFile("thriveversion.ver");
-        std::ifstream mainLayout("../gui/layouts/MainMenu.layout");
-        std::ofstream tempFile("../gui/layouts/MainMenuTemp.layout");
+using namespace Thrive;
 
-        std::string version;
-        std::getline(versionFile, version);
+// Don't look at the mess ahead, just set the variables in your cmake file //
 
-        std::string str;
-        std::string needle = "<Property name=\"Text\" value=\"v";
-        std::size_t found = 0;
-
-        while (std::getline(mainLayout, str)){
-            // Process str
-            found = str.find(needle);
-            if (found != std::string::npos)
-            {
-                tempFile << str.substr(0, str.find("\"v")) << "\"v" << version << "\" />" << '\n';
-                continue;
-                //std::cout << "<Property name=\"Text\" value=\"v" << version << "\" />" << '\n';
-            }
-            tempFile << str << '\n';
-        }
-        versionFile.close();
-        mainLayout.close();
-        tempFile.close();
-
-        if( remove( "../gui/layouts/MainMenu.layout" ) != 0 ){
-            perror( "Error deleting main menu layout" );
-            return 1;
-        }
-
-        int result = std::rename("../gui/layouts/MainMenuTemp.layout", "../gui/layouts/MainMenu.layout");
-        if ( result != 0 ){
-            perror( "Error renaming file" );
-            return 1;
-        }
-        return 0;
-
-    }
-
-#ifdef __cplusplus
+#if LEVIATHAN_USING_BREAKPAD
+#ifdef _WIN32
+#ifndef _DEBUG
+bool DumpCallback(const wchar_t* dump_path,
+    const wchar_t* minidump_id, void* context, EXCEPTION_POINTERS* exinfo,
+    MDRawAssertionInfo* assertion, bool succeeded)
+{
+    
+    const string path = Convert::WstringToString(dump_path);
+    
+    printf("Dump path: %s\n", path.c_str());
+    
+    return succeeded;
+}
+#endif //_DEBUG
+#else
+bool DumpCallback(const google_breakpad::MinidumpDescriptor& descriptor, void* context,
+    bool succeeded)
+{
+    printf("Dump path: %s\n", descriptor.path());
+    return succeeded;
 }
 #endif
+#endif //USE_BREAKPAD
+
+#ifdef _WIN32
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
+    int nCmdShow)
+{
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetReportMode( _CRT_ASSERT, _CRTDBG_MODE_DEBUG);
+#endif
+
+#else
+int main(int argcount, char* args[]){
+#endif
+    
+#ifdef _WIN32
+    int argcount = 1;
+    char* args[] = { lpCmdLine };
+#else
+    // We need to skip the program name
+    args += 1;
+    --argcount;
+#endif
+    
+    int Return = 0;
+    
+#ifdef _WIN32
+    HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, NULL, 0);
+
+    if(SUCCEEDED(CoInitialize(NULL))){
+#else
+
+#endif
+
+#if LEVIATHAN_USING_BREAKPAD
+    // Crash handler //
+#ifdef _WIN32
+#ifndef _DEBUG
+    google_breakpad::ExceptionHandler ExceptionHandler(L"C://tmp", NULL, DumpCallback, NULL, 
+        google_breakpad::ExceptionHandler::HANDLER_ALL,
+        (MINIDUMP_TYPE)(MiniDumpNormal & MiniDumpWithThreadInfo),
+        (const wchar_t*)nullptr, NULL);
+        
+#endif //_DEBUG
+#else
+    google_breakpad::MinidumpDescriptor descriptor("/tmp");
+
+    google_breakpad::ExceptionHandler ExceptionHandler(descriptor, NULL, DumpCallback, NULL,
+        true, -1);
+#endif
+#endif //USE_BREAKPAD
+    
+    // Create program object //
+    ThriveGame app;
+
+    std::unique_ptr<AppDef> ProgramDefinition(AppDef::GenerateAppdefine("ThriveLog",
+            "./EngineConf.conf", 
+        "./ThriveGame.conf", "./ThriveKeybindings.conf", &ThriveGame::CheckGameConfigurationVariables,
+        &ThriveGame::CheckGameKeyConfigVariables));
+
+    // Fail if no definition could be created //
+    if(!ProgramDefinition){
+
+        std::cout << "FATAL: failed to create AppDefine" << std::endl;
+        return 2;
+    }
+    
+    
+    // customize values //
+#ifdef _WIN32
+    ProgramDefinition->SetHInstance(hInstance);
+#endif
+    ProgramDefinition->SetMasterServerParameters(MasterServerInformation("ThriveMasters.txt", "Thrive_" GAME_VERSIONS, "http://revolutionarygamesstudio.com/", "/Thrive/MastersList.png", "ThriveAccountCrecentials.txt", false)).
+        SetApplicationIdentification(
+        "Thrive game version " GAME_VERSIONS, "Thrive",
+        "GAME_VERSIONS");
+    
+    // Create window last //
+    ProgramDefinition->StoreWindowDetails(ThriveGame::GenerateWindowTitle(), true,
+#ifdef _WIN32
+        LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1)),
+#endif
+        &app);
+
+    if(!app.PassCommandLine(argcount, args)){
+
+        std::cout << "Error: Invalid Command Line arguments. Shutting down" << std::endl;
+        return 3;
+    }
+    
+    if(app.Initialize(ProgramDefinition.get())){
+
+        // this is where the game should customize the engine //
+        app.CustomizeEnginePostLoad();
+
+        LOG_INFO("Engine successfully initialized");
+        Return = app.RunMessageLoop();
+    } else {
+        LOG_ERROR("App init failed, closing");
+        app.ForceRelease();
+        Return = 5;
+    }
+#ifdef _WIN32
+    }
+    //_CrtDumpMemoryLeaks();
+    CoUninitialize();
+#endif
+
+    return Return;
+}
---
+++ src/thrive_include.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+
+#define Thrive_VERSION           0.040
+#define Thrive_VERSIONS          "0.0.4.0"
+#define Thrive_VERSIONS_ANSI     "0.0.4.0"
+
+#define Thrive_VERSION_STABLE    0
+#define Thrive_VERSION_MAJOR     0
+#define Thrive_VERSION_MINOR     4
+#define Thrive_VERSION_PATCH     0
+
+#define GAME_VERSION        Thrive_VERSION
+#define GAME_VERSIONS       Thrive_VERSIONS
+#define GAME_VERSIONS_ANSI  Thrive_VERSIONS_ANSI
+
