41ea3c91b2fe4112365d015eb465f9c851783d2a
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-03-16 23:47:07 +0200

    Updated setup and added fast(er) script for recompiling just the engine and thrive

---
+++ QuickRecompile.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# This script runs the setup with options that make recompiling much faster (but it doesn't
+# update everything!)
+require_relative 'RubySetupSystem/RubyCommon.rb'
+
+system("ruby SetupThrive.rb --no-updates --no-packagemanager --no-subproject-deps")
+exit $?.exitstatus == 0
