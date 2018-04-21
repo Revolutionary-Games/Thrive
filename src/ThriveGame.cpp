35ff9ea2a818ad816485866e22315c81f46e8b9d
untrustedlife <bountybayonlineroblox@gmail.com>
2018-04-20 19:22:56 -0500

    clang formatted ThriveGame.cpp

--- src/ThriveGame.cpp
+++ src/ThriveGame.cpp
@@ -44,8 +44,7 @@ public:
         m_menuKeyPresses(std::make_shared<MainMenuKeyPressListener>()),
         m_cellStageKeys(std::make_shared<PlayerMicrobeControl>(
             *game.ApplicationConfiguration->GetKeyConfiguration()))
-    {
-    }
+    {}
 
     //! Releases Ogre things. Needs to be called before shutdown
     void
@@ -548,8 +547,7 @@ void
 // ------------------------------------ //
 void
     ThriveGame::Tick(int mspassed)
-{
-}
+{}
 
 void
     ThriveGame::CustomizeEnginePostLoad()
@@ -767,8 +765,7 @@ void
 void
     ThriveGame::CheckGameConfigurationVariables(Lock& guard,
         GameConfiguration* configobj)
-{
-}
+{}
 
 void
     ThriveGame::CheckGameKeyConfigVariables(Lock& guard,
@@ -1682,9 +1679,7 @@ SpawnerTypeId
 
     return self->addSpawnType(
         [=](CellStageWorld& world, Float3 pos) -> ObjectID {
-
             return wrapper->run(world, pos);
-
         },
         spawnDensity, spawnRadius);
 }
