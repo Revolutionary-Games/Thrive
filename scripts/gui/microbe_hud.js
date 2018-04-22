99ccb922fb9d26b90b4c68c646a3ec0a3197deb8
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-04-22 22:19:54 +0300

    The compound are now shown in the GUI

--- scripts/gui/microbe_hud.js
+++ scripts/gui/microbe_hud.js
@@ -15,13 +15,44 @@ function runMicrobeHUDSetup(){
         Leviathan.OnGeneric("PlayerCompoundAmounts", (event, vars) => {
 
             // Apply the new values
-            document.getElementById("microbeHUDPlayerHitpoints").textContent = vars.hitpoints;
+            document.getElementById("microbeHUDPlayerHitpoints").textContent =
+                vars.hitpoints;
             document.getElementById("microbeHUDPlayerMaxHitpoints").textContent =
                 vars.maxHitpoints;
 
             document.getElementById("microbeHUDPlayerATP").textContent =
                 vars.compoundATP;
-            
+
+            // The bars
+            document.getElementById("microbeHUDPlayerATPCompound").textContent =
+                vars.compoundATP;
+            document.getElementById("microbeHUDPlayerATPMax").textContent =
+                vars.ATPMax;
+
+            document.getElementById("microbeHUDPlayerAminoacids").textContent =
+                vars.compoundAminoacids;
+            document.getElementById("microbeHUDPlayerAminoacidsMax").textContent =
+                vars.AminoacidsMax;
+
+            document.getElementById("microbeHUDPlayerAmmonia").textContent =
+                vars.compoundAmmonia;
+            document.getElementById("microbeHUDPlayerAmmoniaMax").textContent =
+                vars.AmmoniaMax;
+
+            document.getElementById("microbeHUDPlayerGlucose").textContent =
+                vars.compoundGlucose;
+            document.getElementById("microbeHUDPlayerGlucoseMax").textContent =
+                vars.GlucoseMax;
+
+            document.getElementById("microbeHUDPlayerFattyacids").textContent =
+                vars.compoundFattyacids;
+            document.getElementById("microbeHUDPlayerFattyacidsMax").textContent =
+                vars.FattyacidsMax;
+
+            document.getElementById("microbeHUDPlayerOxytoxy").textContent =
+                vars.compoundOxytoxy;
+            document.getElementById("microbeHUDPlayerOxytoxyMax").textContent =
+                vars.OxytoxyMax;
         });
     }
     
