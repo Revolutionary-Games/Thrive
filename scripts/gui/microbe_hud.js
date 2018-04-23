6ff21559b47e25f6acbec209b9ce712ffa90d18d
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-04-23 20:45:45 +0300

    Somewhat working health and ATP bars

--- scripts/gui/microbe_hud.js
+++ scripts/gui/microbe_hud.js
@@ -2,8 +2,8 @@
 
 let microbeHudSetupRan = false;
 
-// Registers all the stuff for this to work. For performance reasons
-// this should only be called
+//! Registers all the stuff for this to work. For performance reasons
+//! this should only be called
 function runMicrobeHUDSetup(){
 
     if(microbeHudSetupRan)
@@ -15,46 +15,78 @@ function runMicrobeHUDSetup(){
         Leviathan.OnGeneric("PlayerCompoundAmounts", (event, vars) => {
 
             // Apply the new values
-            document.getElementById("microbeHUDPlayerHitpoints").textContent =
-                vars.hitpoints;
-            document.getElementById("microbeHUDPlayerMaxHitpoints").textContent =
-                vars.maxHitpoints;
+            updateMicrobeHUDBars(vars);
+        });
+    } else {
 
-            document.getElementById("microbeHUDPlayerATP").textContent =
-                vars.compoundATP;
-
-            // The bars
-            document.getElementById("microbeHUDPlayerATPCompound").textContent =
-                vars.compoundATP;
-            document.getElementById("microbeHUDPlayerATPMax").textContent =
-                vars.ATPMax;
-
-            document.getElementById("microbeHUDPlayerAminoacids").textContent =
-                vars.compoundAminoacids;
-            document.getElementById("microbeHUDPlayerAminoacidsMax").textContent =
-                vars.AminoacidsMax;
-
-            document.getElementById("microbeHUDPlayerAmmonia").textContent =
-                vars.compoundAmmonia;
-            document.getElementById("microbeHUDPlayerAmmoniaMax").textContent =
-                vars.AmmoniaMax;
-
-            document.getElementById("microbeHUDPlayerGlucose").textContent =
-                vars.compoundGlucose;
-            document.getElementById("microbeHUDPlayerGlucoseMax").textContent =
-                vars.GlucoseMax;
-
-            document.getElementById("microbeHUDPlayerFattyacids").textContent =
-                vars.compoundFattyacids;
-            document.getElementById("microbeHUDPlayerFattyacidsMax").textContent =
-                vars.FattyacidsMax;
-
-            document.getElementById("microbeHUDPlayerOxytoxy").textContent =
-                vars.compoundOxytoxy;
-            document.getElementById("microbeHUDPlayerOxytoxyMax").textContent =
-                vars.OxytoxyMax;
+        // Update random values to make it prettier to look at
+        let hp = randomBetween(10, 50);
+        updateMicrobeHUDBars({
+            hitpoints: randomBetween(1, hp),
+            maxHitpoints: hp,
+            compoundATP: randomBetween(10, 100),
+            ATPMax: 100,
+            compoundAminoacids: randomBetween(0, 50),
+            AminoacidsMax: randomBetween(0, 50),
+            compoundAmmonia: randomBetween(0, 50),
+            AmmoniaMax: randomBetween(0, 50),
+            compoundGlucose: randomBetween(0, 50),
+            GlucoseMax: randomBetween(0, 50),
+            compoundFattyacids: randomBetween(0, 50),
+            FattyacidsMax: randomBetween(0, 50),
+            compoundOxytoxy: randomBetween(0, 50),
+            OxytoxyMax: randomBetween(0, 50),
         });
     }
     
     microbeHudSetupRan = true;
+}
+
+//! Updates the GUI bars
+//! values needs to be an object with properties set with values for everything
+function updateMicrobeHUDBars(values){
+    document.getElementById("microbeHUDPlayerHitpoints").textContent =
+        values.hitpoints;
+    document.getElementById("microbeHUDPlayerMaxHitpoints").textContent =
+        values.maxHitpoints;
+    document.getElementById("microbeHUDPlayerHitpointsBar").style.width =
+        barHelper(values.hitpoints, values.maxHitpoints);
+
+    // TODO: remove this debug code
+    document.getElementById("microbeHUDPlayerATP").textContent =
+        values.compoundATP.toFixed(1);
+
+    // The bars
+    // document.getElementById("microbeHUDPlayerATPCompound").textContent =
+    //     values.compoundATP;
+    document.getElementById("microbeHUDPlayerATPMax").textContent =
+        values.ATPMax;
+    document.getElementById("microbeHUDPlayerATPBar").style.width =
+        barHelper(values.compoundATP, values.ATPMax);
+    return;
+
+    document.getElementById("microbeHUDPlayerAminoacids").textContent =
+        values.compoundAminoacids;
+    document.getElementById("microbeHUDPlayerAminoacidsMax").textContent =
+        values.AminoacidsMax;
+
+    document.getElementById("microbeHUDPlayerAmmonia").textContent =
+        values.compoundAmmonia;
+    document.getElementById("microbeHUDPlayerAmmoniaMax").textContent =
+        values.AmmoniaMax;
+
+    document.getElementById("microbeHUDPlayerGlucose").textContent =
+        values.compoundGlucose;
+    document.getElementById("microbeHUDPlayerGlucoseMax").textContent =
+        values.GlucoseMax;
+
+    document.getElementById("microbeHUDPlayerFattyacids").textContent =
+        values.compoundFattyacids;
+    document.getElementById("microbeHUDPlayerFattyacidsMax").textContent =
+        values.FattyacidsMax;
+
+    document.getElementById("microbeHUDPlayerOxytoxy").textContent =
+        values.compoundOxytoxy;
+    document.getElementById("microbeHUDPlayerOxytoxyMax").textContent =
+        values.OxytoxyMax;
 }
