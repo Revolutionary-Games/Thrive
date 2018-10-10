eb73a1f0ff850f192971709a5393c1f1f2594360
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-10-10 20:43:11 +0300

    Removed almost last of the Lua files (I left the files that still need to be reimplemented)

--- scripts/examples/input.lua
+++
@@ -1,4 +0,0 @@
-local mousePosition = Engine.mouse:normalizedPosition() 
-local leftMouseButtonPressed = Engine.mouse.isButtonDown(MouseSystem.MB_Left)
-
-local shiftPressed = Engine:isKeyDown(KeyboardSystem.KC_LSHIFT)
--- scripts/examples/light.lua
+++
@@ -1,6 +0,0 @@
-local entity = Entity("light")
-local lightComponent = OgreLightComponent()
-lightComponent.properties.type = OgreLightComponent.LT_SPOTLIGHT
-lightComponent:setRange(200)
-lightComponent.properties.diffuseColour = ColourValue(255, 255, 0)
-entity:addComponent(lightComponent)
