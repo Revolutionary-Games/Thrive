ae15448d735e7231549a39730429cb9de49da477
1n48yg <xiaoyang5811@gmail.com>
2018-10-28 12:03:25 -0700

    Revoked some of my own code
    I decided to use the way hhyryylainen suggested through angelscript. So this code is useless now

--- src/ThriveGame.h
+++ src/ThriveGame.h
@@ -70,9 +70,6 @@ public:
     void
         exitToMenuClicked();
 
-    void
-        extinctionOccurred();
-
     //! \param amount The amount the camera is moved. Positive moves away
     //! \todo Needs to detect the active camera system. Now always sends to the
     //! cell stage camera system
