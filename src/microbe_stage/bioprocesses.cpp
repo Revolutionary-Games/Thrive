3f1d8c338132428c97dadbc037679a54140535fd
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-12-15 13:40:29 +0200

    Undid the weird json loading for bioprocess. Did refactoring on
    ProcessSystem to avoid copying things and now there is a check against
    invalid process ids.

--- src/microbe_stage/bioprocesses.cpp
+++ src/microbe_stage/bioprocesses.cpp
@@ -11,11 +11,10 @@ BioProcess::BioProcess(Json::Value value)
     // Getting the input compounds.
     std::vector<std::string> compoundInternalNames =
         value["inputs"].getMemberNames();
-    for(std::string compoundInternalName : compoundInternalNames) {
-        // I read that json sometiems has trouble interpretting 0.0 as a double,
-        // so im adding this as a sanity check.
-        double amount = 0.0f;
-        amount = amount + value["inputs"][compoundInternalName].asDouble();
+
+    for(const std::string& compoundInternalName : compoundInternalNames) {
+
+        const double amount = value["inputs"][compoundInternalName].asDouble();
 
         // Getting the compound id from the compound registry.
         size_t id = SimulationParameters::compoundRegistry
@@ -27,11 +26,10 @@ BioProcess::BioProcess(Json::Value value)
 
     // Getting the output compounds.
     compoundInternalNames = value["outputs"].getMemberNames();
-    for(std::string compoundInternalName : compoundInternalNames) {
-        // I read that json sometiems has trouble interpretting 0.0 as a double,
-        // so im adding this as a sanity check.
-        double amount = 0.0f;
-        amount = amount + value["outputs"][compoundInternalName].asDouble();
+
+    for(const std::string& compoundInternalName : compoundInternalNames) {
+
+        const double amount = value["outputs"][compoundInternalName].asDouble();
 
         // Getting the compound id from the compound registry.
         size_t id = SimulationParameters::compoundRegistry
