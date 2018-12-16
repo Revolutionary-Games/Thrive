7435cb7da04a8a8c6c2a35e178254764ef0095f2
1n48yg <xiaoyang5811@gmail.com>
2018-12-15 16:39:05 -0800

    Fixed extinction backdrop still showing after exiting the game

--- scripts/gui/microbe_hud.mjs
+++ scripts/gui/microbe_hud.mjs
@@ -329,6 +329,7 @@ function onExitToMenuClicked() {
     if(common.isInEngine()){
         document.getElementById("extinctionTitle").style.display = "none";
         document.getElementById("extinctionBody").style.display = "none";
+        document.getElementById("extinctionContainer").style.display = "none";
         hideWinText();
         Thrive.exitToMenuClicked();
 
