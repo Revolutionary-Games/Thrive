aacd39b9d5361488ea6ad5012585cf6afb22ee0f
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-12-19 18:33:43 +0200

    Updated engine with different linker settings, should now work with ld.gold

--- SetupThrive.rb
+++ SetupThrive.rb
@@ -78,7 +78,7 @@ WantedURL = "https://#{$svnUser}@boostslair.com/svn/thrive_assets"
 leviathan = Leviathan.new(
   # Use this if you always want the latest commit
   # version: "develop",
-  version: "a71bdfda4095e50f272d53554e1290aaba6d7f1a",
+  version: "3fe2ad36905e4fe213629da7f894c9fce25ccc10",
   # Doesn't actually work, but leviathan doesn't install with sudo by
   # default, or install at all for that matter
   noInstallSudo: true
