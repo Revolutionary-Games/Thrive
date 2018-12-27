7b4dbc1ee37f8d5a164100496f083dd739445250
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-12-27 21:06:13 +0200

    Symbol extraction should now work for linux. And added license to release

--- SetupThrive.rb
+++ SetupThrive.rb
@@ -78,7 +78,7 @@ WantedURL = "https://#{$svnUser}@boostslair.com/svn/thrive_assets"
 leviathan = Leviathan.new(
   # Use this if you always want the latest commit
   # version: "develop",
-  version: "54e40423666f5e6832f1042b875b991a63bb2b1e",
+  version: "003d5eaf9610801a58728c1c3d2eb66bce375078",
   # Doesn't actually work, but leviathan doesn't install with sudo by
   # default, or install at all for that matter
   noInstallSudo: true
