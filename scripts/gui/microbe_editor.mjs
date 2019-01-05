ba6c0edbdef2da12c68cfc2b2313ef66ee8f9d13
Dak2896 <superdani289@gmail.com>
2019-01-06 00:20:33 +0100

    Revert "fix"
    This reverts commit 11b88db2fb1d4e98eaecaa53e09a9ffebf93944e.

--- scripts/gui/microbe_editor.mjs
+++ scripts/gui/microbe_editor.mjs
@@ -274,10 +274,11 @@ function updateSelectedOrganelle(organelle){
     }
 }
 
+
 function changeGuiButtonStatus(nucleus){
     
     // disabled or enabling organelle that to need nucleus
-
+    
     if(!nucleus){
         $("#addMitochondrion").addClass("DisabledButton");
         $("#addChloroplast").addClass("DisabledButton");
