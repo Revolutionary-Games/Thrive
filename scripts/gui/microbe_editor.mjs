9a408d7bf0730d0306e098f613c65defcd894d3c
Dak2896 <superdani289@gmail.com>
2019-01-06 16:10:05 +0100

    Update microbe_editor.mjs

--- scripts/gui/microbe_editor.mjs
+++ scripts/gui/microbe_editor.mjs
@@ -267,7 +267,7 @@ function updateSelectedOrganelle(organelle){
                 }
             }
             element.element.classList.add("Selected");
-            element.element.prepend(selectedOrganelleListItem);    
+            element.element.prepend(selectedOrganelleListItem);  
         } else {
             element.element.classList.remove("Selected");
         }
