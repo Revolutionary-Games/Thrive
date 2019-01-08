0692211c871a5063c78b0d8a590ce7af20fd2b77
Daniele <superdani289@gmail.com>
2019-01-08 13:00:44 +0100

    fix space

--- scripts/gui/microbe_editor.mjs
+++ scripts/gui/microbe_editor.mjs
@@ -130,7 +130,6 @@ export function setupMicrobeEditor(){
 
         element.element.addEventListener("click", (event) => {
             event.stopPropagation();
-
             if(!(element.element.classList.contains("DisabledButton"))) {
                 onSelectNewOrganelle(element.organelle);
             }
