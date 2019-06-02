30c34a5ea4283658b0d4f47da85039d6b2e30d5c
Untrustedlife <bountybayonlineroblox@gmail.com>
2019-06-02 11:08:10 -0500

    Added patch class

---
+++ src/microbe_stage/patch.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Entities/Component.h>
+#include <Entities/Components.h>
+#include <Entities/System.h>
+
+#include <unordered_map>
+
+namespace thrive {
+
+class CellStageWorld;
+
+//!An object that represents a patch
+
+class Patch {
+
+};
+
+}
