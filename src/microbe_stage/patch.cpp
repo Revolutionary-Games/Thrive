ad98ee7a3e66e54190ed564c8440c96e02b2f6d4
Untrustedlife <bountybayonlineroblox@gmail.com>
2019-06-02 11:44:27 -0500

    continued working on patch class

---
+++ src/microbe_stage/patch.cpp
@@ -0,0 +1,31 @@
+#include "microbe_stage/patch.h"
+
+using namespace thrive;
+
+Patch::Patch(std::string name) 
+{
+    this->name = name;
+}
+
+Patch::~Patch() 
+{
+    delete patchBiome;
+}
+
+std::string
+Patch::getName()
+{
+    return this->name;
+}
+
+void
+    Patch::setName(std::string name)
+{
+    this->name = name;
+}
+
+size_t
+Patch::getId()
+{
+    return this->patchId;
+}
--- src/microbe_stage/patch.h
+++ src/microbe_stage/patch.h
@@ -3,7 +3,7 @@
 #include <Entities/Component.h>
 #include <Entities/Components.h>
 #include <Entities/System.h>
-
+#include <microbe_stage/biomes.h>
 #include <unordered_map>
 
 namespace thrive {
@@ -11,8 +11,26 @@ namespace thrive {
 class CellStageWorld;
 
 //!An object that represents a patch
-
 class Patch {
+	public:
+    std::string name;
+    size_t patchId;
+
+    Patch(std::string name);
+    virtual ~Patch();
+
+    std::string 
+		getName();
+    void 
+		setName(std::string name);
+
+	size_t
+        getId();
+
+	private:
+    Biome* patchBiome;
+    std::vector<std::weak_ptr<Patch>> adjacentPatches;
+
 
 };
 
