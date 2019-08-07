c49f19f6d24c0ba0c6eb08672d24fef81801c560
danielekeller <superdani289@gmail.com>
2019-08-07 15:39:40 +0200

    codeFormatting

--- scripts/gui/microbe_hud.mjs
+++ scripts/gui/microbe_hud.mjs
@@ -660,10 +660,7 @@ $(".minusBtn").click(function() {
 //! Updates the GUI bars
 //! values needs to be an object with properties set with values for everything
 function updateMicrobeHUDBars(values){
-
-
     // The bars
-
     document.getElementById("microbeHUDPlayerHitpoints").textContent =
         values.hitpoints;
     document.getElementById("microbeHUDPlayerMaxHitpoints").textContent =
