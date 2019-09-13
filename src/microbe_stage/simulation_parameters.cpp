41e0fe434b325a94e6462497527ea37f7941e477
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2019-09-13 20:55:42 +0300

    Fixed using the removed starting compounds json

--- src/microbe_stage/simulation_parameters.cpp
+++ src/microbe_stage/simulation_parameters.cpp
@@ -23,26 +23,4 @@ void
 
     SimulationParameters::speciesNameController = SpeciesNameController(
         "./Data/Scripts/SimulationParameters/MicrobeStage/SpeciesNames.json");
-
-    // Getting the JSON file where the data is stored.
-    std::ifstream jsonFile;
-    jsonFile.open("./Data/Scripts/SimulationParameters/MicrobeStage/"
-                  "StartingCompounds.json");
-    LEVIATHAN_ASSERT(jsonFile.is_open(), "The file "
-                                         "'./Data/Scripts/SimulationParameters/"
-                                         "MicrobeStage/StartingCompounds.json' "
-                                         "failed to load!");
-    Json::Value rootElement;
-    try {
-        jsonFile >> rootElement;
-    } catch(const Json::RuntimeError& e) {
-        LOG_ERROR(
-            std::string("Syntax error in json file: StartingCompounds.json") +
-            ", description: " + std::string(e.what()));
-        throw e;
-    }
-
-    // TODO: add some sort of validation of the receiving JSON file, otherwise
-    // it fails silently and makes the screen go black.
-    jsonFile.close();
 }
