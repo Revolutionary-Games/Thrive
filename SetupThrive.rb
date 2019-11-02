5ba4ca537be6831d2d696c01cecd18833890fa83
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2019-11-02 22:54:07 +0200

    Engine fix for stopping rendering node processing on first unmarked Position

--- SetupThrive.rb
+++ SetupThrive.rb
@@ -45,7 +45,7 @@ require_relative 'RubySetupSystem/Libraries/SetupLeviathan.rb'
 leviathan = Leviathan.new(
   # Use this if you always want the latest commit
   # version: "develop",
-  version: '342e2c45e1c633f1191edeb4e56f4e1f9343eed0',
+  version: 'e410f6a27808f181daa8b6e82102c505a6527bdd',
   # Doesn't actually work, but leviathan doesn't install with sudo by
   # default, or install at all for that matter
   noInstallSudo: true
