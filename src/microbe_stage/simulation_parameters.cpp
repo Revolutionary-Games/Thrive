1fe4728004bbd663d94e147081828086210c16bd
sowmiyamuthuraman <32141844+sowmiyamuthuraman@users.noreply.github.com>
2019-11-06 21:09:00 +0530

    Rename json files using snakecase (#843)

--- src/microbe_stage/simulation_parameters.cpp
+++ src/microbe_stage/simulation_parameters.cpp
@@ -14,16 +14,18 @@ void
 {
     // Loading the registries.
     SimulationParameters::compoundRegistry = TJsonRegistry<Compound>(
-        "./Data/Scripts/SimulationParameters/MicrobeStage/Compounds.json");
-    SimulationParameters::bioProcessRegistry = TJsonRegistry<BioProcess>(
-        "./Data/Scripts/SimulationParameters/MicrobeStage/BioProcesses.json");
+        "./Data/Scripts/simulation_parameters/microbe_stage/compounds.json");
+    SimulationParameters::bioProcessRegistry =
+        TJsonRegistry<BioProcess>("./Data/Scripts/simulation_parameters/"
+                                  "microbe_stage/bio_processes.json");
     SimulationParameters::biomeRegistry = TJsonRegistry<Biome>(
-        "./Data/Scripts/SimulationParameters/MicrobeStage/Biomes.json");
+        "./Data/Scripts/simulation_parameters/microbe_stage/biomes.json");
     SimulationParameters::backgroundRegistry = TJsonRegistry<Background>(
-        "./Data/Scripts/SimulationParameters/MicrobeStage/backgrounds.json");
+        "./Data/Scripts/simulation_parameters/microbe_stage/backgrounds.json");
     // SimulationParameters::organelleRegistry =
     // TJsonRegistry<OrganelleType>("./Data/Scripts/SimulationParameters/MicrobeStage/Organelles.json");
 
-    SimulationParameters::speciesNameController = SpeciesNameController(
-        "./Data/Scripts/SimulationParameters/MicrobeStage/SpeciesNames.json");
+    SimulationParameters::speciesNameController =
+        SpeciesNameController("./Data/Scripts/simulation_parameters/"
+                              "microbe_stage/species_names.json");
 }
