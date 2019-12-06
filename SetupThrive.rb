46e3a53e08c89ea25525c0b4a84e115fa669803b
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2019-12-06 20:06:33 +0200

    Switched to a new video player using aom av1 codec

--- SetupThrive.rb
+++ SetupThrive.rb
@@ -45,7 +45,7 @@ require_relative 'RubySetupSystem/Libraries/SetupLeviathan.rb'
 leviathan = Leviathan.new(
   # Use this if you always want the latest commit
   # version: "develop",
-  version: '9e462d2d9e677e208c524c22b85230b0de66f828',
+  version: 'ab32ecc690fdc6aa41ea3802eb0a89c7b5beeeac',
   # Doesn't actually work, but leviathan doesn't install with sudo by
   # default, or install at all for that matter
   noInstallSudo: true
