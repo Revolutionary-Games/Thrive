f3c4317b5d5d3dba706af0d9741928eefdf59293
GatoCreador887 <36546666+GatoCreador887@users.noreply.github.com>
2020-01-14 10:23:08 -0500

    Disable the new cell button when not in freebuild (#929)
    Close #924.

--- scripts/gui/microbe_editor.mjs
+++ scripts/gui/microbe_editor.mjs
@@ -173,7 +173,7 @@ export function setupMicrobeEditor(){
 
     // New Cell Button Clicked
     document.getElementById("newButton").addEventListener("click",
-        OnNewCellClicked, true);
+        onNewCellClicked, true);
 
     // Undo Button Clicked
     document.getElementById("Undo").addEventListener("click",
@@ -677,8 +677,14 @@ function onSelectNewOrganelle(organelle){
     }
 }
 
-function onFreeBuildStatus(toggle) {
+function onFreeBuildStatus(toggle){
     limitMovesPerSession = !toggle;
+
+    if(toggle){
+        document.getElementById("newButton").classList.remove("DisabledButton");
+    } else {
+        document.getElementById("newButton").classList.add("DisabledButton");
+    }
 }
 
 //! Updates the GUI buttons based on selected organelle
@@ -993,12 +999,14 @@ function onSymmetryClicked(event){
     event.stopPropagation();
 }
 
-function OnNewCellClicked(event){
-    common.playButtonPressSound();
-    if(common.isInEngine()){
-        Leviathan.CallGenericEvent("NewCellClicked", {});
+function onNewCellClicked(event){
+    if (!document.getElementById("newButton").classList.contains("DisabledButton")){
+        common.playButtonPressSound();
+        if (common.isInEngine()){
+            Leviathan.CallGenericEvent("NewCellClicked", {});
+        }
+        event.stopPropagation();
     }
-    event.stopPropagation();
 }
 
 function onRedoClicked(event){
