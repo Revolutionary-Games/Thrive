5875e2ebfd40742d2339657bd15e738b11ee9608
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-03-01 18:24:51 +0200

    Fixed linting to be as good as it probably can be

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Reflection;
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
+// [assembly: AssemblyDelaySign(false)]
+// [assembly: AssemblyKeyFile("")]
