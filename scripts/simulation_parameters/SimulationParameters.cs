b570933b8c01cd5c3321e213def1164991b64047
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-03-02 14:45:56 +0200

    Added background registry loading and refactored the loading a bit

--- scripts/simulation_parameters/SimulationParameters.cs
+++ scripts/simulation_parameters/SimulationParameters.cs
@@ -7,6 +7,7 @@ public class SimulationParameters
     private static readonly SimulationParameters INSTANCE = new SimulationParameters();
 
     private Dictionary<string, MembraneType> membranes;
+    private Dictionary<string, Background> backgrounds;
 
     static SimulationParameters()
     {
@@ -17,18 +18,37 @@ public class SimulationParameters
     /// </summary>
     private SimulationParameters()
     {
+        membranes = LoadRegistry<MembraneType>(
+            "res://scripts/simulation_parameters/microbe_stage/membranes.json");
+        backgrounds = LoadRegistry<Background>(
+            "res://scripts/simulation_parameters/microbe_stage/backgrounds.json");
+
+        GD.Print("SimulationParameters loading ended");
+    }
+
+    public MembraneType GetMembrane(string name)
+    {
+        return membranes[name];
+    }
+
+    public Background GetBackground(string name)
+    {
+        return backgrounds[name];
+    }
+
+    private Dictionary<string, T> LoadRegistry<T>(string path)
+    {
         using (var file = new File())
         {
-            file.Open("res://scripts/simulation_parameters/microbe_stage/membranes.json",
-                File.ModeFlags.Read);
-            membranes = JsonConvert.DeserializeObject<Dictionary<string, MembraneType>>(
+            file.Open(path, File.ModeFlags.Read);
+            var result = JsonConvert.DeserializeObject<Dictionary<string, T>>(
                 file.GetAsText());
 
             // file.Close();
-        }
 
-        GD.Print("SimulationParameters loading ended");
-        GD.Print("Number of loaded membrane types: ", membranes.Count);
+            GD.Print($"Loaded registry for {typeof(T)} with {result.Count} items");
+            return result;
+        }
     }
 
     public static SimulationParameters Instance
