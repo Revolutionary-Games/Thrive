c7a2f49f163fbcdda4ca524af48ab89022643d57
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-03-03 19:50:01 +0200

    Removed one old script

--- QuickRecompile.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-# This script runs the setup with options that make recompiling much faster (but it doesn't
-# update everything!)
-require_relative 'RubySetupSystem/RubyCommon.rb'
-
-system("ruby SetupThrive.rb --no-packagemanager --no-subproject-deps")
-exit $?.exitstatus == 0
