28500211253d5416abdb7a9e2bf9f7f5fd664f08
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-03-03 20:02:29 +0200

    Changed the headless script back

--- headless_project_refresh.rb
+++ headless_project_refresh.rb
@@ -3,7 +3,7 @@
 require 'English'
 
 puts 'Running solution build with godot, this failing is not serious at this point'
-system(%(godot -e --build-solutions -q))
+system(%(godot -e --build-solutions))
 
 # if $CHILD_STATUS.exitstatus != 0
 #   abort "Failed to compile C# project with Godot"
