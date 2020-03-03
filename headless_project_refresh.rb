0b068a82f24814ea8d862c510207fa3c457aaefc
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-03-03 20:00:25 +0200

    Changed circleci build to be just a single job as godot always needs
    the assets to be good

--- headless_project_refresh.rb
+++ headless_project_refresh.rb
@@ -3,7 +3,7 @@
 require 'English'
 
 puts 'Running solution build with godot, this failing is not serious at this point'
-system(%(godot -e --build-solutions))
+system(%(godot -e --build-solutions -q))
 
 # if $CHILD_STATUS.exitstatus != 0
 #   abort "Failed to compile C# project with Godot"
