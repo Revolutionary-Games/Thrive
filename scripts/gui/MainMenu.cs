7d762044c3929db63933a44e922d93df7c387a42
Kasterisk <microscube@gmail.com>
2020-03-03 22:38:25 +0700

    Some adjustments to the main menu

--- scripts/gui/MainMenu.cs
+++ scripts/gui/MainMenu.cs
@@ -3,6 +3,7 @@ using System;
 
 public class MainMenu : Node
 {
+    public Godot.Collections.Array menuArray;
     public Texture backgroundImage;
     public TextureRect background;
     public ColorRect screenFade;
@@ -11,10 +12,11 @@ public class MainMenu : Node
     public AudioStreamPlayer guiAudio;
     public AudioStream buttonPressSound;
     public VideoPlayer microbeIntro;
-        
+    
     public string currentScene;
     public string currentCutscene;
-    
+    public int currentMenuIndex;
+
     public override void _Ready()
     {
         background = GetNode<TextureRect>("Background");
@@ -23,48 +25,66 @@ public class MainMenu : Node
         musicAudio = GetNode<AudioStreamPlayer>("Music");
         microbeIntro = GetNode<VideoPlayer>("MicrobeIntro");
         screenFade.MouseFilter = Control.MouseFilterEnum.Ignore;
+
+        RunMenuSetup();
         RandomizeBackground();
     }
-        
+    
     public override void _Input(InputEvent @event)
     {
-        if(Input.IsKeyPressed((int)Godot.KeyList.Escape)){
+        if(Input.IsKeyPressed((int)Godot.KeyList.Escape))
+        {
             CancelCutscene();
         }
     }
-        
+    
+    public void RunMenuSetup()
+    {
+        menuArray = GetNode<Control>("MenuContainers/ButtonsCenterContainer/MenuItems")
+            .GetChildren();
+        if(menuArray == null)
+        {
+            GD.PrintErr("Failed to find all the menu items!");
+            return;
+        }
+    }
+
     public void RandomizeBackground()
     {
         Random rand = new Random();
         int num = rand.Next() % 9;
         
-        if (num <= 3){
-        	SetBackground("res://assets/textures/gui/BG_Menu01.png");
-        } else if (num <= 6){
-        	SetBackground("res://assets/textures/gui/BG_Menu02.png");
-        } else if (num <= 9){
-        	SetBackground("res://assets/textures/gui/BG_Menu03.png");
+        if (num <= 3)
+        {
+            SetBackground("res://assets/textures/gui/BG_Menu01.png");
+        } else if (num <= 6)
+        {
+            SetBackground("res://assets/textures/gui/BG_Menu02.png");
+        } else if (num <= 9)
+        {
+            SetBackground("res://assets/textures/gui/BG_Menu03.png");
         }
     }
-        
+    
     public void SetBackground(string filepath)
     {
         backgroundImage = GD.Load<Texture>(filepath);
         background.Texture = backgroundImage;
     }
-        
-    public void playButtonPressSound()
+    
+    public void PlayButtonPressSound()
     {
-        if(buttonPressSound == null){
-        	buttonPressSound = GD.Load<AudioStream>
-        		("res://assets/sounds/soundeffects/gui/button-hover-click.ogg");
+        if(buttonPressSound == null)
+        {
+            buttonPressSound = GD.Load<AudioStream>
+            	("res://assets/sounds/soundeffects/gui/button-hover-click.ogg");
         }
         
         guiAudio.Stream = buttonPressSound;
         guiAudio.Play();
     }
-        
-    public void FadeInWithCutsceneTo(string scene, string cutscene)
+    
+    public void FadeInWithCutsceneTo(string scene, string cutscene, float fadeDuration)
     {
         var fader = GetNode<Tween>("Fader");
         if(fader == null)
@@ -74,12 +94,12 @@ public class MainMenu : Node
         }
         
         screenFade.MouseFilter = Control.MouseFilterEnum.Stop;
-    	fader.InterpolateProperty(screenFade, "color", null, new Color(0, 0, 0, 1), 0.5f);
+    	fader.InterpolateProperty(screenFade, "color", null, new Color(0, 0, 0, 1), fadeDuration);
     	fader.Start();
         currentScene = scene;
         currentCutscene = cutscene;
     }
-        
+    
     public void PlayCutscene(string path)
     {
     	var stream = GD.Load<VideoStream>(path);
@@ -87,7 +107,7 @@ public class MainMenu : Node
     	if(stream != null)
     		microbeIntro.Play();
     }
-        
+    
     public void CancelCutscene()
     {
         if(!microbeIntro.IsPlaying())
@@ -96,34 +116,63 @@ public class MainMenu : Node
         microbeIntro.Stop();
         OnMicrobeIntroEnded();
     }
-        
+    
     public void OnFaderFinished(Godot.Object obj, NodePath key)
     {
         screenFade.Color = new Color(0, 0, 0, 0);
         PlayCutscene(currentCutscene);
     }
-        
+    
     public void OnMicrobeIntroEnded()
     {
         screenFade.MouseFilter = Control.MouseFilterEnum.Ignore;
         GetTree().ChangeScene(currentScene);
     }
-        
+    
+    public void SetCurrentMenu(int index)
+    {
+        var tween = GetNode<Tween>("MenuContainers/MenuTween");
+        var curMenu = (Control)menuArray[currentMenuIndex];
+        var selectedMenu = (Control)menuArray[index];
+
+        // Play the slide down animation
+        curMenu.Hide();
+        selectedMenu.Show();
+        tween.InterpolateProperty(selectedMenu, "custom_constants/separation", -35, 10, 0.3f,
+            Tween.TransitionType.Sine);
+        tween.Start();
+
+        currentMenuIndex = index;
+    }
+
     public void NewGamePressed()
     {
-    	playButtonPressSound();
+    	PlayButtonPressSound();
     	musicAudio.Stop();
     	FadeInWithCutsceneTo("res://src/microbe_stage/MicrobeStage.tscn",
-            "res://assets/videos/microbe_intro2.webm");
+            "res://assets/videos/microbe_intro2.webm", 1f);
     }
-        
-    public void ToolsPressed(){
-    	playButtonPressSound();
+    
+    public void ToolsPressed()
+    {
+    	PlayButtonPressSound();
+        SetCurrentMenu(1);
     }
-        
+
+    public void FreebuildEditorPressed()
+    {
+        PlayButtonPressSound();
+    }
+    
+    public void BackFromToolsPressed()
+    {
+        PlayButtonPressSound();
+        SetCurrentMenu(0);
+    }
+
     public void QuitPressed()
     {
-        playButtonPressSound();
+        PlayButtonPressSound();
         GetTree().Quit();
     }
 }
