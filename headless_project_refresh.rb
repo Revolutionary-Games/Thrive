1781641a03eaaf1247355ce2c5103935f16c65f1
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-03-03 21:18:06 +0200

    Once again tweaked the CI build

--- headless_project_refresh.rb
+++ headless_project_refresh.rb
@@ -2,10 +2,10 @@
 # This script fixes the project for running on headless CI build
 require 'English'
 
-# This doesn't actually work
+# This doesn't work very well
 
 puts 'Running solution build with godot, this failing is not serious at this point'
-system(%(godot -e --build-solutions))
+system(%(timeout --kill-after 15s 45s godot -e --build-solutions -q))
 
 # if $CHILD_STATUS.exitstatus != 0
 #   abort "Failed to compile C# project with Godot"
