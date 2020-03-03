052402457e0d0f185ae2def911e89e00b9ece845
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-03-03 20:08:13 +0200

    Trying circleci godot asset getting with export

--- headless_project_refresh.rb
+++ headless_project_refresh.rb
@@ -2,6 +2,8 @@
 # This script fixes the project for running on headless CI build
 require 'English'
 
+# This doesn't actually work
+
 puts 'Running solution build with godot, this failing is not serious at this point'
 system(%(godot -e --build-solutions))
 
