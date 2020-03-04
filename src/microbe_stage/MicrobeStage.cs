46bc35634b08cd9f818f6521a6e55a7ab1052064
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-03-04 19:38:00 +0200

    Did some setup towards having player controllable microbe spawned

--- src/microbe_stage/MicrobeStage.cs
+++ src/microbe_stage/MicrobeStage.cs
@@ -1,17 +1,31 @@
 using Godot;
 
+/// <summary>
+///   Main class for managing the microbe stage
+/// </summary>
 public class MicrobeStage : Node
 {
     private Node world;
     private Node rootOfDynamicallySpawned;
     private SpawnSystem spawner;
 
-    // Called when the node enters the scene tree for the first time.
+    private PackedScene playerScene;
+    public Microbe Player { get; private set; }
+
+    /// <summary>
+    ///   This should get called the first time the stage scene is put
+    ///   into an active scene tree. So returning from the editor
+    ///   might be safe without it unloading this.
+    /// </summary>
     public override void _Ready()
     {
+        playerScene = GD.Load<PackedScene>("res://src/microbe_stage/Microbe.tscn");
+
         world = GetNode<Node>("World");
         rootOfDynamicallySpawned = GetNode<Node>("World/DynamicallySpawned");
         spawner = new SpawnSystem(rootOfDynamicallySpawned);
+
+        // Do stage setup to spawn things and setup all parts of the stage
         SetupStage();
     }
 
@@ -24,6 +38,20 @@ public class MicrobeStage : Node
             GD.PrintErr("Something bad happened with SimulationParameters loading");
 
         spawner.Init();
+        SpawnPlayer();
+    }
+
+    /// <summary>
+    ///   Spawns the player if there isn't currently a player node existing
+    /// </summary>
+    public void SpawnPlayer()
+    {
+        if (Player != null)
+            return;
+
+        Player = (Microbe)playerScene.Instance();
+        rootOfDynamicallySpawned.AddChild(Player);
+        Player.AddToGroup("player");
     }
 
     public override void _Process(float delta)
