b9ad441638c6ef888e6a198688422cbff1724ed9
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-03-11 19:14:03 +0200

    Added showing what compounds are under the cursor

--- src/microbe_stage/MicrobeHUD.cs
+++ src/microbe_stage/MicrobeHUD.cs
@@ -11,34 +11,58 @@ public class MicrobeHUD : Node
     [Export]
     public NodePath CompoundsLabelPath;
 
-    public RichTextLabel CompoundsLabel;
+    [Export]
+    public NodePath HoveredItemsLabelPath;
+
+    private RichTextLabel compoundsLabel;
+
+    private RichTextLabel hoveredItemsLabel;
 
     /// <summary>
     ///   Access to the stage to retrieve information for display as
     ///   well as call some player initiated actions.
     /// </summary>
-    public MicrobeStage Stage;
+    private MicrobeStage stage;
 
     public override void _Ready()
     {
-        CompoundsLabel = GetNode<RichTextLabel>(CompoundsLabelPath);
+        compoundsLabel = GetNode<RichTextLabel>(CompoundsLabelPath);
+        hoveredItemsLabel = GetNode<RichTextLabel>(HoveredItemsLabelPath);
     }
 
     public override void _Process(float delta)
     {
-        if (Stage == null)
+        if (stage == null)
             return;
 
-        if (Stage.Player != null)
+        if (stage.Player != null)
         {
-            CompoundsLabel.Text = CompoundsToString(Stage.Player.Compounds.Compounds);
+            compoundsLabel.Text = CompoundsToString(stage.Player.Compounds.Compounds);
         }
 
-        // Stage.
+        if (stage.Camera != null)
+        {
+            var compounds = stage.Clouds.GetAllAvailableAt(stage.Camera.CursorWorldPos);
+
+            StringBuilder builder = new StringBuilder("Things at ", 250);
+
+            builder.AppendFormat("{0:F1}, {1:F1}:\n",
+                stage.Camera.CursorWorldPos.x, stage.Camera.CursorWorldPos.z);
+            builder.Append(CompoundsToString(compounds));
+
+            hoveredItemsLabel.Text = builder.ToString();
+        }
+    }
+
+    public void Init(MicrobeStage stage)
+    {
+        this.stage = stage;
     }
 
     private string CompoundsToString(Dictionary<string, float> compounds)
     {
+        var simulation = SimulationParameters.Instance;
+
         StringBuilder compoundsText = new StringBuilder(string.Empty, 150);
 
         bool first = true;
@@ -49,7 +73,9 @@ public class MicrobeHUD : Node
                 compoundsText.Append("\n");
             first = false;
 
-            compoundsText.Append($"{entry.Key}: {entry.Value}");
+            var readableName = simulation.GetCompound(entry.Key).Name;
+            compoundsText.Append(readableName);
+            compoundsText.AppendFormat(": {0:F1}", entry.Value);
         }
 
         return compoundsText.ToString();
