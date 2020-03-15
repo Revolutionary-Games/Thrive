7a64a7a8f2d14d5e2039fd3a293568e297c92447
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-03-15 17:44:44 +0200

    Converted over Patch and PatchMap, added placeholder for the patch map
    generator but there is no implementation here

---
+++ src/microbe_stage/Patch.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using Godot;
+using Newtonsoft.Json;
+
+/// <summary>
+///   A patch is an instance of a Biome with some species in it
+/// </summary>
+public class Patch
+{
+    /// <summary>
+    ///   List of all species and their populations in this patch
+    /// </summary>
+    [JsonProperty]
+    public readonly Dictionary<Species, int> SpeciesInPatch =
+        new Dictionary<Species, int>();
+
+    [JsonProperty]
+    public readonly int ID;
+
+    [JsonProperty]
+    public readonly ISet<Patch> Adjacent = new HashSet<Patch>();
+
+    [JsonProperty]
+    private readonly Biome biomeTemplate;
+
+    public Patch(string name, int id, Biome biomeTemplate)
+    {
+        Name = name;
+        this.ID = id;
+        this.biomeTemplate = biomeTemplate;
+    }
+
+    [JsonProperty]
+    public string Name { get; private set; }
+
+    /// <summary>
+    ///   Coordinates this patch is to be displayed in the GUI
+    /// </summary>
+    public Vector2 ScreenCoordinates { get; set; } = new Vector2(0, 0);
+
+    /// <summary>
+    ///   Adds a connection to patch
+    /// </summary>
+    /// <returns>True if this was new, false if already added</returns>
+    public bool AddNeighbour(Patch patch)
+    {
+        return Adjacent.Add(patch);
+    }
+
+    /// <summary>
+    ///   Looks for a species with the specified name in this patch
+    /// </summary>
+    public Species FindSpeciesByID(uint id)
+    {
+        foreach (var entry in SpeciesInPatch)
+        {
+            if (entry.Key.ID == id)
+                return entry.Key;
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    ///   Adds a new species to this patch
+    /// </summary>
+    /// <returns>True when added. False if the species was already in this patch</returns>
+    public bool AddSpecies(Species species, int population =
+        Constants.INITIAL_SPECIES_POPULATION)
+    {
+        if (SpeciesInPatch.ContainsKey(species))
+            return false;
+
+        SpeciesInPatch[species] = population;
+        return true;
+    }
+
+    /// <summary>
+    ///   Removes a species from this patch
+    /// </summary>
+    /// <returns>True when a species was removed</returns>
+    public bool RemoveSpecies(Species species)
+    {
+        return SpeciesInPatch.Remove(species);
+    }
+
+    /// <summary>
+    ///   Updates a species population in this patch
+    /// </summary>
+    /// <returns>True on success</returns>
+    public bool UpdateSpeciesPopulation(Species species, int newPopulation)
+    {
+        if (!SpeciesInPatch.ContainsKey(species))
+            return false;
+
+        SpeciesInPatch[species] = newPopulation;
+        return true;
+    }
+
+    public int GetSpeciesPopulation(Species species)
+    {
+        if (!SpeciesInPatch.ContainsKey(species))
+            return 0;
+
+        return SpeciesInPatch[species];
+    }
+}
