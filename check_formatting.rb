6b0cde3bf7563bf8e56c4afe18712aeebdb2671f
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-04-17 15:14:52 +0300

    Added json linting

--- check_formatting.rb
+++ check_formatting.rb
@@ -1,24 +1,57 @@
 #!/usr/bin/env ruby
 # This script first builds using msbuild treating warnings as errors
 # and then runs some custom line length checks
+require 'optparse'
 require 'find'
+require 'digest'
 
 require_relative 'bootstrap_rubysetupsystem'
 require_relative 'RubySetupSystem/RubyCommon'
 
 MAX_LINE_LENGTH = 120
 
-if runSystemSafe('msbuild', 'Thrive.sln', '/t:Clean,Build', '/warnaserror') != 0
-  error "\nBuild generated warnings or errors."
-  exit 1
-end
+VALID_CHECKS = %w[compile files].freeze
+
+@options = {
+  checks: VALID_CHECKS,
+  skip_file_types: []
+}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$PROGRAM_NAME} [options]"
+
+  opts.on('-c', '--checks check1,check2', Array,
+          'Select checks to do. Default is all') do |checks|
+    @options[:checks] = checks
+  end
+  opts.on('-s', '--skip filetype1,filetype2', Array,
+          'Skips files checks on the specified types') do |skip|
+    @options[:skip_file_types] = skip
+  end
+end.parse!
+
+onError "Unhandled parameters: #{ARGV}" unless ARGV.empty?
+
+info "Starting formatting checks with the following checks: #{@options[:checks]}"
+
+# Helper functions
 
 # Skip some files that would otherwise be processed
 def skip_file?(path)
-  path =~ %r{/ThirdParty/}i || path =~ %r{^\.\/\.\/} || path =~ /GlobalSuppressions.cs/
+  path =~ %r{/ThirdParty/}i || path =~ %r{^\.\/\.\/} || path =~ /GlobalSuppressions.cs/ ||
+    path =~ %r{/RubySetupSystem/} || path =~ %r{/\.mono/}
 end
 
-# Different handle functions
+def file_type_skipped?(path)
+  if @options[:skip_file_types].include? File.extname(path)[1..-1]
+    puts "Skipping file '#{path}'"
+    true
+  else
+    false
+  end
+end
+
+# Different handle functions for file checks
 def handle_gd_file(_path)
   error 'GD scripts should not exist'
   true
@@ -49,19 +82,50 @@ def handle_cs_file(path)
   errors
 end
 
-# Forwards the file handling to a specific handler function if
-# something should be done with the file type
-def handle_file(path)
-  if path =~ /\.gd$/
-    handle_gd_file path
-  elsif path =~ /\.cs$/
-    handle_cs_file path
+def handle_json_file(path)
+  digest_before = Digest::MD5.hexdigest File.read(path)
+
+  if runSystemSafe('jsonlint', '-i', path, '--indent', '    ') != 0
+    error 'JSONLint failed on file'
+    return true
+  end
+
+  digest_after = Digest::MD5.hexdigest File.read(path)
+
+  if digest_before != digest_after
+    error 'JSONLint made formatting changes'
+    true
   else
     false
   end
 end
 
-def run
+# Forwards the file handling to a specific handler function if
+# something should be done with the file type
+def handle_file(path)
+  return false if file_type_skipped? path
+
+  if path =~ /\.gd$/
+    handle_gd_file path
+  elsif path =~ /\.cs$/
+    handle_cs_file path
+  elsif path =~ %r{scripts/.*\.json$}
+    handle_json_file path
+  else
+    false
+  end
+end
+
+# Run functions for the specific checks
+
+def run_compile
+  if runSystemSafe('msbuild', 'Thrive.sln', '/t:Clean,Build', '/warnaserror') != 0
+    error "\nBuild generated warnings or errors."
+    exit 1
+  end
+end
+
+def run_files
   issues_found = false
   Find.find('.') do |path|
     # path = path[2..-1]
@@ -80,13 +144,21 @@ def run
     end
   end
 
-  issues_found
-end
+  return unless issues_found
 
-if run
   error 'Code format issues detected'
   exit 2
-else
-  success 'No code format issues found'
-  exit 0
 end
+
+@options[:checks].each do |check|
+  if check == 'compile'
+    run_compile
+  elsif check == 'files'
+    run_files
+  else
+    onError "Unknown check type: #{check}"
+  end
+end
+
+success 'No code format issues found'
+exit 0
