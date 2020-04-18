8b25a27b2f8999db0ec9f932581aa7212be3be3f
crodnu <crodnu@gmail.com>
2020-04-18 17:08:53 -0300

    readding the death souns
    We make a new scene to achieve this

---
+++ src/microbe_stage/MicrobeDeathEffect.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public class MicrobeDeathEffect : Spatial, ITimedLife
+{
+    public float TimeToLiveRemaining { get; set; } = 3.0f;
+
+    public void OnTimeOver() => QueueFree();
+    public override void _Ready() => AddToGroup(Constants.TIMED_GROUP);
+}
