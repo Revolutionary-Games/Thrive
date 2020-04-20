4923d3fe38ffa9d583155a3035d9130c1ff74290
Kasterisk <54026083+Kasterisk@users.noreply.github.com>
2020-04-20 16:14:13 +0700

    Added win popup (#1061)
    * Added win popup
    
    * Fixed comments

--- src/microbe_stage/MicrobeStage.cs
+++ src/microbe_stage/MicrobeStage.cs
@@ -22,6 +22,8 @@ public class MicrobeStage : Node
     /// </summary>
     private bool gameOver = false;
 
+    private bool wonOnce = false;
+
     private float playerRespawnTimer;
 
     public Microbe Player { get; private set; }
@@ -229,15 +231,13 @@ public class MicrobeStage : Node
     {
         // Now the editor increases the generation so we don't do that here anymore
 
-        // TODO: fix
-        // // Call event that checks win conditions
-        // if(!GetThriveGame().playerData().isFreeBuilding()){
-        //     GenericEvent@ event = GenericEvent("CheckWin");
-        //     NamedVars@ vars = event.GetNamedVars();
-        //     vars.AddValue(ScriptSafeVariableBlock("generation", playerSpecies.generation));
-        //     vars.AddValue(ScriptSafeVariableBlock("population", playerSpecies.population));
-        //     GetEngine().GetEventHandler().CallEvent(event);
-        // }
+        // Check win conditions
+        if (!CurrentGame.FreeBuild && Player.Species.Generation >= 20 &&
+            Player.Species.Population >= 300 && !wonOnce)
+        {
+            HUD.ToggleWinBox();
+            wonOnce = true;
+        }
 
         // Make sure player is spawned
         SpawnPlayer();
