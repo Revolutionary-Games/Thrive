af74c7c8fe65c0385a8d26cdd556f5b4ab7eabb0
IceDjuro <milandjurovic625@gmail.com>
2020-04-21 23:43:06 -0700

    Load cursor in _Ready()

--- src/engine/CursorLoader.cs
+++ src/engine/CursorLoader.cs
@@ -2,12 +2,14 @@ using Godot;
 
 /// <summary>
 ///   Replaces default cursor shapes with custom made
+///   Default cursor is set in Project Settings
 /// </summary>
 public class CursorLoader : Node
 {
-    private Resource hoverCursor = GD.Load<Resource>("res://assets/textures/gui/cursors/cursor_hover.png");
+    private Resource hoverCursor;
     public override void _Ready()
     {
+        hoverCursor = GD.Load<Resource>("res://assets/textures/gui/cursors/cursor_hover.png");
         Input.SetCustomMouseCursor(hoverCursor, Input.CursorShape.PointingHand);
     }
 }
