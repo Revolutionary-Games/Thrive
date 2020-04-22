1906b247ebb89998b6a855afe221ba68ed0fe8e6
IceDjuro <milandjurovic625@gmail.com>
2020-04-22 12:35:54 -0700

    Added FPS Counter

---
+++ src/engine/FPSCounter.cs
@@ -0,0 +1,33 @@
+using Godot;
+
+/// <summary>
+///   Shows FPS at top left of the screen
+///   Toggled with F3
+/// </summary>
+public class FPSCounter : Control
+{
+    private Label label;
+    public override void _Ready()
+    {
+        label = (Label)GetNode("Label");
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event.IsActionPressed("toggle_FPS"))
+        {
+            if (Visible)
+                Hide();
+            else
+                Show();
+        }
+    }
+
+    public override void _Process(float delta)
+    {
+        if (Visible)
+        {
+            label.Text = "FPS: " + Engine.GetFramesPerSecond().ToString();
+        }
+    }
+}
