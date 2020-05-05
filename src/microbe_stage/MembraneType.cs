b40ac65b9646638bc3864199067711b584684539
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-05-05 16:16:04 +0300

    Add missing membrane texture (#1188)
    * Improved the membrane image checking on load
    
    * Added the missing calcium carbonate image file

--- src/microbe_stage/MembraneType.cs
+++ src/microbe_stage/MembraneType.cs
@@ -1,4 +1,6 @@
-﻿/// <summary>
+﻿using Godot;
+
+/// <summary>
 ///   Defines properties of a membrane type
 /// </summary>
 public class MembraneType : IRegistryType
@@ -18,10 +20,24 @@ public class MembraneType : IRegistryType
 
     public void Check(string name)
     {
-        if (NormalTexture == string.Empty || DamagedTexture == string.Empty)
+        if (string.IsNullOrEmpty(NormalTexture) || string.IsNullOrEmpty(DamagedTexture))
         {
             throw new InvalidRegistryData(name, this.GetType().Name,
                 "Empty normal or damaged texture");
+        }
+
+        var directory = new Directory();
+
+        string[] membranes = { NormalTexture, DamagedTexture };
+
+        foreach (var resource in membranes)
+        {
+            // When exported only the .import files exist, so this check is done accordingly
+            if (!directory.FileExists(resource + ".import"))
+            {
+                throw new InvalidRegistryData(name, this.GetType().Name,
+                    "Membrane uses non-existant image: " + resource);
+            }
         }
     }
 }
