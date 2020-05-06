20f29453e737ddd1eae7434bc0aaf6dfb554de9b
IceDjuro <milandjurovic625@gmail.com>
2020-05-05 22:31:58 -0700

    Fixed scripts folder

--- simulation_parameters/Constants.cs
+++ simulation_parameters/Constants.cs
@@ -36,6 +36,8 @@ public class Constants
 
     public const float CLOUD_Y_COORDINATE = 0;
 
+    public const float CLOUD_DIFFUSION_RATE = 0.007f;
+
     public const int MEMBRANE_RESOLUTION = 10;
 
     /// <summary>
@@ -68,7 +70,7 @@ public class Constants
     /// <summary>
     ///   The maximum force that can be applied by currents in the fluid system
     /// </summary>
-    public const float MAX_FORCE_APPLIED_BY_CURRENTS = 0.525f;
+    public const float MAX_FORCE_APPLIED_BY_CURRENTS = 0.0525f;
 
     /// <summary>
     ///   Added 2 seconds here to make the random implementation look a bit better
