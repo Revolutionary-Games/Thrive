4b4a46bf26d13632ba2aecef3f8a94740c906e3b
Kasterisk <54026083+Kasterisk@users.noreply.github.com>
2020-05-10 15:34:11 +0700

    Probably fixed the cutscene zoom issue (#1224)

--- src/gui_common/Cutscene.cs
+++ src/gui_common/Cutscene.cs
@@ -24,35 +24,8 @@ public class Cutscene : CanvasLayer, ITransition
         FrameSize = CutsceneVideoPlayer.RectSize;
 
         CutsceneVideoPlayer.Connect("finished", this, nameof(OnFinished));
-        GetViewport().Connect("size_changed", this, nameof(OnCutsceneResized));
-
-        // Initially adjust video player frame size
-        OnCutsceneResized();
 
         ControlNode.Hide();
-    }
-
-    /// <summary>
-    ///   Keeps aspect ratio of the cutscene whenever
-    ///   the window is being resized.
-    /// </summary>
-    public void OnCutsceneResized()
-    {
-        var currentSize = OS.WindowSize;
-
-        // Scaling factors
-        var scaleHeight = currentSize.x / FrameSize.x;
-        var scaleWidth = currentSize.y / FrameSize.y;
-
-        var scale = Math.Min(scaleHeight, scaleWidth);
-
-        var newSize = new Vector2(FrameSize.x * scale,
-            FrameSize.y * scale);
-
-        // Adjust the cutscene size and center it
-        CutsceneVideoPlayer.SetSize(newSize);
-        CutsceneVideoPlayer.SetAnchorsAndMarginsPreset(
-            Control.LayoutPreset.Center, Control.LayoutPresetMode.KeepSize);
     }
 
     public void OnStarted()
