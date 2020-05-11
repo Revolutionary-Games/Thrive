11bc241a0fd7fe958b26dd135e1641e4895a0711
Kasterisk <54026083+Kasterisk@users.noreply.github.com>
2020-05-11 19:16:45 +0700

    Reimplemented microbe HUD environmental bars functionality (#1234)
    * Microbe HUD environmental gasses bar now updates based on current patch
    
    * Removed trailing whitespace
    
    * Also included temperature to be updated, took too long to commit this I know lol
    
    * Made the sunlight bar color dark a bit

--- src/microbe_stage/MicrobeStage.cs
+++ src/microbe_stage/MicrobeStage.cs
@@ -112,9 +112,7 @@ public class MicrobeStage : Node
 
         CreatePatchManagerIfNeeded();
 
-        patchManager.ApplyChangedPatchSettingsIfNeeded(GameWorld.Map.CurrentPatch);
-        HUD.UpdatePatchInfo(GameWorld.Map.CurrentPatch.Name);
-        UpdateBackground();
+        UpdatePatchSettings();
 
         SpawnPlayer();
         Camera.ResetHeight();
@@ -254,9 +252,7 @@ public class MicrobeStage : Node
     /// </summary>
     public void OnReturnFromEditor()
     {
-        patchManager.ApplyChangedPatchSettingsIfNeeded(GameWorld.Map.CurrentPatch);
-        HUD.UpdatePatchInfo(GameWorld.Map.CurrentPatch.Name);
-        UpdateBackground();
+        UpdatePatchSettings();
 
         // Now the editor increases the generation so we don't do that here anymore
 
@@ -314,6 +310,16 @@ public class MicrobeStage : Node
             // Player is not extinct, so can respawn
             SpawnPlayer();
         }
+    }
+
+    private void UpdatePatchSettings()
+    {
+        patchManager.ApplyChangedPatchSettingsIfNeeded(GameWorld.Map.CurrentPatch);
+
+        HUD.UpdatePatchInfo(GameWorld.Map.CurrentPatch.Name);
+        HUD.UpdateEnvironmentalBars(GameWorld.Map.CurrentPatch.Biome);
+
+        UpdateBackground();
     }
 
     private void UpdateBackground()
