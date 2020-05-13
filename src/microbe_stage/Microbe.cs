a0a7844e9730dfbc818e5bae53bb627012d6540c
Holomanga <holomanga@gmail.com>
2020-05-13 12:46:00 +0100

    Try random organelles for corpse chunks before falling back on mitochondria.

--- src/microbe_stage/Microbe.cs
+++ src/microbe_stage/Microbe.cs
@@ -665,6 +665,18 @@ public class Microbe : RigidBody, ISpawned, IProcessable, IMicrobeAI
             var organelleToUse = organelles.Organelles.Random(random).Definition;
 
             var sceneToUse = new Biome.ChunkConfiguration.ChunkScene();
+            
+            var organellesToTryUsing = organelles.Organelles;
+            var organelleToTryUsing = organellesToTryUsing.Random(random);
+            
+            organelleToUse = organellesToTryUsing.Random(random).Definition;
+            
+            while (organelleToUse.DisplayScene == string.Empty & organellesToTryUsing.Count != 0)
+            {
+                organelleToTryUsing = organellesToTryUsing.Random(random);
+                organellesToTryUsing.Remove(organelleToTryUsing);
+                organelleToUse = organelleToTryUsing.Definition;
+            }
 
             if (organelleToUse.DisplayScene != string.Empty)
             {
