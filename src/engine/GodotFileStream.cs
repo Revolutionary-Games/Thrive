97af84faec30f92b5182b58959e30f69499db9dd
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-05-19 15:08:37 +0300

    Added some basic save writing code
    untested

---
+++ src/engine/GodotFileStream.cs
@@ -0,0 +1,86 @@
+using System;
+using System.IO;
+using Godot;
+using File = Godot.File;
+
+/// <summary>
+///   Wraps a Godot.File as a Stream
+/// </summary>
+public class GodotFileStream : Stream
+{
+    private readonly File file;
+
+    public GodotFileStream(Godot.File file)
+    {
+        this.file = file;
+    }
+
+    public override bool CanRead => true;
+    public override bool CanSeek => true;
+    public override bool CanWrite => true;
+    public override long Length => file.GetLen();
+
+    public override long Position
+    {
+        get
+        {
+            return file.GetPosition();
+        }
+        set
+        {
+            file.Seek(value);
+        }
+    }
+
+    public override void Flush()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        var data = file.GetBuffer(count);
+        Array.Copy(data, 0, buffer, offset, data.Length);
+        return data.Length;
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        switch (origin)
+        {
+            case SeekOrigin.Begin:
+                file.Seek(offset);
+                break;
+            case SeekOrigin.Current:
+                file.Seek(file.GetPosition() + offset);
+                break;
+            case SeekOrigin.End:
+                file.SeekEnd(offset);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(origin), origin, null);
+        }
+
+        return file.GetPosition();
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        if (offset == 0 && count == buffer.Length)
+        {
+            file.StoreBuffer(buffer);
+        }
+        else
+        {
+            // We need a temporary copy
+            byte[] copy = new byte[count];
+            Array.Copy(buffer, offset, copy, 0, count);
+            file.StoreBuffer(copy);
+        }
+    }
+}
---
+++ src/general/PathUtils.cs
@@ -0,0 +1,31 @@
+/// <summary>
+///   Helpers for dealing with paths
+/// </summary>
+public static class PathUtils
+{
+    public const char PATH_SEPARATOR = '/';
+
+    public static string Join(string first, string second)
+    {
+        if (string.IsNullOrEmpty(first))
+            return second;
+        if (string.IsNullOrEmpty(second))
+            return first;
+
+        bool firstSeparator = first[first.Length - 1] == PATH_SEPARATOR;
+        bool secondSeparator = second[0] == PATH_SEPARATOR;
+
+        if (firstSeparator && secondSeparator)
+        {
+            return first.Substring(0, first.Length - 1) + second;
+        }
+        else if (firstSeparator || secondSeparator)
+        {
+            return first + second;
+        }
+        else
+        {
+            return first + PATH_SEPARATOR + second;
+        }
+    }
+}
