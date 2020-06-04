d0c551b8a2f020c4d8891756f1661ff3ed93c77a
coreyhendrey <coreyhendrey@gmail.com>
2020-06-04 16:15:01 +1000

    Refund MP cost when deleting Organelle that were placed this session. (Fix)

--- src/microbe_stage/OrganelleTemplate.cs
+++ src/microbe_stage/OrganelleTemplate.cs
@@ -19,6 +19,8 @@ public class OrganelleTemplate : IPositionedOrganelle, ICloneable
         Orientation = rotation;
     }
 
+    public bool PlacedThisSession {get; set; }
+
     public Hex Position { get; set; }
 
     /// <summary>
