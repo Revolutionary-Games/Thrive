4f494973ee43e0ac23be518cca941d28de05c89f
Holomanga <holomanga@gmail.com>
2020-06-05 15:50:05 +0100

    Increase energy needed for population by factor of 10.

--- src/auto-evo/simulation/PopulationSimulation.cs
+++ src/auto-evo/simulation/PopulationSimulation.cs
@@ -137,16 +137,16 @@
 
             var biome = patch.Biome;
 
-            var sunlightInPatch = biome.Compounds[Sunlight].Dissolved * 10000;
+            var sunlightInPatch = biome.Compounds[Sunlight].Dissolved * 1000;
 
             var hydrogenSulfideInPatch = biome.Compounds[HydrogenSulfide].Density
-                * biome.Compounds[HydrogenSulfide].Amount * 1000;
+                * biome.Compounds[HydrogenSulfide].Amount * 100;
 
             var glucoseInPatch = (biome.Compounds[Glucose].Density
                 * biome.Compounds[Glucose].Amount
-                + patch.GetPatchChunkTotalCompoundAmount(Glucose)) * 1000;
+                + patch.GetPatchChunkTotalCompoundAmount(Glucose)) * 100;
 
-            var ironInPatch = patch.GetPatchChunkTotalCompoundAmount(Iron) * 1000;
+            var ironInPatch = patch.GetPatchChunkTotalCompoundAmount(Iron) * 100;
 
             // TODO: this is where the proper auto-evo algorithm goes
 
