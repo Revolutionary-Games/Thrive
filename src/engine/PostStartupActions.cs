c2c2d7d2b7714eaa281adf84348a9cca1fd62d97
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-06-21 19:30:55 +0300

    Added a log print for the logs folder, also moved the version number (#1415)
    setting in window title to an autoload to make it always work

---
+++ src/engine/PostStartupActions.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+/// <summary>
+///   This is the last autoloaded class to perform some actions there
+/// </summary>
+public class PostStartupActions : Node
+{
+    private PostStartupActions()
+    {
+        // Queue window title set as setting it in the autoloads doesn't work yet
+        Invoke.Instance.Perform(() =>
+        {
+            OS.SetWindowTitle("Thrive - " + Constants.Version);
+        });
+    }
+}
