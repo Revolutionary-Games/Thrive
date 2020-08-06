d1c5e35de920347276246959c917ac11c5249381
Alpha Sop <joeyluuson@gmail.com>
2020-08-05 23:57:08 -0500

    Added a basic mod loader

---
+++ src/general/ModLoader.cs
@@ -0,0 +1,144 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+public class ModLoader : Control
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+	[Export]
+	public NodePath UnloadedItemListPath;
+ 	[Export]
+	public NodePath ModInfoNamePath;
+	[Export]
+	public NodePath ModInfoAuthorPath;
+ 	[Export]
+	public NodePath ModInfoVersionPath;
+	[Export]
+	public NodePath ModInfoDescriptionPath;
+	[Export]
+	public NodePath LoadedItemListPath;
+	
+	private ItemList unloadedItemList;
+	private ItemList loadedItemList;
+	private Label modInfoName;
+	private Label modInfoAuthor;
+	private Label modInfoVersion;
+	private Label modInfoDescription;
+	
+	private List<ModInfo> modList = new List<ModInfo>();	
+	private List<ModInfo> loadedModList = new List<ModInfo>();	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		 unloadedItemList = GetNode<ItemList>(UnloadedItemListPath);
+		 loadedItemList = GetNode<ItemList>(LoadedItemListPath);
+		 modInfoName = GetNode<Label>(ModInfoNamePath);
+		 modInfoAuthor = GetNode<Label>(ModInfoAuthorPath);
+		 modInfoVersion = GetNode<Label>(ModInfoVersionPath);
+		 modInfoDescription = GetNode<Label>(ModInfoDescriptionPath);
+		
+		 DirectoryInfo modFolder = System.IO.Directory.CreateDirectory($"{System.IO.Directory.GetCurrentDirectory()}\\mods");
+		 foreach(DirectoryInfo currentMod in modFolder.EnumerateDirectories()){
+			if(!System.IO.File.Exists(currentMod.FullName + "/mod_info.json")) continue;
+			var currentModInfo = JsonConvert.DeserializeObject<ModInfo>(ReadJSONFile(currentMod.FullName + "/mod_info.json"));
+			currentModInfo.Location = currentMod.FullName;
+			modList.Add(currentModInfo);
+			unloadedItemList.AddItem(currentModInfo.Name);
+		}
+	}
+	// Copied From The PauseMenu.cs
+	private static string ReadJSONFile(string path)
+	{
+		using (var file = new Godot.File())
+		{
+			file.Open(path, Godot.File.ModeFlags.Read);
+			var result = file.GetAsText();
+
+			// This might be completely unnecessary
+			file.Close();
+
+			return result;
+		}
+	}
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+	private void OnUnloadedModSelected(int index)
+	{
+		var tempModInfo = modList[index];
+		modInfoName.Text = tempModInfo.Name;
+		modInfoAuthor.Text = "Author: " + tempModInfo.Author;
+		modInfoVersion.Text = "Version: " + tempModInfo.Version;
+		modInfoDescription.Text = tempModInfo.Description;
+		modInfoDescription.Text = tempModInfo.Description;
+		loadedItemList.UnselectAll();
+	}
+	private void OnLoadedModSelected(int index)
+	{
+		var tempModInfo = loadedModList[index];
+		modInfoName.Text = tempModInfo.Name;
+		modInfoAuthor.Text = "Author: " + tempModInfo.Author;
+		modInfoVersion.Text = "Version: " + tempModInfo.Version;
+		modInfoDescription.Text = tempModInfo.Description;
+		modInfoDescription.Text = tempModInfo.Description;
+		unloadedItemList.UnselectAll();
+	}
+	private void OnMoveToLoadPressed()
+	{
+		GUICommon.Instance.PlayButtonPressSound();
+		if(unloadedItemList.GetSelectedItems().Length <= 0) return;
+		var selectedItem = unloadedItemList.GetSelectedItems()[0];
+		loadedModList.Add(modList[selectedItem]);
+		loadedItemList.AddItem(modList[selectedItem].Name);
+		modList.RemoveAt(selectedItem);
+		unloadedItemList.RemoveItem(selectedItem);
+	}
+	private void OnMoveToUnloadPressed()
+	{
+		GUICommon.Instance.PlayButtonPressSound();
+		if(loadedItemList.GetSelectedItems().Length <= 0) return;
+		var selectedItem = loadedItemList.GetSelectedItems()[0];
+		modList.Add(loadedModList[selectedItem]);
+		unloadedItemList.AddItem(loadedModList[selectedItem].Name);
+		loadedModList.RemoveAt(selectedItem);
+		loadedItemList.RemoveItem(selectedItem);
+	}
+	private void OnRefreshPressed()
+	{
+		GUICommon.Instance.PlayButtonPressSound();
+		modList.Clear();
+		loadedModList.Clear();
+		unloadedItemList.Clear();
+		loadedItemList.Clear();
+		DirectoryInfo modFolder = System.IO.Directory.CreateDirectory($"{System.IO.Directory.GetCurrentDirectory()}\\mods");
+		foreach(DirectoryInfo currentMod in modFolder.EnumerateDirectories()){
+			if(!System.IO.File.Exists(currentMod.FullName + "/mod_info.json")) continue;
+			var currentModInfo = JsonConvert.DeserializeObject<ModInfo>(ReadJSONFile(currentMod.FullName + "/mod_info.json"));
+			currentModInfo.Location = currentMod.FullName;
+			modList.Add(currentModInfo);
+			unloadedItemList.AddItem(currentModInfo.Name);
+		}
+	}
+	private void OnLoadPressed()
+	{
+		GUICommon.Instance.PlayButtonPressSound();
+		if(loadedModList.Count <= 0) return;
+		foreach(ModInfo currentMod in loadedModList){
+			if(currentMod.Dll != null || currentMod.Dll != ""){
+				if(!System.IO.File.Exists(currentMod.Location + "/" + currentMod.Dll)) continue;
+				Assembly.LoadFile(currentMod.Location + "/" + currentMod.Dll);
+			}
+			if(!System.IO.File.Exists(currentMod.Location + "/mod.pck")) continue;
+			ProjectSettings.LoadResourcePack(currentMod.Location + "/mod.pck");
+			GD.Print("Loaded mod: " + currentMod.Name);
+		}
+		GD.Print("All mods loaded");
+	}
+}
+
--- src/gui_common/MainMenu.cs
+++ src/gui_common/MainMenu.cs
@@ -10,292 +10,292 @@ using Array = Godot.Collections.Array;
 /// </summary>
 public class MainMenu : Node
 {
-    /// <summary>
-    ///   Index of the current menu.
-    /// </summary>
-    [Export]
-    public uint CurrentMenuIndex;
+	/// <summary>
+	///   Index of the current menu.
+	/// </summary>
+	[Export]
+	public uint CurrentMenuIndex;
 
-    [Export]
-    public NodePath ThriveLogoPath;
+	[Export]
+	public NodePath ThriveLogoPath;
 
-    [SuppressMessage("ReSharper", "CollectionNeverUpdated.Global", Justification = "Set from editor")]
-    [Export]
-    public List<Texture> MenuBackgrounds;
+	[SuppressMessage("ReSharper", "CollectionNeverUpdated.Global", Justification = "Set from editor")]
+	[Export]
+	public List<Texture> MenuBackgrounds;
 
-    [Export]
-    public NodePath NewGameButtonPath;
+	[Export]
+	public NodePath NewGameButtonPath;
 
-    [Export]
-    public NodePath FreebuildButtonPath;
+	[Export]
+	public NodePath FreebuildButtonPath;
 
-    public Array MenuArray;
-    public TextureRect Background;
+	public Array MenuArray;
+	public TextureRect Background;
 
-    public bool IsReturningToMenu = false;
+	public bool IsReturningToMenu = false;
 
-    private TextureRect thriveLogo;
-    private OptionsMenu options;
-    private AnimationPlayer guiAnimations;
-    private SaveManagerGUI saves;
+	private TextureRect thriveLogo;
+	private OptionsMenu options;
+	private AnimationPlayer guiAnimations;
+	private SaveManagerGUI saves;
 
-    private Button newGameButton;
-    private Button freebuildButton;
+	private Button newGameButton;
+	private Button freebuildButton;
 
-    public override void _Ready()
-    {
-        RunMenuSetup();
+	public override void _Ready()
+	{
+		RunMenuSetup();
 
-        // Start intro video
-        if (Settings.Instance.PlayIntroVideo && !IsReturningToMenu)
-        {
-            TransitionManager.Instance.AddCutscene("res://assets/videos/intro.webm");
-            TransitionManager.Instance.StartTransitions(this, nameof(OnIntroEnded));
-        }
-        else
-        {
-            OnIntroEnded();
-        }
-    }
+		// Start intro video
+		if (Settings.Instance.PlayIntroVideo && !IsReturningToMenu)
+		{
+			TransitionManager.Instance.AddCutscene("res://assets/videos/intro.webm");
+			TransitionManager.Instance.StartTransitions(this, nameof(OnIntroEnded));
+		}
+		else
+		{
+			OnIntroEnded();
+		}
+	}
 
-    public void StartMusic()
-    {
-        Jukebox.Instance.PlayingCategory = "Menu";
-        Jukebox.Instance.Resume();
-    }
+	public void StartMusic()
+	{
+		Jukebox.Instance.PlayingCategory = "Menu";
+		Jukebox.Instance.Resume();
+	}
 
-    /// <summary>
-    ///   Sets the current menu index and then switches the menu
-    /// </summary>
-    /// <param name="index">Index of the menu</param>
-    /// <param name="slide">If false then the menu slide animation will not be played</param>
-    public void SetCurrentMenu(uint index, bool slide = true)
-    {
-        // Allow disabling all the menus for going to the options menu
-        if (index > MenuArray.Count - 1 && index != uint.MaxValue)
-        {
-            GD.PrintErr("Selected menu index is out of range!");
-            return;
-        }
+	/// <summary>
+	///   Sets the current menu index and then switches the menu
+	/// </summary>
+	/// <param name="index">Index of the menu</param>
+	/// <param name="slide">If false then the menu slide animation will not be played</param>
+	public void SetCurrentMenu(uint index, bool slide = true)
+	{
+		// Allow disabling all the menus for going to the options menu
+		if (index > MenuArray.Count - 1 && index != uint.MaxValue)
+		{
+			GD.PrintErr("Selected menu index is out of range!");
+			return;
+		}
 
-        CurrentMenuIndex = index;
+		CurrentMenuIndex = index;
 
-        if (slide)
-        {
-            guiAnimations.Play("MenuSlide");
-        }
-        else
-        {
-            // Just switch the menu
-            SwitchMenu();
-        }
-    }
+		if (slide)
+		{
+			guiAnimations.Play("MenuSlide");
+		}
+		else
+		{
+			// Just switch the menu
+			SwitchMenu();
+		}
+	}
 
-    /// <summary>
-    ///   Setup the main menu.
-    /// </summary>
-    private void RunMenuSetup()
-    {
-        Background = GetNode<TextureRect>("Background");
-        guiAnimations = GetNode<AnimationPlayer>("GUIAnimations");
-        thriveLogo = GetNode<TextureRect>(ThriveLogoPath);
-        newGameButton = GetNode<Button>(NewGameButtonPath);
-        freebuildButton = GetNode<Button>(FreebuildButtonPath);
+	/// <summary>
+	///   Setup the main menu.
+	/// </summary>
+	private void RunMenuSetup()
+	{
+		Background = GetNode<TextureRect>("Background");
+		guiAnimations = GetNode<AnimationPlayer>("GUIAnimations");
+		thriveLogo = GetNode<TextureRect>(ThriveLogoPath);
+		newGameButton = GetNode<Button>(NewGameButtonPath);
+		freebuildButton = GetNode<Button>(FreebuildButtonPath);
 
-        MenuArray?.Clear();
+		MenuArray?.Clear();
 
-        // Get all of menu items
-        MenuArray = GetTree().GetNodesInGroup("MenuItem");
+		// Get all of menu items
+		MenuArray = GetTree().GetNodesInGroup("MenuItem");
 
-        if (MenuArray == null)
-        {
-            GD.PrintErr("Failed to find all the menu items!");
-            return;
-        }
+		if (MenuArray == null)
+		{
+			GD.PrintErr("Failed to find all the menu items!");
+			return;
+		}
 
-        RandomizeBackground();
+		RandomizeBackground();
 
-        options = GetNode<OptionsMenu>("OptionsMenu");
-        saves = GetNode<SaveManagerGUI>("SaveManagerGUI");
+		options = GetNode<OptionsMenu>("OptionsMenu");
+		saves = GetNode<SaveManagerGUI>("SaveManagerGUI");
 
-        // Load settings
-        options.SetSettingsFrom(Settings.Instance);
+		// Load settings
+		options.SetSettingsFrom(Settings.Instance);
 
-        // Set initial menu
-        SwitchMenu();
-    }
+		// Set initial menu
+		SwitchMenu();
+	}
 
-    /// <summary>
-    ///   Randomizes background images.
-    /// </summary>
-    private void RandomizeBackground()
-    {
-        Random rand = new Random();
+	/// <summary>
+	///   Randomizes background images.
+	/// </summary>
+	private void RandomizeBackground()
+	{
+		Random rand = new Random();
 
-        // Exported lists will crash the game, so as a workaround ToList() is added
-        // https://github.com/godotengine/godot/issues/37934
-        // This is a Godot issue that may get fixed in 4.0
-        var chosenBackground = MenuBackgrounds.ToList().Random(rand);
+		// Exported lists will crash the game, so as a workaround ToList() is added
+		// https://github.com/godotengine/godot/issues/37934
+		// This is a Godot issue that may get fixed in 4.0
+		var chosenBackground = MenuBackgrounds.ToList().Random(rand);
 
-        SetBackground(chosenBackground);
-    }
+		SetBackground(chosenBackground);
+	}
 
-    private void SetBackground(Texture backgroundImage)
-    {
-        Background.Texture = backgroundImage;
-    }
+	private void SetBackground(Texture backgroundImage)
+	{
+		Background.Texture = backgroundImage;
+	}
 
-    /// <summary>
-    ///   Stops any currently playing animation and plays
-    ///   the given one instead
-    /// </summary>
-    private void PlayGUIAnimation(string animation)
-    {
-        if (guiAnimations.IsPlaying())
-            guiAnimations.Stop();
+	/// <summary>
+	///   Stops any currently playing animation and plays
+	///   the given one instead
+	/// </summary>
+	private void PlayGUIAnimation(string animation)
+	{
+		if (guiAnimations.IsPlaying())
+			guiAnimations.Stop();
 
-        guiAnimations.Play(animation);
-    }
+		guiAnimations.Play(animation);
+	}
 
-    /// <summary>
-    ///   Switches the displayed menu
-    /// </summary>
-    private void SwitchMenu()
-    {
-        // Hide other menus and only show the one of the current index
-        foreach (Control menu in MenuArray)
-        {
-            menu.Hide();
+	/// <summary>
+	///   Switches the displayed menu
+	/// </summary>
+	private void SwitchMenu()
+	{
+		// Hide other menus and only show the one of the current index
+		foreach (Control menu in MenuArray)
+		{
+			menu.Hide();
 
-            if (menu.GetIndex() == CurrentMenuIndex)
-            {
-                menu.Show();
-            }
-        }
-    }
+			if (menu.GetIndex() == CurrentMenuIndex)
+			{
+				menu.Show();
+			}
+		}
+	}
 
-    private void OnIntroEnded()
-    {
-        TransitionManager.Instance.AddScreenFade(Fade.FadeType.FadeOut, 0.5f, false);
-        TransitionManager.Instance.StartTransitions(null, string.Empty);
+	private void OnIntroEnded()
+	{
+		TransitionManager.Instance.AddScreenFade(Fade.FadeType.FadeOut, 0.5f, false);
+		TransitionManager.Instance.StartTransitions(null, string.Empty);
 
-        // Start music after the video
-        StartMusic();
-    }
+		// Start music after the video
+		StartMusic();
+	}
 
-    private void OnMicrobeIntroEnded()
-    {
-        // TODO: Add loading screen while changing between scenes
-        SceneManager.Instance.SwitchToScene(MainGameState.MicrobeStage);
-    }
+	private void OnMicrobeIntroEnded()
+	{
+		// TODO: Add loading screen while changing between scenes
+		SceneManager.Instance.SwitchToScene(MainGameState.MicrobeStage);
+	}
 
-    private void OnFreebuildFadeInEnded()
-    {
-        // Instantiate a new editor scene
-        var editor = (MicrobeEditor)SceneManager.Instance.LoadScene(MainGameState.MicrobeEditor).Instance();
+	private void OnFreebuildFadeInEnded()
+	{
+		// Instantiate a new editor scene
+		var editor = (MicrobeEditor)SceneManager.Instance.LoadScene(MainGameState.MicrobeEditor).Instance();
 
-        // Start freebuild game
-        editor.CurrentGame = GameProperties.StartNewMicrobeGame(true);
+		// Start freebuild game
+		editor.CurrentGame = GameProperties.StartNewMicrobeGame(true);
 
-        // Switch to the editor scene
-        SceneManager.Instance.SwitchToScene(editor);
-    }
+		// Switch to the editor scene
+		SceneManager.Instance.SwitchToScene(editor);
+	}
 
-    private void NewGamePressed()
-    {
-        GUICommon.Instance.PlayButtonPressSound();
+	private void NewGamePressed()
+	{
+		GUICommon.Instance.PlayButtonPressSound();
 
-        // Ignore mouse event on the button to prevent it being clicked twice
-        newGameButton.MouseFilter = Control.MouseFilterEnum.Ignore;
+		// Ignore mouse event on the button to prevent it being clicked twice
+		newGameButton.MouseFilter = Control.MouseFilterEnum.Ignore;
 
-        // Stop music for the video (stop is used instead of pause to stop the menu music playing a bit after the video
-        // before the stage music starts)
-        Jukebox.Instance.Stop();
+		// Stop music for the video (stop is used instead of pause to stop the menu music playing a bit after the video
+		// before the stage music starts)
+		Jukebox.Instance.Stop();
 
-        if (Settings.Instance.PlayMicrobeIntroVideo)
-        {
-            TransitionManager.Instance.AddScreenFade(Fade.FadeType.FadeIn, 0.5f);
-            TransitionManager.Instance.AddCutscene("res://assets/videos/microbe_intro2.webm");
-        }
-        else
-        {
-            // People who disable the cutscene are impatient anyway so use a reduced fade time
-            TransitionManager.Instance.AddScreenFade(Fade.FadeType.FadeIn, 0.2f);
-        }
+		if (Settings.Instance.PlayMicrobeIntroVideo)
+		{
+			TransitionManager.Instance.AddScreenFade(Fade.FadeType.FadeIn, 0.5f);
+			TransitionManager.Instance.AddCutscene("res://assets/videos/microbe_intro2.webm");
+		}
+		else
+		{
+			// People who disable the cutscene are impatient anyway so use a reduced fade time
+			TransitionManager.Instance.AddScreenFade(Fade.FadeType.FadeIn, 0.2f);
+		}
 
-        TransitionManager.Instance.StartTransitions(this, nameof(OnMicrobeIntroEnded));
-    }
+		TransitionManager.Instance.StartTransitions(this, nameof(OnMicrobeIntroEnded));
+	}
 
-    private void ToolsPressed()
-    {
-        GUICommon.Instance.PlayButtonPressSound();
-        SetCurrentMenu(1);
-    }
+	private void ToolsPressed()
+	{
+		GUICommon.Instance.PlayButtonPressSound();
+		SetCurrentMenu(1);
+	}
 
-    private void FreebuildEditorPressed()
-    {
-        GUICommon.Instance.PlayButtonPressSound();
+	private void FreebuildEditorPressed()
+	{
+		GUICommon.Instance.PlayButtonPressSound();
 
-        // Ignore mouse event on the button to prevent it being clicked twice
-        freebuildButton.MouseFilter = Control.MouseFilterEnum.Ignore;
+		// Ignore mouse event on the button to prevent it being clicked twice
+		freebuildButton.MouseFilter = Control.MouseFilterEnum.Ignore;
 
-        TransitionManager.Instance.AddScreenFade(Fade.FadeType.FadeIn, 0.3f, false);
-        TransitionManager.Instance.StartTransitions(this, nameof(OnFreebuildFadeInEnded));
-    }
+		TransitionManager.Instance.AddScreenFade(Fade.FadeType.FadeIn, 0.3f, false);
+		TransitionManager.Instance.StartTransitions(this, nameof(OnFreebuildFadeInEnded));
+	}
 
-    private void BackFromToolsPressed()
-    {
-        GUICommon.Instance.PlayButtonPressSound();
-        SetCurrentMenu(0);
-    }
+	private void BackFromToolsPressed()
+	{
+		GUICommon.Instance.PlayButtonPressSound();
+		SetCurrentMenu(0);
+	}
 
-    private void QuitPressed()
-    {
-        GUICommon.Instance.PlayButtonPressSound();
-        GetTree().Quit();
-    }
+	private void QuitPressed()
+	{
+		GUICommon.Instance.PlayButtonPressSound();
+		GetTree().Quit();
+	}
 
-    private void OptionsPressed()
-    {
-        GUICommon.Instance.PlayButtonPressSound();
+	private void OptionsPressed()
+	{
+		GUICommon.Instance.PlayButtonPressSound();
 
-        // Hide all the other menus
-        SetCurrentMenu(uint.MaxValue, false);
+		// Hide all the other menus
+		SetCurrentMenu(uint.MaxValue, false);
 
-        // Show the options
-        options.Visible = true;
+		// Show the options
+		options.Visible = true;
 
-        thriveLogo.Hide();
-    }
+		thriveLogo.Hide();
+	}
 
-    private void OnReturnFromOptions()
-    {
-        options.Visible = false;
+	private void OnReturnFromOptions()
+	{
+		options.Visible = false;
 
-        SetCurrentMenu(0, false);
+		SetCurrentMenu(0, false);
 
-        thriveLogo.Show();
-    }
+		thriveLogo.Show();
+	}
 
-    private void LoadGamePressed()
-    {
-        GUICommon.Instance.PlayButtonPressSound();
+	private void LoadGamePressed()
+	{
+		GUICommon.Instance.PlayButtonPressSound();
 
-        // Hide all the other menus
-        SetCurrentMenu(uint.MaxValue, false);
+		// Hide all the other menus
+		SetCurrentMenu(uint.MaxValue, false);
 
-        // Show the options
-        saves.Visible = true;
+		// Show the options
+		saves.Visible = true;
 
-        thriveLogo.Hide();
-    }
+		thriveLogo.Hide();
+	}
 
-    private void OnReturnFromLoadGame()
-    {
-        saves.Visible = false;
+	private void OnReturnFromLoadGame()
+	{
+		saves.Visible = false;
 
-        SetCurrentMenu(0, false);
+		SetCurrentMenu(0, false);
 
-        thriveLogo.Show();
-    }
+		thriveLogo.Show();
+	}
 }
