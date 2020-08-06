0b2bdd48e3915037abd8fc0bb2e391f6606e50a1
Alpha Sop <joeyluuson@gmail.com>
2020-08-05 23:57:08 -0500

    Added a basic mod loader

---
+++ src/general/ModInfo.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public class ModInfo : Node
+{
+	public string Name;
+	public string Author;
+	public string Version;
+	public string Description;
+	public string Location;
+	public string Dll;
+}
