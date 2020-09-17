c538479bd32ab15c2fe2a0351e8aa38bc6ff1fdc
DTanxxx <55208219+DTanxxx@users.noreply.github.com>
2020-09-17 18:16:55 +1200

    Added numbers to show ATP balance (#1607)
    * Added numbers to show ATP balance
    
    * Added labels underneath the bars
    
    * Removed unnecessary newline
    
    * Made labels to the right of bars
    
    * Accidentally made a hidden label visible
    
    * More accidental changes in editor
    
    * More accidental changes
    
    * Applied feedback
    
    * Applied more feedback
    
    Co-authored-by: David Tan <>

--- src/microbe_stage/editor/MicrobeEditorGUI.cs
+++ src/microbe_stage/editor/MicrobeEditorGUI.cs
@@ -58,6 +58,12 @@ public class MicrobeEditorGUI : Node
     public NodePath ATPBalanceLabelPath;
 
     [Export]
+    public NodePath ATPProductionLabelPath;
+
+    [Export]
+    public NodePath ATPConsumptionLabelPath;
+
+    [Export]
     public NodePath ATPBarContainerPath;
 
     [Export]
@@ -240,6 +246,8 @@ public class MicrobeEditorGUI : Node
     private TextureButton symmetryButton;
     private TextureRect symmetryIcon;
     private Label atpBalanceLabel;
+    private Label atpProductionLabel;
+    private Label atpConsumptionLabel;
     private SegmentedBar atpProductionBar;
     private SegmentedBar atpConsumptionBar;
     private Label glucoseReductionLabel;
@@ -313,6 +321,8 @@ public class MicrobeEditorGUI : Node
         symmetryButton = GetNode<TextureButton>(SymmetryButtonPath);
         finishButton = GetNode<Button>(FinishButtonPath);
         atpBalanceLabel = GetNode<Label>(ATPBalanceLabelPath);
+        atpProductionLabel = GetNode<Label>(ATPProductionLabelPath);
+        atpConsumptionLabel = GetNode<Label>(ATPConsumptionLabelPath);
         atpProductionBar = GetNode<SegmentedBar>(ATPProductionBarPath);
         atpConsumptionBar = GetNode<SegmentedBar>(ATPConsumptionBarPath);
         glucoseReductionLabel = GetNode<Label>(GlucoseReductionLabelPath);
@@ -459,6 +469,9 @@ public class MicrobeEditorGUI : Node
             atpBalanceLabel.Text = ATP_BALANCE_DEFAULT_TEXT + " - ATP PRODUCTION TOO LOW!";
             atpBalanceLabel.AddColorOverride("font_color", new Color(1.0f, 0.2f, 0.2f));
         }
+
+        atpProductionLabel.Text = string.Format(CultureInfo.CurrentCulture, "{0:F1}", energyBalance.TotalProduction);
+        atpConsumptionLabel.Text = string.Format(CultureInfo.CurrentCulture, "{0:F1}", energyBalance.TotalConsumption);
 
         float maxValue = Math.Max(energyBalance.TotalConsumption, energyBalance.TotalProduction);
         atpProductionBar.MaxValue = maxValue;
