cdc0dd62cb6911ec4e0536bb997a7fc28f136b03
Shadow <cdegraw8747@gmail.com>
2020-09-19 15:46:09 -0700

    Moved some classes to their own files. Moved property initialization to be inline with declarations. Other minor cleanup.

---
+++ src/engine/SettingValue.cs
@@ -0,0 +1,99 @@
+/// <summary>
+///   Wrapper class for settings options containing the value and a delegate that provides a callback for
+///   when the value is changed.
+/// </summary>
+public class SettingValue<TValueType> : IAssignableSetting
+{
+    private TValueType value;
+
+    public SettingValue(TValueType value)
+    {
+        this.value = value;
+    }
+
+    /// <summary>
+    ///   Generic delegate used for alerting when a setting value was changed.
+    /// </summary>
+    public delegate void SettingValueChangedDelegate<TSettingValueType>(TSettingValueType value);
+
+    public event SettingValueChangedDelegate<TValueType> OnChanged;
+
+    public TValueType Value
+    {
+        get => value;
+        set
+        {
+            if (!Equals(this.value, value))
+            {
+                this.value = value;
+
+                OnChanged?.Invoke(value);
+            }
+        }
+    }
+
+    public static implicit operator TValueType(SettingValue<TValueType> value)
+    {
+        return value.value;
+    }
+
+    public static bool operator ==(SettingValue<TValueType> lhs, SettingValue<TValueType> rhs)
+    {
+        return Equals(lhs, rhs);
+    }
+
+    public static bool operator !=(SettingValue<TValueType> lhs, SettingValue<TValueType> rhs)
+    {
+        return !(lhs == rhs);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+
+        if (GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        return Equals((SettingValue<TValueType>)obj);
+    }
+
+    public bool Equals(SettingValue<TValueType> obj)
+    {
+        if (!value.Equals(obj.value))
+            return false;
+
+        return true;
+    }
+
+    public override int GetHashCode()
+    {
+        return 17 ^ value.GetHashCode();
+    }
+
+    /// <summary>
+    ///   Casts a parameter object into a SettingValue generic of a matching type (if possible) and then
+    ///   copies the value from it.
+    /// </summary>
+    public void AssignFrom(object obj)
+    {
+        // Convert the object to the correct concrete type if possible.
+        var settingObject = obj as SettingValue<TValueType>;
+
+        if (settingObject == null)
+            return;
+
+        // Matching types, so we copy the value from the other SettingValue.
+        if (!value.Equals(settingObject.value))
+        {
+            value = settingObject.value;
+
+            // Call any registered listeners through the delegate to inform them of the value change.
+            OnChanged?.Invoke(value);
+        }
+    }
+}
--- src/engine/Settings.cs
+++ src/engine/Settings.cs
@@ -17,54 +17,6 @@ public class Settings
 
     private Settings()
     {
-        // Initialize properties to default settings.
-
-        // Graphics Properties
-        FullScreen = new SettingValue<bool>(true);
-        VSync = new SettingValue<bool>(true);
-        MSAAResolution = new SettingValue<Viewport.MSAA>(Viewport.MSAA.Msaa2x);
-        ColourblindSetting = new SettingValue<int>(0);
-        ChromaticAmount = new SettingValue<float>(15.0f);
-        ChromaticEnabled = new SettingValue<bool>(true);
-
-        // Sound Properties
-        VolumeMaster = new SettingValue<float>(0.0f);
-        VolumeMasterMuted = new SettingValue<bool>(false);
-        VolumeMusic = new SettingValue<float>(0.0f);
-        VolumeMusicMuted = new SettingValue<bool>(false);
-        VolumeAmbiance = new SettingValue<float>(0.0f);
-        VolumeAmbianceMuted = new SettingValue<bool>(false);
-        VolumeSFX = new SettingValue<float>(0.0f);
-        VolumeSFXMuted = new SettingValue<bool>(false);
-        VolumeGUI = new SettingValue<float>(0.0f);
-        VolumeGUIMuted = new SettingValue<bool>(false);
-
-        // Performance Properties
-        CloudUpdateInterval = new SettingValue<float>(0.040f);
-        CloudResolution = new SettingValue<int>(2);
-        RunAutoEvoDuringGamePlay = new SettingValue<bool>(true);
-
-        // Misc Properties
-        PlayIntroVideo = new SettingValue<bool>(true);
-        PlayMicrobeIntroVideo = new SettingValue<bool>(true);
-        AutoSaveEnabled = new SettingValue<bool>(true);
-        MaxAutoSaves = new SettingValue<int>(5);
-        MaxQuickSaves = new SettingValue<int>(5);
-        TutorialsEnabled = new SettingValue<bool>(true);
-        CheatsEnabled = new SettingValue<bool>(false);
-    }
-
-    /// <summary>
-    ///   Generic delegate used for alerting when a setting value was changed.
-    /// </summary>
-    public delegate void SettingValueChangedDelegate<TValueType>(TValueType value);
-
-    /// <summary>
-    ///   Interface used for the SettingValue class to allow casting of an object to the correct concrete class.
-    /// </summary>
-    public interface IAssignableSetting
-    {
-        void AssignFrom(object obj);
     }
 
     public static Settings Instance => SingletonInstance;
@@ -74,85 +26,86 @@ public class Settings
     /// <summary>
     ///   Sets whether the game window is in fullscreen mode
     /// </summary>
-    public SettingValue<bool> FullScreen { get; set; }
+    public SettingValue<bool> FullScreen { get; set; } = new SettingValue<bool>(true);
 
     /// <summary>
     ///   Sets whether the game window will use vsync
     /// </summary>
-    public SettingValue<bool> VSync { get; set; }
+    public SettingValue<bool> VSync { get; set; } = new SettingValue<bool>(true);
 
     /// <summary>
     ///   Sets amount of MSAA to apply to the viewport
     /// </summary>
-    public SettingValue<Viewport.MSAA> MSAAResolution { get; set; }
+    public SettingValue<Viewport.MSAA> MSAAResolution { get; set; } =
+        new SettingValue<Viewport.MSAA>(Viewport.MSAA.Msaa2x);
 
     /// <summary>
     ///   Optionally applies a colour filter to the screen to aid colourblind individuals
     ///   0 = None, 1 = Red/Green, 2 = Blue/Yellow
     /// </summary>
-    public SettingValue<int> ColourblindSetting { get; set; }
+    public SettingValue<int> ColourblindSetting { get; set; } = new SettingValue<int>(0);
 
     /// <summary>
     ///   The amount of Chromatic Aberration to apply to the screen
     /// </summary>
-    public SettingValue<float> ChromaticAmount { get; set; }
+    public SettingValue<float> ChromaticAmount { get; set; } = new SettingValue<float>(15.0f);
 
     /// <summary>
     ///   Enable or Disable Chromatic Aberration for screen
     /// </summary>
-    public SettingValue<bool> ChromaticEnabled { get; set; }
+    public SettingValue<bool> ChromaticEnabled { get; set; } = new SettingValue<bool>(true);
 
     // Sound Properties
 
     /// <summary>
     ///   The Db value to be added to the master audio bus
     /// </summary>
-    public SettingValue<float> VolumeMaster { get; set; }
+    public SettingValue<float> VolumeMaster { get; set; } = new SettingValue<float>(0.0f);
 
     /// <summary>
     ///   If true all sounds are muted
     /// </summary>
-    public SettingValue<bool> VolumeMasterMuted { get; set; }
+    public SettingValue<bool> VolumeMasterMuted { get; set; } = new SettingValue<bool>(false);
 
     /// <summary>
     ///   The Db value to be added to the music audio bus
     /// </summary>
-    public SettingValue<float> VolumeMusic { get; set; }
+    public SettingValue<float> VolumeMusic { get; set; } = new SettingValue<float>(0.0f);
 
     /// <summary>
     ///   If true music is muted
     /// </summary>
-    public SettingValue<bool> VolumeMusicMuted { get; set; }
+    public SettingValue<bool> VolumeMusicMuted { get; set; } = new SettingValue<bool>(false);
 
     /// <summary>
     ///   The Db value to be added to the ambiance audio bus
     /// </summary>
-    public SettingValue<float> VolumeAmbiance { get; set; }
+    public SettingValue<float> VolumeAmbiance { get; set; } = new SettingValue<float>(0.0f);
 
     /// <summary>
     ///   If true ambiance is muted
     /// </summary>
-    public SettingValue<bool> VolumeAmbianceMuted { get; set; }
+    public SettingValue<bool> VolumeAmbianceMuted { get; set; } = new SettingValue<bool>(false);
 
     /// <summary>
     ///   The Db value to be added to the sfx audio bus
     /// </summary>
-    public SettingValue<float> VolumeSFX { get; set; }
+    public SettingValue<float> VolumeSFX { get; set; } = new SettingValue<float>(0.0f);
 
     /// <summary>
     ///   If true sfx is muted
     /// </summary>
-    public SettingValue<bool> VolumeSFXMuted { get; set; }
+    public SettingValue<bool> VolumeSFXMuted { get; set; } = new SettingValue<bool>(false);
 
     /// <summary>
     ///   The Db value to be added to the gui audio bus
     /// </summary>
-    public SettingValue<float> VolumeGUI { get; set; }
+    public SettingValue<float> VolumeGUI { get; set; } = new SettingValue<float>(0.0f);
 
     /// <summary>
     ///   If true gui audio bus is muted
     /// </summary>
-    public SettingValue<bool> VolumeGUIMuted { get; set; }
+    public SettingValue<bool> VolumeGUIMuted { get; set; } = new SettingValue<bool>(false);
 
     // Performance Properties
 
@@ -168,58 +121,58 @@ public class Settings
     ///     0.0f, 0.020f, 0.040f, 0.1f, 0.25f
     ///   </para>
     /// </remarks>
-    public SettingValue<float> CloudUpdateInterval { get; set; }
+    public SettingValue<float> CloudUpdateInterval { get; set; } = new SettingValue<float>(0.040f);
 
     /// <summary>
     ///   This can be freely adjusted to adjust the performance The
     ///   higher this value is the smaller the size of the simulated
     ///   cloud is and the performance is better.
     /// </summary>
-    public SettingValue<int> CloudResolution { get; set; }
+    public SettingValue<int> CloudResolution { get; set; } = new SettingValue<int>(2);
 
     /// <summary>
     ///   If true an auto-evo run is started during gameplay,
     ///   taking up one of the background threads.
     /// </summary>
-    public SettingValue<bool> RunAutoEvoDuringGamePlay { get; set; }
+    public SettingValue<bool> RunAutoEvoDuringGamePlay { get; set; } = new SettingValue<bool>(true);
 
     // Misc Properties
 
     /// <summary>
     ///   When true the main intro is played
     /// </summary>
-    public SettingValue<bool> PlayIntroVideo { get; set; }
+    public SettingValue<bool> PlayIntroVideo { get; set; } = new SettingValue<bool>(true);
 
     /// <summary>
     ///   When true the microbe intro is played on new game
     /// </summary>
-    public SettingValue<bool> PlayMicrobeIntroVideo { get; set; }
+    public SettingValue<bool> PlayMicrobeIntroVideo { get; set; } = new SettingValue<bool>(true);
 
     /// <summary>
     ///   If false auto saving will be disabled
     /// </summary>
-    public SettingValue<bool> AutoSaveEnabled { get; set; }
+    public SettingValue<bool> AutoSaveEnabled { get; set; } = new SettingValue<bool>(true);
 
     /// <summary>
     ///   Number of auto saves to keep
     /// </summary>
-    public SettingValue<int> MaxAutoSaves { get; set; }
+    public SettingValue<int> MaxAutoSaves { get; set; } = new SettingValue<int>(5);
 
     /// <summary>
     ///   Number of quick saves to keep
     /// </summary>
-    public SettingValue<int> MaxQuickSaves { get; set; }
+    public SettingValue<int> MaxQuickSaves { get; set; } = new SettingValue<int>(5);
 
     /// <summary>
     ///   Saves the current settings by writing them to the settings configuration file.
     ///   Show tutorial messages
     /// </summary>
-    public SettingValue<bool> TutorialsEnabled { get; set; }
+    public SettingValue<bool> TutorialsEnabled { get; set; } = new SettingValue<bool>(true);
 
     /// <summary>
     ///   When true cheats are enabled
     /// </summary>
-    public SettingValue<bool> CheatsEnabled { get; set; }
+    public SettingValue<bool> CheatsEnabled { get; set; } = new SettingValue<bool>(false);
 
     public int CloudSimulationWidth => Constants.CLOUD_X_EXTENT / CloudResolution;
 
@@ -415,7 +368,8 @@ public class Settings
 
             if (error != Error.Ok)
             {
-                GD.Print("Failed to load settings configuration file, using default settings.");
+                GD.Print("Failed to open settings configuration file, file is missing or unreadable. "
+                    + "Using default settings instead.");
 
                 var settings = new Settings();
                 settings.Save();
@@ -433,7 +387,8 @@ public class Settings
             }
             catch
             {
-                GD.Print("Settings configuration file is unreadable, using default settings.");
+                GD.Print("Failed to deserialize settings file data, data may be improperly formatted. "
+                    + "Using default settings instead.");
 
                 var settings = new Settings();
                 settings.Save();
@@ -460,101 +415,6 @@ public class Settings
             var setting = property.GetValue(this) as IAssignableSetting;
 
             setting?.AssignFrom(property.GetValue(settings));
-        }
-    }
-
-    /// <summary>
-    ///   Wrapper class for settings options containing the value and a delegate that provides a callback for
-    ///   when the value is changed.
-    /// </summary>
-    public class SettingValue<TValueType> : IAssignableSetting
-    {
-        private TValueType value;
-
-        public SettingValue(TValueType value)
-        {
-            this.value = value;
-        }
-
-        public event SettingValueChangedDelegate<TValueType> OnChanged;
-
-        public TValueType Value
-        {
-            get => value;
-            set
-            {
-                if (!Equals(this.value, value))
-                {
-                    this.value = value;
-
-                    OnChanged?.Invoke(value);
-                }
-            }
-        }
-
-        public static implicit operator TValueType(SettingValue<TValueType> value)
-        {
-            return value.value;
-        }
-
-        public static bool operator ==(SettingValue<TValueType> lhs, SettingValue<TValueType> rhs)
-        {
-            return Equals(lhs, rhs);
-        }
-
-        public static bool operator !=(SettingValue<TValueType> lhs, SettingValue<TValueType> rhs)
-        {
-            return !(lhs == rhs);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null)
-            {
-                return false;
-            }
-
-            if (GetType() != obj.GetType())
-            {
-                return false;
-            }
-
-            return Equals((SettingValue<TValueType>)obj);
-        }
-
-        public bool Equals(SettingValue<TValueType> obj)
-        {
-            if (!value.Equals(obj.value))
-                return false;
-
-            return true;
-        }
-
-        public override int GetHashCode()
-        {
-            return 17 ^ value.GetHashCode();
-        }
-
-        /// <summary>
-        ///   Casts a parameter object into a SettingValue generic of a matching type (if possible) and then
-        ///   copies the value from it.
-        /// </summary>
-        public void AssignFrom(object obj)
-        {
-            // Convert the object to the correct concrete type if possible.
-            SettingValue<TValueType> settingObject = obj as SettingValue<TValueType>;
-
-            if (settingObject == null)
-                return;
-
-            // Matching types, so we copy the value from the other SettingValue.
-            if (!value.Equals(settingObject.value))
-            {
-                value = settingObject.value;
-
-                // Call any registered listeners through the delegate to inform them of the value change.
-                OnChanged?.Invoke(value);
-            }
         }
     }
 }
