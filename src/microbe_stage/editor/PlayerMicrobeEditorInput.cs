7e287c65e90850ac1067e69ef8903f49ddb350db
Jan Führer <jan_fue@icloud.com>
2020-10-19 10:10:30 +0200

    Begin using the new input system in editor

---
+++ src/microbe_stage/editor/PlayerMicrobeEditorInput.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+/// <summary>
+///   Handles key input in the microbe editor
+/// </summary>
+public class PlayerMicrobeEditorInput : Node
+{
+    /// <summary>
+    ///   A reference to the editor is kept to notify it about inputs.
+    /// </summary>
+    private MicrobeEditor editor;
+}
