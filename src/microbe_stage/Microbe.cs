6a8fd137703120e677e5fa21923d64904408097b
holomanga <holomanga@gmail.com>
2020-10-24 19:23:12 +0100

    Reset engulf mode to false if the new membrane doesn't allow it.

--- src/microbe_stage/Microbe.cs
+++ src/microbe_stage/Microbe.cs
@@ -386,6 +386,10 @@ public class Microbe : RigidBody, ISpawned, IProcessable, IMicrobeAI
         Membrane.Type = Species.MembraneType;
         Membrane.Tint = Species.Colour;
         Membrane.Dirty = true;
+        if (Membrane.Type.CellWall) {
+            // Reset engulf mode if the new membrane doesn't allow it
+            EngulfMode = false;
+        }
 
         SetupMicrobeHitpoints();
     }
