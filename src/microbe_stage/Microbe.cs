aaed22f06eebeb9bbf4083a126ce8ea7e90708db
Holomanga <holomanga@gmail.com>
2020-10-25 15:01:42 +0000

    1726 disable engulfing for wall type membranes (#1752)
    * Disable player engulfing if membrane is cell wall.
    
    * Disable NPC engulfing if membrane is cell wall.
    
    * Add information about engulfing to tooltips.
    
    * Reset engulf mode to false if the new membrane doesn't allow it.
    
    * Formatting.
    
    * Move check for CellWall when engulfing into Microbe class
    
    * Reset engulf mode if new membrane doesn't allow it
    
    * Bold "Cannot engulf" text
    
    * Formatting
    
    * Put check in MicrobeAI back

--- src/microbe_stage/Microbe.cs
+++ src/microbe_stage/Microbe.cs
@@ -193,7 +193,17 @@ public class Microbe : RigidBody, ISpawned, IProcessable, IMicrobeAI
     public bool EngulfMode
     {
         get => engulfMode;
-        set => engulfMode = value;
+        set
+        {
+            if (!Membrane.Type.CellWall)
+            {
+                engulfMode = value;
+            }
+            else
+            {
+                engulfMode = false;
+            }
+        }
     }
 
     [JsonIgnore]
@@ -386,6 +396,12 @@ public class Microbe : RigidBody, ISpawned, IProcessable, IMicrobeAI
         Membrane.Type = Species.MembraneType;
         Membrane.Tint = Species.Colour;
         Membrane.Dirty = true;
+
+        if (Membrane.Type.CellWall)
+        {
+            // Reset engulf mode if the new membrane doesn't allow it
+            EngulfMode = false;
+        }
 
         SetupMicrobeHitpoints();
     }
