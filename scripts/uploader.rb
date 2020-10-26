9f83ceb58e6aa3499f7436f67348543e6c275e24
Sourciluss667 <qcormand@gmail.com>
2020-10-26 16:47:19 +0100

    Better waiting

--- scripts/uploader.rb
+++ scripts/uploader.rb
@@ -219,7 +219,7 @@ class DevBuildUploader
         
         if response.code == 503
           puts "Error 503: waiting 3 minutes..."
-          sleep(180)
+          sleep(10 + i*20)
         elsif response.code != needed_response_code
           puts "Response: #{response}"
           raise "unexpected response code: #{response.code}"
