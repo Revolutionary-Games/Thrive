a1fa382b778732726adbd53728d239822ac32283
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-10-29 21:01:58 +0200

    Fixed duplicate call to quick load causing two loads to happen at
    once, moved quick load to the new input handler to make it proofed
    against duplicate input events (which wasn't really a problem but I
    already did this code before finding the actual problem)

--- src/gui_common/QuickLoadHandler.cs
+++ src/gui_common/QuickLoadHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Godot;
 
 /// <summary>
@@ -8,7 +9,19 @@ public class QuickLoadHandler : Node
     [Export]
     public NodePath DifferentVersionDialogPath;
 
+    /// <summary>
+    ///   Used to avoid problems with duplicate inputs
+    /// </summary>
+    private readonly InputGroup inputs;
+
+    private readonly InputTrigger load = new InputTrigger("quick_load");
+
     private AcceptDialog differentVersionDialog;
+
+    public QuickLoadHandler()
+    {
+        inputs = new InputGroup(new List<IInputReceiver> { load });
+    }
 
     public override void _Ready()
     {
@@ -20,20 +33,39 @@ public class QuickLoadHandler : Node
 
     public override void _Input(InputEvent @event)
     {
-        if (@event.IsActionPressed("quick_load"))
+        if (inputs.CheckInput(@event))
+        {
+            GetTree().SetInputAsHandled();
+        }
+    }
+
+    public override void _Notification(int focus)
+    {
+        // If the window goes out of focus, we don't receive the key released events
+        // We reset our held down keys if the player tabs out while pressing a key
+        if (focus == MainLoop.NotificationWmFocusOut)
+        {
+            inputs.FocusLost();
+        }
+    }
+
+    public override void _Process(float delta)
+    {
+        inputs.OnFrameChanged();
+
+        if (load.ReadTrigger())
         {
             if (!InProgressLoad.IsLoading)
             {
                 GD.Print("Quick load pressed, attempting to load latest save");
-                SaveHelper.QuickLoad();
+
+                if (!SaveHelper.QuickLoad())
+                    differentVersionDialog.PopupCenteredMinsize();
             }
             else
             {
                 GD.Print("Quick load pressed, cancelled because another is already in progress");
             }
-
-            if (!SaveHelper.QuickLoad())
-                differentVersionDialog.PopupCenteredMinsize();
         }
     }
 }
