d4c6b178c1f0e002f5190bfaae8d907f64072da0
Jan Führer <jan_fue@icloud.com>
2020-10-31 21:41:42 +0100

    Added docs
    Fixed crash
    
    Added more docs
    
    More comments

--- src/engine/input/key_mapping/InputDataList.cs
+++ src/engine/input/key_mapping/InputDataList.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Godot;
 
-public class InputDataList
+/// <summary>
+///   Stores an godot input action and its associated events
+/// </summary>
+public class InputDataList : ICloneable
 {
     public InputDataList(Dictionary<string, List<InputEventWithModifiers>> data)
     {
@@ -11,4 +15,30 @@ public class InputDataList
     public Dictionary<string, List<InputEventWithModifiers>> Data { get; }
 
     public List<InputEventWithModifiers> this[string index] => Data[index];
+
+    public object Clone()
+    {
+        var result = new Dictionary<string, List<InputEventWithModifiers>>();
+        foreach (var keyValuePair in Data)
+        {
+            result[keyValuePair.Key] = new List<InputEventWithModifiers>();
+            foreach (var inputEventWithModifiers in keyValuePair.Value)
+            {
+                InputEventWithModifiers newEvent = inputEventWithModifiers switch
+                {
+                    InputEventKey key => new InputEventKey { Scancode = key.Scancode, },
+                    InputEventMouseButton mouse => new InputEventMouseButton { ButtonIndex = mouse.ButtonIndex, },
+                    _ => throw new NotSupportedException($"InputType {inputEventWithModifiers.GetType()} not supported"),
+                };
+
+                newEvent.Alt = inputEventWithModifiers.Alt;
+                newEvent.Control = inputEventWithModifiers.Control;
+                newEvent.Shift = inputEventWithModifiers.Shift;
+
+                result[keyValuePair.Key].Add(newEvent);
+            }
+        }
+
+        return new InputDataList(result);
+    }
 }
--- src/engine/input/key_mapping/InputEventItem.cs
+++ src/engine/input/key_mapping/InputEventItem.cs
@@ -2,30 +2,66 @@
 using Godot;
 using Newtonsoft.Json;
 
+/// <summary>
+///   Defines one specific event associated to an <see cref="InputActionItem">InputActionItem</see>.
+/// </summary>
 public class InputEventItem : Node
 {
     [Export]
     public NodePath ButtonPath;
 
-    internal bool JustAdded;
-
     private Button button;
 
+    /// <summary>
+    ///   If it is currently awaiting the user to press a button
+    /// </summary>
     public bool WaitingForInput { get; private set; }
 
+    /// <summary>
+    ///   The currently assigned godot input event.
+    ///   null if this event was just created
+    /// </summary>
     [JsonProperty]
     [JsonConverter(typeof(InputEventWithModifiersConverter))]
     public InputEventWithModifiers AssociatedEvent { get; set; }
 
+    /// <summary>
+    ///   The action this event is associated with
+    /// </summary>
     [JsonIgnore]
     internal InputActionItem AssociatedAction { get; set; }
 
+    /// <summary>
+    ///   If the event was just created and has never been assigned with a value
+    /// </summary>
+    [JsonIgnore]
+    internal bool JustAdded { get; set; }
+
+    /// <summary>
+    ///   Deleted this event from the associated action and updated the godot InputMap
+    /// </summary>
     public void Delete()
     {
         AssociatedAction.Inputs.Remove(this);
         InputGroupList.Instance.ControlsChanged();
     }
 
+    public override void _Ready()
+    {
+        button = GetNode<Button>(ButtonPath);
+        if (JustAdded)
+            OnButtonPressed();
+        else
+            UpdateButtonText();
+    }
+
+    /// <summary>
+    ///   Performs the key reassigning.
+    ///   Checks if it is waiting for a user input
+    ///   Checks if there are any conflicts and opens the dialog if so
+    ///   Overrides the old input with the new one
+    ///   Update the godot InputMap
+    /// </summary>
     public override void _Input(InputEvent @event)
     {
         if (InputGroupList.Instance.IsConflictDialogOpen())
@@ -34,6 +70,7 @@ public class InputEventItem : Node
         if (!WaitingForInput)
             return;
 
+        // Only InputEventMouseButton and InputEventKey are supported now
         if (!(@event is InputEventMouseButton) && !(@event is InputEventKey))
             return;
 
@@ -52,11 +89,15 @@ public class InputEventItem : Node
             }
         }
 
+        // The old godot input event. Null if this event is assigned a value the first time.
         var old = AssociatedEvent;
         AssociatedEvent = (InputEventWithModifiers)@event;
+
+        // Get the conflicts with the new input.
         var conflict = InputGroupList.Instance.Conflicts(this);
         if (conflict != null)
         {
+            // If there are conflicts detected reset the changes and ask the user.
             InputGroupList.Instance.ShowInputConflictDialog(this, conflict, AssociatedEvent);
             AssociatedEvent = old;
             return;
@@ -80,19 +121,14 @@ public class InputEventItem : Node
         WaitingForInput = false;
         JustAdded = false;
         InputGroupList.Instance.WasListeningForInput = false;
+
+        // Update the godot InputMap
         InputGroupList.Instance.ControlsChanged();
 
+        // Update the button text
         UpdateButtonText();
     }
 
-    public override void _Ready()
-    {
-        button = GetNode<Button>(ButtonPath);
-        if (JustAdded)
-            OnButtonPressed();
-        else
-            UpdateButtonText();
-    }
 
     public override bool Equals(object obj)
     {
@@ -124,9 +160,17 @@ public class InputEventItem : Node
         button.Text = AsText();
     }
 
+    /// <summary>
+    ///   Creates a string for the button to show.
+    /// </summary>
+    /// <returns>
+    ///   A human readable string.
+    /// </returns>
     private string AsText()
     {
         var text = string.Empty;
+
+        // Should never happen, just a fallback
         if (AssociatedEvent == null)
             return text;
 
