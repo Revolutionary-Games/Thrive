fa2625e86c8284e004d5a95dcd4c118c7c4b0fa3
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-11-09 19:30:54 +0200

    Updated the locale extraction script and documentation, reverted a
    change to the log file name

--- scripts/update_localization.rb
+++ scripts/update_localization.rb
@@ -1,16 +1,30 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+
+# List of locales, edit this to add new ones:
+LOCALES = %w[en fr].freeze
+
 require_relative '../RubySetupSystem/RubyCommon'
 
+ROOT_FOLDER = File.expand_path(File.join(File.dirname(__FILE__), '../'))
+LOCALE_FOLDER = File.join ROOT_FOLDER, 'locale'
+
+puts "Detected Thrive root folder: #{ROOT_FOLDER}"
+
 puts 'Extracting .pot file'
-runOpen3Checked 'pybabel', 'extract', '-F', '../locale/babelrc', '-k', 'LineEdit', '-k', 'text', '-k', 'DisplayName', '-k', 'window_title', '-k', 'dialog_text', '-k', 'Translate', '-o', '../locale/messages.pot', '../.'
-puts 'Done extracting'
+runOpen3Checked 'pybabel', 'extract', '-F', File.join(LOCALE_FOLDER, 'babelrc'), '-k',
+                'LineEdit', '-k', 'text', '-k', 'DisplayName', '-k', 'window_title', '-k',
+                'dialog_text', '-k', 'Translate', '-o',
+                File.join(LOCALE_FOLDER, '/messages.pot'), ROOT_FOLDER
+success 'Done extracting .pot file'
 
-puts 'Extracting .po files'
-LOCALES = %w[en fr]
+info 'Extracting .po files'
 
-LOCALES.each{|locale|
-    puts 'Extracting ' + locale + '.po'
-    runOpen3Checked 'msgmerge', '--update', '--backup=none', '../locale/' + locale + '.po', '../locale/messages.pot'
-}
+LOCALES.each do |locale|
+  puts "Extracting #{locale}.po"
+  runOpen3Checked 'msgmerge', '--update', '--backup=none',
+                  File.join(LOCALE_FOLDER, locale + '.po'),
+                  File.join(LOCALE_FOLDER, 'messages.pot')
+end
+
 success 'Done extracting .po files'
