c9da2a4e1d28b49ded15f40ebadffd087782c042
David Tan <>
2020-11-17 11:18:50 +1300

    Fixed jetbrains error

--- src/saving/serializers/RandomConverter.cs
+++ src/saving/serializers/RandomConverter.cs
@@ -13,16 +13,24 @@ public class RandomConverter : JsonConverter
         FieldInfo inextInfo = type.GetField("_inext", BindingFlags.NonPublic | BindingFlags.Instance);
         FieldInfo inextpInfo = type.GetField("_inextp", BindingFlags.NonPublic | BindingFlags.Instance);
 
-        int[] seedArray = (int[])seedArrayInfo.GetValue((Random)value);
-        int inext = (int)inextInfo.GetValue((Random)value);
-        int inextp = (int)inextpInfo.GetValue((Random)value);
-        int currSeed = seedArray[inext] - seedArray[inextp];
-
-        var formatter = new BinaryFormatter();
-        using (var dataWriter = new MemoryStream())
+        if (seedArrayInfo != null && inextInfo != null && inextpInfo != null)
         {
-            formatter.Serialize(dataWriter, currSeed);
-            serializer.Serialize(writer, Convert.ToBase64String(dataWriter.GetBuffer()));
+            int[] seedArray = (int[])seedArrayInfo.GetValue((Random)value);
+            int inext = (int)inextInfo.GetValue((Random)value);
+            int inextp = (int)inextpInfo.GetValue((Random)value);
+            int currSeed = seedArray[inext] - seedArray[inextp];
+
+            var formatter = new BinaryFormatter();
+            using (var dataWriter = new MemoryStream())
+            {
+                formatter.Serialize(dataWriter, currSeed);
+                serializer.Serialize(writer, Convert.ToBase64String(dataWriter.GetBuffer()));
+            }
+        }
+        else
+        {
+            throw new NullReferenceException("One or more fields did not exist in the type or did not match the " +
+                "specified binding constraints");
         }
     }
 
