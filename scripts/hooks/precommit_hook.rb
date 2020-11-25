fd07ef96c78124ae3016079f85a7e99ced7c1028
Paulpicotin <44963927+Paulpicotin@users.noreply.github.com>
2020-11-25 14:50:59 +0100

    writting output change

--- scripts/hooks/precommit_hook.rb
+++ scripts/hooks/precommit_hook.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
-
+  
 # Runs the check_formatting.rb script before committing
 require 'English'
 
 CHANGED_FILE = 'files_to_check.txt'
 
-output = system 'git diff --cached --name-only ' 
+output = system 'git diff --cached --name-only' 
 
-File.write output 
+File.write(CHANGED_FILE, output)
 
 system(output) 
 
