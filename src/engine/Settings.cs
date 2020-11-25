2468ccdc1bac9df9d93060d2514a6d2bf4737c3f
Sourciluss667 <qcormand@gmail.com>
2020-11-25 21:08:12 +0100

    Fix syntax

--- src/engine/Settings.cs
+++ src/engine/Settings.cs
@@ -339,7 +339,7 @@ public class Settings
             ApplyGraphicsSettings();
             ApplySoundSettings();
         }
-        
+
         ApplyLanguageSettings();
         ApplyWindowSettings();
     }
