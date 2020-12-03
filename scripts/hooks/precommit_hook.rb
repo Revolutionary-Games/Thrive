0d101cd1770cd13470f89969af919210242b1753
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-12-03 14:51:25 +0200

    Ran rubocop on the precommit_hook script and changed chomp to strip

--- scripts/hooks/precommit_hook.rb
+++ scripts/hooks/precommit_hook.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
-  
+
 # Runs the check_formatting.rb script before committing
 require 'English'
 
 CHANGED_FILE = 'files_to_check.txt'
 
-output =  `git diff --cached --name-only`.chomp
+output = `git diff --cached --name-only`.strip
 
 File.write CHANGED_FILE, output
 
