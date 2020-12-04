8216229bfca562510ce868b83b18ecbdb313b370
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-12-04 09:48:53 +0200

    Renamed the Chinese translation files to be Godot compatible
    hopefully weblate will still like them

--- scripts/update_localization.rb
+++ scripts/update_localization.rb
@@ -3,7 +3,7 @@
 
 # List of locales, edit this to add new ones:
 LOCALES = %w[bg ca cs de en es_AR es fi fr he id it pl pt_BR pt_PT ru tr
-             zh_Hans zh_Hant].freeze
+             zh_CN zh_TW].freeze
 
 require 'optparse'
 require_relative '../RubySetupSystem/RubyCommon'
