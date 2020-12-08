c6284ffada6e80b82ab6ae213103aff9564ff35f
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-12-08 19:04:35 +0200

    Final bit of refactoring to get the key rebinding code up to scratch
    One last update of the locale files

--- src/engine/input/key_mapping/InputEventItem.cs
+++ src/engine/input/key_mapping/InputEventItem.cs
@@ -2,10 +2,14 @@ using System;
 using Godot;
 
 /// <summary>
-///   Defines one specific event associated to an <see cref="InputActionItem">InputActionItem</see>.
+///   Defines one specific event associated with an <see cref="InputActionItem">InputActionItem</see>.
 ///   Handles the input rebinding and the asking the user for a new input action.
-///   Used by OptionsMenu>Inputs>InputGroupContainer>InputGroupItem>InputActionItem>InputEventItem
 /// </summary>
+/// <remarks>
+///   <para>
+///     Used by OptionsMenu>Inputs>InputGroupContainer>InputGroupItem>InputActionItem>InputEventItem
+///   </para>
+/// </remarks>
 public class InputEventItem : Node
 {
     [Export]
@@ -15,48 +19,90 @@ public class InputEventItem : Node
     public NodePath XButtonPath;
 
     private Button button;
-    private Button xbutton;
+    private Button xButton;
     private bool wasPressingButton;
 
     /// <summary>
-    ///   If it is currently awaiting the user to press a button
+    ///   If this is currently awaiting the user to press a button (for rebinding purposes)
     /// </summary>
     public bool WaitingForInput { get; private set; }
 
     /// <summary>
-    ///   The currently assigned input event.
-    ///   null if this event was just created
+    ///   The currently assigned key binding for this event.
+    ///   null if this InputEventItem was just created
     /// </summary>
-    public SpecifiedInputKey AssociatedEvent { get; set; }
+    public SpecifiedInputKey AssociatedEvent { get; private set; }
 
     /// <summary>
-    ///   The action this event is associated with
+    ///   The game action InputEventItem is associated with
     /// </summary>
     internal WeakReference<InputActionItem> AssociatedAction { get; set; }
 
     /// <summary>
-    ///   If the event was just created and has never been assigned with a value
+    ///   If this key binding was just created and has never been assigned with a value, so immediately prompt the user
+    ///   for a value
     /// </summary>
     internal bool JustAdded { get; set; }
 
-    /// <summary>
-    ///   Delete this event from the associated action and updated the godot InputMap
-    /// </summary>
-    public void Delete()
+    private InputActionItem Action
     {
-        GetAction().Inputs.Remove(this);
-        GetGroupList().ControlsChanged();
+        get
+        {
+            if (!AssociatedAction.TryGetTarget(out var associatedAction))
+                return null;
+
+            return associatedAction;
+        }
+    }
+
+    private InputGroupItem Group
+    {
+        get
+        {
+            var action = Action;
+
+            InputGroupItem associatedGroup = null;
+            action?.AssociatedGroup.TryGetTarget(out associatedGroup);
+
+            return associatedGroup;
+        }
+    }
+
+    private InputGroupList GroupList
+    {
+        get
+        {
+            var group = Group;
+
+            InputGroupList associatedList = null;
+            group?.AssociatedList.TryGetTarget(out associatedList);
+
+            return associatedList;
+        }
     }
 
     public override void _Ready()
     {
         button = GetNode<Button>(ButtonPath);
-        xbutton = GetNode<Button>(XButtonPath);
+        xButton = GetNode<Button>(XButtonPath);
 
         if (JustAdded)
-            OnButtonLeftPressed();
+        {
+            OnRebindButtonPressed();
+        }
         else
+        {
             UpdateButtonText();
+        }
+    }
+
+    /// <summary>
+    ///   Delete this event from the associated action and update the godot InputMap
+    /// </summary>
+    public void Delete()
+    {
+        Action.Inputs.Remove(this);
+        GroupList.ControlsChanged();
     }
 
     /// <summary>
@@ -68,24 +114,31 @@ public class InputEventItem : Node
     /// </summary>
     public override void _Input(InputEvent @event)
     {
-        var groupList = GetGroupList();
+        var groupList = GroupList;
+
+        if (groupList == null)
+        {
+            GD.PrintErr("InputEventItem has no group list");
+            return;
+        }
 
         if (groupList.IsConflictDialogOpen())
-            return;
-
-        if (!WaitingForInput)
             return;
 
         // Only InputEventMouseButton and InputEventKey are supported now
         if (!(@event is InputEventMouseButton) && !(@event is InputEventKey))
             return;
 
+        // Hacky custom button press detection
         if ((@event is InputEventMouseButton inputMouse) && inputMouse.ButtonIndex == (int)ButtonList.Left &&
-            xbutton.IsHovered())
+            xButton.IsHovered())
         {
             Delete();
             return;
         }
+
+        if (!WaitingForInput)
+            return;
 
         if (wasPressingButton)
         {
@@ -93,9 +146,9 @@ public class InputEventItem : Node
             return;
         }
 
-        if (@event is InputEventKey iek)
+        if (@event is InputEventKey key)
         {
-            switch (iek.Scancode)
+            switch (key.Scancode)
             {
                 case (uint)KeyList.Escape:
                     if (AssociatedEvent == null)
@@ -115,50 +168,15 @@ public class InputEventItem : Node
             }
         }
 
-        // The old godot input event. Null if this event is assigned a value the first time.
+        // The old key input event. Null if this event is assigned a value the first time.
         var old = AssociatedEvent;
         AssociatedEvent = new SpecifiedInputKey((InputEventWithModifiers)@event);
 
-        // Get the conflicts with the new input.
-        var conflict = groupList.Conflicts(this);
-        if (conflict != null)
-        {
-            AssociatedEvent = old;
-
-            // If there are conflicts detected reset the changes and ask the user.
-            groupList.ShowInputConflictDialog(this, conflict, (InputEventWithModifiers)@event);
+        // Check conflicts, and don't proceed if there is a conflict
+        if (CheckNewKeyConflicts(@event, groupList, old))
             return;
-        }
 
-        var associatedAction = GetAction();
-
-        // Check if the input is already defined for this action
-        // This code works by finding a pair
-        for (var i = 0; i < associatedAction.Inputs.Count; i++)
-        {
-            for (var x = i + 1; x < associatedAction.Inputs.Count; x++)
-            {
-                // Pair found (input already defined)
-                if (!associatedAction.Inputs[i].Equals(associatedAction.Inputs[x]))
-                    continue;
-
-                // Set AssociatedEvent to null to not delete the wrong InputEventItem,
-                // because Equals threats it the same with the same AssociatedEvent.
-                AssociatedEvent = null;
-                Delete();
-                return;
-            }
-        }
-
-        WaitingForInput = false;
-        JustAdded = false;
-        InputGroupList.WasListeningForInput = false;
-
-        // Update the godot InputMap
-        groupList.ControlsChanged();
-
-        // Update the button text
-        UpdateButtonText();
+        OnKeybindingSuccessfullyChanged();
     }
 
     public override bool Equals(object obj)
@@ -174,17 +192,70 @@ public class InputEventItem : Node
         return AssociatedEvent.GetHashCode();
     }
 
-    internal static InputEventItem BuildGUI(InputActionItem caller, SpecifiedInputKey @event)
+    internal static InputEventItem BuildGUI(InputActionItem associatedAction, SpecifiedInputKey @event)
     {
-        var res = (InputEventItem)InputGroupList.InputEventItemScene.Instance();
-        res.AssociatedAction = new WeakReference<InputActionItem>(caller);
-        res.AssociatedEvent = @event;
-        return res;
+        var result = (InputEventItem)associatedAction.GroupList.InputEventItemScene.Instance();
+
+        result.AssociatedAction = new WeakReference<InputActionItem>(associatedAction);
+        result.AssociatedEvent = @event;
+        return result;
     }
 
+    private bool CheckNewKeyConflicts(InputEvent @event, InputGroupList groupList, SpecifiedInputKey old)
+    {
+        // Get the conflicts with the new input.
+        var conflict = groupList.Conflicts(this);
+        if (conflict != null)
+        {
+            AssociatedEvent = old;
+
+            // If there are conflicts detected reset the changes and ask the user.
+            groupList.ShowInputConflictDialog(this, conflict, (InputEventWithModifiers)@event);
+            return true;
+        }
+
+        var associatedAction = Action;
+
+        // Check if the input is already defined for this action
+        // This code works by finding a pair
+        for (var i = 0; i < associatedAction.Inputs.Count; i++)
+        {
+            for (var x = i + 1; x < associatedAction.Inputs.Count; x++)
+            {
+                // Pair found (input already defined)
+                if (!associatedAction.Inputs[i].Equals(associatedAction.Inputs[x]))
+                    continue;
+
+                // Set AssociatedEvent to null to not delete the wrong InputEventItem,
+                // because Equals threats it the same with the same AssociatedEvent.
+                AssociatedEvent = null;
+                Delete();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void OnKeybindingSuccessfullyChanged()
+    {
+        WaitingForInput = false;
+        JustAdded = false;
+        InputGroupList.WasListeningForInput = false;
+
+        // Update the godot InputMap
+        GroupList?.ControlsChanged();
+
+        // Update the button text
+        UpdateButtonText();
+    }
+
+    /// <summary>
+    ///   Detect mouse button presses on this input key Control
+    /// </summary>
     private void OnButtonPressed(InputEvent @event)
     {
-        var groupList = GetGroupList();
+        var groupList = GroupList;
 
         if (groupList.IsConflictDialogOpen())
             return;
@@ -199,7 +270,7 @@ public class InputEventItem : Node
         {
             case (int)ButtonList.Left:
                 wasPressingButton = true;
-                OnButtonLeftPressed();
+                OnRebindButtonPressed();
                 break;
             case (int)ButtonList.Right:
                 Delete();
@@ -207,42 +278,16 @@ public class InputEventItem : Node
         }
     }
 
-    private void OnButtonLeftPressed()
+    private void OnRebindButtonPressed()
     {
         WaitingForInput = true;
-        button.Text = "Press a key...";
-        xbutton.Visible = true;
+        button.Text = TranslationServer.Translate("PRESS_KEY_DOT_DOT_DOT");
+        xButton.Visible = true;
     }
 
     private void UpdateButtonText()
     {
         button.Text = AssociatedEvent.ToString();
-        xbutton.Visible = false;
-    }
-
-    private InputActionItem GetAction()
-    {
-        if (!AssociatedAction.TryGetTarget(out var associatedAction))
-            return null;
-
-        return associatedAction;
-    }
-
-    private InputGroupItem GetGroup()
-    {
-        var action = GetAction();
-
-        action.AssociatedGroup.TryGetTarget(out var associatedGroup);
-
-        return associatedGroup;
-    }
-
-    private InputGroupList GetGroupList()
-    {
-        var group = GetGroup();
-
-        group.AssociatedList.TryGetTarget(out var associatedList);
-
-        return associatedList;
+        xButton.Visible = false;
     }
 }
