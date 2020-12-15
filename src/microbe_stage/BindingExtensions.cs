6f228ab8fb7bdb4e48d28d3332f91ec706ae7383
Jan Führer <jan_fue@icloud.com>
2020-12-15 17:46:31 +0100

    docs

--- src/microbe_stage/BindingExtensions.cs
+++ src/microbe_stage/BindingExtensions.cs
@@ -3,6 +3,10 @@ using System.Runtime.CompilerServices;
 
 public static class BindingExtensions
 {
+    /// <summary>
+    ///   Counts the members of my colony.
+    /// </summary>
+    /// <returns>Returns the number of colony members. Returns 0 if the microbe is not in a colony.</returns>
     public static int CountColonyMembers(this Microbe microbe, int runningValue = 0, bool fromAbove = false)
     {
         if (microbe.Colony == null)
@@ -21,21 +25,41 @@ public static class BindingExtensions
         return CountColonyMembers(microbe.Colony.Master.Microbe, 0, false);
     }
 
+    /// <summary>
+    ///   Gets the average value of the property across the whole colony.
+    /// </summary>
+    /// <param name="property">The property in question</param>
+    /// <returns>Returns the average value across the colony.</returns>
     public static double GetColonyValueAvg(this Microbe microbe, string property)
     {
         return GetColonyValueAvg(microbe, typeof(Microbe).GetProperty(property));
     }
 
+    /// <summary>
+    ///   Gets the average value of the property across the whole colony.
+    /// </summary>
+    /// <param name="property">The property in question</param>
+    /// <returns>Returns the average value across the colony.</returns>
     public static double GetColonyValueAvg(this Microbe microbe, PropertyInfo property)
     {
         return GetColonyValueSum(microbe, property) / CountColonyMembers(microbe);
     }
 
+    /// <summary>
+    ///   Gets the sum of the values of the properties across the whole colony.
+    /// </summary>
+    /// <param name="property">The property in question</param>
+    /// <returns>Returns the sum across the colony.</returns>
     public static double GetColonyValueSum(this Microbe microbe, string property)
     {
         return GetColonyValueSum(microbe, typeof(Microbe).GetProperty(property));
     }
 
+    /// <summary>
+    ///   Gets the sum of the values of the properties across the whole colony.
+    /// </summary>
+    /// <param name="property">The property in question</param>
+    /// <returns>Returns the sum across the colony.</returns>
     public static double GetColonyValueSum(
         this Microbe microbe, PropertyInfo property, bool fromAbove = false, double currValue = 0)
     {
@@ -56,6 +80,11 @@ public static class BindingExtensions
         return GetColonyValueSum(microbe.Colony.Master.Microbe, property, false, currValue);
     }
 
+    /// <summary>
+    ///   Gets the value from the colony.
+    /// </summary>
+    /// <param name="property">The property in question</param>
+    /// <returns>Returns the value.</returns>
     public static T GetColonyValue<T>(this Microbe microbe, [CallerMemberName] string property = "")
     {
         if (!microbe.ColonyValues.ContainsKey(property))
@@ -67,6 +96,10 @@ public static class BindingExtensions
         return (T)microbe.ColonyValues[property];
     }
 
+    /// <summary>
+    ///   Sets the value to the colony.
+    /// </summary>
+    /// <param name="property">The property in question</param>
     public static void SetColonyValue<T>(
         this Microbe microbe, T value, [CallerMemberName] string property = "", bool fromAbove = false)
     {
