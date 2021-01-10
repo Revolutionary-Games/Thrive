497da3f8355f269abbc899f0104467cf5f77271c
Jan Führer <jan_fue@icloud.com>
2021-01-10 18:57:52 +0100

    limit spawns on screen

--- src/microbe_stage/SpawnSystem.cs
+++ src/microbe_stage/SpawnSystem.cs
@@ -303,6 +303,9 @@ public class SpawnSystem
             spawned += 1;
             --spawnsLeftThisFrame;
 
+            if (spawnType.SpawnedEntities.Count + spawned > spawnType.MaxOnScreen)
+                return true;
+
             // Check if we are out of quota for this frame
 
             // TODO: this is a bit awkward if this
