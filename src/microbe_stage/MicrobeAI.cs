d9b8d84c929ac5e4cc6354fb2301ee847e70bd6e
Untrustedlife <bountybayonlineroblox@gmail.com>
2021-02-11 13:57:23 -0600

    Update MicrobeAI.cs
    Changed it so that prey is not set to null every think interval, since the switch statement relies on prey not always being null

--- src/microbe_stage/MicrobeAI.cs
+++ src/microbe_stage/MicrobeAI.cs
@@ -115,13 +115,12 @@ public class MicrobeAI
         preyMicrobes.Clear();
         chunkList.Clear();
 
-        prey = null;
-
         // 30 seconds about
         if (boredom == (int)random.Next(SpeciesFocus * 2, 1000.0f + SpeciesFocus * 2))
         {
             // Occasionally you need to reevaluate things
             boredom = 0;
+            prey = null;
             if (RollCheck(SpeciesActivity, 400, random))
             {
                 lifeState = LifeState.PLANTLIKE_STATE;
