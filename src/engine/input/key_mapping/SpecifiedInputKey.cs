723f0d260ec9877ac14b8d1fa9eca448004f839f
Jan Führer <jan_fue@icloud.com>
2021-04-04 12:04:59 +0200

    basic implementation

--- src/engine/input/key_mapping/SpecifiedInputKey.cs
+++ src/engine/input/key_mapping/SpecifiedInputKey.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using Godot;
 
 /// <summary>
@@ -57,9 +58,8 @@ public class SpecifiedInputKey : ICloneable
 
         if (Type == InputType.Key)
         {
-            // TODO: add translation support for key names like Right, and Left etc.
-            // TODO: https://github.com/Revolutionary-Games/Thrive/issues/1895
-            text += ((KeyList)Code).ToString();
+            // If the key is not defined in KeyNames.cs, the string will just be returned unmodified by Translate()
+            text += TranslationServer.Translate(((KeyList)Code).ToString().ToUpper(CultureInfo.InvariantCulture));
         }
         else if (Type == InputType.MouseButton)
         {
