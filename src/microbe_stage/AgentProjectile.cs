009b9b113d0a71ad708af456ddd41478d45585b3
Jan Führer <jan_fue@icloud.com>
2021-04-08 11:36:05 +0200

    removed ApplyPropertiesFromSave

--- src/microbe_stage/AgentProjectile.cs
+++ src/microbe_stage/AgentProjectile.cs
@@ -62,18 +62,6 @@ public class AgentProjectile : RigidBody, ITimedLife
         FadeTimeRemaining = Constants.PROJECTILE_DESPAWN_DELAY;
     }
 
-    public void ApplyPropertiesFromSave(AgentProjectile projectile)
-    {
-        NodeGroupSaveHelper.CopyGroups(this, projectile);
-
-        TimeToLiveRemaining = projectile.TimeToLiveRemaining;
-        Amount = projectile.Amount;
-        Properties = projectile.Properties;
-        Transform = projectile.Transform;
-        LinearVelocity = projectile.LinearVelocity;
-        AngularVelocity = projectile.AngularVelocity;
-    }
-
     private void Destroy()
     {
         // We should probably get some *POP* effect here.
