1be8babda432ee739f7c4d146e312db50b2c37bc
Jan Führer <jan_fue@icloud.com>
2021-04-08 10:28:08 +0200

    added docs

--- src/microbe_stage/organelle_components/BindingAgentComponent.cs
+++ src/microbe_stage/organelle_components/BindingAgentComponent.cs
@@ -1,4 +1,7 @@
-﻿public class BindingAgentComponent : IOrganelleComponent
+﻿/// <summary>
+///   Used to detect if a binding agent is present
+/// </summary>
+public class BindingAgentComponent : IOrganelleComponent
 {
     public void OnAttachToCell(PlacedOrganelle organelle)
     {
