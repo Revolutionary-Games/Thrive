715e88c87d4a981a07bdda28cad51b4b01ff6d3b
adQuid <brian.christopher.zito@gmail.com>
2021-05-19 12:19:13 -0400

    Shorten set logic
    Co-authored-by: Henri Hyyryläinen <henri.hyyrylainen@gmail.com>

--- src/microbe_stage/MicrobeAI.cs
+++ src/microbe_stage/MicrobeAI.cs
@@ -131,7 +131,7 @@ public class MicrobeAI
                 ((microbe.EngulfSize * (SpeciesOpportunism / Constants.OPPORTUNISM_DIVISOR)) >
                     chunk.Size))
             {
-                if (chunk.ContainedCompounds.Compounds.Where(x => microbe.Compounds.IsUseful(x.Key)).Any())
+                if (chunk.ContainedCompounds.Compounds.Any(x => microbe.Compounds.IsUseful(x.Key)))
                 {
                     chunkList.Add(chunk);
                     var thisPosition = chunk.Translation;
