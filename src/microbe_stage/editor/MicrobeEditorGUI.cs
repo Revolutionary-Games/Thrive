ceea2329d900fffb26f67da6c131eb17636edd39
84634E1A607A <60734649+84634E1A607A@users.noreply.github.com>
2021-05-22 17:22:49 +0800

    Revert "Fix as discussed"
    This reverts commit 2ccf5926c4fe5ade5b73f8b331affcd72157dcac.

--- src/microbe_stage/editor/MicrobeEditorGUI.cs
+++ src/microbe_stage/editor/MicrobeEditorGUI.cs
@@ -1131,12 +1131,6 @@ public class MicrobeEditorGUI : Node, ISaveLoadedTracked
 
     internal void OnFinishEditingClicked()
     {
-        // Prevent exiting when the transition hasn't finished
-        if (energyBalanceInfo != null)
-        {
-            return;
-        }
-
         if (editor.MovingOrganelle != null)
         {
             OnActionBlockedWhileMoving();
@@ -1146,7 +1140,8 @@ public class MicrobeEditorGUI : Node, ISaveLoadedTracked
         GUICommon.Instance.PlayButtonPressSound();
 
         // Show warning popup if trying to exit with negative atp production
-        if (energyBalanceInfo.TotalProduction < energyBalanceInfo.TotalConsumptionStationary)
+        if (energyBalanceInfo != null &&
+            energyBalanceInfo.TotalProduction < energyBalanceInfo.TotalConsumptionStationary)
         {
             negativeAtpPopup.PopupCenteredShrink();
             return;
