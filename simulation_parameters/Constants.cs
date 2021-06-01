79f34d3e678c4d47bc664489ce1c2debf56d5263
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-06-01 19:08:33 +0300

    Cloud visual tweaks and fixed flickering (#2287)
    * Made shader blend mode add
    
    * Trying to get a multiplier for the noise texture to work
    
    can't get it to work right with the cloud repositioning
    
    * Added a very helpful comment...
    
    * Removed the broken noise texture scaling, now the clouds maybe look like they have a bit more texture
    
    but this isn't as good looking than if the noise texture caused smaller areas of the clouds to have different brightness
    
    * Removed the alternative intensity calculation formulas that were commented out

--- simulation_parameters/Constants.cs
+++ simulation_parameters/Constants.cs
@@ -46,6 +46,9 @@ public static class Constants
     // Should be the same as its counterpart in shaders/CompoundCloudPlane.shader
     public const float CLOUD_MAX_INTENSITY_SHOWN = 1000;
 
+    // Should be the same as its counterpart in shaders/CompoundCloudPlane.shader
+    public const float CLOUD_NOISE_UV_OFFSET_MULTIPLIER = 2.5f;
+
     public const int MEMBRANE_RESOLUTION = 10;
 
     /// <summary>
