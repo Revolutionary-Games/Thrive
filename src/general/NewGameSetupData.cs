25c290c230e4158d4441ade24e66f61e7e34243c
what is this used for <no@thanks.com>
2021-06-15 07:40:34 -0400

    added new class

---
+++ src/general/NewGameSetupData.cs
@@ -0,0 +1,30 @@
+﻿namespace Thrive.src.general
+{
+    /// <summary>
+    ///    A singleton used as a message queue, used by main menu to specify rules for a new game
+    /// </summary>
+    public class NewGameSetupData
+    {
+        private static NewGameSetupData Instance;
+
+        public static void PushInstance(NewGameSetupData setupData)
+        {
+            Instance = setupData;
+        }
+
+        public static NewGameSetupData PopInstance()
+        {
+            var instanceToReturn = Instance;
+            Instance = null;
+            return instanceToReturn;
+        }
+
+        public float Difficulty;
+
+        public NewGameSetupData(float Difficulty)
+        {
+            this.Difficulty = Difficulty;
+        }
+
+    }
+}
