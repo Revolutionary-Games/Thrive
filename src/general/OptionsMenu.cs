06332443dba40f5b1d63ebb20a57c0d13b272522
Kemikal1 <alex_goku2007@yahoo.com>
2021-06-18 18:02:45 +0300

    Languages cached

--- src/general/OptionsMenu.cs
+++ src/general/OptionsMenu.cs
@@ -163,7 +163,8 @@ public class OptionsMenu : Control
     public NodePath CustomUsernamePath;
 
     // Sound tab static variables
-    private static List<string> languages = TranslationServer.GetLoadedLocales().Cast<string>().OrderBy(i => i, StringComparer.InvariantCulture).ToList();
+    private static List<string> languages = TranslationServer.GetLoadedLocales().Cast<string>()
+    .OrderBy(i => i, StringComparer.InvariantCulture).ToList();
 
     private Button resetButton;
     private Button saveButton;
