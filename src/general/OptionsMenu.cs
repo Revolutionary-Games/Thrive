3daad4ed17a1da56d88ef88b1b93c6864d4a3af1
84634E1A607A <60734649+84634E1A607A@users.noreply.github.com>
2021-06-19 20:09:53 +0800

    Adjusted to using custom input system. Failed...

--- src/general/OptionsMenu.cs
+++ src/general/OptionsMenu.cs
@@ -7,7 +7,7 @@ using Environment = System.Environment;
 /// <summary>
 ///   Handles the logic for the options menu GUI.
 /// </summary>
-public class OptionsMenu : Control
+public class OptionsMenu : ControlWithInput
 {
     /*
       GUI Control Paths
@@ -704,6 +704,7 @@ public class OptionsMenu : Control
       GUI Control Callbacks
     */
 
+    [RunOnKeyDown("g_return")]
     private void OnBackPressed()
     {
         GUICommon.Instance.PlayButtonPressSound();
