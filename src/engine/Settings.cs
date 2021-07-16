41d2476ddba1e9959946927acebb9587e1c59b30
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-07-16 20:37:24 +0300

    Print C# and Godot locales on start and after applying language settings (#2414)

--- src/engine/Settings.cs
+++ src/engine/Settings.cs
@@ -557,6 +557,8 @@ public class Settings
         // Set locale for the game. Called after C# locale change so that string
         // formatting uses could also get updated properly.
         TranslationServer.SetLocale(language);
+
+        GD.Print("Set C# locale to: ", cultureInfo, " Godot locale is: ", TranslationServer.GetLocale());
     }
 
     /// <summary>
