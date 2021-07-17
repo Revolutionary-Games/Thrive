0a6b8ffd393a10a8ed4bf3df5cc146600ba7cb91
84634E1A607A <60734649+84634E1A607A@users.noreply.github.com>
2021-07-17 20:11:10 +0800

    a

--- simulation_parameters/Constants.cs
+++ simulation_parameters/Constants.cs
@@ -369,6 +369,7 @@ public static class Constants
 
     public const float AI_BASE_MOVEMENT = 1.0f;
     public const float AI_FOCUSED_MOVEMENT = 1.0f;
+    public const float AI_ENGULF_STOP_DISTANCE = 0.8f;
 
     // Personality Mutation
     public const float MAX_SPECIES_PERSONALITY_MUTATION = 20.0f;
@@ -447,6 +448,8 @@ public static class Constants
     public const int PAUSE_MENU_CANCEL_PRIORITY = -2;
 
     public const int SUBMENU_CANCEL_PRIORITY = -1;
+
+    public const int DIALOG_WINDOW_CANCEL_PRIORITY = 1;
 
     /// <summary>
     ///   Maximum amount of snapshots to store in patch history.
--- src/microbe_stage/MicrobeColony.cs
+++ src/microbe_stage/MicrobeColony.cs
@@ -57,6 +57,9 @@ public class MicrobeColony
         if (!Equals(microbe.Colony, this))
             throw new ArgumentException("Cannot remove a colony member who isn't a member");
 
+        if (State == Microbe.MicrobeState.Unbinding)
+            State = Microbe.MicrobeState.Normal;
+
         foreach (var colonyMember in ColonyMembers)
             colonyMember.OnColonyMemberRemoved(microbe);
 
@@ -76,9 +79,6 @@ public class MicrobeColony
 
         microbe.ColonyParent = null;
         microbe.ColonyChildren = null;
-
-        if (State == Microbe.MicrobeState.Unbinding)
-            State = Microbe.MicrobeState.Normal;
     }
 
     public void AddToColony(Microbe microbe, Microbe master)
