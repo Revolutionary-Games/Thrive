5546b59f99a9b2e372ff50b4dba51ea30a176821
Maxonovien <maxime.caute@free.fr>
2021-07-30 11:43:09 +0200

    Added XML comment to the class

--- src/general/SpeciesInfo.cs
+++ src/general/SpeciesInfo.cs
@@ -1,3 +1,9 @@
+/// <summary>
+///   Class that stores species-wide information.
+///   It can be expanded to have any species parameter we might want to keep,
+///   e.g. base structure, behavioral values... and such to draw species history.
+/// </summary>
+/// <remarks> This class does not account for individual variations!</remarks>
 public class SpeciesInfo
 {
     public uint ID;
