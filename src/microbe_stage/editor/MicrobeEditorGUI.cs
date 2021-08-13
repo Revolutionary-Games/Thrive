3156de8f036b30ebd304273c7115f222fe49a0f8
Kasterisk <54026083+Kasterisk@users.noreply.github.com>
2021-08-13 13:17:03 +0700

    Mild refactor of the tooltip system (#2455)
    * Disconnect tooltip signals on tooltip unregister for complete disconnection, fixed typo
    
    * Adjustments to tooltips and editor GUI.
    
    - Tweaked the display delay of some tooltips in the editor.
    - Connected mouse enter/exit signals for the editor organism statistics
      panel to the editor GUI code, changed some of the control's mouse
      filter to Pass. This should make the hover not display while mousing
      over the statistics panel.
    - Unshortened chartLegendPhysConds.
    
    * Medium refactoring of the tooltip system to fix quirky behaviors
    
    * Simplified condition check, used `when` keyword in switch statemnt which I just know now is possible
    
    * Improved comment clarity, suppressed warning
    
    * Precede comment with blank line
    
    * Summary tweak and readded BOM
    
    * Reworded comment and adjusted switch expression

--- src/microbe_stage/editor/MicrobeEditorGUI.cs
+++ src/microbe_stage/editor/MicrobeEditorGUI.cs
@@ -1772,9 +1772,9 @@ public class MicrobeEditorGUI : Node, ISaveLoadedTracked
 
         // TODO: fix the short name used in chartLegendPhysicalConditions (abbreviated in the string literal below)
         // ReSharper disable StringLiteralTypo
-        temperatureButton.RegisterToolTipForControl("temperature", "chartLegendPhysConds");
+        temperatureButton.RegisterToolTipForControl("temperature", "chartLegendPhysicalConditions");
 
-        sunlightButton.RegisterToolTipForControl("sunlight", "chartLegendPhysConds");
+        sunlightButton.RegisterToolTipForControl("sunlight", "chartLegendPhysicalConditions");
 
         // ReSharper restore StringLiteralTypo
     }
