3dd894149daa5c67085ca495062ebbd11e52bdf4
Kemikal1 <alex_goku2007@yahoo.com>
2021-08-13 20:00:30 +0300

    Update MicrobeAI.cs

--- src/microbe_stage/MicrobeAI.cs
+++ src/microbe_stage/MicrobeAI.cs
@@ -192,7 +192,7 @@ public class MicrobeAI
             }
 
             var rivalThreshold = SpeciesOpportunism < Constants.MAX_SPECIES_OPPORTUNISM / 3 ? 1 :
-                SpeciesOpportunism < Constants.MAX_SPECIES_OPPORTUNISM * 2 / 3 ? 3 :
+                SpeciesOpportunism < Constants.MAX_SPECIES_OPPORTUNISM * 2 / 3              ? 3 :
                     5;
 
             // In rare instances, microbes will choose to be much more ambitious
