7b65c9c96c70cfd589597ac2da73004044f09117
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-08-21 20:51:30 +0300

    Added a new tutorial to prompt the player to press the editor button
    if the button is active too long without being pressed

--- simulation_parameters/Constants.cs
+++ simulation_parameters/Constants.cs
@@ -440,6 +440,7 @@ public static class Constants
     public const float GLUCOSE_TUTORIAL_COLLECT_BEFORE_COMPLETE = 0.21f;
     public const float MICROBE_REPRODUCTION_TUTORIAL_DELAY = 180;
     public const float HIDE_MICROBE_STAYING_ALIVE_TUTORIAL_AFTER = 60;
+    public const float MICROBE_EDITOR_BUTTON_TUTORIAL_DELAY = 20;
 
     /// <summary>
     ///   Used to limit how often the hover indicator panel are
