126e7a8ef5530073e51d62b65777c6395fa4e34a
Kemikal1 <alex_goku2007@yahoo.com>
2021-08-24 19:21:29 +0300

    cut the comment

--- src/microbe_stage/FloatingChunk.cs
+++ src/microbe_stage/FloatingChunk.cs
@@ -242,7 +242,6 @@ public class FloatingChunk : RigidBody, ISpawned, ISaveLoadedTracked
         // Check contacts
         foreach (var microbe in touchingMicrobes)
         {
-            // TODO: is it possible that this throws the disposed exception?
             if (microbe == null)
                 continue;
 
