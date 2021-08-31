84454b4a4d5f40c575316b0ed3e60cee8713d592
Maxonovien <maxime.caute@free.fr>
2021-08-31 20:16:00 +0200

    Fixed typo in comment
    Co-authored-by: Kasterisk <54026083+Kasterisk@users.noreply.github.com>

--- src/microbe_stage/MicrobeColony.cs
+++ src/microbe_stage/MicrobeColony.cs
@@ -44,7 +44,7 @@ public class MicrobeColony
 
     /// <summary>
     ///   Creates a colony for a microbe, with this microbe as the master,
-    ///   and handles related updates (like icrobe's colony and access to the editor button).
+    ///   and handles related updates (like microbe's colony and access to the editor button).
     /// </summary>
     /// <remarks> Should be used instead of the colony constructor, unless for loading from Json.</remarks>
     public static void CreateColonyForMicrobe(Microbe microbe)
