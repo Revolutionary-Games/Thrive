fd01c53a6f883f807e3387736826c0ac3baf84fb
Maxonovien <maxime.caute@free.fr>
2021-08-31 20:16:31 +0200

    Update comment
    Co-authored-by: Kasterisk <54026083+Kasterisk@users.noreply.github.com>

--- src/microbe_stage/MicrobeColony.cs
+++ src/microbe_stage/MicrobeColony.cs
@@ -43,7 +43,7 @@ public class MicrobeColony
     public Microbe Master { get; private set; }
 
     /// <summary>
-    ///   Creates a colony for a microbe, with this microbe as the master,
+    ///   Creates a colony for a microbe, with the given microbe as the master,
     ///   and handles related updates (like microbe's colony and access to the editor button).
     /// </summary>
     /// <remarks> Should be used instead of the colony constructor, unless for loading from Json.</remarks>
