d863def518fd8fcf7f46635a73355892646f3651
adQuid <28126232+adQuid@users.noreply.github.com>
2021-09-01 20:03:26 -0400

    update to account for other factors

--- src/auto-evo/simulation/food_source/FloatingChunkFoodSource.cs
+++ src/auto-evo/simulation/food_source/FloatingChunkFoodSource.cs
@@ -7,12 +7,14 @@ public class FloatingChunkFoodSource : FoodSource
     private readonly Compound glucose = SimulationParameters.Instance.GetCompound("glucose");
 
     private Patch patch;
+    private BiomeConditions biomeConditions;
     private float totalEnergy;
     private float chunkSize;
 
     public FloatingChunkFoodSource(Patch patch)
     {
         this.patch = patch;
+        biomeConditions = patch.Biome;
 
         if (patch.Biome.Chunks.ContainsKey("marineSnow"))
         {
@@ -27,6 +29,11 @@ public class FloatingChunkFoodSource : FoodSource
         var microbeSpecies = (MicrobeSpecies)species;
 
         var predatorSize = microbeSpecies.Organelles.Organelles.Sum(organelle => organelle.Definition.HexCount);
+        if (microbeSpecies.IsBacteria)
+        {
+            predatorSize /= 2;
+        }
+
         var predatorSpeed = microbeSpecies.BaseSpeed;
         predatorSpeed += ProcessSystem
             .ComputeEnergyBalance(microbeSpecies.Organelles.Organelles, patch.Biome,
@@ -34,10 +41,15 @@ public class FloatingChunkFoodSource : FoodSource
 
         var score = predatorSpeed * species.Activity;
 
-        if (predatorSize < chunkSize * Constants.ENGULF_SIZE_RATIO_REQ)
+        // If the species can't engulf, then they are dependent on only eating the runoff compounds
+        if (microbeSpecies.MembraneType.CellWall || predatorSize < chunkSize * Constants.ENGULF_SIZE_RATIO_REQ)
         {
             score *= Constants.AUTO_EVO_CHUNK_LEAK_MULTIPLIER;
         }
+
+        score /= ProcessSystem.ComputeEnergyBalance(
+            microbeSpecies.Organelles.Organelles,
+            biomeConditions, microbeSpecies.MembraneType).FinalBalanceStationary;
 
         return score;
     }
