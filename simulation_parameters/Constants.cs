ff00ca794f2817543878ccce2bed5e8d53cceee1
adQuid <28126232+adQuid@users.noreply.github.com>
2021-09-01 20:51:00 -0400

    halfed energy from floating chunks

--- simulation_parameters/Constants.cs
+++ simulation_parameters/Constants.cs
@@ -425,7 +425,7 @@ public static class Constants
     public const float AUTO_EVO_PREDATION_ENERGY_MULTIPLIER = 0.4f;
     public const float AUTO_EVO_SUNLIGHT_ENERGY_AMOUNT = 100000;
     public const float AUTO_EVO_COMPOUND_ENERGY_AMOUNT = 100;
-    public const float AUTO_EVO_CHUNK_ENERGY_AMOUNT = 100;
+    public const float AUTO_EVO_CHUNK_ENERGY_AMOUNT = 50;
 
     public const float GLUCOSE_REDUCTION_RATE = 0.8f;
     public const float GLUCOSE_MIN = 0.0f;
