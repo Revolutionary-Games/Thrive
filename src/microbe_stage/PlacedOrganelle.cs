158a3dc3669dd491c9a925af8d195148fdfbfd21
Kemikal1 <alex_goku2007@yahoo.com>
2021-09-13 16:54:07 +0300

    Update PlacedOrganelle.cs

--- src/microbe_stage/PlacedOrganelle.cs
+++ src/microbe_stage/PlacedOrganelle.cs
@@ -45,6 +45,7 @@ public class PlacedOrganelle : Spatial, IPositionedOrganelle, ISaveLoadedTracked
     }
 
     public OrganelleDefinition Definition { get; set; }
+
     public Hex Position { get; set; }
 
     public int Orientation { get; set; }
@@ -197,6 +198,7 @@ public class PlacedOrganelle : Spatial, IPositionedOrganelle, ISaveLoadedTracked
     {
         if (Definition == null)
             throw new Exception("PlacedOrganelle has no definition set");
+
         if (ParentMicrobe != null)
             throw new InvalidOperationException("PlacedOrganelle is already in a microbe");
 
