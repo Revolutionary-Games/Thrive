1a6509af61ad0a71b86099f3503d720e1624307e
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-11-05 11:09:46 +0200

    Major auto-evo framework updates for species splitting (#2689)
    * Added species splitting, new species splitting when low number, and auto-evo for player
    
    also disabled the mutated copy of player appearing each time
    
    * Add sanity check to make sure player species is marked as the player species
    
    * Don't have microbe editor cheat menu visible in the godot editor
    
    * Fix warning from C# 9 upgrade
    
    * Fixed behaviour tab contents being set as visible in Godot editor
    
    * Added panel showing the predicted auto-evo results in the editor
    
    * Added testing tutorial for auto-evo prediction panel
    
    needs proper triggering logic
    
    * Fixed up the trigger condition for the auto-evo prediction tutorial
    
    * Fixed comment sentence ending abruptly and incorrectly hidden GUI element

---
+++ src/auto-evo/EditorAutoEvoRun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using AutoEvo;
+
+/// <summary>
+///   Custom auto-evo run for use in editor simulations to predict population numbers given specified changes
+/// </summary>
+public class EditorAutoEvoRun : AutoEvoRun
+{
+    public EditorAutoEvoRun(GameWorld world, Species originalEditedSpecies, Species modifiedProperties) : base(world)
+    {
+        OriginalEditedSpecies = originalEditedSpecies;
+        ModifiedProperties = modifiedProperties;
+    }
+
+    public Species OriginalEditedSpecies { get; }
+    public Species ModifiedProperties { get; }
+
+    protected override void GatherInfo(Queue<IRunStep> steps)
+    {
+        // Custom run setup for editor's use
+        var map = Parameters.World.Map;
+
+        steps.Enqueue(new CalculatePopulation(map, new List<Species> { ModifiedProperties },
+            new List<Species> { OriginalEditedSpecies }) { CanRunConcurrently = false });
+
+        AddPlayerSpeciesPopulationChangeClampStep(steps, map,
+            OriginalEditedSpecies.PlayerSpecies ? ModifiedProperties : null, OriginalEditedSpecies);
+    }
+}
