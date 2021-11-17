91a5a25e539dfa5dbe4503b081bb651a2a39b798
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-11-17 19:29:43 +0200

    Support for store versions of thrive (#2720)
    * Refactored Thrive version reader to a separate file
    
    * Trying to add script for downloading godot steam templates
    
    * Disable the GodotSteam download as it doesn't include mono support
    
    * Add note about checking GodotSteam compatibility
    
    * Improve windows ruby setup instructions
    
    * Fix StyleCop version upgrade from missing other directory build props
    
    * Change the stylecop version in main csproj to be consistent
    
    * Added basic general code side for Steam integration
    
    * Added specifically enableable Steam build helpers and client code
    
    * Moved SteamHandler.cs PauseMode set to _Ready
    
    * Added the new fullscreen state to the print for the key that changes it
    
    * Added label showing current steam user name if loaded
    
    also made the steam error translatable
    
    * Added special steam build mode and export presets for it
    
    * Change paths for steam export on Windows
    
    * Implemented some more Steam callbacks, game now quits when requested from Steam
    
    * Moved the logged in steam user label to be underneath the main menus
    
    * Added a warning note message to make_release script when making Steam release
    
    * Fixed licenses display option not working in Steam version
    
    * Fixed the credits for the Steam version
    
    * Added "crossfade" to dictionary
    
    * Update make_release.rb
    
    * Update src/general/MainMenu.tscn
    
    * Update src/steam/SteamClient.cs
    
    * Changed ConnectSignals to use generics to show that the interface is required
    
    * Update src/steam/ISteamClient.cs
    
    * Change the steam license readme wording

---
+++ scripts/disable_steam_build.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative 'steam_version_helpers'
+
+disable_steam_build
---
+++ src/steam/ISteamClient.cs
@@ -0,0 +1,31 @@
+﻿using Godot;
+
+/// <summary>
+///   Interface specifying the Steam operations we use. Implemented in the SteamClient.cs file
+/// </summary>
+public interface ISteamClient : ISteamSignalReceiver
+{
+    /// <summary>
+    ///   True if the Steam client has been initialized
+    /// </summary>
+    bool IsLoaded { get; }
+
+    string DisplayName { get; }
+
+    void Init();
+
+    /// <summary>
+    ///   Sets up receiver to receive callbacks
+    /// </summary>
+    /// <param name="receiver">The receiver</param>
+    /// <typeparam name="T">Typeof the receiver object</typeparam>
+    /// <remarks>
+    ///   <para>
+    ///     Note that for many of the received signals they need to be forwarded to this object for further processing.
+    ///   </para>
+    /// </remarks>
+    void ConnectSignals<T>(T receiver)
+        where T : Object, ISteamSignalReceiver;
+
+    void Process(float delta);
+}
