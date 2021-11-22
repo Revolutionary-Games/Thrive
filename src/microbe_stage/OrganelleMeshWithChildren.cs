8bf9e7e24b7980c41c529d8b15c04309dda51be9
Jan Führer <jan_fue@icloud.com>
2021-11-22 12:18:41 +0100

    refactored submesh system

---
+++ src/microbe_stage/OrganelleMeshWithChildren.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Godot;
+using Godot.Collections;
+
+/// <summary>
+///   Applies the tint to the defined children
+/// </summary>
+public class OrganelleMeshWithChildren : MeshInstance
+{
+    public void SetTintOfChildren(Color value)
+    {
+        foreach (GeometryInstance mesh in GetChildren())
+        {
+            if (mesh.MaterialOverride is ShaderMaterial shaderMaterial)
+            {
+                shaderMaterial.SetShaderParam("tint", value);
+            }
+        }
+    }
+}
