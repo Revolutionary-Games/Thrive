e9cac008de7a97ebe20f08b75b3b8f71f903456e
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-11-25 19:18:47 +0200

    Added tutorial for trying to stay small and to check the help menu (#2849)
    also added the mouse to zoom control in the help menu, also refactored a common class out of nth editor entry tutorial

---
+++ src/tutorial/microbe_stage/CheckTheHelpMenu.cs
@@ -0,0 +1,51 @@
+﻿namespace Tutorial
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class CheckTheHelpMenu : TutorialPhase
+    {
+        public const string TUTORIAL_NAME = "CheckTheHelpMenu";
+
+        private const int TriggersOnNthSwimmingSession = 3;
+
+        public CheckTheHelpMenu()
+        {
+            CanTrigger = false;
+        }
+
+        public override string ClosedByName => TUTORIAL_NAME;
+
+        [JsonProperty]
+        public int NumberOfMicrobeStageEntries { get; set; }
+
+        public override void ApplyGUIState(MicrobeTutorialGUI gui)
+        {
+            gui.CheckTheHelpMenuVisible = ShownCurrently;
+        }
+
+        public override bool CheckEvent(TutorialState overallState, TutorialEventType eventType, EventArgs args,
+            object sender)
+        {
+            switch (eventType)
+            {
+                case TutorialEventType.EnteredMicrobeStage:
+                {
+                    if (!HasBeenShown)
+                    {
+                        ++NumberOfMicrobeStageEntries;
+                        if (NumberOfMicrobeStageEntries >= TriggersOnNthSwimmingSession &&
+                            !overallState.TutorialActive())
+                        {
+                            Show();
+                        }
+                    }
+
+                    break;
+                }
+            }
+
+            return false;
+        }
+    }
+}
