127afc615dd82a1db7ead194cc411fb42f3054c2
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-11-28 20:01:12 +0200

    Add note about cache data becoming outdated if moved elsewhere

--- src/auto-evo/simulation/SimulationCache.cs
+++ src/auto-evo/simulation/SimulationCache.cs
@@ -5,6 +5,12 @@
     /// <summary>
     ///   Caches some information in auto-evo runs to speed them up
     /// </summary>
+    /// <remarks>
+    ///   <para>
+    ///     Some information will get outdated when data that the auto-evo relies on changes. If in the future
+    ///     caching is moved to a higher level in the auto-evo, that needs to be considered.
+    ///   </para>
+    /// </remarks>
     public class SimulationCache
     {
         private readonly Dictionary<(MicrobeSpecies, Patch), EnergyBalanceInfo> cachedEnergyBalances = new();
