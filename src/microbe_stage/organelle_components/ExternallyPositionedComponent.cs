3d6b7d4096711447c8ac9365cfdd61472c604969
Kemikal1 <alex_goku2007@yahoo.com>
2021-12-05 20:11:10 +0200

    appeasement

--- src/microbe_stage/organelle_components/ExternallyPositionedComponent.cs
+++ src/microbe_stage/organelle_components/ExternallyPositionedComponent.cs
@@ -26,6 +26,7 @@ public abstract class ExternallyPositionedComponent : IOrganelleComponent
     {
         this.organelle = organelle;
         organellePos = Hex.AxialToCartesian(organelle.Position);
+
         CustomAttach();
     }
 
