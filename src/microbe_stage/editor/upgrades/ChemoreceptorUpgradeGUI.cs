df76bd0c187f7772e243c1bfa7e459459921dfe7
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-01-27 09:06:05 +0200

    Add chemoreceptor organelle (#3023)
    * Added chemoreceptor as a placeable organelle
    
    doesn't perform any function yet
    
    * Fixed venv enable instructions for Windows
    
    * Added a todo comment for fluid system time saving
    
    * Chemoreceptor lines pointing to compounds now works
    
    * Implemented organelle upgrade menu and made it able to configure chemoreceptor
    
    * Fixed a few issues and tweaked the chemoreceptor code a bit

---
+++ src/microbe_stage/editor/upgrades/ChemoreceptorUpgradeGUI.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+public class ChemoreceptorUpgradeGUI : VBoxContainer, IOrganelleUpgrader
+{
+    [Export]
+    public NodePath CompoundsPath;
+
+    [Export]
+    public NodePath MaximumDistancePath;
+
+    [Export]
+    public NodePath MinimumAmountPath;
+
+    [Export]
+    public NodePath ColourPath;
+
+    private OptionButton compounds;
+    private Slider maximumDistance;
+    private Slider minimumAmount;
+    private TweakedColourPicker colour;
+
+    private List<Compound> shownChoices;
+    private OrganelleTemplate storedOrganelle;
+
+    public override void _Ready()
+    {
+        compounds = GetNode<OptionButton>(CompoundsPath);
+        maximumDistance = GetNode<Slider>(MaximumDistancePath);
+        minimumAmount = GetNode<Slider>(MinimumAmountPath);
+        colour = GetNode<TweakedColourPicker>(ColourPath);
+
+        compounds.Clear();
+
+        maximumDistance.MinValue = Constants.CHEMORECEPTOR_RANGE_MIN;
+        maximumDistance.MaxValue = Constants.CHEMORECEPTOR_RANGE_MAX;
+
+        minimumAmount.MinValue = Constants.CHEMORECEPTOR_AMOUNT_MIN;
+        minimumAmount.MaxValue = Constants.CHEMORECEPTOR_AMOUNT_MAX;
+    }
+
+    public void OnStartFor(OrganelleTemplate organelle)
+    {
+        storedOrganelle = organelle;
+        shownChoices = SimulationParameters.Instance.GetCloudCompounds();
+
+        foreach (var choice in shownChoices)
+        {
+            compounds.AddItem(choice.Name);
+        }
+
+        // Select glucose by default
+        var defaultCompoundIndex =
+            shownChoices.FindIndex(c => c.InternalName == Constants.CHEMORECEPTOR_DEFAULT_COMPOUND_NAME);
+
+        if (defaultCompoundIndex < 0)
+            defaultCompoundIndex = 0;
+
+        // Apply current upgrade values or defaults
+        if (organelle.Upgrades?.CustomUpgradeData is ChemoreceptorUpgrades configuration)
+        {
+            compounds.Selected = shownChoices.FindIndex(c => c == configuration.TargetCompound);
+            maximumDistance.Value = configuration.SearchRange;
+            minimumAmount.Value = configuration.SearchAmount;
+            colour.Color = configuration.LineColour;
+        }
+        else
+        {
+            compounds.Selected = defaultCompoundIndex;
+            maximumDistance.Value = Constants.CHEMORECEPTOR_RANGE_DEFAULT;
+            minimumAmount.Value = Constants.CHEMORECEPTOR_AMOUNT_DEFAULT;
+            colour.Color = Colors.White;
+        }
+    }
+
+    public void ApplyChanges(MicrobeEditor editor)
+    {
+        // Force some compound to be selected
+        if (compounds.Selected == -1)
+            compounds.Selected = 0;
+
+        // TODO: make an undoable action
+        storedOrganelle.SetCustomUpgradeObject(new ChemoreceptorUpgrades
+        {
+            TargetCompound = shownChoices[compounds.Selected],
+            SearchRange = (float)maximumDistance.Value,
+            SearchAmount = (float)minimumAmount.Value,
+            LineColour = colour.Color,
+        });
+    }
+}
---
+++ src/microbe_stage/editor/upgrades/OrganelleUpgradeGUI.cs
@@ -0,0 +1,61 @@
+﻿using Godot;
+
+public class OrganelleUpgradeGUI : Control
+{
+    [Export]
+    public NodePath PopupPath;
+
+    [Export]
+    public NodePath OrganelleSpecificContentPath;
+
+    [Export]
+    public NodePath ScrollContainerPath;
+
+    private CustomConfirmationDialog popup;
+    private Container organelleSpecificContent;
+    private ScrollContainer scrollContainer;
+
+    private MicrobeEditor storedEditor;
+    private IOrganelleUpgrader upgrader;
+
+    public override void _Ready()
+    {
+        popup = GetNode<CustomConfirmationDialog>(PopupPath);
+        organelleSpecificContent = GetNode<Container>(OrganelleSpecificContentPath);
+        scrollContainer = GetNode<ScrollContainer>(ScrollContainerPath);
+    }
+
+    public void OpenForOrganelle(OrganelleTemplate organelle, string upgraderScene, MicrobeEditor editor)
+    {
+        var scene = GD.Load<PackedScene>(upgraderScene);
+
+        if (scene == null)
+        {
+            GD.PrintErr($"Failed to load upgrader scene for organelle of type {organelle.Definition.InternalName}");
+            return;
+        }
+
+        var instance = scene.Instance();
+        upgrader = (IOrganelleUpgrader)instance;
+
+        organelleSpecificContent.FreeChildren();
+        organelleSpecificContent.AddChild(instance);
+
+        popup.PopupCenteredShrink();
+
+        scrollContainer.ScrollVertical = 0;
+        upgrader.OnStartFor(organelle);
+        storedEditor = editor;
+    }
+
+    private void OnAccept()
+    {
+        GUICommon.Instance.PlayButtonPressSound();
+        upgrader.ApplyChanges(storedEditor);
+    }
+
+    private void OnCancel()
+    {
+        GUICommon.Instance.PlayButtonPressSound();
+    }
+}
