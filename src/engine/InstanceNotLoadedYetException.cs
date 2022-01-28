cb1a2373faede1863beaf6cd5379f1b287f84605
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-01-28 19:06:15 +0200

    Switch InstanceNotLoadedYetException to be allowed from a property

--- src/engine/InstanceNotLoadedYetException.cs
+++ src/engine/InstanceNotLoadedYetException.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 ///   Thrown when trying to access a static instance that has not been loaded yet
 /// </summary>
 [Serializable]
-public class InstanceNotLoadedYetException : Exception
+public class InstanceNotLoadedYetException : InvalidOperationException
 {
     public InstanceNotLoadedYetException() { }
 
