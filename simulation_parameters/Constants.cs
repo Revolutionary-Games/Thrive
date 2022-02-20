2d300d56e02d7d2837d17c04443fc1a7fa4e83c3
Kasterisk <54026083+Kasterisk@users.noreply.github.com>
2022-02-20 12:45:16 +0700

    Increased cloud spawn cheat density for easier debugging

--- simulation_parameters/Constants.cs
+++ simulation_parameters/Constants.cs
@@ -51,7 +51,7 @@ public static class Constants
     // Should be the same as its counterpart in shaders/CompoundCloudPlane.shader
     public const float CLOUD_NOISE_UV_OFFSET_MULTIPLIER = 2.5f;
 
-    public const float CLOUD_CHEAT_DENSITY = 16000.0f;
+    public const float CLOUD_CHEAT_DENSITY = 1600000.0f;
 
     public const int MEMBRANE_RESOLUTION = 10;
 
