279d7d319f86d81abd992603263df12c74d833a4
Kemikal1 <alex_goku2007@yahoo.com>
2022-03-03 15:01:02 +0200

    fix ver chg

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@
 
 [assembly: AssemblyVersion("0.5.8.0")]
 
-[assembly: AssemblyInformationalVersion("alpha")]
+[assembly: AssemblyInformationalVersion("-alpha")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
