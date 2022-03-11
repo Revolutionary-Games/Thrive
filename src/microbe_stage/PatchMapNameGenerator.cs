87bf6a990f48d86ca6f721eb0d6eeb3986d82398
Kemikal1 <alex_goku2007@yahoo.com>
2022-03-11 17:02:47 +0200

    next function

--- src/microbe_stage/PatchMapNameGenerator.cs
+++ src/microbe_stage/PatchMapNameGenerator.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Godot;
-
+using System.Linq;
 public class PatchMapNameGenerator : IRegistryType
 {
     private int syllabelsLowerLimit;
@@ -13,6 +13,7 @@ public class PatchMapNameGenerator : IRegistryType
     private List<string> syllabels = null!;
     [JsonRequired]
     private List<string> sufixes = null!;
+    private string vowels = "aeiou";
     public string InternalName{ get; set; } = null!;
     public PatchMapNameGenerator(int lowerLimit, int higherLimit)
     {
@@ -27,9 +28,38 @@ public class PatchMapNameGenerator : IRegistryType
     {
         return patchName;
     }
-    public void Next()
+    public string Next()
     {
-        
+        Random random = new Random();
+
+        int nameLength = random.Next(syllabelsLowerLimit, syllabelsHigherLimit + 1);
+        string name = "";
+        int sufixIndex;
+
+        for (int i = 0; i < nameLength; i++)
+        {
+            int syllabelsIndex = random.Next(0,syllabels.Count);
+            name += syllabels [syllabelsIndex];
+        }
+
+        if (vowels.Contains(name[name.Length - 1]))
+        {
+            sufixIndex = 1 + 2 * random.Next(0, 2);
+        }
+        else
+        {
+            sufixIndex = 2 + 2 * random.Next(0, 2);
+        }
+
+        name += sufixes[sufixIndex];
+
+        // Convert first letter to uppercase
+        char[] charName = name.ToCharArray();
+        charName[0] = Char.ToUpper(name[0]);
+        name = charName.ToString();
+
+        return name;
+
     }
     public void Check(string name)
     {
