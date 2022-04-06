4d09fb99a9cefdda01d56405e9ec2da789292811
Kemikal1 <alex_goku2007@yahoo.com>
2022-04-06 09:23:20 +0300

    Colony mass and speed (#2693)
    * Added mass correctly
    
    * Added flagella movement of children.
    
    * Update MovementComponent.cs
    
    * Revert "Update MovementComponent.cs"
    
    This reverts commit b607edc72a98932c4b31332288b97680cf00feda.
    
    * Revert "Added flagella movement of children."
    
    This reverts commit 0e1e62f060b5d3c778a2d02eaa1c838fe3af032a.
    
    * Flagella movement.
    
    * format
    
    * format
    
    * format
    
    * for....mat...
    
    * f...
    
    * format
    
    * form
    
    * null prop
    
    * Update src/microbe_stage/Microbe.cs
    
    * Update src/microbe_stage/organelle_components/MovementComponent.cs
    
    * format
    
    * Update Microbe.cs
    
    * Some refactor using newly merged code
    
    * Update src/microbe_stage/PlacedOrganelle.cs
    
    * appeased concerns
    
    * more appeasement
    
    * format
    
    * format
    
    * Fixed style issues and a typo
    
    * Fix the jitt
    
    * format
    
    * corrected movement direction
    
    * Created Update Step
    
    * format and prints deleting.
    
    * format
    
    * corrected savestep and mass from unbind
    
    * cleanup
    
    * clean
    
    * clena
    
    * version chg
    
    * format
    
    * Update export_presets.cfg
    
    * fix ver chg
    
    * mass loading from json
    
    * revert
    
    * f
    
    * reverted save step
    
    * format appeasement
    
    * cleaning
    
    * some fix
    
    * fixed duplicated mass from organelles
    
    * format

--- src/microbe_stage/organelle_components/MovementComponent.cs
+++ src/microbe_stage/organelle_components/MovementComponent.cs
@@ -105,9 +105,12 @@ public class MovementComponent : ExternallyPositionedComponent
         // The movementDirection is the player or AI input
         Vector3 direction = microbe.MovementDirection;
 
-        var forceMagnitude = force.Dot(direction);
+        // Real force the flagella applied to the colony (considering rotation)
+        var realForce = organelle!.RotatedPositionInsideColony(force);
+        var forceMagnitude = realForce.Dot(direction);
+
         if (forceMagnitude <= 0 || direction.LengthSquared() < MathUtils.EPSILON ||
-            force.LengthSquared() < MathUtils.EPSILON)
+            realForce.LengthSquared() < MathUtils.EPSILON)
         {
             if (movingTail)
             {
@@ -140,7 +143,14 @@ public class MovementComponent : ExternallyPositionedComponent
             forceMagnitude / 100.0f;
 
         // Rotate the 'thrust' based on our orientation
-        direction = microbe.Transform.basis.Xform(direction);
+        if (microbe.Colony?.Master == null)
+        {
+            direction = microbe.Transform.basis.Xform(direction);
+        }
+        else
+        {
+            direction = microbe.Colony.Master.Transform.basis.Xform(direction);
+        }
 
         SetSpeedFactor(animationSpeed);
 
