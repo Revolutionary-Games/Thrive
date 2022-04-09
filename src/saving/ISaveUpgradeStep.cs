9aec0f5af90dfb16a1c9a2ed13d02b9afe8edb47
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-04-09 12:35:02 +0300

    Fixed save upgrade for 0.5.7 to 0.5.8

--- src/saving/ISaveUpgradeStep.cs
+++ src/saving/ISaveUpgradeStep.cs
@@ -177,12 +177,14 @@
         }
     }
 
-    internal class UpgradeStep057To058 : BaseJSONUpgradeStep
+    internal class UpgradeStep057To058 : BaseRecursiveJSONWalkerStep
     {
         protected override string VersionAfter => "0.5.8.0-alpha";
 
         protected override void PerformUpgradeOnJSON(JObject saveData)
         {
+            base.PerformUpgradeOnJSON(saveData);
+
             // We are just interested in updating the microbe editor properties (if it is present)
             var editor = saveData.GetValue("MicrobeEditor") as JObject;
 
@@ -258,6 +260,25 @@
                 ["actions"] = new JArray(),
                 ["actionIndex"] = 0,
             };
+        }
+
+        protected override void CheckAndUpdateProperty(JProperty property)
+        {
+            if (property.Name is "organelles" or "Organelles" or "editedMicrobeOrganelles" &&
+                property.Value.Type == JTokenType.Object)
+            {
+                var asObject = (JObject)property.Value;
+
+                // Organelle layout has Organelles key which we want to update to the new hex layout
+                if (asObject.TryGetValue("Organelles", out var organelleList))
+                {
+                    if (organelleList.Type != JTokenType.Array)
+                        return;
+
+                    asObject.Remove("Organelles");
+                    asObject["existingHexes"] = organelleList;
+                }
+            }
         }
     }
 
