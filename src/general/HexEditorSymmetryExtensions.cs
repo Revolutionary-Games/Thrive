9b506e785159138aef5d133f19fe050ed1086564
Jan <jan_fue@icloud.com>
2022-05-06 12:52:15 +0200

    Dynamic MP calculation (#2445)
    * added Interference logic
    
    * moved cost calculation to Action class
    
    * more MP calculation changes
    
    * more removal of the old system
    
    * fixed crashes
    
    * debug MP bar
    
    * combine method for MembraneActionData
    
    * handled more edge-cases
    
    * basic interferiance alghorithm
    
    * sum up the MP
    
    * fixed interference
    
    * bugfixes
    
    * huge performance boost
    
    * bugfix for undo and redo
    
    * check for negative mp
    
    * block action when not enough mp
    
    * removed old changes
    
    * code fixes
    
    * shortened long line
    
    * fixed loading
    
    * fixed history
    
    * fixed replacedCytoplasma
    
    * moved action call
    
    * removed redundant clone
    
    * Removed redundant brackets
    
    * ugly fix
    
    * fixed replacing cyto
    
    * removed redundant using
    
    * modified range check
    
    * renamed vars
    
    * moved to own fiels
    
    * made CombineGuaranteed abstract
    
    * added comments
    
    * made switch expr
    
    * using System
    
    * fixed bug
    
    * fixed CI issues
    
    * fixed symmetry mode
    
    * reduced code duplication
    
    * removed mp check in freebuild
    
    * fixed highlighting while moving
    
    * fixed ci
    
    * fixed mp calculation updating
    
    * improved replaced cytoplasm
    
    * fixed mp bar when replacing cytoplasm
    
    * improved cytoplasm replacement logic
    
    * removed exception when invalid mp
    
    * improved debug output format
    
    * fixed ci problems
    
    * further improvements to cytoplasm replacement
    
    * fixed line breaks
    
    * fixed bugs with combination logic
    
    * handled even more edge cases
    
    * removed blank line
    
    * added many docs
    
    * symmetry move
    
    * fixed indentation
    
    * block symmetry button when moving
    
    * added disabled texture for symmetry button
    
    * show sum of mp costs
    
    * updated localizations
    
    * fixed another bug...
    
    * Removed GLES2 mode
    
    * "Reset locales to master"
    
    * reverted tscn changes
    
    * repaired MicrobeEditor.tscn
    
    * repaired MicrobeEdior.cs
    
    * updated localization
    
    * fixed tuple capitaization
    
    * Updated RubySetupSystem
    
    * updated localization
    
    * complied to nullable requirements
    
    * fixed disabled button import params
    
    * removed subActions
    
    * Many bugfixes
    General code improvements
    Treat actions in symmetry mode as a single action
    Even more bugfixes
    
    * disallowed nulls in SelectedOrganelles in popup
    
    * reduced coupling
    
    * reimplemented placed this session material
    
    * shortened long lines
    
    * take orientation into account
    
    * Hid actions a bit more
    
    Renamed ActionData
    
    * Code style improvements
    
    * set cache to null instead of recalculating in undo and redo
    
    * simplified access to SelectedOrganelles
    
    * added ResetsHistory flag to editor actions
    
    * moved setting of GetActionPrice
    
    * renamed button status to button enabled
    
    * Changed ArgumentException to InvalidOperationException in OnMovePressed
    
    Co-authored-by: Henri Hyyryläinen <hhyyrylainen@revolutionarygamesstudio.com>
    
    * improved mutation points cache handling
    
    * added summary to CombinableActionData
    
    * put para in MicrobeActionInterferanceMode on its own line
    
    * renamed MicrobeActionInterferenceMode
    
    * added self call check to GetInterferenceModeWith
    
    * Changed Excepion types
    
    Co-authored-by: Henri Hyyryläinen <hhyyrylainen@revolutionarygamesstudio.com>
    
    * renamed MultiMicrobeEditorAction
    
    * fixed crash and wrong blocked detection
    
    * fixed bug with overlapping movements
    
    * moved putting the main element to the head to caller of ShowOrganelleMenu
    
    * renamed RemoveOrganelle method
    
    * changed float to int for mpCost
    
    * added DiscardNulls function
    
    * removed many unneccecary DiscardNulls calls
    
    * simplified movement and removal calling
    
    * fixed CI problems
    
    * Updated translations
    
    * Fixed save loading not working
    
    * Fixed formatting detected issues
    
    Co-authored-by: Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
    Co-authored-by: Henri Hyyryläinen <hhyyrylainen@revolutionarygamesstudio.com>

---
+++ src/general/HexEditorSymmetryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class HexEditorSymmetryExtensions
+{
+    public static int PositionCount(this HexEditorSymmetry symmetry)
+    {
+        switch (symmetry)
+        {
+            case HexEditorSymmetry.None:
+                return 1;
+            case HexEditorSymmetry.XAxisSymmetry:
+                return 2;
+            case HexEditorSymmetry.FourWaySymmetry:
+                return 4;
+            case HexEditorSymmetry.SixWaySymmetry:
+                return 6;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(symmetry), symmetry, null);
+        }
+    }
+}
