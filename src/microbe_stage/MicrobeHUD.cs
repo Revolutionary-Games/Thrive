92a8ce034f53b954c485b47100a8d14ce31e5019
Oliveriver <oliveriver1@gmail.com>
2022-06-01 19:03:46 +0100

    Pause hotkey (#3363)
    * Test implementation of pause hotkey
    
    * More tweaks
    
    * Unpause prompt now displays correctly, plus translations and input option
    
    * Fix misplaced resource
    
    * Another tscn file fix
    
    * And another...
    
    * Set text in scene file rather than code
    
    * Disable pause if other buttons have focus
    
    * Fix translation merge issues
    
    * Remove visible pause menu that slipped in
    
    * Update translations
    
    * Review change

--- src/microbe_stage/MicrobeHUD.cs
+++ src/microbe_stage/MicrobeHUD.cs
@@ -200,6 +200,12 @@ public class MicrobeHUD : Control
     [Export]
     public NodePath MicrobeControlRadialPath = null!;
 
+    [Export]
+    public NodePath PausePromptPath = null!;
+
+    [Export]
+    public NodePath PauseInfoPath = null!;
+
     // Formatter and code checks disagree here
     // ReSharper disable RedundantNameQualifier
     private readonly System.Collections.Generic.Dictionary<Species, int> hoveredSpeciesCounts = new();
@@ -296,6 +302,9 @@ public class MicrobeHUD : Control
 
     private ProcessPanel processPanel = null!;
     private TextureButton processPanelButton = null!;
+
+    private Control pausePrompt = null!;
+    private CustomRichTextLabel pauseInfo = null!;
 
     /// <summary>
     ///   Access to the stage to retrieve information for display as
@@ -439,6 +448,9 @@ public class MicrobeHUD : Control
         processPanel = GetNode<ProcessPanel>(ProcessPanelPath);
         processPanelButton = GetNode<TextureButton>(ProcessPanelButtonPath);
 
+        pausePrompt = GetNode<Control>(PausePromptPath);
+        pauseInfo = GetNode<CustomRichTextLabel>(PauseInfoPath);
+
         OnAbilitiesHotBarDisplayChanged(Settings.Instance.DisplayAbilitiesHotBar);
         Settings.Instance.DisplayAbilitiesHotBar.OnChanged += OnAbilitiesHotBarDisplayChanged;
 
@@ -467,6 +479,7 @@ public class MicrobeHUD : Control
         multicellularButton.Visible = false;
         macroscopicButton.Visible = false;
 
+        UpdatePausePrompt();
         UpdateEnvironmentPanelState();
         UpdateCompoundsPanelState();
     }
@@ -729,6 +742,36 @@ public class MicrobeHUD : Control
         // TODO: pressure?
     }
 
+    public void PauseButtonPressed()
+    {
+        if (menu.Visible)
+            return;
+
+        GUICommon.Instance.PlayButtonPressSound();
+
+        paused = !paused;
+        if (paused)
+        {
+            pauseButton.Hide();
+            resumeButton.Show();
+            pausePrompt.Show();
+            pauseButton.Pressed = false;
+
+            // Pause the game
+            PauseManager.Instance.AddPause(nameof(MicrobeHUD));
+        }
+        else
+        {
+            pauseButton.Show();
+            resumeButton.Hide();
+            pausePrompt.Hide();
+            resumeButton.Pressed = false;
+
+            // Unpause the game
+            PauseManager.Instance.Resume(nameof(MicrobeHUD));
+        }
+    }
+
     /// <summary>
     ///   Updates the GUI bars to show only needed compounds
     /// </summary>
@@ -761,6 +804,11 @@ public class MicrobeHUD : Control
                 bar.Hide();
             }
         }
+    }
+
+    private void UpdatePausePrompt()
+    {
+        pauseInfo.ExtendedBbcode = TranslationServer.Translate("PAUSE_PROMPT");
     }
 
     private void UpdateEnvironmentPanelState()
@@ -1277,31 +1325,6 @@ public class MicrobeHUD : Control
     {
         GUICommon.Instance.PlayButtonPressSound();
         menu.Open();
-    }
-
-    private void PauseButtonPressed()
-    {
-        GUICommon.Instance.PlayButtonPressSound();
-
-        paused = !paused;
-        if (paused)
-        {
-            pauseButton.Hide();
-            resumeButton.Show();
-            pauseButton.Pressed = false;
-
-            // Pause the game
-            PauseManager.Instance.AddPause(nameof(MicrobeHUD));
-        }
-        else
-        {
-            pauseButton.Show();
-            resumeButton.Hide();
-            resumeButton.Pressed = false;
-
-            // Unpause the game
-            PauseManager.Instance.Resume(nameof(MicrobeHUD));
-        }
     }
 
     private void CompoundButtonPressed()
