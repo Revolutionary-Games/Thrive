f4ac1e42981382584d93caca0cf788dbe2d554c5
adQuid <28126232+adQuid@users.noreply.github.com>
2022-06-05 16:36:27 -0400

    linter appeasement

--- src/microbe_stage/MicrobeAI.cs
+++ src/microbe_stage/MicrobeAI.cs
@@ -237,7 +237,7 @@ public class MicrobeAI
                     MoveToLocation(player.GlobalTransform.origin);
                     return;
                 }
-                
+
                 hasBeenNearPlayer = true;
             }
         }
