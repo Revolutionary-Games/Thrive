44595f2842d71e82f3ed3d2dd39df5a56e0dee62
andeason <65811631+andeason@users.noreply.github.com>
2022-06-12 03:16:49 -0500

    Add GUI Control Highlighting for editor button available tutorial (#3388)
    * Setup Button highlight for cell editor.
    
    * Changed for style.
    
    * need to re-add
    
    * Add space.
    
    * Fixed to hide editor button tutorial if player dies.
    
    * Update src/microbe_stage/MicrobeHUD.cs
    
    * Should Hopefully address issues

--- src/microbe_stage/MicrobeHUD.cs
+++ src/microbe_stage/MicrobeHUD.cs
@@ -775,6 +775,11 @@ public class MicrobeHUD : Control
         }
     }
 
+    public void SendEditorButtonToTutorial(TutorialState tutorialState)
+    {
+        tutorialState.MicrobePressEditorButton.PressEditorButtonControl = editorButton;
+    }
+
     /// <summary>
     ///   Updates the GUI bars to show only needed compounds
     /// </summary>
