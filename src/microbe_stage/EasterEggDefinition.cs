24a1339c886257c95d3990d1fb6487fe990a7dd6
Oliveriver <oliveriver1@gmail.com>
2022-06-16 17:51:09 +0100

    Initial work that didn't go anywhere

---
+++ src/microbe_stage/EasterEggDefinition.cs
@@ -0,0 +1,34 @@
+﻿using Godot;
+using Newtonsoft.Json;
+
+public class EasterEggDefinition : IRegistryType
+{
+    public string? DisplayScene;
+
+    public string? DisplaySceneModelPath;
+
+    public string? DisplaySceneAnimation;
+
+    [JsonIgnore]
+    public PackedScene? LoadedScene;
+
+    public string InternalName { get; set; } = null!;
+
+    public void Check(string name)
+    {
+    }
+
+    public void Resolve(SimulationParameters parameters)
+    {
+        // Preload the scene for instantiating in microbes
+        if (!string.IsNullOrEmpty(DisplayScene))
+        {
+            LoadedScene = GD.Load<PackedScene>(DisplayScene);
+        }
+    }
+
+    public void ApplyTranslations()
+    {
+        TranslationHelper.ApplyTranslations(this);
+    }
+}
