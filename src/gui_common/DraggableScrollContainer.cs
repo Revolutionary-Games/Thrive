dd0b874ea7427e4ea1886ea40c30f36335521c40
Kasterisk <54026083+Kasterisk@users.noreply.github.com>
2022-07-02 01:44:50 +0700

    Zooming fixes

--- src/gui_common/DraggableScrollContainer.cs
+++ src/gui_common/DraggableScrollContainer.cs
@@ -10,27 +10,36 @@ public class DraggableScrollContainer : ScrollContainer
     public NodePath? ContentPath;
 
     /// <summary>
-    ///   Whether we're currently scrolling, to prevent dragging.
-    /// </summary>
-    private bool scrolling;
-
-    /// <summary>
     ///   Whether we're currently dragging, to prevent us starting dragging again.
     /// </summary>
     private bool dragging;
 
     /// <summary>
-    ///   Timer used for scroll bug workaround in Godot (https://github.com/godotengine/godot/issues/22936).
+    ///   Whether we're currently zooming with mouse wheel, to prevent unwanted scroll while zooming.
     /// </summary>
-    private float scrollingTimer;
-
-    private bool showScrollbars = true;
+    private bool zooming;
 
     private Control content = null!;
-    private Vector2 contentScale = Vector2.One;
+    private bool showScrollbars = true;
+    private float contentScale = 1;
+
+    /// <summary>
+    ///   Unwanted scrolling happen when zooming with mouse wheel, this is to reset the scroll values back to its
+    ///   last state before zooming started.
+    /// </summary>
+    private Int2 lastScrollValues;
 
     [Export]
-    public float ZoomFactor { get; set; } = 1.05f;
+    public float MaxZoom { get; set; } = 2.5f;
+
+    [Export]
+    public float MinZoom { get; set; } = 0.4f;
+
+    [Export]
+    public float ZoomFactor { get; set; } = 1.0f;
+
+    [Export]
+    public float ZoomStep { get; set; } = 0.05f;
 
     /// <summary>
     ///   Controls whether the horizontal and vertical scrollbars should be shown or not. This doesn't enable nor
@@ -62,28 +71,16 @@ public class DraggableScrollContainer : ScrollContainer
 
         content = GetNode<Control>(ContentPath);
 
-        GetVScrollbar().Connect("scrolling", this, nameof(OnScrollStarted));
-        GetHScrollbar().Connect("scrolling", this, nameof(OnScrollStarted));
+        // Workaround a bug in Godot (https://github.com/godotengine/godot/issues/22936).
+        GetVScrollbar().Connect("value_changed", this, nameof(OnScrollStarted));
+        GetHScrollbar().Connect("value_changed", this, nameof(OnScrollStarted));
 
         UpdateScrollbars();
     }
 
-    public override void _Process(float delta)
-    {
-        base._Process(delta);
-
-        if (scrollingTimer > 0)
-        {
-            scrollingTimer -= delta;
-
-            if (scrollingTimer <= 0)
-                OnScrollEnded();
-        }
-    }
-
     public override void _GuiInput(InputEvent @event)
     {
-        if (!Visible || scrolling)
+        if (!Visible)
         {
             dragging = false;
             return;
@@ -107,16 +104,32 @@ public class DraggableScrollContainer : ScrollContainer
         {
             dragging = true;
         }
+        else if (@event is InputEventMouseButton buttonDown && buttonDown.Pressed)
+        {
+            if (buttonDown.ButtonIndex == (int)ButtonList.WheelUp)
+            {
+                contentScale += ZoomFactor * ZoomStep;
+                zooming = true;
+                Update();
+            }
+            else if (buttonDown.ButtonIndex == (int)ButtonList.WheelDown)
+            {
+                contentScale -= ZoomFactor * ZoomStep;
+                zooming = true;
+                Update();
+            }
+        }
         else if (@event is InputEventMouseMotion motion && dragging)
         {
-            ScrollHorizontal -= (int)(motion.Relative.x * (1 / contentScale.x));
-            ScrollVertical -= (int)(motion.Relative.y * (1 / contentScale.y));
+            // Multiplied by an inverse of the content's scale to make panning faster when zoomed out and vice versa.
+            ScrollHorizontal -= (int)(motion.Relative.x * (1 / contentScale));
+            ScrollVertical -= (int)(motion.Relative.y * (1 / contentScale));
         }
     }
 
     public override void _Input(InputEvent @event)
     {
-        if (!Visible || scrolling)
+        if (!Visible)
         {
             dragging = false;
             return;
@@ -129,36 +142,23 @@ public class DraggableScrollContainer : ScrollContainer
         {
             dragging = false;
         }
-        else if (@event is InputEventMouseButton button && button.Pressed)
-        {
-            if (button.ButtonIndex == (int)ButtonList.WheelUp)
-            {
-                contentScale *= ZoomFactor;
-                Update();
-                GetTree().SetInputAsHandled();
-            }
-            else if (button.ButtonIndex == (int)ButtonList.WheelDown)
-            {
-                contentScale *= 1 / ZoomFactor;
-                Update();
-                GetTree().SetInputAsHandled();
-            }
-        }
     }
 
     public override void _Draw()
     {
         base._Draw();
 
-        content.RectPivotOffset = new Vector2(ScrollHorizontal, ScrollVertical) + GetRect().End / 2;
+        content.RectPivotOffset = new Vector2(ScrollHorizontal, ScrollVertical) + GetRect().End * 0.5f;
+        contentScale = Mathf.Clamp(contentScale, MinZoom, MaxZoom);
+        var pairValue = new Vector2(contentScale, contentScale);
 
-        if (content.RectScale != contentScale)
-            content.RectScale = contentScale;
+        if (content.RectScale != pairValue)
+            content.RectScale = pairValue;
     }
 
     public void ResetZoom()
     {
-        contentScale = Vector2.One;
+        contentScale = 1;
         Update();
     }
 
@@ -167,18 +167,20 @@ public class DraggableScrollContainer : ScrollContainer
         GetHScrollbar().RectScale = GetVScrollbar().RectScale = ShowScrollbars ? Vector2.One : Vector2.Zero;
     }
 
-    private void OnScrollStarted()
+    private void OnScrollStarted(float value)
     {
-        if (scrollingTimer <= 0)
-        {
-            scrolling = true;
-            scrollingTimer = 0.1f;
-        }
-    }
+        _ = value;
 
-    private void OnScrollEnded()
-    {
-        scrolling = false;
-        scrollingTimer = 0;
+        if (zooming)
+        {
+            ScrollHorizontal = lastScrollValues.x;
+            ScrollVertical = lastScrollValues.y;
+            zooming = false;
+            return;
+        }
+        else
+        {
+            lastScrollValues = new Int2(ScrollHorizontal, ScrollVertical);
+        }
     }
 }
