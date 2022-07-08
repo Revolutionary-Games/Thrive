485f6743f67ffca6e7e44b5a6d2f456ea789f37a
Kemikal1 <alex_goku2007@yahoo.com>
2022-07-08 15:34:12 +0300

    Procedural patchmap (#3278)
    * Points generation
    
    * refinements
    
    * Random planar graph added
    
    * Some comments
    
    * added some patchregion class
    
    * Added more
    
    * More Logic
    
    * so much logic
    
    * Close to finish
    
    * patch copying
    
    * working map
    
    * added names to regions
    
    * no more region overlap
    
    * position fix
    
    * another fix
    
    * added ice shelf and some proper sorting
    
    * added scroll and vents
    
    * space fix
    
    * connected all regions and added underwatercave
    
    * added highlithing to see possible patches connections
    
    * proper adjecency highlight
    
    * Nicer drawing, and line highlitghing
    
    * made the map fit all the regions
    
    * Nicer connection drawings
    
    * proper region to region drawing
    
    * Patch map visuals done
    
    * format
    
    * format
    
    * add comment
    
    * made it working again
    
    * Fixed loading
    
    * proper load
    
    * better region link drawing.
    
    * format
    
    * better highlighting and scroll centering
    
    * linter
    
    * better centering and depth to regions
    
    * Click-and-drag navigation and find current patch button
    
    * Fixed a few style issues with DraggableScrollContainer.cs and made it work nicer with other components
    
    * Fixed typo warnings and probably fixed a bug with underwater cave key names
    
    * Add zooming feature
    
    * Revert hugely irritating bug that turns out to be still happening everytime
    
    * Integrate settings and do some cleanup
    
    * Settings-related review changes
    
    * Zooming fixes
    
    * refactering
    
    * Add zoom and drag smoothing
    
    * two reviews
    
    * more refactering
    
    * more refactering
    
    * even more refactering
    
    * style
    
    * Add helpful navigation tooltip
    
    * used array as the value to make later changes easier
    
    * merged predefined ids into a enum
    
    * predefined patch instead of map
    
    * Fix navigation tooltip
    
    * adressed some concerns
    
    * Fix duplicate tooltip
    
    * Fixed one place where nullability propagation warning happened
    
    * resolve ci issues
    
    * reverted a mistake someone made with margins and moved logic
    
    * linter
    
    * Refactored specia lregions
    
    * conflict
    
    * Fix extinction box sizing
    
    * Fixed typos
    
    * Fixed outdated text in the patch map tutorial
    
    * Unify code style regarding child resource use before _Ready in PatchMapNode.cs
    
    * Improved code styling in PatchMap.cs, didn't fix special regions yet
    
    * Last bit of easy refactoring, loading doesn't work currently
    
    I don't think my refactoring could have caused it, I'll maybe merge this anyway and make a follow up fixing branch
    
    Co-authored-by: Oliveriver <oliveriver1@gmail.com>
    Co-authored-by: Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
    Co-authored-by: Kasterisk <54026083+Kasterisk@users.noreply.github.com>
    Co-authored-by: 84634E1A607A <ajax-dong@outlook.com>

---
+++ src/microbe_stage/PatchRegion.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using Godot;
+using Newtonsoft.Json;
+
+/// <summary>
+///   A region is a something like a continent/ocean that contains multiple patches.
+/// </summary>
+[UseThriveSerializer]
+public class PatchRegion
+{
+    // TODO: Move these to Constants.cs
+
+    [JsonIgnore]
+    public readonly float RegionLineWidth = 4.0f;
+
+    [JsonIgnore]
+    public readonly float PatchMargin = 4.0f;
+
+    [JsonConstructor]
+    public PatchRegion(int id, LocalizedString name, RegionType type, Vector2 screenCoordinates,
+        float height, float width, List<Patch> patches, bool isForDrawingOnly)
+    {
+        ID = id;
+        Name = name;
+        Type = type;
+        Patches = patches;
+        ScreenCoordinates = screenCoordinates;
+        Height = height;
+        Width = width;
+        IsForDrawingOnly = isForDrawingOnly;
+    }
+
+    public PatchRegion(int id, LocalizedString name, RegionType regionType, Vector2 screenCoordinates)
+    {
+        ID = id;
+        Patches = new List<Patch>();
+        Name = name;
+        Height = 0;
+        Width = 0;
+        Type = regionType;
+        ScreenCoordinates = screenCoordinates;
+    }
+
+    public enum RegionType
+    {
+        Predefined,
+        Sea,
+        Ocean,
+        Continent,
+        Vent,
+        Cave,
+    }
+
+    [JsonProperty]
+    public int ID { get; }
+
+    /// <summary>
+    ///   Regions this is next to
+    /// </summary>
+    /// <remarks>
+    ///   <para>
+    ///     TODO: this may contain non-simulation regions (drawing only) which must be refactored out of here
+    ///   </para>
+    /// </remarks>
+    [JsonIgnore]
+    public ISet<PatchRegion> Adjacent { get; } = new HashSet<PatchRegion>();
+
+    [JsonProperty]
+    public RegionType Type { get; }
+
+    [JsonProperty]
+    public float Height { get; set; }
+
+    [JsonProperty]
+    public float Width { get; set; }
+
+    [JsonIgnore]
+    public Vector2 Size
+    {
+        get => new(Width, Height);
+        set
+        {
+            Width = value.x;
+            Height = value.y;
+        }
+    }
+
+    [JsonProperty]
+    public List<Patch> Patches { get; set; }
+
+    [JsonProperty]
+    public LocalizedString Name { get; private set; }
+
+    /// <summary>
+    ///   Coordinates this region is to be displayed at in the GUI
+    /// </summary>
+    [JsonProperty]
+    public Vector2 ScreenCoordinates { get; set; }
+
+    [JsonProperty]
+    public bool IsForDrawingOnly { get; set; }
+
+    /// <summary>
+    ///   Adds a connection to region
+    /// </summary>
+    /// <returns>True if this was new, false if already added</returns>
+    public bool AddNeighbour(PatchRegion region)
+    {
+        return Adjacent.Add(region);
+    }
+}
