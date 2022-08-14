91eefb2e39f882a8ca68698e415ef12b6171e339
GameDungeon <gamedungeon@protonmail.com>
2022-08-14 11:45:53 -0500

    Handle Time

--- src/general/DayNightCycle.cs
+++ src/general/DayNightCycle.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using Godot;
 
-public class DayNightCycle
+public class DayNightCycle : Godot.Node
 {
     /*
 
@@ -12,49 +12,9 @@ public class DayNightCycle
         methods to feed timeOfDay to relevant shaders
         methods to manipulate environment node for lighting and post-process
 
-    * for simplicity's sake, the time can just be a float property (hour.minute)
+    * for simplicity's sake, the time can just be a float property (minute)
         public float Time{ get; set; };
 
-    * a state machine is the natural solution for this class, maybe state pattern too.
-      doesn't necessarily have to be an enum and switch state machine specifically.
-        public enum TimeOfDay
-        {
-            Dawn,
-            Day,
-            Dusk,
-            Night
-        }
-
-        public TimeOfDay timeOfDay;
-
-        switch(timeOfDay)
-            case TimeOfDay.Dawn
-            {
-                Patch.LightLevel = DawnLightLevel(this.Time);
-                break;
-            }
-
-            case TimeOfDay.Day
-            {
-                if (Patch.LightLevel != DAY_LIGHT_LEVEL)
-                    Patch.LightLevel = DAY_LIGHT_LEVEL;
-                else
-                    break;
-            }
-
-            case TimeOfDay.Dusk
-            {
-                Patch.LightLevel = DuskLightLevel(this.Time);
-                break;
-            }
-
-            case TimeOfDay.Night
-            {
-                if (Patch.LightLevel != NIGHT_LIGHT_LEVEL)
-                    Patch.LightLevel = NIGHT_LIGHT_LEVEL;
-                else
-                    break;
-            }
 
     * we need various parameters that can be changed on a per planet basis like:
         how long a day is
@@ -62,4 +22,35 @@ public class DayNightCycle
         graphical particularities like color during sunrise/sunset, when to apply post fx
 
     */
+
+    /*
+
+     Gamedungeons Notes:
+
+     * Almost all of these should be converted to json. I really don't know how.
+
+    */
+
+    private const float HoursPerDay = 24;
+
+    /// <summary>
+    ///   This is how long it takes to complete a full day in realtime seconds.
+    /// </summary>
+    private const float RealTimePerDay = 300;
+
+    /// <summary>
+    ///   The current time in hours
+    /// </summary>
+    public float Time { get; private set; }
+
+    public float PercentOfDayElapsed
+    {
+        get { return HoursPerDay / Time; }
+    }
+
+    public override void _Process(float delta)
+    {
+        Time += (1 / RealTimePerDay) * HoursPerDay * delta;
+        Time = Time % HoursPerDay;
+    }
 }
