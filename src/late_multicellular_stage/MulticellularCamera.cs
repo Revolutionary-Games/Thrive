18eb6dc2cb6e33cb4b90a897fa009196affeff5f
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-08-23 15:16:20 +0300

    Added current listener control for MulticellularCamera

--- src/late_multicellular_stage/MulticellularCamera.cs
+++ src/late_multicellular_stage/MulticellularCamera.cs
@@ -7,15 +7,29 @@
 public class MulticellularCamera : Spatial
 {
     private Camera camera = null!;
+    private Listener listener = null!;
 
     public bool Current
     {
         get => camera.Current;
-        set => camera.Current = value;
+        set
+        {
+            camera.Current = value;
+
+            if (value)
+            {
+                listener.MakeCurrent();
+            }
+            else if (!value && listener.IsCurrent())
+            {
+                listener.ClearCurrent();
+            }
+        }
     }
 
     public override void _Ready()
     {
         camera = GetNode<Camera>("CameraPosition/SpringArm/Camera");
+        listener = GetNode<Listener>("CameraPosition/SpringArm/Camera/Listener");
     }
 }
