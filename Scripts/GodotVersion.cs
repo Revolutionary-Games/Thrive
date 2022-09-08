d71e06099497d8b59fc0c3e6cd8e38c5c22dccce
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-09-08 11:17:12 +0300

    Handled a few other scripts

--- Scripts/GodotVersion.cs
+++ Scripts/GodotVersion.cs
@@ -6,4 +6,9 @@ public class GodotVersion
     ///   The Godot version we use. UPDATE THIS IF WE UPDATE THE GODOT VERSION
     /// </summary>
     public const string GODOT_VERSION = "3.5";
+
+    /// <summary>
+    ///   The full name of the version. Don't edit this!
+    /// </summary>
+    public const string GODOT_VERSION_FULL = $"{GODOT_VERSION}.stable.mono";
 }
