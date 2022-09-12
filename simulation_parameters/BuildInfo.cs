b4853d84c8ea394fe73d514e72ca5d5d6eeda237
Henri Hyyryläinen <hhyyrylainen@revolutionarygamesstudio.com>
2022-09-12 20:58:32 +0300

    Show exact build commit in the game, and show more info in devbuilds (#3769)
    * Added build info printing to logs
    
    * Updated packaging process to write the current build info
    
    * Added commit showing in the options menu and devbuild in the main menu version
    
    * Made the package tool delete the version file after export
    
    * Changed the version layout a bit in options as certain hashes could be too long
    
    * Get the CI branch correctly from environment variable

---
+++ simulation_parameters/BuildInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Newtonsoft.Json;
+
+/// <summary>
+///   Some general info about when the game was exported (only really valid in exported versions of the game as the
+///   package script is the only place that updates this)
+/// </summary>
+public class BuildInfo : IRegistryType
+{
+    public BuildInfo(string commit, string branch, DateTime builtAt, bool devBuild)
+    {
+        Commit = commit;
+        Branch = branch;
+        BuiltAt = builtAt;
+        DevBuild = devBuild;
+    }
+
+    [JsonProperty]
+    public string Commit { get; }
+
+    [JsonProperty]
+    public string Branch { get; }
+
+    [JsonProperty]
+    public DateTime BuiltAt { get; }
+
+    [JsonProperty]
+    public bool DevBuild { get; }
+
+    /// <summary>
+    ///   Unused
+    /// </summary>
+    public string InternalName { get; set; } = null!;
+
+    public void Check(string name)
+    {
+        if (string.IsNullOrEmpty(Commit))
+        {
+            throw new InvalidRegistryDataException("BuildInfo", GetType().Name,
+                "commit is empty");
+        }
+
+        if (string.IsNullOrEmpty(Branch))
+        {
+            throw new InvalidRegistryDataException("BuildInfo", GetType().Name,
+                "branch is empty");
+        }
+    }
+
+    public void ApplyTranslations()
+    {
+    }
+}
