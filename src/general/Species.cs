07d18c63c24e8e3e163364ef6761f7357f61ef86
Seb W <thelayeredmind@gmail.com>
2022-10-17 10:38:15 +0200

    Remove Whitespace in GUIColour Summary

--- src/general/Species.cs
+++ src/general/Species.cs
@@ -46,7 +46,7 @@ public abstract class Species : ICloneable
 
     public Color Colour { get; set; } = new(1, 1, 1);
     /// <summary>
-    /// Get the color value for GUI Components affected by a Species colour. 
+    /// Get the color value for GUI Components affected by a Species colour.
     /// To add additional guards like a full alpha value.
     /// </summary>
     public Color GUIColour
