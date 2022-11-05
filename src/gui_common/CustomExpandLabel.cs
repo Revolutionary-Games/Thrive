42eda7ab3bc0c18ebcb711c7ace87352fe3f6bd6
Oliveriver <oliveriver1@gmail.com>
2022-11-05 23:19:23 +0000

    Fix expanding label bug

---
+++ src/gui_common/CustomExpandLabel.cs
@@ -0,0 +1,34 @@
+using Godot;
+
+/// <summary>
+///   Custom label with word wrap which dynamically expands to fill space.
+/// </summary>
+/// <remarks>
+///   <para>
+///     Due to a Godot bug, ordinary labels with word wrap and the expand fill flag initialise with a width of zero,
+///     so each character ends up on a new line. This massively expands the height of the label, which doesn't reset
+///     afterwards. See https://github.com/godotengine/godot/issues/47005.
+///   </para>
+///   <para>
+///     This custom class fixes the bug by only enabling word wrap once the label becomes visible.
+///   </para>
+/// </remarks>
+public class CustomExpandingLabel : Label
+{
+    public CustomExpandingLabel(string text)
+    {
+        Text = text;
+        Align = Label.AlignEnum.Center;
+        SizeFlagsHorizontal = (int)SizeFlags.ExpandFill;
+    }
+
+    public override void _Notification(int what)
+    {
+        base._Notification(what);
+
+        if (what == NotificationVisibilityChanged && Visible)
+        {
+            Autowrap = true;
+        }
+    }
+}
