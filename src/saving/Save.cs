446c5d45fbc107526198182e7678621c7cc16768
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2022-11-20 11:14:43 +0200

    Make file write streams explicit and dispose them

--- src/saving/Save.cs
+++ src/saving/Save.cs
@@ -246,7 +246,8 @@ public class Save
             throw new IOException("Cannot open: " + target);
         }
 
-        using Stream gzoStream = new GZipOutputStream(new GodotFileStream(file));
+        using var fileStream = new GodotFileStream(file);
+        using Stream gzoStream = new GZipOutputStream(fileStream);
         using var tar = new TarOutputStream(gzoStream, Encoding.UTF8);
 
         TarHelper.OutputEntry(tar, SAVE_INFO_JSON, Encoding.UTF8.GetBytes(justInfo));
