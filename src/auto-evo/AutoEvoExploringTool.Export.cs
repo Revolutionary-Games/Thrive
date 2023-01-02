bd44e0cecf2af8d9ffcc17e7bd109e938c86b0ae
ajax <ajax-dong@outlook.com>
2023-01-02 10:57:19 +0800

    Current-world export

--- src/auto-evo/AutoEvoExploringTool.Export.cs
+++ src/auto-evo/AutoEvoExploringTool.Export.cs
@@ -11,8 +11,14 @@ using Path = System.IO.Path;
 /// </summary>
 public partial class AutoEvoExploringTool
 {
-    private static readonly List<OrganelleDefinition> Organelles =
+    private static readonly List<OrganelleDefinition> AllOrganelles =
         SimulationParameters.Instance.GetAllOrganelles().ToList();
+
+    private static readonly Dictionary<uint, string> CurrentWorldSpecies = new();
+
+    private static List<string>? currentWorldSpeciesHeader;
+
+    private static List<string>? currentWorldPatchHeader;
 
     private void ExportCurrentWorld()
     {
@@ -24,9 +30,13 @@ public partial class AutoEvoExploringTool
         }
 
         currentWorldExportButton.Disabled = true;
-        var basePath = Path.Combine(Constants.AUTO_EVO_EXPORT_FOLDER, DateTime.Now.ToString("yy_MM_dd_hh_mm_ss"));
+        var basePath = Path.Combine(Constants.AUTO_EVO_EXPORT_FOLDER, DateTime.Now.ToString("yyyyMMdd_hh_mm_ss"));
 
         FileHelpers.MakeSureDirectoryExists(basePath);
+
+        InitWorldSpecies();
+        currentWorldSpeciesHeader = null;
+        currentWorldPatchHeader = null;
 
         if (currentWorldExportSettings.HasFlag(CurrentWorldExportSettings.CurrentSpeciesDetails))
         {
@@ -43,39 +53,57 @@ public partial class AutoEvoExploringTool
             ExportCurrentWorldPerSpeciesDetailedHistory(basePath);
         }
 
+        if (currentWorldExportSettings.HasFlag(CurrentWorldExportSettings.PerPatchHistory))
+        {
+            ExportCurrentWorldPerPatchHistory(basePath);
+        }
+
         currentWorldExportButton.Disabled = false;
         exportSuccessNotificationDialog.DialogText =
             TranslationServer.Translate("CURRENT_WORLD_EXPORTATION_SUCCESS").FormatSafe(basePath);
         exportSuccessNotificationDialog.PopupCenteredShrink();
     }
 
-    private IEnumerable<string> GenerateSpeciesHeader()
+    private void InitWorldSpecies()
     {
-        var header = new List<string>();
-        header.AddRange(new[] { "Population", "Color" });
-        header.AddRange(Enum.GetNames(typeof(BehaviouralValueType))
+        CurrentWorldSpecies.Clear();
+
+        for (uint speciesId = 1; speciesId < world.SpeciesHistoryList.Last().Max(s => s.Key); ++speciesId)
+        {
+            CurrentWorldSpecies.Add(speciesId,
+                world.SpeciesHistoryList.First(d => d.ContainsKey(speciesId))[speciesId].FormattedName);
+        }
+    }
+
+    private IEnumerable<string> GetSpeciesHeader()
+    {
+        if (currentWorldSpeciesHeader != null)
+            return currentWorldSpeciesHeader;
+
+        currentWorldSpeciesHeader = new List<string> { "Population", "Color" };
+        currentWorldSpeciesHeader.AddRange(Enum.GetNames(typeof(BehaviouralValueType))
             .OrderBy(n => Enum.Parse(typeof(BehaviouralValueType), n)));
 
-        header.AddRange(new[]
+        currentWorldSpeciesHeader.AddRange(new[]
         {
             "Membrane type", "Membrane rigidity", "Base speed", "Base rotation speed", "Storage capacity", "Bacteria",
             "Organelle count",
         });
 
-        header.AddRange(Organelles.Select(o => o.Name));
+        currentWorldSpeciesHeader.AddRange(AllOrganelles.Select(o => o.Name));
 
-        header.AddRange(world.GameProperties.GameWorld.Map.Patches.Values
+        currentWorldSpeciesHeader.AddRange(world.GameProperties.GameWorld.Map.Patches.Values
             .OrderBy(p => p.ID)
             .Select(p => p.Name.ToString()));
 
-        return header;
+        return currentWorldSpeciesHeader;
     }
 
-    private IEnumerable<string> GenerateSpeciesData(uint speciesId, int generation)
+    private IEnumerable<string> GetSpeciesData(uint speciesId, int generation)
     {
         if (!world.SpeciesHistoryList[generation].TryGetValue(speciesId, out var species))
         {
-            var emptyData = new string[2 + Enum.GetNames(typeof(BehaviouralValueType)).Length + 7 + Organelles.Count +
+            var emptyData = new string[2 + Enum.GetNames(typeof(BehaviouralValueType)).Length + 7 + AllOrganelles.Count +
                 world.PatchHistoryList[generation].Count];
 
             emptyData[0] = "Species doesn't exist";
@@ -83,8 +111,7 @@ public partial class AutoEvoExploringTool
             return emptyData;
         }
 
-        var data = new List<string>();
-        data.AddRange(new[] { species.Population.ToString(), species.Colour.ToHtml() });
+        var data = new List<string> { species.Population.ToString(), species.Colour.ToHtml() };
         data.AddRange(species.Behaviour.OrderBy(p => p.Key)
             .Select(p => p.Value.ToString(CultureInfo.InvariantCulture)));
 
@@ -101,12 +128,12 @@ public partial class AutoEvoExploringTool
                 microbeSpecies.Organelles.Count.ToString(),
             });
 
-            data.AddRange(Organelles
+            data.AddRange(AllOrganelles
                 .Select(o => microbeSpecies.Organelles.Count(ot => ot.Definition == o).ToString()));
         }
         else
         {
-            data.AddRange(new string[7 + Organelles.Count]);
+            data.AddRange(new string[7 + AllOrganelles.Count]);
         }
 
         data.AddRange(world.PatchHistoryList[generation]
@@ -118,53 +145,19 @@ public partial class AutoEvoExploringTool
 
     private void ExportCurrentWorldCurrentSpeciesDetails(string basePath)
     {
-        var path = Path.Combine(basePath, nameof(CurrentWorldExportSettings.CurrentPatchDetails) + ".csv");
-
+        var path = Path.Combine(basePath, nameof(CurrentWorldExportSettings.CurrentSpeciesDetails) + ".csv");
         var file = new File();
         file.Open(path, File.ModeFlags.Write);
 
         var header = new List<string> { "Name" };
-        header.AddRange(GenerateSpeciesHeader());
+        header.AddRange(GetSpeciesHeader());
 
         file.StoreCsvLine(header.ToArray());
 
         foreach (var species in world.GameProperties.GameWorld.Species.Values)
         {
             var data = new List<string> { species.FormattedName };
-            data.AddRange(GenerateSpeciesData(species.ID, world.CurrentGeneration));
-            file.StoreCsvLine(data.ToArray());
-        }
-
-        file.Close();
-    }
-
-    private void ExportCurrentWorldCurrentPatchDetails(string basePath)
-    {
-        var path = Path.Combine(basePath, nameof(CurrentWorldExportSettings.CurrentPatchDetails) + ".csv");
-
-        var file = new File();
-        file.Open(path, File.ModeFlags.Write);
-
-        var header = new List<string>();
-        header.AddRange(new[] { "Name", "Type" });
-        header.AddRange(world.GameProperties.GameWorld.Map.Patches.First().Value.Biome.Compounds.Keys
-            .Select(c => c.Name).OrderBy(s => s));
-
-        header.AddRange(world.GameProperties.GameWorld.Species.Values.Select(s => s.FormattedName)
-            .OrderBy(s => s));
-
-        file.StoreCsvLine(header.ToArray());
-
-        foreach (var patch in world.GameProperties.GameWorld.Map.Patches.Values)
-        {
-            var data = new List<string>();
-            data.AddRange(new[] { patch.Name.ToString(), patch.BiomeType.ToString() });
-            data.AddRange(patch.Biome.CurrentCompoundAmounts.OrderBy(p => p.Key.Name).Select(p =>
-                $"Amount = {p.Value.Amount}; Ambient = {p.Value.Ambient}; Density = {p.Value.Density}"));
-
-            data.AddRange(world.GameProperties.GameWorld.Species.Values.OrderBy(s => s.FormattedName)
-                .Select(s => patch.SpeciesInPatch.ContainsKey(s) ? patch.SpeciesInPatch[s].ToString() : "0"));
-
+            data.AddRange(GetSpeciesData(species.ID, world.CurrentGeneration));
             file.StoreCsvLine(data.ToArray());
         }
 
@@ -176,22 +169,97 @@ public partial class AutoEvoExploringTool
         basePath = Path.Combine(basePath, nameof(CurrentWorldExportSettings.PerSpeciesDetailedHistory));
         FileHelpers.MakeSureDirectoryExists(basePath);
 
+        var headerList = new List<string> { "Generation" };
+        headerList.AddRange(GetSpeciesHeader());
+        var header = headerList.ToArray();
+
         for (uint speciesId = 1; speciesId < world.SpeciesHistoryList.Last().Max(s => s.Key); ++speciesId)
         {
-            var path = Path.Combine(basePath,
-                world.SpeciesHistoryList.First(d => d.ContainsKey(speciesId))[speciesId].FormattedName + ".csv");
-
+            var path = Path.Combine(basePath, CurrentWorldSpecies[speciesId] + ".csv");
             var file = new File();
             file.Open(path, File.ModeFlags.Write);
 
-            var header = new List<string> { "Generation" };
-            header.AddRange(GenerateSpeciesHeader());
-            file.StoreCsvLine(header.ToArray());
+            file.StoreCsvLine(header);
 
             for (int generation = 0; generation <= world.CurrentGeneration; ++generation)
             {
                 var data = new List<string> { generation.ToString() };
-                data.AddRange(GenerateSpeciesData(speciesId, generation));
+                data.AddRange(GetSpeciesData(speciesId, generation));
+                file.StoreCsvLine(data.ToArray());
+            }
+
+            file.Close();
+        }
+    }
+
+    private IEnumerable<string> GetPatchHeader()
+    {
+        if (currentWorldPatchHeader != null)
+            return currentWorldPatchHeader;
+
+        currentWorldPatchHeader = new List<string> { "Type" };
+        currentWorldPatchHeader.AddRange(world.GameProperties.GameWorld.Map.Patches.First().Value.Biome.Compounds.Keys
+            .Select(c => c.Name).OrderBy(s => s));
+
+        currentWorldPatchHeader.AddRange(CurrentWorldSpecies.OrderBy(p => p.Key).Select(p => p.Value));
+
+        return currentWorldPatchHeader;
+    }
+
+    private IEnumerable<string> GetPatchData(Patch patch, PatchSnapshot snapshot)
+    {
+        var data = new List<string> { patch.BiomeType.ToString() };
+        data.AddRange(snapshot.Biome.CurrentCompoundAmounts.OrderBy(p => p.Key.Name).Select(p =>
+            $"Amount = {p.Value.Amount}; Ambient = {p.Value.Ambient}; Density = {p.Value.Density}"));
+
+        data.AddRange(CurrentWorldSpecies.OrderBy(p => p.Value)
+            .Select(p => snapshot.SpeciesInPatch.FirstOrDefault(s => s.Key.ID == p.Key).Value.ToString()));
+
+        return data;
+    }
+
+    private void ExportCurrentWorldCurrentPatchDetails(string basePath)
+    {
+        var path = Path.Combine(basePath, nameof(CurrentWorldExportSettings.CurrentPatchDetails) + ".csv");
+        var file = new File();
+        file.Open(path, File.ModeFlags.Write);
+
+        var header = new List<string> { "name" };
+        header.AddRange(GetPatchHeader());
+
+        file.StoreCsvLine(header.ToArray());
+
+        foreach (var patch in world.GameProperties.GameWorld.Map.Patches.Values)
+        {
+            var data = new List<string> { patch.Name.ToString() };
+            data.AddRange(GetPatchData(patch, patch.CurrentSnapshot));
+
+            file.StoreCsvLine(data.ToArray());
+        }
+
+        file.Close();
+    }
+
+    private void ExportCurrentWorldPerPatchHistory(string basePath)
+    {
+        basePath = Path.Combine(basePath, nameof(CurrentWorldExportSettings.PerPatchHistory));
+        FileHelpers.MakeSureDirectoryExists(basePath);
+
+        var headerList = new List<string> { "Generation" };
+        headerList.AddRange(GetPatchHeader());
+        var header = headerList.ToArray();
+
+        foreach (var patch in world.GameProperties.GameWorld.Map.Patches)
+        {
+            var path = Path.Combine(basePath, patch.Value.Name + ".csv");
+            var file = new File();
+            file.Open(path, File.ModeFlags.Write);
+            file.StoreCsvLine(header);
+
+            for (int generation = 0; generation <= world.CurrentGeneration; ++generation)
+            {
+                var data = new List<string> { generation.ToString() };
+                data.AddRange(GetPatchData(patch.Value, world.PatchHistoryList[generation][patch.Key]));
                 file.StoreCsvLine(data.ToArray());
             }
 
