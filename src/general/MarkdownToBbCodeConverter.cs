03633a21e6c97108e8b921d174f6bf9d580d7d60
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-02-07 14:13:52 +0200

    Added support for plain links in markdown to bbcode converter

--- src/general/MarkdownToBbCodeConverter.cs
+++ src/general/MarkdownToBbCodeConverter.cs
@@ -10,6 +10,8 @@ public static class MarkdownToBbCodeConverter
     private static readonly Regex BasicHtmlLink =
         new(@"<a\s+[^>]*href=\""(http\S+)\""[^>]*>\s*(.+)\s*</a\s*>", RegexOptions.Compiled | RegexOptions.Multiline);
 
+    private static readonly Regex UrlWithoutMarkup = new(@"[^=](https?:\/\/[^\s)\]}]+)");
+
     public static string Convert(string markdown)
     {
         // Convert markdown links
@@ -20,7 +22,9 @@ public static class MarkdownToBbCodeConverter
         converted = BasicHtmlLink.Replace(converted,
             $"{Constants.CLICKABLE_TEXT_BBCODE}[url=$1]$2[/url]{Constants.CLICKABLE_TEXT_BBCODE_END}");
 
-        // TODO: handle plain URLs not contained in any kind of markup
+        // Handle plain URLs not contained in any kind of markup
+        converted = UrlWithoutMarkup.Replace(converted,
+            $"{Constants.CLICKABLE_TEXT_BBCODE}[url=$1]$1[/url]{Constants.CLICKABLE_TEXT_BBCODE_END}");
 
         // TODO: convert text emphasis
 
