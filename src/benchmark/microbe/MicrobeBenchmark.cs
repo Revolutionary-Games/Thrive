f28d437008c6a8227892bba2e8d839bdc40775b5
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-02-08 15:24:13 +0200

    Add some initial files for starting to work on the microbe benchmark

---
+++ src/benchmark/microbe/MicrobeBenchmark.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+
+/// <summary>
+///   Benchmarking tool for the microbe stage. Used for checking performance impact of changes or for players to see
+///   how fast their computer is compared to other ones.
+/// </summary>
+public class MicrobeBenchmark : Node
+{
+    public override void _Ready()
+    {
+    }
+
+    public override void _Process(float delta)
+    {
+    }
+}
