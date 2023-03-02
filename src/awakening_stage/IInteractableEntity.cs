9f3c0eb3184ed2d9955a0e67dec97c9586da746d
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-03-02 14:31:37 +0200

    Started on a basic interactable interface for the awakening stage entities

---
+++ src/awakening_stage/IInteractableEntity.cs
@@ -0,0 +1,13 @@
+﻿using Godot;
+
+/// <summary>
+///   A game entity the player can interact with
+/// </summary>
+public interface IInteractableEntity : IEntity
+{
+    public float InteractDistanceOffset { get; }
+
+    public Vector3? OffsetToInteractCenter { get; }
+
+    public Vector3? ExtraInteractOverlayOffset { get; }
+}
