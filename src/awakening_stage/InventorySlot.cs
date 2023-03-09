9a7677e34f5e44b1607d58d86446a5c6cc84d509
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-03-09 14:35:26 +0200

    Some progress on dragging between inventory slots

--- src/awakening_stage/InventorySlot.cs
+++ src/awakening_stage/InventorySlot.cs
@@ -1,5 +1,9 @@
-﻿using Godot;
+﻿using System;
+using Godot;
 
+/// <summary>
+///   GUI control showing an inventory slot that can be interacted with
+/// </summary>
 public class InventorySlot : Button
 {
 #pragma warning disable CA2213
@@ -7,6 +11,26 @@ public class InventorySlot : Button
 #pragma warning restore CA2213
 
     private IInventoryItem? item;
+
+    private bool takeOnly;
+
+    [Signal]
+    public delegate void OnSelected();
+
+    [Signal]
+    public delegate void OnPressed();
+
+    [Signal]
+    public delegate void OnDragStarted();
+
+    // Customization callbacks to override default behaviour for more control on item moving and knowing when it
+    // happens
+    public delegate bool AllowDrop(InventorySlot toSlot, InventoryDragData dragData);
+
+    public delegate DragResult PerformDrop(InventorySlot toSlot, InventoryDragData dragData);
+
+    public AllowDrop? AllowDropHandler { get; set; }
+    public PerformDrop? PerformDropHandler { get; set; }
 
     public IInventoryItem? Item
     {
@@ -21,10 +45,16 @@ public class InventorySlot : Button
             if (item != null)
             {
                 Icon = item.Icon;
+
+                if (TakeOnly)
+                    Locked = false;
             }
             else
             {
                 Icon = null;
+
+                if (TakeOnly)
+                    Locked = true;
             }
         }
     }
@@ -51,14 +81,133 @@ public class InventorySlot : Button
     }
 
     /// <summary>
+    ///   Only allow taking items from this
+    /// </summary>
+    public bool TakeOnly
+    {
+        get => takeOnly;
+        set
+        {
+            takeOnly = value;
+
+            // Lock when this is take only and empty to disallow putting anything here
+            if (takeOnly && Item == null)
+                Locked = true;
+        }
+    }
+
+    /// <summary>
     ///   Slot ID metadata to tie this to the inventory slot IDs in the "backend" data
     /// </summary>
     public int SlotId { get; set; }
+
+    public static Control CreateDragPreviewForItem(IInventoryItem item)
+    {
+        // TODO: when dragging to a different popup, this causes flickering, and in general this seems to flicker with
+        // the cursor, so find a way around that or report a Godot bug
+        return new TextureRect
+        {
+            Expand = true,
+            StretchMode = TextureRect.StretchModeEnum.Scale,
+            Texture = item.Icon,
+            RectMinSize = new Vector2(32, 32),
+        };
+    }
 
     public override void _Ready()
     {
         tooHeavyIndicator = GetNode<Control>("Overlays/TooHeavyToCarry");
 
         // TODO: tooltip for showing the name of the item / extra details if any exist
+    }
+
+    public override object? GetDragData(Vector2 position)
+    {
+        if (Item == null || Locked)
+            return null;
+
+        SetDragPreview(CreateDragPreviewForItem(Item));
+
+        EmitSignal(nameof(OnDragStarted));
+
+        return new InventoryDragData(this, Item);
+    }
+
+    public override bool CanDropData(Vector2 position, object data)
+    {
+        if (Locked)
+            return false;
+
+        if (data is not InventoryDragData inventoryDragData)
+            return false;
+
+        if (AllowDropHandler != null)
+        {
+            return AllowDropHandler.Invoke(this, inventoryDragData);
+        }
+
+        // Fall back to the default behaviour of just allowing
+        return true;
+    }
+
+    public override void DropData(Vector2 position, object data)
+    {
+        if (data is not InventoryDragData inventoryDragData)
+            throw new InvalidCastException($"Can't accept drop data of type: {data.GetType()}");
+
+        DragResult result;
+        if (PerformDropHandler != null)
+        {
+            result = PerformDropHandler.Invoke(this, inventoryDragData);
+        }
+        else
+        {
+            // Default handling
+            result = DragResult.Success;
+
+            if (Item != null)
+                result = DragResult.Replaced;
+        }
+
+        if (result == DragResult.Failure)
+            return;
+
+        InventoryDragData? restartDragWith = null;
+
+        if (result == DragResult.Replaced)
+        {
+            if (Item != null)
+            {
+                restartDragWith = new InventoryDragData(this, Item);
+            }
+            else
+            {
+                GD.PrintErr($"Can't replace drag without having an item in {nameof(InventorySlot)}");
+            }
+        }
+
+        Item = inventoryDragData.Item;
+
+        // Unset from the original slot
+        if (inventoryDragData.FromSlot.Item == Item)
+            inventoryDragData.FromSlot.Item = null;
+
+        if (restartDragWith != null)
+        {
+            // TODO: how do we ensure the drag data is not lost if it cannot be dropped on anything?
+
+            ForceDrag(restartDragWith, CreateDragPreviewForItem(restartDragWith.Item));
+        }
+    }
+
+    private void OnPress()
+    {
+        EmitSignal(nameof(OnPressed));
+    }
+
+    private void OnToggle(bool pressed)
+    {
+        if (pressed)
+            EmitSignal(nameof(OnSelected));
     }
 }
---
+++ src/gui_common/DragResult.cs
@@ -0,0 +1,10 @@
+﻿public enum DragResult
+{
+    Success,
+    Failure,
+
+    /// <summary>
+    ///   The dropped item is accepted but the target's item should start a forced drag
+    /// </summary>
+    Replaced,
+}
