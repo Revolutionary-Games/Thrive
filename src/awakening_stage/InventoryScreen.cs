ab47b4ed26a021b610c6fa88fa0e87e53d93729a
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-03-14 15:58:55 +0200

    Added missing crafting panel closed call when toggled with the button
    this was probably cause of a situation where the ghost property was not reset correctly

--- src/awakening_stage/InventoryScreen.cs
+++ src/awakening_stage/InventoryScreen.cs
@@ -525,6 +525,7 @@ public class InventoryScreen : ControlWithInput
         {
             craftingPanelPopup.CustomHide();
             craftingPanelManuallyHidden = true;
+            OnCraftingPanelClosed();
         }
         else
         {
