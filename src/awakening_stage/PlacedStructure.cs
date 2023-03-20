89d5426126f4c38b3116d2f8395e6aa59db86ade
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-03-20 16:22:02 +0200

    Started on the structure placing feature

---
+++ src/awakening_stage/PlacedStructure.cs
@@ -0,0 +1,10 @@
+﻿using Godot;
+
+public class PlacedStructure : Spatial, IInteractableEntity
+{
+    public bool Completed { get; private set; }
+
+    public override void _Ready()
+    {
+    }
+}
