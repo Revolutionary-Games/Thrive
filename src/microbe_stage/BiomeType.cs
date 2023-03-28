ce9eb688564aa5c9f5f1ea69d65a8bd6b00a2826
Matt <mcbehling86@gmail.com>
2023-03-27 20:41:57 -0500

    Updated BiomeType.cs remarks and format cleanup

--- src/microbe_stage/BiomeType.cs
+++ src/microbe_stage/BiomeType.cs
@@ -1,7 +1,12 @@
 ﻿/// <summary>
 ///   This contains predefined biome types
 /// </summary>
-
+/// <remarks>
+///   <para>
+///     The order of this enum CANNOT be changed. 
+///They are saved in the gamesave file, and re-ordering would break existing saves
+///   </para>
+/// </remarks>
 public enum BiomeType
 {
     Coastal,
