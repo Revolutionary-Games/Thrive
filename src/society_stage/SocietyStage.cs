4d057c33b61d9fdac37daead275c576d89d9c01a
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-03-28 09:17:08 +0300

    Added really simple beginning of SocietyStage scene

---
+++ src/society_stage/SocietyStage.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+
+// TODO: new stage base class for the strategy phases
+public class SocietyStage : Node
+{
+    public override void _Ready()
+    {
+        // TODO: fade in
+    }
+
+    public override void _Process(float delta)
+    {
+    }
+}
