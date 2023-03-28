652d884aa4ed2f144ff9f47c5cdfe6174a222cda
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-03-28 15:17:14 +0300

    Put some basic stuff in the society stage scene

---
+++ src/society_stage/StrategicCamera.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+/// <summary>
+///   Camera for the strategy stages of the game
+/// </summary>
+public class StrategicCamera : Camera
+{
+    public override void _Ready()
+    {
+    }
+
+    public override void _Process(float delta)
+    {
+        // TODO: positioning
+    }
+}
