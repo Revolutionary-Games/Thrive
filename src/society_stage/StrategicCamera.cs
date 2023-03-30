6cfc40eb49a4d5be86ee0709ef955ab2944de0cc
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-03-30 09:22:15 +0300

    Some initial strategic camera positioning logic code

--- src/society_stage/StrategicCamera.cs
+++ src/society_stage/StrategicCamera.cs
@@ -5,12 +5,23 @@ using Godot;
 /// </summary>
 public class StrategicCamera : Camera
 {
+    /// <summary>
+    ///   The position the camera is over
+    /// </summary>
+    [Export]
+    public Vector3 WorldLocation { get; set; }
+
+    [Export]
+    public float ZoomLevel { get; set; } = 1;
+
     public override void _Ready()
     {
+        ProcessPriority = 1000;
     }
 
     public override void _Process(float delta)
     {
-        // TODO: positioning
+        // TODO: interpolating if there's a small movement for more smoothness?
+        GlobalTransform = StrategicCameraHelpers.CalculateCameraPosition(WorldLocation, ZoomLevel);
     }
 }
