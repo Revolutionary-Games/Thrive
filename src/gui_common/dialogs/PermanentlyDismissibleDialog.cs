a8c33d136f5d1d9a46bbf5a086a1d232819324df
Kasterisk <54026083+Kasterisk@users.noreply.github.com>
2023-04-06 10:07:16 +0700

    Remove redundant `this` prefix

--- src/gui_common/dialogs/PermanentlyDismissibleDialog.cs
+++ src/gui_common/dialogs/PermanentlyDismissibleDialog.cs
@@ -44,7 +44,7 @@ public class PermanentlyDismissibleDialog : CustomConfirmationDialog
     {
         if (!Settings.Instance.IsNoticePermanentlyDismissed(NoticeType))
         {
-            this.PopupCenteredShrink();
+            PopupCenteredShrink();
             return true;
         }
 
