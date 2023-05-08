5d878922fde5ea9f9b5fb5c2e35698ac78ca9296
GameDungeon <60719255+GameDungeon@users.noreply.github.com>
2023-05-08 08:39:29 -0500

    ScreenEffect settings save Bug Fix (#4347)
    * ScreenEffectFix
    
    * Switched the effect setting to be like my suggestion
    
    to not change the SettingsValue instance data multiple times
    
    ---------
    
    Co-authored-by: Henri Hyyryläinen <henri.hyyrylainen@gmail.com>

--- src/general/OptionsMenu.cs
+++ src/general/OptionsMenu.cs
@@ -2280,8 +2280,12 @@ public class OptionsMenu : ControlWithInput
 
     private void OnScreenEffectSelected(int index)
     {
-        Settings.Instance.CurrentScreenEffect.Value =
-            SimulationParameters.Instance.GetScreenEffectByIndex(index);
+        var effect = SimulationParameters.Instance.GetScreenEffectByIndex(index);
+
+        if (effect == SimulationParameters.Instance.GetScreenEffectByIndex(0))
+            effect = null;
+
+        Settings.Instance.CurrentScreenEffect.Value = effect;
 
         UpdateResetSaveButtonState();
     }
