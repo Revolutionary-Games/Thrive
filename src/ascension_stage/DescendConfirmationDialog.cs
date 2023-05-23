b46eb5b13cde661b68e5b84e9e775df98bdc9bdf
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-05-23 09:27:40 +0300

    Put some placeholder logic for the descend dialog

--- src/ascension_stage/DescendConfirmationDialog.cs
+++ src/ascension_stage/DescendConfirmationDialog.cs
@@ -3,8 +3,50 @@ using Godot;
 
 public class DescendConfirmationDialog : CustomConfirmationDialog
 {
+    private GameProperties? game;
+
+    public void ShowForGame(GameProperties gameProperties)
+    {
+        game = gameProperties;
+        Open();
+    }
+
+    protected override void OnOpen()
+    {
+        base.OnOpen();
+
+        PauseManager.Instance.AddPause(nameof(DescendConfirmationDialog));
+    }
+
+    protected override void OnClose()
+    {
+        PauseManager.Instance.Resume(nameof(DescendConfirmationDialog));
+        base.OnClose();
+    }
+
     private void OnConfirmed()
     {
+        // Make sure this is closed to unpause the game
+        Close();
+
+        if (game == null)
+        {
+            GD.PrintErr("Current game not set");
+            return;
+        }
+
+        GD.Print("Fading out the ascension stage");
+        TransitionManager.Instance.AddSequence(ScreenFade.FadeType.FadeOut, 0.5f, OnSwitchToSetupScreen, false);
+    }
+
+    private void OnSwitchToSetupScreen()
+    {
+        if (game == null)
+        {
+            GD.PrintErr("Current game has disappeared set");
+            return;
+        }
+
         GD.Print("Switching to new game setup to finish descending");
         throw new NotImplementedException();
     }
