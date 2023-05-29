7400ece965adf2d0e59022fcba383974ee6e82e3
0 <Kormur@protonmail.com>
2023-05-29 12:54:46 -0400

    Updated microbe.cs comment

--- src/microbe_stage/Microbe.cs
+++ src/microbe_stage/Microbe.cs
@@ -1078,7 +1078,7 @@ public partial class Microbe : RigidBody, ISpawned, IProcessable, IMicrobeAI, IS
     {
         var scale = new Vector3(1.0f, 1.0f, 1.0f);
 
-        // Bacteria are 40% the size of other cells
+        // Bacteria are 50% the size of other cells
         if (CellTypeProperties.IsBacteria)
             scale = new Vector3(0.5f, 0.5f, 0.5f);
 
