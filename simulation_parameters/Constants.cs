d38c32bcf57b80bce281ce3e0a3a1ef38d49e7d3
adQuid <28126232+adQuid@users.noreply.github.com>
2023-08-23 11:47:37 -0400

    AI will now usually choose not to eat what it can't digest (#4443)
    * AI will now usually choose not to eat what it can't digest
    
    * linter appeasement
    
    * linter appeasement
    
    * linter appeasement
    
    * update comment format
    
    * linter appeasement
    
    * tweak variable
    
    * corrected comment
    
    * linter appeasement
    
    * remove excess whitespace

--- simulation_parameters/Constants.cs
+++ simulation_parameters/Constants.cs
@@ -740,6 +740,12 @@ public static class Constants
     // Cooldown for AI for toggling engulfing
     public const float AI_ENGULF_INTERVAL = 300;
 
+    /// <summary>
+    ///   Probability, rolled at each AI step (which happens very often), that the AI will try to engulf something
+    ///   it can't eat
+    /// </summary>
+    public const float AI_BAD_ENGULF_CHANCE = 0.15f;
+
     // Average number of calls to think method before doing expensive cloud-finding calculations
     public const int AI_STEPS_PER_SMELL = 20;
 
