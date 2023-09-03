cf22382ab67ab4c6b042e293d5328640a0e95f55
Severin Lochschmidt <seviron53@gmail.com>
2023-09-03 16:41:08 +0200

    Chemoreceptor can now detect cells (#4261)
    * Add framework for detecting cells
    
    * Implement cell search
    
    * Prevent cell detecting itself
    
    * Fix double reporting
    
    * Add all species to detection for testing
    
    * Add missing logic and clarify variable name
    
    * Use correct translation for colony cells
    
    * Enable separate update rate for species guidance lines
    
    * Add critical check for species
    
    * Separate chemoreceptor search from line updates
    
    * Reduce GlobalTranslation calls and refactor
    
    * Pass current game world to upgrade GUIs
    
    * Add selection detecting either compound or species
    
    * Fix and improve chemoreceptor upgrade GUI
    
    * Refactor chemoreceptor gui
    
    * Add translation keys and translation for en,de,fr
    
    * Refactor and fix formatting
    
    * Sort compound and species lists alphabetically
    
    * Perform code cleanup
    
    * Fix culture specific string comparison
    
    * Appease the style police
    
    * Use `GlobalTranslation` instead of local
    
    * Reduce casting of `RUNNABLE_MICROBE_GROUP` to `microbe` array
    
    * Remove minimum detection range threshold
    
    * Create and dispose `NodePath`s correctly
    
    * Initialize `microbes` as `null`
    
    * Fix style of visibility check
    
    * Move static options to scene file
    
    * Hide `minimumAmount` slider when detecting species
    
    * Shorten null-coalescing assignment
    
    * Use existing `FormattedName`
    
    * Use `var`
    
    * Add enum for detection `TargetType`
    
    * Update localization
    
    * Use colony master instead of parent
    
    * Make initialization lazier
    
    * Improve variable naming
    
    * Fix readability
    
    * Simplify distance checks
    
    * Fix formatting
    
    * Remove modification of `SimulationParameters` and shorten sorting
    
    * Rename related variables
    
    * Update localization
    
    * Fix merge formatting

--- src/microbe_stage/editor/upgrades/ChemoreceptorUpgradeGUI.cs
+++ src/microbe_stage/editor/upgrades/ChemoreceptorUpgradeGUI.cs
@@ -1,94 +1,179 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Godot;
 
 public class ChemoreceptorUpgradeGUI : VBoxContainer, IOrganelleUpgrader
 {
     [Export]
-    public NodePath? CompoundsPath;
+    public NodePath? TargetTypeSelectorPath;
 
     [Export]
-    public NodePath MaximumDistancePath = null!;
+    public NodePath CompoundsSelectorPath = null!;
 
     [Export]
-    public NodePath MinimumAmountPath = null!;
+    public NodePath CompoundsLabelPath = null!;
 
     [Export]
-    public NodePath ColourPath = null!;
+    public NodePath SpeciesSelectorPath = null!;
+
+    [Export]
+    public NodePath SpeciesLabelPath = null!;
+
+    [Export]
+    public NodePath MaximumDistanceSliderPath = null!;
+
+    [Export]
+    public NodePath MinimumAmountSliderPath = null!;
+
+    [Export]
+    public NodePath MinimumAmountLabelPath = null!;
+
+    [Export]
+    public NodePath ColourSelectorPath = null!;
 
 #pragma warning disable CA2213
-    private OptionButton compounds = null!;
-    private Slider maximumDistance = null!;
-    private Slider minimumAmount = null!;
-    private TweakedColourPicker colour = null!;
+    private OptionButton targetTypeSelector = null!;
+    private OptionButton compoundsSelector = null!;
+    private Label compoundLabel = null!;
+    private OptionButton speciesSelector = null!;
+    private Label speciesLabel = null!;
+    private Slider maximumDistanceSlider = null!;
+    private Slider minimumAmountSlider = null!;
+    private Label minimumAmountLabel = null!;
+    private TweakedColourPicker colourSelector = null!;
 #pragma warning restore CA2213
 
-    private List<Compound>? shownChoices;
+    private List<Compound>? shownCompoundChoices;
+    private List<Species>? shownSpeciesChoices;
+
+    private enum TargetType
+    {
+        Compound,
+        Species,
+    }
 
     public override void _Ready()
     {
-        compounds = GetNode<OptionButton>(CompoundsPath);
-        maximumDistance = GetNode<Slider>(MaximumDistancePath);
-        minimumAmount = GetNode<Slider>(MinimumAmountPath);
-        colour = GetNode<TweakedColourPicker>(ColourPath);
+        targetTypeSelector = GetNode<OptionButton>(TargetTypeSelectorPath);
+        compoundsSelector = GetNode<OptionButton>(CompoundsSelectorPath);
+        compoundLabel = GetNode<Label>(CompoundsLabelPath);
+        speciesSelector = GetNode<OptionButton>(SpeciesSelectorPath);
+        speciesLabel = GetNode<Label>(SpeciesLabelPath);
+        maximumDistanceSlider = GetNode<Slider>(MaximumDistanceSliderPath);
+        minimumAmountSlider = GetNode<Slider>(MinimumAmountSliderPath);
+        minimumAmountLabel = GetNode<Label>(MinimumAmountLabelPath);
+        colourSelector = GetNode<TweakedColourPicker>(ColourSelectorPath);
 
-        compounds.Clear();
+        compoundsSelector.Clear();
+        speciesSelector.Clear();
 
-        maximumDistance.MinValue = Constants.CHEMORECEPTOR_RANGE_MIN;
-        maximumDistance.MaxValue = Constants.CHEMORECEPTOR_RANGE_MAX;
+        maximumDistanceSlider.MinValue = Constants.CHEMORECEPTOR_RANGE_MIN;
+        maximumDistanceSlider.MaxValue = Constants.CHEMORECEPTOR_RANGE_MAX;
+        minimumAmountSlider.MinValue = Constants.CHEMORECEPTOR_AMOUNT_MIN;
+        minimumAmountSlider.MaxValue = Constants.CHEMORECEPTOR_AMOUNT_MAX;
 
-        minimumAmount.MinValue = Constants.CHEMORECEPTOR_AMOUNT_MIN;
-        minimumAmount.MaxValue = Constants.CHEMORECEPTOR_AMOUNT_MAX;
+        TypeChanged((int)TargetType.Compound);
     }
 
-    public void OnStartFor(OrganelleTemplate organelle)
+    public void OnStartFor(OrganelleTemplate organelle, GameProperties currentGame)
     {
-        shownChoices = SimulationParameters.Instance.GetCloudCompounds();
+        shownCompoundChoices = SimulationParameters.Instance.GetCloudCompounds()
+            .OrderBy(c => c.Name, StringComparer.OrdinalIgnoreCase).ToList();
 
-        foreach (var choice in shownChoices)
+        foreach (var choice in shownCompoundChoices)
         {
-            compounds.AddItem(choice.Name);
+            compoundsSelector.AddItem(choice.Name);
         }
 
-        // Select glucose by default
-        var defaultCompoundIndex =
-            shownChoices.FindIndex(c => c.InternalName == Constants.CHEMORECEPTOR_DEFAULT_COMPOUND_NAME);
+        shownSpeciesChoices = currentGame.GameWorld.Map.FindAllSpeciesWithPopulation()
+            .OrderBy(c => c.FormattedName, StringComparer.OrdinalIgnoreCase).ToList();
 
-        if (defaultCompoundIndex < 0)
-            defaultCompoundIndex = 0;
+        foreach (var choice in shownSpeciesChoices)
+        {
+            speciesSelector.AddItem(choice.FormattedName);
+        }
 
         // Apply current upgrade values or defaults
         if (organelle.Upgrades?.CustomUpgradeData is ChemoreceptorUpgrades configuration)
         {
-            compounds.Selected = shownChoices.FindIndex(c => c == configuration.TargetCompound);
-            maximumDistance.Value = configuration.SearchRange;
-            minimumAmount.Value = configuration.SearchAmount;
-            colour.Color = configuration.LineColour;
+            LoadConfiguration(configuration, shownCompoundChoices, shownSpeciesChoices);
         }
         else
         {
-            compounds.Selected = defaultCompoundIndex;
-            maximumDistance.Value = Constants.CHEMORECEPTOR_RANGE_DEFAULT;
-            minimumAmount.Value = Constants.CHEMORECEPTOR_AMOUNT_DEFAULT;
-            colour.Color = shownChoices[defaultCompoundIndex].Colour;
+            var defaultCompound =
+                SimulationParameters.Instance.GetCompound(Constants.CHEMORECEPTOR_DEFAULT_COMPOUND_NAME);
+            var defaultConfiguration = new ChemoreceptorUpgrades(defaultCompound, null,
+                Constants.CHEMORECEPTOR_RANGE_DEFAULT, Constants.CHEMORECEPTOR_AMOUNT_DEFAULT, defaultCompound.Colour);
+            LoadConfiguration(defaultConfiguration, shownCompoundChoices, shownSpeciesChoices);
         }
     }
 
     public bool ApplyChanges(ICellEditorComponent editorComponent, OrganelleUpgrades organelleUpgrades)
     {
-        if (shownChoices == null)
+        if (shownCompoundChoices == null || shownSpeciesChoices == null)
         {
             GD.PrintErr("Chemoreceptor upgrade GUI was not opened properly");
             return false;
         }
 
-        // Force some compound to be selected
-        if (compounds.Selected == -1)
-            compounds.Selected = 0;
+        // Force some type/compound/species to be selected
+        if (targetTypeSelector.Selected == -1)
+            targetTypeSelector.Selected = (int)TargetType.Compound;
+        if (compoundsSelector.Selected == -1)
+            compoundsSelector.Selected = 0;
+        if (speciesSelector.Selected == -1)
+            speciesSelector.Selected = 0;
 
-        organelleUpgrades.CustomUpgradeData = new ChemoreceptorUpgrades(shownChoices[compounds.Selected],
-            (float)maximumDistance.Value, (float)minimumAmount.Value, colour.Color);
+        // Only one type of object can be detected
+        Compound? compoundChoice = null;
+        Species? speciesChoice = null;
+
+        if (targetTypeSelector.Selected == (int)TargetType.Compound)
+        {
+            compoundChoice = shownCompoundChoices[compoundsSelector.Selected];
+        }
+        else if (targetTypeSelector.Selected == (int)TargetType.Species)
+        {
+            speciesChoice = shownSpeciesChoices[speciesSelector.Selected];
+        }
+
+        organelleUpgrades.CustomUpgradeData = new ChemoreceptorUpgrades(
+            compoundChoice, speciesChoice,
+            (float)maximumDistanceSlider.Value, (float)minimumAmountSlider.Value, colourSelector.Color);
         return true;
+    }
+
+    public void SelectionChanged(int index)
+    {
+        ApplySelectionColour();
+    }
+
+    public void TypeChanged(int index)
+    {
+        // Make either species or compound menu visible
+        speciesSelector.Visible = false;
+        speciesLabel.Visible = false;
+        compoundsSelector.Visible = false;
+        compoundLabel.Visible = false;
+        minimumAmountSlider.Visible = false;
+        minimumAmountLabel.Visible = false;
+
+        switch ((TargetType)index)
+        {
+            case TargetType.Compound:
+                compoundsSelector.Visible = true;
+                compoundLabel.Visible = true;
+                minimumAmountSlider.Visible = true;
+                minimumAmountLabel.Visible = true;
+                break;
+            case TargetType.Species:
+                speciesSelector.Visible = true;
+                speciesLabel.Visible = true;
+                break;
+        }
+
+        ApplySelectionColour();
     }
 
     public Vector2 GetMinDialogSize()
@@ -96,29 +181,66 @@ public class ChemoreceptorUpgradeGUI : VBoxContainer, IOrganelleUpgrader
         return new Vector2(400, 320);
     }
 
-    public void CompoundChanged(int index)
-    {
-        // If the currently selected colour is in the shownChoices list change the colour to the colour of the newly
-        // selected compound to make setting up chemoreceptors easier
-        if (shownChoices?.Any(c => c.Colour == colour.Color) == true)
-        {
-            colour.Color = shownChoices[index].Colour;
-        }
-    }
-
     protected override void Dispose(bool disposing)
     {
         if (disposing)
         {
-            if (CompoundsPath != null)
+            if (TargetTypeSelectorPath != null)
             {
-                CompoundsPath.Dispose();
-                MaximumDistancePath.Dispose();
-                MinimumAmountPath.Dispose();
-                ColourPath.Dispose();
+                TargetTypeSelectorPath.Dispose();
+                CompoundsSelectorPath.Dispose();
+                CompoundsLabelPath.Dispose();
+                SpeciesSelectorPath.Dispose();
+                SpeciesLabelPath.Dispose();
+                MaximumDistanceSliderPath.Dispose();
+                MinimumAmountSliderPath.Dispose();
+                MinimumAmountLabelPath.Dispose();
+                ColourSelectorPath.Dispose();
             }
         }
 
         base.Dispose(disposing);
+    }
+
+    /// <summary>
+    ///   Sets the GUI up to reflect an existing configuration
+    /// </summary>
+    private void LoadConfiguration(ChemoreceptorUpgrades configuration,
+        List<Compound> shownCompoundChoices, List<Species> shownSpeciesChoices)
+    {
+        if (configuration.TargetCompound != null)
+        {
+            TypeChanged((int)TargetType.Compound);
+            targetTypeSelector.Selected = (int)TargetType.Compound;
+            compoundsSelector.Selected = shownCompoundChoices.FindIndex(c => c == configuration.TargetCompound);
+        }
+        else if (configuration.TargetSpecies != null)
+        {
+            TypeChanged((int)TargetType.Species);
+            targetTypeSelector.Selected = (int)TargetType.Species;
+            speciesSelector.Selected = shownSpeciesChoices.FindIndex(c => c == configuration.TargetSpecies);
+        }
+
+        maximumDistanceSlider.Value = configuration.SearchRange;
+        minimumAmountSlider.Value = configuration.SearchAmount;
+        colourSelector.Color = configuration.LineColour;
+    }
+
+    /// <summary>
+    ///   Applies the color of a selected target to the color picker
+    /// </summary>
+    private void ApplySelectionColour()
+    {
+        if (targetTypeSelector.Selected == (int)TargetType.Compound && shownCompoundChoices != null
+            && compoundsSelector.Selected >= 0)
+        {
+            colourSelector.Color = shownCompoundChoices[compoundsSelector.Selected].Colour;
+        }
+
+        if (targetTypeSelector.Selected == (int)TargetType.Species && shownSpeciesChoices != null
+            && speciesSelector.Selected >= 0)
+        {
+            colourSelector.Color = shownSpeciesChoices[speciesSelector.Selected].Colour;
+        }
     }
 }
