5cf509fa5f98c85411ba0b77ff47121231aae2a5
Nils K <97975579+Brabygg@users.noreply.github.com>
2023-09-19 13:46:50 +0200

    Art gallery slideshow now works properly for models (#4475)
    * Art gallery slideshow works properly for models
    
    * Tiny fix to get rid of build errors
    
    * Tweak for art gallery slideshow fix
    
    * Directly call gallery viewport fade
    
    ---------
    
    Co-authored-by: Henri Hyyryläinen <henri.hyyrylainen@gmail.com>

---
+++ src/gui_common/art_gallery/CrossFadableGalleryViewport.cs
@@ -0,0 +1,43 @@
+﻿using Godot;
+
+/// <summary>
+///   Handles fade effects in the gallery's model viewer. For generic fades, see
+///   <see cref="CrossFadableTextureRect"/> or <see cref="ScreenFade"/>.
+/// </summary>
+public class CrossFadableGalleryViewport : ViewportContainer
+{
+#pragma warning disable CA2213
+    private Tween tween = null!;
+#pragma warning restore CA2213
+
+    [Signal]
+    public delegate void Faded();
+
+    [Export]
+    public float FadeDuration { get; set; } = 0.5f;
+
+    public override void _Ready()
+    {
+        tween = GetNode<Tween>("Tween");
+    }
+
+    public void BeginFade()
+    {
+        tween.InterpolateProperty(this, "modulate", null, Colors.Black, FadeDuration);
+        tween.Start();
+
+        tween.CheckAndConnect(
+            "tween_completed", this, nameof(OnFaded), null, (uint)ConnectFlags.Oneshot);
+    }
+
+    private void OnFaded(Object @object, NodePath key)
+    {
+        _ = @object;
+        _ = key;
+
+        EmitSignal(nameof(Faded));
+
+        tween.InterpolateProperty(this, "modulate", null, Colors.White, FadeDuration);
+        tween.Start();
+    }
+}
