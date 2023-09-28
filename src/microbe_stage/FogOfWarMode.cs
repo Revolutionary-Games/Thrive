168e00888e45f2aa3aca97be8f3a7fc30f736dc5
ender-titan1 <supszym02@gmail.com>
2023-09-28 22:20:27 +0200

    Update FogOfWarMode.cs

--- src/microbe_stage/FogOfWarMode.cs
+++ src/microbe_stage/FogOfWarMode.cs
@@ -2,12 +2,12 @@
 ///   Whether the fog of war should be disabled,
 ///   mark patches adjacent to the player as explored,
 ///   or mark only the patch the player has entered as explored.
+/// </summary>
 /// <remarks>
 ///   <para>
 ///     Do not modify the order of the elements, as that would break the UI functionality
 ///   </para>
 /// </remarks>
-/// </summary>
 public enum FogOfWarMode
 {
     Disabled,
