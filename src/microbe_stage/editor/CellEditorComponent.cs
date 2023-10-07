abe453aca5c6c25ae10f1b5758e3aa4a90bfa0fe
Szymon <81180880+ender-titan1@users.noreply.github.com>
2023-10-07 12:39:54 +0200

    Remove trailing whitespace

--- src/microbe_stage/editor/CellEditorComponent.cs
+++ src/microbe_stage/editor/CellEditorComponent.cs
@@ -1628,7 +1628,7 @@ public partial class CellEditorComponent :
         // For multi hex organelles we keep track of positions that got filled in
         var usedHexes = new HashSet<Hex>();
 
-        HexEditorSymmetry? overrideSymmetry = 
+        HexEditorSymmetry? overrideSymmetry =
             componentBottomLeftButtons.SymmetryEnabled ? null : HexEditorSymmetry.None;
 
         RunWithSymmetry(q, r,
