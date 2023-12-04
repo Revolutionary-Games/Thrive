e39906c16f5949b4320ddf397639165bad0dcb93
adQuid <28126232+adQuid@users.noreply.github.com>
2023-12-04 16:35:55 -0500

    rebalance constants

--- simulation_parameters/Constants.cs
+++ simulation_parameters/Constants.cs
@@ -209,13 +209,13 @@ public static class Constants
 
     public const float MICROBE_MOVEMENT_SOUND_EMIT_COOLDOWN = 1.3f;
 
-    // Note that the speed is reversed, i.e. lower values mean faster
+    // Note that the rotation speed is reversed, i.e. lower values mean faster
     public const float CELL_MAX_ROTATION = 10.0f;
     public const float CELL_MIN_ROTATION = 0.1f;
-    public const float CELL_ROTATION_INFLECTION_INERTIA = 65.0f;
-    public const float CELL_ROTATION_RADIUS_FACTOR = 0.4f;
-    public const float CILIA_ROTATION_FACTOR = 320.0f;
-    public const float CILIA_RADIUS_FACTOR_MULTIPLIER = 20.0f;
+    public const float CELL_ROTATION_INFLECTION_INERTIA = 25000000.0f;
+    public const float CELL_ROTATION_RADIUS_FACTOR = 200.0f;
+    public const float CILIA_ROTATION_FACTOR = 720000.0f;
+    public const float CILIA_RADIUS_FACTOR_MULTIPLIER = 200000.0f;
 
     // These speed values are also reversed like the above
     public const float CELL_COLONY_MAX_ROTATION_MULTIPLIER = 2.5f;
