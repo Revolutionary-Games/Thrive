660872c7fb93dd1f25fa601aef411cc4ee4d8486
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-12-08 15:02:30 +0200

    Fixed one new indent code warning

--- src/general/base_stage/HexEditorComponentBase.cs
+++ src/general/base_stage/HexEditorComponentBase.cs
@@ -7,10 +7,9 @@ using Newtonsoft.Json;
 /// <summary>
 ///   Editor component that specializes in hex-based stuff editing
 /// </summary>
-public abstract class
-    HexEditorComponentBase<TEditor, TCombinedAction, TAction, THexMove, TContext> :
-        EditorComponentWithActionsBase<TEditor, TCombinedAction>,
-        ISaveLoadedTracked, IChildPropertiesLoadCallback
+public abstract class HexEditorComponentBase<TEditor, TCombinedAction, TAction, THexMove, TContext> :
+    EditorComponentWithActionsBase<TEditor, TCombinedAction>,
+    ISaveLoadedTracked, IChildPropertiesLoadCallback
     where TEditor : class, IHexEditor, IEditorWithActions
     where TCombinedAction : CombinedEditorAction
     where TAction : EditorAction
