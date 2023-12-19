8cd2a67eead75f691fad995fff50a9c1ac75f73b
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-12-19 11:21:42 +0200

    Made chemoreceptor tutorial close when placing the chemoreceptor

--- src/tutorial/microbe_editor/ChemoreceptorPlacementTutorial.cs
+++ src/tutorial/microbe_editor/ChemoreceptorPlacementTutorial.cs
@@ -1,10 +1,15 @@
 ﻿namespace Tutorial
 {
+    using System;
+
     /// <summary>
     ///   Notifies the player about the chemoreceptor
     /// </summary>
     public class ChemoreceptorPlacementTutorial : EditorEntryCountingTutorial
     {
+        private readonly OrganelleDefinition chemoreceptor =
+            SimulationParameters.Instance.GetOrganelleType("chemoreceptor");
+
         public ChemoreceptorPlacementTutorial()
         {
             CanTrigger = false;
@@ -17,6 +22,36 @@
         public override void ApplyGUIState(MicrobeEditorTutorialGUI gui)
         {
             gui.ChemoreceptorPlacementTutorialVisible = ShownCurrently;
+        }
+
+        public override bool CheckEvent(TutorialState overallState, TutorialEventType eventType, EventArgs args,
+            object sender)
+        {
+            if (base.CheckEvent(overallState, eventType, args, sender))
+                return true;
+
+            switch (eventType)
+            {
+                case TutorialEventType.MicrobeEditorOrganellePlaced:
+                {
+                    if (((OrganellePlacedEventArgs)args).Definition.InternalName != chemoreceptor.InternalName)
+                        break;
+
+                    if (ShownCurrently)
+                    {
+                        Hide();
+                    }
+                    else if (!HasBeenShown)
+                    {
+                        // Don't show the tutorial later if the player has already figured out placing a chemoreceptor
+                        Inhibit();
+                    }
+
+                    break;
+                }
+            }
+
+            return false;
         }
     }
 }
