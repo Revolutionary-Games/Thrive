834bd0cb5bd4dba93af133436e0b3acd0f02cdb6
Wojciech Makowski <c009.1234.c009@gmail.com>
2024-01-02 18:41:17 +0100

    Muscle bonus speed cap and adjustments

--- src/late_multicellular_stage/MulticellularCreature.cs
+++ src/late_multicellular_stage/MulticellularCreature.cs
@@ -208,7 +208,7 @@ public class MulticellularCreature : RigidBody, ISaveLoadedTracked, ICharacterIn
             if (MovementDirection != Vector3.Zero)
             {
                 // TODO: movement force calculation
-                ApplyCentralImpulse(Mass * MovementDirection * delta * (5 * Species.MuscularPower + 1));
+                ApplyCentralImpulse(Mass * MovementDirection * delta * 2 * (Mathf.Clamp(Species.MuscularPower, 0, 5 * Mass) + 1));
             }
         }
         else
@@ -216,7 +216,7 @@ public class MulticellularCreature : RigidBody, ISaveLoadedTracked, ICharacterIn
             if (MovementDirection != Vector3.Zero)
             {
                 // TODO: movement force calculation
-                ApplyCentralImpulse(Mass * MovementDirection * delta * 50 * (Species.MuscularPower + 1));
+                ApplyCentralImpulse(Mass * MovementDirection * delta * 15 * (Mathf.Clamp(Species.MuscularPower, 0, 5 * Mass) + 1));
             }
         }
 
