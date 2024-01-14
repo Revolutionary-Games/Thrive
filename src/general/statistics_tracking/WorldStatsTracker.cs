ffd899597ee2e7957ad7a4bd292015725fcf24e2
ender-titan1 <supszym02@gmail.com>
2024-01-14 12:29:33 +0100

    Delete property

--- src/general/statistics_tracking/WorldStatsTracker.cs
+++ src/general/statistics_tracking/WorldStatsTracker.cs
@@ -8,26 +8,6 @@ using UnlockConstraints;
 [UseThriveSerializer]
 public class WorldStatsTracker : IUnlockStateDataSource
 {
-    public WorldStatsTracker()
-    {
-        AllStatistics = new Lazy<IStatistic[]>(() =>
-        {
-            return new IStatistic[]
-            {
-                TotalEngulfedByPlayer,
-                TotalDigestedByPlayer,
-                TotalPlayerDeaths,
-                PlayerReproductionStatistic,
-            };
-        });
-    }
-
-    /// <summary>
-    ///   All the statistics recorded in <see cref="WorldStatsTracker"/>
-    /// </summary>
-    [JsonProperty]
-    public Lazy<IStatistic[]> AllStatistics { get; private set; }
-
     [JsonProperty]
     public SimpleStatistic TotalEngulfedByPlayer { get; private set; } = new();
 
