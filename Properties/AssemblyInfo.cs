b7ccdcc976b5ed3deed4f1603572d7a3706c5d80
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-02-16 15:31:38 +0200

    Bump version to 0.6.5-rc1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@
 
 [assembly: AssemblyVersion("0.6.5")]
 
-[assembly: AssemblyInformationalVersion("-alpha")]
+[assembly: AssemblyInformationalVersion("-rc1")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
