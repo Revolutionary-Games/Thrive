a1490b593479cbd28cc7d3bb723b398cabcb45cb
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-03-20 11:23:13 +0200

    Disable fullscreen toggle in case it triggers in the editor

--- src/engine/FullScreenToggle.cs
+++ src/engine/FullScreenToggle.cs
@@ -16,6 +16,12 @@ public partial class FullScreenToggle : NodeWithInput
     [RunOnKeyDown("toggle_fullscreen", OnlyUnhandled = false)]
     public void ToggleFullScreenPressed()
     {
+        if (Engine.IsEditorHint())
+        {
+            GD.Print("Ignoring fullscreen change key press inside the editor");
+            return;
+        }
+
         Settings.Instance.FullScreen.Value = !Settings.Instance.FullScreen.Value;
         GD.Print("Toggling fullscreen with keypress, new value: ", Settings.Instance.FullScreen.Value);
         Settings.Instance.ApplyWindowSettings();
