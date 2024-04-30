7523c99f0a8bc39337a9631e35225cbcbd9db977
Ethan Weller <gamedungeon@protonmail.com>
2024-04-29 19:48:25 -0500

    Fix Errors

--- src/auto-evo/Miche.cs
+++ src/auto-evo/Miche.cs
@@ -1,208 +1,207 @@
 ﻿namespace AutoEvo;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
 
-    public class Miche
+public class Miche
+{
+    public Miche? Parent = null;
+    public List<Miche> Children = new();
+
+    // This should always be null if this Miche has children
+    public MicrobeSpecies? Occupant = null;
+    public SelectionPressure Pressure;
+
+    public Miche(SelectionPressure pressure) : this(pressure, (List<Miche>?)null) { }
+
+    public Miche(SelectionPressure pressure, Miche child) : this(pressure, new List<Miche> { child }) { }
+
+    public Miche(SelectionPressure pressure, List<Miche>? children)
     {
-        public Miche? Parent = null;
-        public List<Miche> Children = new();
+        Children = new List<Miche>();
 
-        // This should always be null if this Miche has children
-        public MicrobeSpecies? Occupant = null;
-        public SelectionPressure Pressure;
-
-        public Miche(SelectionPressure pressure) : this(pressure, (List<Miche>?)null) { }
-
-        public Miche(SelectionPressure pressure, Miche child) : this(pressure, new List<Miche> { child }) { }
-
-        public Miche(SelectionPressure pressure, List<Miche>? children)
+        if (children != null)
         {
-            Children = new List<Miche>();
-
-            if (children != null)
-            {
-                AddChildren(children);
-            }
-
-            Pressure = pressure;
+            AddChildren(children);
         }
 
-        public bool IsLeafNode()
+        Pressure = pressure;
+    }
+
+    public bool IsLeafNode()
+    {
+        return Children.Count == 0;
+    }
+
+    public Miche Root()
+    {
+        if (Parent == null)
         {
-            return Children.Count == 0;
+            return this;
         }
 
-        public Miche Root()
-        {
-            if (Parent == null)
-            {
-                return this;
-            }
+        return Parent.Root();
+    }
 
-            return Parent.Root();
+    public IEnumerable<Miche> AllLeafNodes()
+    {
+        if (IsLeafNode())
+        {
+            return new List<Miche> { this };
         }
 
-        public IEnumerable<Miche> AllLeafNodes()
+        var nodes = new List<Miche>();
+
+        foreach (var child in Children)
         {
-            if (IsLeafNode())
-            {
-                return new List<Miche> { this };
-            }
-
-            var nodes = new List<Miche>();
-
-            foreach (var child in Children)
-            {
-                nodes.AddRange(child.AllLeafNodes());
-            }
-
-            return nodes;
+            nodes.AddRange(child.AllLeafNodes());
         }
 
-        public IEnumerable<MicrobeSpecies> AllOccupants()
+        return nodes;
+    }
+
+    public IEnumerable<MicrobeSpecies> AllOccupants()
+    {
+        var occupants = new List<MicrobeSpecies>();
+
+        if (Occupant != null)
         {
-            var occupants = new List<MicrobeSpecies>();
-
-            if (Occupant != null)
-            {
-                occupants.Add(Occupant);
-            }
-
-            foreach (var child in Children)
-            {
-                occupants.AddRange(child.AllOccupants());
-            }
-
-            return occupants;
+            occupants.Add(Occupant);
         }
 
-        public void AddChild(Miche newChild)
+        foreach (var child in Children)
         {
-            Children.Add(newChild);
-            newChild.Parent = this;
+            occupants.AddRange(child.AllOccupants());
         }
 
-        public void AddChildren(IEnumerable<Miche> newChildren)
+        return occupants;
+    }
+
+    public void AddChild(Miche newChild)
+    {
+        Children.Add(newChild);
+        newChild.Parent = this;
+    }
+
+    public void AddChildren(IEnumerable<Miche> newChildren)
+    {
+        foreach (var child in newChildren)
         {
-            foreach (var child in newChildren)
+            AddChild(child);
+        }
+    }
+
+    public bool InsertSpecies(MicrobeSpecies species, SimulationCache cache)
+    {
+        var scoresDictionary = AllOccupants().Distinct().ToDictionary(x => x, _ => 0.0f);
+
+        scoresDictionary[species] = 0.0f;
+
+        return InsertSpecies(species, scoresDictionary, cache);
+    }
+
+    /// <summary>
+    ///   Inserts a species into any spots on the tree where the species is a better fit than any current occupants
+    /// </summary>
+    /// <param name="species">new species being inserted</param>
+    /// <returns>
+    ///   Returns a bool based on if the species was inserted into a leaf node
+    /// </returns>
+    public bool InsertSpecies(MicrobeSpecies species, Dictionary<MicrobeSpecies, float> scoresSoFar,
+        SimulationCache cache)
+    {
+        var myScore = Pressure.Score(species, cache);
+
+        // Prune branch if species fails any pressures
+        if (myScore < 0)
+            return false;
+
+        if (IsLeafNode() && Occupant == null)
+        {
+            Occupant = species;
+            return true;
+        }
+
+        var newScores = new Dictionary<MicrobeSpecies, float>();
+
+        foreach (var currentSpecies in AllOccupants())
+        {
+            newScores[currentSpecies] = scoresSoFar[currentSpecies] +
+                Pressure.WeightedComparedScores(myScore, Pressure.Score(currentSpecies, cache));
+        }
+
+        // We know Occupant isn't null because of an earlier check
+        // We check here to see if scores more than 0, beacuse
+        // scores is relative to the inserted species
+        if (IsLeafNode() && newScores[Occupant!] > 0)
+        {
+            Occupant = species;
+            return true;
+        }
+
+        var inserted = false;
+
+        foreach (var child in Children)
+        {
+            if (child.InsertSpecies(species, newScores, cache))
             {
-                AddChild(child);
+                inserted = true;
             }
         }
 
-        public bool InsertSpecies(MicrobeSpecies species, SimulationCache cache)
+        return inserted;
+    }
+
+    public List<List<Miche>> AllTraversals()
+    {
+        if (IsLeafNode())
         {
-            var scoresDictionary = AllOccupants().Distinct().ToDictionary(x => x, _ => 0.0f);
-
-            scoresDictionary[species] = 0.0f;
-
-            return InsertSpecies(species, scoresDictionary, cache);
+            return new List<List<Miche>> { new() { this } };
         }
 
-        /// <summary>
-        ///   Inserts a species into any spots on the tree where the species is a better fit than any current occupants
-        /// </summary>
-        /// <param name="species">new species being inserted</param>
-        /// <returns>
-        ///   Returns a bool based on if the species was inserted into a leaf node
-        /// </returns>
-        public bool InsertSpecies(MicrobeSpecies species, Dictionary<MicrobeSpecies, float> scoresSoFar,
-            SimulationCache cache)
+        var traversals = Children.SelectMany(x => x.AllTraversals());
+        var retval = new List<List<Miche>>();
+
+        foreach (var list in traversals)
         {
-            var myScore = Pressure.Score(species, cache);
-
-            // Prune branch if species fails any pressures
-            if (myScore < 0)
-                return false;
-
-            if (IsLeafNode() && Occupant == null)
-            {
-                Occupant = species;
-                return true;
-            }
-
-            var newScores = new Dictionary<MicrobeSpecies, float>();
-
-            foreach (var currentSpecies in AllOccupants())
-            {
-                newScores[currentSpecies] = scoresSoFar[currentSpecies] +
-                    Pressure.WeightedComparedScores(myScore, Pressure.Score(currentSpecies, cache));
-            }
-
-            // We know Occupant isn't null because of an earlier check
-            // We check here to see if scores more than 0, beacuse
-            // scores is relative to the inserted species
-            if (IsLeafNode() && newScores[Occupant!] > 0)
-            {
-                Occupant = species;
-                return true;
-            }
-
-            var inserted = false;
-
-            foreach (var child in Children)
-            {
-                if (child.InsertSpecies(species, newScores, cache))
-                {
-                    inserted = true;
-                }
-            }
-
-            return inserted;
+            list.Insert(0, this);
+            retval.Add(list);
         }
 
-        public List<List<Miche>> AllTraversals()
+        return retval;
+    }
+
+    public IEnumerable<Miche> BackTraversal()
+    {
+        return BackTraversal(new List<Miche>());
+    }
+
+    public IEnumerable<Miche> BackTraversal(List<Miche> currentTraversal)
+    {
+        currentTraversal.Insert(0, this);
+
+        if (Parent == null)
+            return currentTraversal;
+
+        return Parent.BackTraversal(currentTraversal);
+    }
+
+    public Miche DeepCopy()
+    {
+        // This doesn't copy pressures, but it shouldn't really need to
+        // Pressures should be deterministic
+
+        if (IsLeafNode())
         {
-            if (IsLeafNode())
+            return new Miche(Pressure)
             {
-                return new List<List<Miche>> { new() { this } };
-            }
-
-            var traversals = Children.SelectMany(x => x.AllTraversals());
-            var retval = new List<List<Miche>>();
-
-            foreach (var list in traversals)
-            {
-                list.Insert(0, this);
-                retval.Add(list);
-            }
-
-            return retval;
+                Occupant = Occupant,
+            };
         }
 
-        public IEnumerable<Miche> BackTraversal()
-        {
-            return BackTraversal(new List<Miche>());
-        }
+        var newChildren = new List<Miche>(Children).Select(child => child.DeepCopy()).ToList();
 
-        public IEnumerable<Miche> BackTraversal(List<Miche> currentTraversal)
-        {
-            currentTraversal.Insert(0, this);
-
-            if (Parent == null)
-                return currentTraversal;
-
-            return Parent.BackTraversal(currentTraversal);
-        }
-
-        public Miche DeepCopy()
-        {
-            // This doesn't copy pressures, but it shouldn't really need to
-            // Pressures should be deterministic
-
-            if (IsLeafNode())
-            {
-                return new Miche(Pressure)
-                {
-                    Occupant = Occupant,
-                };
-            }
-
-            var newChildren = new List<Miche>(Children).Select(child => child.DeepCopy()).ToList();
-
-            return new Miche(Pressure, newChildren);
-        }
+        return new Miche(Pressure, newChildren);
     }
 }
