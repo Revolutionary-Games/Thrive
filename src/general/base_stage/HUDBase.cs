4cd3922212544d5b3a071c64f1bf6147627d3607
CI09 <c009.1234.c009@gmail.com>
2024-05-24 21:55:25 +0200

    Check if previous transition has completed before starting another

--- src/general/base_stage/HUDBase.cs
+++ src/general/base_stage/HUDBase.cs
@@ -35,6 +35,9 @@ public partial class HUDBase : Control, IStageHUD
     protected void AddFadeIn(IStageBase stageBase, bool longerDuration)
     {
         // Fade out for that smooth satisfying transition
+        if (!stageBase.TransitionFinished)
+            return;
+
         stageBase.TransitionFinished = false;
 
         TransitionManager.Instance.AddSequence(ScreenFade.FadeType.FadeIn, longerDuration ? 1.0f : 0.5f,
