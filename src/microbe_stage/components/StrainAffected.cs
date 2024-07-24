025376c796d10cb82e98ffb2af45180116917bec
CI09 <c009.1234.c009@gmail.com>
2024-07-24 20:54:53 +0200

    Style fix

--- src/microbe_stage/components/StrainAffected.cs
+++ src/microbe_stage/components/StrainAffected.cs
@@ -1,34 +1,31 @@
-﻿namespace Components
+﻿using System;
+
+/// <summary>
+///   Contains variables related to strain
+/// </summary>
+[JSONDynamicTypeAllowed]
+public struct StrainAffected
 {
-    using System;
+    /// <summary>
+    ///   The current amount of strain
+    /// </summary>
+    public float CurrentStrain;
 
     /// <summary>
-    ///   Contains variables related to strain
+    ///   The amount of time the player has to wait before <see cref="CurrentStrain"/> sarts to fall
     /// </summary>
-    [JSONDynamicTypeAllowed]
-    public struct StrainAffected
+    public float StrainDecreaseCooldown;
+
+    /// <summary>
+    ///   True when sprinting or when strain is supposed to be otherwise generated
+    /// </summary>
+    public bool IsUnderStrain;
+}
+
+public static class StrainAffectedHelpers
+{
+    public static float CalculateStrainFraction(this ref StrainAffected affected)
     {
-        /// <summary>
-        ///   The current amount of strain
-        /// </summary>
-        public float CurrentStrain;
-
-        /// <summary>
-        ///   The amount of time the player has to wait before <see cref="CurrentStrain"/> sarts to fall
-        /// </summary>
-        public float StrainDecreaseCooldown;
-
-        /// <summary>
-        ///   True when sprinting or when strain is supposed to be otherwise generated
-        /// </summary>
-        public bool IsUnderStrain;
-    }
-
-    public static class StrainAffectedHelpers
-    {
-        public static float CalculateStrainFraction(this ref StrainAffected affected)
-        {
-            return Math.Max(0, affected.CurrentStrain - Constants.CANCELED_STRAIN) / Constants.MAX_STRAIN_PER_ENTITY;
-        }
+        return Math.Max(0, affected.CurrentStrain - Constants.CANCELED_STRAIN) / Constants.MAX_STRAIN_PER_ENTITY;
     }
 }
--- src/microbe_stage/systems/StrainSystem.cs
+++ src/microbe_stage/systems/StrainSystem.cs
@@ -1,59 +1,56 @@
-﻿namespace Systems
+﻿using Components;
+using DefaultEcs;
+using DefaultEcs.System;
+using DefaultEcs.Threading;
+using Godot;
+using World = DefaultEcs.World;
+
+[With(typeof(OrganelleContainer))]
+[With(typeof(StrainAffected))]
+[ReadsComponent(typeof(OrganelleContainer))]
+[ReadsComponent(typeof(MicrobeControl))]
+public sealed class StrainSystem : AEntitySetSystem<float>
 {
-    using Components;
-    using DefaultEcs;
-    using DefaultEcs.System;
-    using DefaultEcs.Threading;
-    using Godot;
-    using World = DefaultEcs.World;
-
-    [With(typeof(OrganelleContainer))]
-    [With(typeof(StrainAffected))]
-    [ReadsComponent(typeof(OrganelleContainer))]
-    [ReadsComponent(typeof(MicrobeControl))]
-    public sealed class StrainSystem : AEntitySetSystem<float>
+    public StrainSystem(World world, IParallelRunner runner) : base(world, runner)
     {
-        public StrainSystem(World world, IParallelRunner runner) : base(world, runner)
+    }
+
+    protected override void Update(float delta, in Entity entity)
+    {
+        ref var strain = ref entity.Get<StrainAffected>();
+        ref var organelles = ref entity.Get<OrganelleContainer>();
+
+        if (strain.IsUnderStrain)
         {
+            var strainIncrease = Constants.SPRINTING_STRAIN_INCREASE_PER_SECOND * delta;
+            strainIncrease += organelles.HexCount * Constants.SPRINTING_STRAIN_INCREASE_PER_HEX;
+
+            strain.CurrentStrain += strainIncrease;
+
+            if (strain.CurrentStrain > Constants.MAX_STRAIN_PER_ENTITY)
+                strain.CurrentStrain = Constants.MAX_STRAIN_PER_ENTITY;
+
+            strain.StrainDecreaseCooldown = Constants.STRAIN_DECREASE_COOLDOWN_SECONDS;
         }
-
-        protected override void Update(float delta, in Entity entity)
+        else
         {
-            ref var strain = ref entity.Get<StrainAffected>();
-            ref var organelles = ref entity.Get<OrganelleContainer>();
-
-            if (strain.IsUnderStrain)
+            if (strain.StrainDecreaseCooldown <= Mathf.Epsilon)
             {
-                var strainIncrease = Constants.SPRINTING_STRAIN_INCREASE_PER_SECOND * delta;
-                strainIncrease += organelles.HexCount * Constants.SPRINTING_STRAIN_INCREASE_PER_HEX;
-
-                strain.CurrentStrain += strainIncrease;
-
-                if (strain.CurrentStrain > Constants.MAX_STRAIN_PER_ENTITY)
-                    strain.CurrentStrain = Constants.MAX_STRAIN_PER_ENTITY;
-
-                strain.StrainDecreaseCooldown = Constants.STRAIN_DECREASE_COOLDOWN_SECONDS;
+                ReduceStrain(ref strain, delta);
             }
             else
             {
-                if (strain.StrainDecreaseCooldown <= Mathf.Epsilon)
-                {
-                    ReduceStrain(ref strain, delta);
-                }
-                else
-                {
-                    strain.StrainDecreaseCooldown -= delta;
-                    ReduceStrain(ref strain, delta, Constants.PASSIVE_STRAIN_DECREASE_PRE_COOLDOWN_DIVISOR);
-                }
+                strain.StrainDecreaseCooldown -= delta;
+                ReduceStrain(ref strain, delta, Constants.PASSIVE_STRAIN_DECREASE_PRE_COOLDOWN_DIVISOR);
             }
         }
+    }
 
-        private void ReduceStrain(ref StrainAffected strain, float delta, float divisor = 1.0f)
-        {
-            strain.CurrentStrain -= Constants.PASSIVE_STRAIN_DECREASE_PER_SECOND * delta / divisor;
+    private void ReduceStrain(ref StrainAffected strain, float delta, float divisor = 1.0f)
+    {
+        strain.CurrentStrain -= Constants.PASSIVE_STRAIN_DECREASE_PER_SECOND * delta / divisor;
 
-            if (strain.CurrentStrain < 0)
-                strain.CurrentStrain = 0;
-        }
+        if (strain.CurrentStrain < 0)
+            strain.CurrentStrain = 0;
     }
 }
