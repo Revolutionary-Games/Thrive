95d788f8ba351be1fd5b988f871d2c8c4a02ea98
C09 <100433934+CI09@users.noreply.github.com>
2024-07-25 16:51:15 +0200

    Try update CompoundCloudPlane.cs

--- src/microbe_stage/CompoundCloudPlane.cs
+++ src/microbe_stage/CompoundCloudPlane.cs
@@ -401,7 +401,7 @@ public partial class CompoundCloudPlane : CsgMesh3D, ISaveLoadedTracked
                     newValue = seenCurrentAmount + density;
                 }
                 while (System.Threading.Interlocked.CompareExchange(
-                 ref Density[x, y].X, newValue, seenCurrentAmount) != seenCurrentAmount);
+                    ref Density[x, y].X, newValue, seenCurrentAmount) != seenCurrentAmount);
 
                 break;
             }
@@ -427,7 +427,7 @@ public partial class CompoundCloudPlane : CsgMesh3D, ISaveLoadedTracked
                     newValue = seenCurrentAmount + density;
                 }
                 while (System.Threading.Interlocked.CompareExchange(ref Density[x, y].Z,
-                 newValue, seenCurrentAmount) != seenCurrentAmount);
+                    newValue, seenCurrentAmount) != seenCurrentAmount);
 
                 break;
             }
