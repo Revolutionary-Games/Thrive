ea4d92b9fbb688f72a5f0188a012d04e4a1c6c12
CI09 <c009.1234.c009@gmail.com>
2024-08-04 15:12:51 +0200

    Attempt at making all cilia parts change colors

--- src/microbe_stage/OrganelleMeshWithChildren.cs
+++ src/microbe_stage/OrganelleMeshWithChildren.cs
@@ -1,22 +1,42 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Godot;
+using Godot.Collections;
 
 /// <summary>
 ///   Applies organelle shader parameters to child nodes
 /// </summary>
 public partial class OrganelleMeshWithChildren : MeshInstance3D
 {
+#pragma warning disable CA2213
+    [Export]
+    private Array<GeometryInstance3D>? meshChildren = null!;
+#pragma warning restore CA2213
+
     public void GetChildrenMaterials(List<ShaderMaterial> result, bool quiet = false)
     {
         bool found = false;
 
-        foreach (var mesh in GetChildren().OfType<GeometryInstance3D>())
+        if (meshChildren == null)
         {
-            if (mesh.MaterialOverride is ShaderMaterial shaderMaterial)
+            foreach (var mesh in GetChildren().OfType<GeometryInstance3D>())
             {
-                result.Add(shaderMaterial);
-                found = true;
+                if (mesh.MaterialOverride is ShaderMaterial shaderMaterial)
+                {
+                    result.Add(shaderMaterial);
+                    found = true;
+                }
+            }
+        }
+        else
+        {
+            foreach (var mesh in meshChildren)
+            {
+                if (mesh.MaterialOverride is ShaderMaterial shaderMaterial)
+                {
+                    result.Add(shaderMaterial);
+                    found = true;
+                }
             }
         }
 
