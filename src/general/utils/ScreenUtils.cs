f66ea70612d6c4b445304fcffc0637556e54ae27
dligr <dellpls43@gmail.com>
2024-08-05 00:13:36 +0300

    Format changes

--- src/general/utils/ScreenUtils.cs
+++ src/general/utils/ScreenUtils.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Godot;
+﻿using Godot;
 
 /// <summary>
 ///   Utility for screen and view-related effects
@@ -9,10 +8,7 @@ public static class ScreenUtils
     /// <summary>
     ///   Applies barrel distortion to the given position
     /// </summary>
-    /// <param name="pos">
-    ///   Position in the range of [0; resolution.X] for x and [0; resolution.Y] for y
-    /// </param>
-    public static Vector2 BarrelDistortion(Vector2 pos, float distortion, Vector2 viewportSize)
+    public static Vector2 BarrelDistortion(Vector2 screenPos, float distortion, Vector2 viewportSize)
     {
         Vector2 resolution = DisplayServer.WindowGetSize();
 
@@ -23,20 +19,20 @@ public static class ScreenUtils
         distortion *= distortionMultiplier;
 
         // Convert from [0, size] to [0; 1]
-        pos /= viewportSize;
+        screenPos /= viewportSize;
 
         Vector2 oversizeVector = Distort(new Vector2(1.0f, 1.0f), distortion);
-        pos = RemapVector(pos, new Vector2(1.0f, 1.0f) - oversizeVector, oversizeVector);
+        screenPos = RemapVector(screenPos, new Vector2(1.0f, 1.0f) - oversizeVector, oversizeVector);
 
         // Convert from [0, 1] to [-1; 1]
-        pos = (pos * 2.0f) - new Vector2(1.0f, 1.0f);
-        pos = Distort(pos, distortion * 1.25f);
+        screenPos = (screenPos * 2.0f) - new Vector2(1.0f, 1.0f);
+        screenPos = Distort(screenPos, distortion * 1.25f);
 
         // Return to [0, size]
-        pos = (pos + new Vector2(1.0f, 1.0f)) / 2.0f;
-        pos *= viewportSize;
+        screenPos = (screenPos + new Vector2(1.0f, 1.0f)) / 2.0f;
+        screenPos *= viewportSize;
 
-        return pos;
+        return screenPos;
     }
 
     private static Vector2 Distort(Vector2 pos, float distortion)
