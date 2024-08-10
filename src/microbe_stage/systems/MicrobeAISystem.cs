50e0c60337b8af54d82b2b145214b4ecfbec75d5
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-08-10 13:38:15 +0300

    Fix microbe chunk cache build assuming everything has TimedLife
    when in reality only a few chunk-like things do

--- src/microbe_stage/systems/MicrobeAISystem.cs
+++ src/microbe_stage/systems/MicrobeAISystem.cs
@@ -1272,11 +1272,14 @@ public sealed class MicrobeAISystem : AEntitySetSystem<float>, ISpeciesMemberLoc
 
             foreach (ref readonly var chunk in chunksSet.GetEntities())
             {
-                // Ignore already despawning chunks
-                ref var timed = ref chunk.Get<TimedLife>();
+                if (chunk.Has<TimedLife>())
+                {
+                    // Ignore already despawning chunks
+                    ref var timed = ref chunk.Get<TimedLife>();
 
-                if (timed.TimeToLiveRemaining <= 0)
-                    continue;
+                    if (timed.TimeToLiveRemaining <= 0)
+                        continue;
+                }
 
                 // Ignore chunks that wouldn't yield any useful compounds when absorbing
                 ref var compounds = ref chunk.Get<CompoundStorage>();
