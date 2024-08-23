224145db935d177737d9c779331fc50877ed8970
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-08-23 15:25:50 +0300

    Revert MigrateSpecies.cs to master branch version
    as I had already done further tweaks there based on your earlier commit that I cherry-picked

--- src/auto-evo/steps/MigrateSpecies.cs
+++ src/auto-evo/steps/MigrateSpecies.cs
@@ -40,24 +40,59 @@ public class MigrateSpecies : IRunStep
 
         // To limit species migrations to the actual limit, we need to pick random patches to try to migrate from
         // to ensure all patches have a chance to eventually send some population
-        var attemptsLeft = worldSettings.AutoEvoConfiguration.MoveAttemptsPerSpecies;
+        int attemptsLeft = worldSettings.AutoEvoConfiguration.MoveAttemptsPerSpecies;
+
+        var sourcePatches = new List<Patch>();
+
+        // To prevent generating duplicate migrations this needs to remember target patches. This limitation is a
+        // data model limitation where a single target patch cannot have a species migrate to it form multiple patches
+        // at once.
+        var usedTargets = new HashSet<Patch>();
 
         foreach (var patch in map.Patches.Values)
         {
             if (!patch.SpeciesInPatch.ContainsKey(species))
                 continue;
 
+            sourcePatches.Add(patch);
+        }
+
+        sourcePatches.Shuffle(random);
+
+        // To not randomly pick the same adjacent patch multiple times as a migration target
+        var shuffledNeighbours = new List<Patch>();
+
+        foreach (var patch in sourcePatches)
+        {
+            if (attemptsLeft <= 0)
+            {
+                // Stop checking once all attempts are used up even if there are still patches this species is in
+                break;
+            }
+
             var population = patch.GetSpeciesSimulationPopulation(species);
 
             if (population < Constants.AUTO_EVO_MINIMUM_MOVE_POPULATION)
                 continue;
 
-            for (var i = 0; i < attemptsLeft; ++i)
+            // Try all neighbour patches in random order
+            shuffledNeighbours.Clear();
+            foreach (var adjacent in patch.Adjacent)
             {
-                // Select a random adjacent target patch
-                // TODO: could prefer patches this species is not already in or about to go extinct, or really anything
-                // other than random selection
-                var target = patch.Adjacent.Random(random);
+                shuffledNeighbours.Add(adjacent);
+            }
+
+            // TODO: could prefer patches this species is not already in or about to go extinct, or really anything
+            // other than random selection
+            shuffledNeighbours.Shuffle(random);
+
+            foreach (var target in shuffledNeighbours)
+            {
+                // Skip checking population send to the same patch multiple times
+                if (usedTargets.Contains(target))
+                    continue;
+
+                --attemptsLeft;
                 var targetMiche = results.GetMicheForPatch(target);
 
                 // Calculate random amount of population to send
@@ -67,8 +102,14 @@ public class MigrateSpecies : IRunStep
                 if (moveAmount > 0 && targetMiche.InsertSpecies(species, patch, null, cache, true, speciesWorkMemory))
                 {
                     results.AddMigrationResultForSpecies(species, new SpeciesMigration(patch, target, moveAmount));
+                    usedTargets.Add(target);
+
+                    // Only one migration per patch
                     break;
                 }
+
+                if (attemptsLeft <= 0)
+                    break;
             }
         }
 
