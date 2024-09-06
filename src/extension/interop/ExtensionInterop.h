3082dbc5de96449db35052ca55e657d362812a43
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-09-06 15:00:23 +0300

    Clarify one method and improve build a bit

--- src/extension/interop/ExtensionInterop.h
+++ src/extension/interop/ExtensionInterop.h
@@ -13,5 +13,9 @@ extern "C"
     // ------------------------------------ //
     // General
 
+    /// \brief Gets the version of this. This requires an existing instance of ThriveConfig object to be created
+    ///
+    /// This mostly exists to check everything was loaded fine and can now be used
+    /// \return The version number
     [[maybe_unused]] THRIVE_NATIVE_API int32_t ExtensionGetVersion(ThriveConfig* thriveConfig);
 }
