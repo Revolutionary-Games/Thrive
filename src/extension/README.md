ddc70258933ee689e953a707907ee0f27d1ef3ae
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-09-06 17:23:06 +0300

    Added a GDExtension module for Thrive for future use (#5464)
    * Added godot-cpp as a submodule
    
    * First setup of build scripts with the godot-cpp module
    
    * Basic setup for the GDExtension code
    
    * Building and bringing back installing for native libraries
    
    * Correct native lib paths
    
    * Improve the build a tiny bit
    
    * Try to bind some methods
    
    * Updated Jolt version
    
    * Wrote the interop and setup calling code for the extension
    
    * Updated build scripts a bit
    
    * Tried some TaskSystem.hpp changes but they didn't help
    
    * Add CI native libs install step which is necessary
    
    now for GDExtension copying to work
    
    * Fixed undefined symbol coming from Jolt due to no RTTI
    
    turns out this was just a new option
    
    * Bump the updated LLVM version
    
    * Implemented intercommunication passing
    
    * Made extension shutdown work and cleaned up the code a bit
    
    * Fixed the extension build for Windows
    
    * Updated native script to work on both debug and non-debug at once
    
    to simplify the process now that debug variants are basically always needed
    
    * Switched to using the final library name in editor
    
    this was done to make sure there isn't future work needed to make debug symbol matching work
    
    * Improved local library install
    
    * Load GDExtension into C# through already loaded info
    
    to avoid any path resolving issues that may cause the data to be inconsistent between what Godot loads and what we load
    
    * Updated packaging script for the new requirements
    
    * Add safety check about library load failure
    
    * Fixed upload native libs script
    
    * Clarify one method and improve build a bit
    
    * Try to get Extension API to export symbols for Windows
    
    * Actually got the previous thing working with a correct windows check
    
    * Made extension version check method catch errors
    
    * Make distributable debug warning print just once

---
+++ src/extension/README.md
@@ -0,0 +1,6 @@
+# Thrive GDExtension
+
+This folder contains a [GDExtension](https://docs.godotengine.org/en/stable/tutorials/scripting/gdextension/index.html)
+for Thrive written in C++.
+
+Other non-Godot interfacing native code is in the "native" sibling folder.
---
+++ src/extension/core/RegisterThriveTypes.cpp
@@ -0,0 +1,50 @@
+// ------------------------------------ //
+#include "RegisterThriveTypes.hpp"
+
+#include "ThriveConfig.hpp"
+
+// ------------------------------------ //
+namespace Thrive
+{
+void InitializeThriveModule(godot::ModuleInitializationLevel level)
+{
+    if (level != godot::MODULE_INITIALIZATION_LEVEL_SCENE)
+    {
+        return;
+    }
+
+    GDREGISTER_CLASS(Thrive::ThriveConfig);
+}
+
+void UnInitializeThriveModule(godot::ModuleInitializationLevel level)
+{
+    if (level != godot::MODULE_INITIALIZATION_LEVEL_SCENE)
+    {
+        return;
+    }
+}
+
+} // namespace Thrive
+
+extern "C"
+{
+    // Ignore diagnostic to use the exact same syntax as the Godot example
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "misc-misplaced-const"
+
+    // This is how this module is initialized for use in Godot
+    // GDE_EXPORT does the same as our macro API_EXPORT
+    GDExtensionBool GDE_EXPORT ThriveExtensionLibraryInit(GDExtensionInterfaceGetProcAddress p_get_proc_address,
+        const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization* r_initialization)
+    {
+        godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+
+        init_obj.register_initializer(Thrive::InitializeThriveModule);
+        init_obj.register_terminator(Thrive::UnInitializeThriveModule);
+        init_obj.set_minimum_library_initialization_level(godot::MODULE_INITIALIZATION_LEVEL_SCENE);
+
+        return init_obj.init();
+    }
+
+#pragma clang diagnostic pop
+}
