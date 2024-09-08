306749b538b455934ad4038529b3257bb2d01b29
dligr <dellpls43@gmail.com>
2024-09-08 15:02:01 +0300

    Document patch getters

--- src/microbe_stage/editor/IEditorReportData.cs
+++ src/microbe_stage/editor/IEditorReportData.cs
@@ -3,10 +3,20 @@
 /// </summary>
 public interface IEditorReportData : IEditor
 {
+    /// <summary>
+    ///   Returns the patch the player is currently in
+    /// </summary>
     public Patch CurrentPatch { get; }
 
+    /// <summary>
+    ///   Returns the patch the player wants to move after editing. If the player doesn't want to move, returns
+    ///   the same as <see cref="CurrentPatch"/>
+    /// </summary>
     public Patch? TargetPatch { get; }
 
+    /// <summary>
+    ///   Returns the patch the player selected in the patch map
+    /// </summary>
     public Patch? SelectedPatch { get; }
 
     public void SendAutoEvoResultsToReportComponent();
