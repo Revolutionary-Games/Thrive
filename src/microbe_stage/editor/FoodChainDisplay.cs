cb2213406a79c5434847c273e08da4fcdcf7c3de
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-09-26 15:30:26 +0300

    Initial GUI files for the food chain tab

---
+++ src/microbe_stage/editor/FoodChainDisplay.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using AutoEvo;
+using Godot;
+
+/// <summary>
+///   Displays a food chain from auto-evo results in the GUI for the player to inspect
+/// </summary>
+public partial class FoodChainDisplay : Control
+{
+    private readonly List<SpeciesResultButton> resultButtons = new();
+
+#pragma warning disable CA2213
+    private PackedScene speciesResultButtonScene = null!;
+#pragma warning restore CA2213
+
+    private RunResults? lastResults;
+    private Patch? lastPatch;
+
+    public override void _Ready()
+    {
+        speciesResultButtonScene = GD.Load<PackedScene>("res://src/microbe_stage/editor/SpeciesResultButton.tscn");
+    }
+
+    public override void _Process(double delta)
+    {
+    }
+
+    public override void _Draw()
+    {
+        base._Draw();
+
+        DrawLine(new Vector2(0, 0), new Vector2(Size.X, Size.Y), Colors.Aquamarine, 2, true);
+    }
+
+    public void DisplayFoodChainIfRequired(RunResults autoEvoResults, Patch forPatch)
+    {
+        // Only update if data has changed
+        if (autoEvoResults == lastResults && forPatch == lastPatch)
+            return;
+
+        lastResults = autoEvoResults;
+        lastPatch = forPatch;
+
+        // TODO: reuse nodes that can be to make this faster
+        this.QueueFreeChildren();
+        resultButtons.Clear();
+
+        // autoEvoResults.
+
+        // Queue a redraw to draw all the connection lines again
+        QueueRedraw();
+    }
+}
