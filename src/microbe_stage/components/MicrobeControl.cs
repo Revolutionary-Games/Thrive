ed17bd5003108b7721bf659b299709fb4f32dd05
dligr <dellpls43@gmail.com>
2024-09-26 19:16:20 +0300

    Fix mucocyst activation for colonies

--- src/microbe_stage/components/MicrobeControl.cs
+++ src/microbe_stage/components/MicrobeControl.cs
@@ -347,19 +347,38 @@ public static class MicrobeControlHelpers
 
         if (state)
         {
-            // Don't allow spamming if not enough mucocyst. This is inside this if to allow exiting mucocyst shield
-            // mode even without enough mucilage remaining.
-            if (availableCompounds.Compounds.GetCompoundAmount(mucilageCompound) < Constants.MUCOCYST_MINIMUM_MUCILAGE)
-                return;
+            if (entity.Has<MicrobeColony>())
+            {
+                ref var colony = ref entity.Get<MicrobeColony>();
 
-            control.State = MicrobeState.MucocystShield;
+                if (colony.ColonyState != MicrobeState.MucocystShield)
+                {
+                    colony.ColonyState = MicrobeState.MucocystShield;
 
-            // TODO: maybe it is too loud if all cells in a colony play the sound?
-            entity.Get<SoundEffectPlayer>().PlaySoundEffect("res://assets/sounds/soundeffects/microbe-slime-jet.ogg");
+                    foreach (var colonyMember in colony.ColonyMembers)
+                    {
+                        // The IsAlive check should be unnecessary here but as this is a general method there's this
+                        // extra safety against crashing due to colony bugs
+                        if (colonyMember != entity && colonyMember.IsAlive)
+                        {
+                            // Don't allow spamming if not enough mucocyst. This is inside this if to allow exiting mucocyst shield
+                            // mode even without enough mucilage remaining.
+                            if (availableCompounds.Compounds.GetCompoundAmount(mucilageCompound) < Constants.MUCOCYST_MINIMUM_MUCILAGE)
+                                return;
+
+                            ref var memberControl = ref colonyMember.Get<MicrobeControl>();
+                            memberControl.State = MicrobeState.MucocystShield;
+
+                            // TODO: maybe it is too loud if all cells in a colony play the sound?
+                            entity.Get<SoundEffectPlayer>().PlaySoundEffect("res://assets/sounds/soundeffects/microbe-slime-jet.ogg");
+                        }
+                    }
+                }
+            }
         }
         else
         {
-            control.State = MicrobeState.Normal;
+            SetStateColonyAware(ref control, in entity, MicrobeState.Normal);
         }
     }
 
