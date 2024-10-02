344f8975383362e70018f4bcc81c68a1fe893113
adQuid <28126232+adQuid@users.noreply.github.com>
2024-10-02 09:07:30 -0400

    add organelle upgrade mutation strategy

---
+++ src/auto-evo/mutation_strategy/UpgradeOrganelle.cs
@@ -0,0 +1,42 @@
+﻿using AutoEvo;
+using System;
+using System.Collections.Frozen;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class UpgradeOrganelle : IMutationStrategy<MicrobeSpecies>
+{
+
+    private readonly FrozenSet<OrganelleDefinition> allOrganelles;
+    private IComponentSpecificUpgrades upgrade;
+
+    public UpgradeOrganelle(Func<OrganelleDefinition, bool> criteria, IComponentSpecificUpgrades upgrade)
+    {
+        allOrganelles = SimulationParameters.Instance.GetAllOrganelles().Where(criteria).ToFrozenSet();
+        this.upgrade = upgrade;
+    }
+
+    public bool Repeatable => true;
+
+    public List<Tuple<MicrobeSpecies, float>>? MutationsOf(MicrobeSpecies baseSpecies, float mp, bool lawk,
+    Random random)
+    {
+        var mutated = new List<Tuple<MicrobeSpecies, float>>();
+
+        MicrobeSpecies newSpecies = (MicrobeSpecies)baseSpecies.Clone();
+
+        foreach (OrganelleTemplate organelle in newSpecies.Organelles.Where(x => allOrganelles.Contains(x.Definition)))
+        {
+
+            if (organelle.Upgrades == null)
+            {
+                organelle.Upgrades = new OrganelleUpgrades();
+            }
+            organelle.Upgrades.CustomUpgradeData = upgrade;
+        }
+
+        mutated.Add(new Tuple<MicrobeSpecies, float>(newSpecies, mp));
+
+        return mutated;
+    }
+}
