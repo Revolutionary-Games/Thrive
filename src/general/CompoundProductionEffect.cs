c4b819735051ef3bc533f608934565fe322177b0
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-10-02 10:56:55 +0300

    Refactored CompoundDiffusionEffect to be cleaner code
    as it was looking up things constantly and not going through the proper methods for compound retrieval and process handling

--- src/general/CompoundProductionEffect.cs
+++ src/general/CompoundProductionEffect.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json;
 using Systems;
 
 /// <summary>
-///   An effect reducing the glucose amount
+///   Removes and adds compounds to patches based on alive cells binding up resources and also producing compounds
 /// </summary>
 [JSONDynamicTypeAllowed]
 public class CompoundProductionEffect : IWorldEffect
@@ -33,54 +33,56 @@ public class CompoundProductionEffect : IWorldEffect
         var environmentalModifier = 100.0f;
         var modifier = 0.0000005f;
 
+        var totalAdded = new Dictionary<Compound, float>();
+
         foreach (var patchKeyValue in targetWorld.Map.Patches)
         {
-            var totalAdded = new Dictionary<Compound, float>();
+            totalAdded.Clear();
 
             var patch = patchKeyValue.Value;
 
             foreach (var species in patch.SpeciesInPatch)
             {
+                // TODO: this should take the actual reproduction cost into account, not just the base cost which
+                // doesn't include the organelle costs
                 foreach (var reproductionCompound in species.Key.BaseReproductionCost)
                 {
                     var add = -species.Value * modifier * reproductionCompound.Value *
                         patch.Biome.AverageCompounds[reproductionCompound.Key].Density;
-                    if (totalAdded.ContainsKey(reproductionCompound.Key))
-                    {
-                        totalAdded[reproductionCompound.Key] += add;
-                    }
-                    else
-                    {
-                        totalAdded.Add(reproductionCompound.Key, add);
-                    }
+
+                    totalAdded.TryGetValue(reproductionCompound.Key, out var existing);
+                    totalAdded[reproductionCompound.Key] = existing + add;
                 }
 
                 // Microbe species handling
                 if (species.Key is MicrobeSpecies microbeSpecies)
                 {
-                    foreach (var organelle in microbeSpecies.Organelles.Organelles)
+                    var organelles = microbeSpecies.Organelles.Organelles;
+                    int count = organelles.Count;
+
+                    for (int i = 0; i < count; ++i)
                     {
-                        if (organelle.Definition.Processes == null)
-                            continue;
+                        var organelle = organelles[i];
 
-                        foreach (var process in organelle.Definition.Processes!)
+                        foreach (var process in organelle.Definition.RunnableProcesses)
                         {
-                            var bioProcess = SimulationParameters.Instance.GetBioProcess(process.Key);
-
-                            var rate = ProcessSystem.CalculateProcessMaximumSpeed(new TweakedProcess(bioProcess),
+                            var rate = ProcessSystem.CalculateProcessMaximumSpeed(process,
                                 patch.Biome, CompoundAmountType.Biome, true);
 
                             // Inputs
-                            foreach (var input in bioProcess.Inputs)
+                            foreach (var input in process.Process.Inputs)
                             {
+                                // TODO: why would this remove sunlight? (and not ignore it here)
                                 if (input.Key.ID == Compound.Temperature)
                                     continue;
 
                                 float add;
 
-                                if (patch.Biome.AverageCompounds.ContainsKey(input.Key.ID))
+                                // TODO: is it really the case that this should use the day-averaged compound values?
+                                if (patch.Biome.TryGetCompound(input.Key.ID, CompoundAmountType.Average,
+                                        out var patchAmount))
                                 {
-                                    if (patch.Biome.AverageCompounds[input.Key.ID].Ambient > 0)
+                                    if (patchAmount.Ambient > 0)
                                     {
                                         add = -species.Value * modifier * input.Value * environmentalModifier
                                             * rate.CurrentSpeed;
@@ -88,7 +90,7 @@ public class CompoundProductionEffect : IWorldEffect
                                     else
                                     {
                                         add = -species.Value * modifier * input.Value * inputModifier *
-                                            patch.Biome.AverageCompounds[input.Key.ID].Density * rate.CurrentSpeed;
+                                            patchAmount.Density * rate.CurrentSpeed;
                                     }
                                 }
                                 else
@@ -96,22 +98,17 @@ public class CompoundProductionEffect : IWorldEffect
                                     add = -species.Value * modifier * input.Value * inputModifier;
                                 }
 
-                                if (totalAdded.ContainsKey(input.Key.ID))
-                                {
-                                    totalAdded[input.Key.ID] += add;
-                                }
-                                else
-                                {
-                                    totalAdded.Add(input.Key.ID, add);
-                                }
+                                totalAdded.TryGetValue(input.Key.ID, out var existing);
+                                totalAdded[input.Key.ID] = existing + add;
                             }
 
                             // Outputs
-                            foreach (var output in bioProcess.Outputs)
+                            foreach (var output in process.Process.Outputs)
                             {
-                                if (output.Key.IsAgent || output.Key.ID == Compound.ATP
-                                    || output.Key.ID == Compound.Temperature)
+                                if (output.Key.IsAgent || output.Key.ID is Compound.ATP or Compound.Temperature)
+                                {
                                     continue;
+                                }
 
                                 float add;
 
@@ -126,14 +123,8 @@ public class CompoundProductionEffect : IWorldEffect
                                         * patch.Biome.AverageCompounds[output.Key.ID].Density * rate.CurrentSpeed;
                                 }
 
-                                if (totalAdded.ContainsKey(output.Key.ID))
-                                {
-                                    totalAdded[output.Key.ID] += add;
-                                }
-                                else
-                                {
-                                    totalAdded.Add(output.Key.ID, add);
-                                }
+                                totalAdded.TryGetValue(output.Key.ID, out var existing);
+                                totalAdded[output.Key.ID] = existing + add;
                             }
                         }
                     }
@@ -141,28 +132,22 @@ public class CompoundProductionEffect : IWorldEffect
             }
 
             // Apply results
-            foreach (var compound in patch.Biome.ChangeableCompounds)
+            foreach (var entry in totalAdded)
             {
-                var tweakedBiomeConditions = compound.Value;
+                var tweakedBiomeConditions = patch.Biome.GetCompound(entry.Key, CompoundAmountType.Biome);
 
-                if (totalAdded.ContainsKey(compound.Key))
+                if (SimulationParameters.Instance.GetCompoundDefinition(entry.Key).IsEnvironmental)
                 {
-                    if (SimulationParameters.Instance.GetCompoundDefinition(compound.Key).IsEnvironmental)
-                    {
-                        tweakedBiomeConditions.Ambient = Math.Clamp(
-                            patch.BiomeTemplate.Conditions.ChangeableCompounds[compound.Key].Ambient
-                            + totalAdded[compound.Key], 0, 1);
-                    }
-                    else
-                    {
-                        tweakedBiomeConditions.Density = Math.Clamp(
-                            patch.BiomeTemplate.Conditions.ChangeableCompounds[compound.Key].Density
-                            + totalAdded[compound.Key], 0, 1);
-                    }
+                    tweakedBiomeConditions.Ambient =
+                        Math.Clamp(tweakedBiomeConditions.Ambient + totalAdded[entry.Key], 0, 1);
+                }
+                else
+                {
+                    tweakedBiomeConditions.Density =
+                        Math.Clamp(tweakedBiomeConditions.Density + totalAdded[entry.Key], 0, 1);
                 }
 
-                targetWorld.Map.Patches[patchKeyValue.Key].Biome.ModifyLongTermCondition(compound.Key,
-                    tweakedBiomeConditions);
+                patch.Biome.ModifyLongTermCondition(entry.Key, tweakedBiomeConditions);
             }
         }
     }
