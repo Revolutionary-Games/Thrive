e91f9ce3768bb11dfde6ccd9a9e57a802e05dd7b
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-10-14 15:40:35 +0300

    Added a volcanism effect to produce some constant CO2

--- src/general/GameWorld.cs
+++ src/general/GameWorld.cs
@@ -89,6 +89,7 @@ public class GameWorld : ISaveLoadable
             // A limited photosynthesis-only compound production effects that were added as these are much simpler than
             // the full effect to get balanced well enough
             TimedEffects.RegisterEffect("photosynthesis_production", new PhotosynthesisProductionEffect(this));
+            TimedEffects.RegisterEffect("volcanism", new VolcanismEffect(this));
             TimedEffects.RegisterEffect("compound_diffusion", new CompoundDiffusionEffect(this));
 
             // Register glucose reduction
