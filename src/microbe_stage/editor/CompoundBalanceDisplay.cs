07641015a35f2f4b3219767ecd5ac0c832965c9b
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-10-17 10:58:11 +0300

    Don't show gas compounds in cell compound balances or storage
    as gases aren't stored by cells so it doesn't make sense to show how long it takes to fill up on them as it isn't possible to do so

--- src/microbe_stage/editor/CompoundBalanceDisplay.cs
+++ src/microbe_stage/editor/CompoundBalanceDisplay.cs
@@ -45,8 +45,14 @@ public partial class CompoundBalanceDisplay : VBoxContainer
     {
         childCache.UnMarkAll();
 
+        var simulationParameters = SimulationParameters.Instance;
+
         foreach (var entry in balances)
         {
+            // Skip any gas compounds as they aren't stored by cells so no point in showing in the balance
+            if (simulationParameters.GetCompoundDefinition(entry.Key).IsGas)
+                continue;
+
             var compoundControl = childCache.GetChild(entry.Key);
             var amount = entry.Value.Balance;
             compoundControl.Amount = amount;
