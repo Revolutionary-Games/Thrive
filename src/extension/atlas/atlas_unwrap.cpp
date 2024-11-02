355c4c453af01d9a6758c72d36f4057c62f7a902
dligr <dellpls43@gmail.com>
2024-11-02 16:57:58 +0300

    Refactor variable names according to the naming convention

--- src/extension/atlas/atlas_unwrap.cpp
+++ src/extension/atlas/atlas_unwrap.cpp
@@ -52,85 +52,85 @@ bool Thrive::Unwrap(godot::ArrayMesh& mesh, float texelSize)
     LocalVector<float> normals;
     LocalVector<int> indices;
 
-    uint64_t surface_format = mesh.surface_get_format(0);
+    uint64_t surfaceFormat = mesh.surface_get_format(0);
 
     Array arrays = mesh.surface_get_arrays(0);
 
-    PackedVector3Array rvertices = arrays[Mesh::ARRAY_VERTEX];
-    uint64_t vc = rvertices.size();
+    PackedVector3Array initialVertices = arrays[Mesh::ARRAY_VERTEX];
+    uint64_t vertexCount = initialVertices.size();
 
-    PackedVector3Array rnormals = arrays[Mesh::ARRAY_NORMAL];
+    PackedVector3Array initialNormals = arrays[Mesh::ARRAY_NORMAL];
 
-    vertices.resize((uint32_t)(vc * 3));
-    normals.resize((uint32_t)(vc * 3));
+    vertices.resize((uint32_t)(vertexCount * 3));
+    normals.resize((uint32_t)(vertexCount * 3));
 
-    for (int j = 0; j < vc; j++)
+    for (int j = 0; j < vertexCount; j++)
     {
-        Vector3 v = rvertices[j];
-        Vector3 n = rnormals[j];
+        Vector3 vertex = initialVertices[j];
+        Vector3 normal = initialNormals[j];
 
-        vertices[j * 3 + 0] = v.x;
-        vertices[j * 3 + 1] = v.y;
-        vertices[j * 3 + 2] = v.z;
-        normals[j * 3 + 0] = n.x;
-        normals[j * 3 + 1] = n.y;
-        normals[j * 3 + 2] = n.z;
+        vertices[j * 3 + 0] = vertex.x;
+        vertices[j * 3 + 1] = vertex.y;
+        vertices[j * 3 + 2] = vertex.z;
+        normals[j * 3 + 0] = normal.x;
+        normals[j * 3 + 1] = normal.y;
+        normals[j * 3 + 2] = normal.z;
     }
 
-    PackedInt32Array rindices = arrays[Mesh::ARRAY_INDEX];
-    uint64_t ic = rindices.size();
+    PackedInt32Array initialIndices = arrays[Mesh::ARRAY_INDEX];
+    uint64_t indexCount = initialIndices.size();
 
     // Taken from xatlas.h
     float eps = 1.19209290e-7F;
-    for (int j = 0; j < ic / 3; j++)
+    for (int j = 0; j < indexCount / 3; j++)
     {
-        Vector3 p0 = rvertices[rindices[j * 3 + 0]];
-        Vector3 p1 = rvertices[rindices[j * 3 + 1]];
-        Vector3 p2 = rvertices[rindices[j * 3 + 2]];
+        Vector3 p0 = initialVertices[initialIndices[j * 3 + 0]];
+        Vector3 p1 = initialVertices[initialIndices[j * 3 + 1]];
+        Vector3 p2 = initialVertices[initialIndices[j * 3 + 2]];
 
         if ((p0 - p1).length_squared() < eps || (p1 - p2).length_squared() < eps || (p2 - p0).length_squared() < eps)
         {
             continue;
         }
 
-        indices.push_back(rindices[j * 3 + 0]);
-        indices.push_back(rindices[j * 3 + 1]);
-        indices.push_back(rindices[j * 3 + 2]);
+        indices.push_back(initialIndices[j * 3 + 0]);
+        indices.push_back(initialIndices[j * 3 + 1]);
+        indices.push_back(initialIndices[j * 3 + 2]);
     }
 
     // set up input mesh
-    xatlas::MeshDecl input_mesh;
-    input_mesh.indexData = indices.ptr();
-    input_mesh.indexCount = indices.size();
-    input_mesh.indexFormat = xatlas::IndexFormat::UInt32;
+    xatlas::MeshDecl inputMesh;
+    inputMesh.indexData = indices.ptr();
+    inputMesh.indexCount = indices.size();
+    inputMesh.indexFormat = xatlas::IndexFormat::UInt32;
 
-    input_mesh.vertexCount = vertices.size() / 3;
-    input_mesh.vertexPositionData = vertices.ptr();
-    input_mesh.vertexPositionStride = sizeof(float) * 3;
-    input_mesh.vertexNormalData = normals.ptr();
-    input_mesh.vertexNormalStride = sizeof(float) * 3;
-    input_mesh.vertexUvData = nullptr;
-    input_mesh.vertexUvStride = 0;
+    inputMesh.vertexCount = vertices.size() / 3;
+    inputMesh.vertexPositionData = vertices.ptr();
+    inputMesh.vertexPositionStride = sizeof(float) * 3;
+    inputMesh.vertexNormalData = normals.ptr();
+    inputMesh.vertexNormalStride = sizeof(float) * 3;
+    inputMesh.vertexUvData = nullptr;
+    inputMesh.vertexUvStride = 0;
 
-    xatlas::ChartOptions chart_options;
-    chart_options.fixWinding = true;
+    xatlas::ChartOptions chartOptions;
+    chartOptions.fixWinding = true;
 
     ERR_FAIL_COND_V_MSG(texelSize <= 0.0f, false, "Texel size must be greater than 0.");
 
-    xatlas::PackOptions pack_options;
-    pack_options.padding = 1;
+    xatlas::PackOptions packOptions;
+    packOptions.padding = 1;
 
     // Lightmap atlassing needs 2 for padding between meshes, so 4096-2
-    pack_options.maxChartSize = 1022;
-    pack_options.blockAlign = true;
-    pack_options.texelsPerUnit = 1.0f / texelSize;
+    packOptions.maxChartSize = 1022;
+    packOptions.blockAlign = true;
+    packOptions.texelsPerUnit = 1.0f / texelSize;
 
     xatlas::Atlas* atlas = xatlas::Create();
 
-    xatlas::AddMeshError err = xatlas::AddMesh(atlas, input_mesh, 1);
+    xatlas::AddMeshError err = xatlas::AddMesh(atlas, inputMesh, 1);
     ERR_FAIL_COND_V_MSG(err != xatlas::AddMeshError::Success, false, xatlas::StringForEnum(err));
 
-    xatlas::Generate(atlas, chart_options, pack_options);
+    xatlas::Generate(atlas, chartOptions, packOptions);
 
     ERR_FAIL_COND_V_MSG(atlas->chartCount == 0, false, "No charts generated");
 
@@ -147,72 +147,70 @@ bool Thrive::Unwrap(godot::ArrayMesh& mesh, float texelSize)
 
     mesh.clear_surfaces();
 
-    Ref<SurfaceTool> surfaces_tools;
-    surfaces_tools.instantiate();
-    surfaces_tools->begin(Mesh::PRIMITIVE_TRIANGLES);
+    Ref<SurfaceTool> surfacesTools;
+    surfacesTools.instantiate();
+    surfacesTools->begin(Mesh::PRIMITIVE_TRIANGLES);
+    indexCount = output.indexCount;
 
-    vc = output.vertexCount;
-    ic = output.indexCount;
+    PackedColorArray initialColors = arrays[Mesh::ARRAY_COLOR];
+    PackedVector2Array initialUV2 = arrays[Mesh::ARRAY_TEX_UV2];
+    PackedFloat32Array initialTangents = arrays[Mesh::ARRAY_TANGENT];
+    PackedInt32Array initialBones = arrays[Mesh::ARRAY_BONES];
+    PackedFloat32Array initialWeights = arrays[Mesh::ARRAY_WEIGHTS];
 
-    PackedColorArray rcolors = arrays[Mesh::ARRAY_COLOR];
-    PackedVector2Array r_uv2 = arrays[Mesh::ARRAY_TEX_UV2];
-    PackedFloat32Array rtangent = arrays[Mesh::ARRAY_TANGENT];
-    PackedInt32Array rbones = arrays[Mesh::ARRAY_BONES];
-    PackedFloat32Array rweights = arrays[Mesh::ARRAY_WEIGHTS];
-
-    for (int i = 0; i < ic; i += 3)
+    for (int i = 0; i < indexCount; i += 3)
     {
         for (int j = 0; j < 3; j++)
         {
             // Index of an original vertex corresponding to the generated one.
             int vertex_id = output.vertexArray[output.indexArray[i + j]].xref;
 
-            if (surface_format & Mesh::ARRAY_FORMAT_COLOR)
+            if (surfaceFormat & Mesh::ARRAY_FORMAT_COLOR)
             {
-                surfaces_tools->set_color(rcolors[vertex_id]);
+                surfacesTools->set_color(initialColors[vertex_id]);
             }
-            if (surface_format & Mesh::ARRAY_FORMAT_TEX_UV2)
+            if (surfaceFormat & Mesh::ARRAY_FORMAT_TEX_UV2)
             {
-                surfaces_tools->set_uv2(r_uv2[vertex_id]);
+                surfacesTools->set_uv2(initialUV2[vertex_id]);
             }
-            if (surface_format & Mesh::ARRAY_FORMAT_NORMAL)
+            if (surfaceFormat & Mesh::ARRAY_FORMAT_NORMAL)
             {
-                surfaces_tools->set_normal(rnormals[vertex_id]);
+                surfacesTools->set_normal(initialNormals[vertex_id]);
             }
-            if (surface_format & Mesh::ARRAY_FORMAT_TANGENT)
+            if (surfaceFormat & Mesh::ARRAY_FORMAT_TANGENT)
             {
                 Plane t;
-                t.normal = Vector3(rtangent[vertex_id * 4], rtangent[vertex_id * 4 + 1], rtangent[vertex_id * 4 + 2]);
-                t.d = rtangent[vertex_id * 4 + 3];
-                surfaces_tools->set_tangent(t);
+                t.normal = Vector3(initialTangents[vertex_id * 4], initialTangents[vertex_id * 4 + 1], initialTangents[vertex_id * 4 + 2]);
+                t.d = initialTangents[vertex_id * 4 + 3];
+                surfacesTools->set_tangent(t);
             }
-            if (surface_format & Mesh::ARRAY_FORMAT_BONES)
+            if (surfaceFormat & Mesh::ARRAY_FORMAT_BONES)
             {
-                surfaces_tools->set_bones(rbones.slice(vertex_id, vertex_id + 3));
+                surfacesTools->set_bones(initialBones.slice(vertex_id, vertex_id + 3));
             }
-            if (surface_format & Mesh::ARRAY_FORMAT_WEIGHTS)
+            if (surfaceFormat & Mesh::ARRAY_FORMAT_WEIGHTS)
             {
-                surfaces_tools->set_weights(rweights.slice(vertex_id, vertex_id + 3));
+                surfacesTools->set_weights(initialWeights.slice(vertex_id, vertex_id + 3));
             }
 
             Vector2 uv(output.vertexArray[output.indexArray[i + j]].uv[0] / w,
                 output.vertexArray[output.indexArray[i + j]].uv[1] / h);
 
-            surfaces_tools->set_uv(uv);
+            surfacesTools->set_uv(uv);
 
-            surfaces_tools->add_vertex(rvertices[vertex_id]);
+            surfacesTools->add_vertex(initialVertices[vertex_id]);
         }
     }
 
-    surfaces_tools->index();
-    callable_mp_static(&FinishUnwrap).call_deferred(surfaces_tools, Ref<ArrayMesh>(&mesh), surface_format);
+    surfacesTools->index();
+    callable_mp_static(&FinishUnwrap).call_deferred(surfacesTools, Ref<ArrayMesh>(&mesh), surfaceFormat);
 
     xatlas::Destroy(atlas);
 
     return true;
 }
 
-static void Thrive::FinishUnwrap(Ref<SurfaceTool> surfaces_tools, Ref<ArrayMesh> mesh, uint64_t surface_format)
+void Thrive::FinishUnwrap(Ref<SurfaceTool> surfacesTools, Ref<ArrayMesh> mesh, uint64_t surfaceFormat)
 {
-    surfaces_tools->commit(mesh, surface_format);
+    surfacesTools->commit(mesh, surfaceFormat);
 }
