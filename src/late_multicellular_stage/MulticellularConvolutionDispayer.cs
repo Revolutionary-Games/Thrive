9c69c890a77fc69dff8b094d104048c0aa4b464e
dligr <dellpls43@gmail.com>
2024-11-10 18:03:08 +0300

    Make a better texture-drawing algorithm

--- src/late_multicellular_stage/MulticellularConvolutionDispayer.cs
+++ src/late_multicellular_stage/MulticellularConvolutionDispayer.cs
@@ -129,14 +129,15 @@ public partial class MulticellularConvolutionDispayer : MeshInstance3D, IMetabal
     {
         int dimension = 2048;
 
-        Image image = Image.CreateEmpty(dimension, dimension, false, Image.Format.Rgba8);
-        image.Fill(Colors.White);
+        var image = Image.CreateEmpty(dimension, dimension, false, Image.Format.Rgba8);
 
         var arrays = mesh.SurfaceGetArrays(0);
 
         // var vertices = arrays[(int)Mesh.ArrayType.Vertex].AsVector3Array();
         var indices = arrays[(int)Mesh.ArrayType.Index].AsInt32Array();
         var uvs = arrays[(int)Mesh.ArrayType.TexUV].AsVector2Array();
+
+        float pixelWidth = 1.0f / dimension;
 
         for (int i = 0; i < indices.Length; i += 3)
         {
@@ -154,27 +155,78 @@ public partial class MulticellularConvolutionDispayer : MeshInstance3D, IMetabal
             var color = new Color(random.NextFloat(), random.NextFloat(), random.NextFloat());
 
             // Drawing image based on triangles
-            // TODO: Find a better way
-            float abStep = 1.0f / (MathF.Abs(aUV.DistanceTo(bUV)) * dimension);
 
-            for (float aCoef = -abStep; aCoef <= 1.0f + abStep; aCoef += abStep)
+            float minY = MathF.Min(aUV.Y, MathF.Min(bUV.Y, cUV.Y));
+            float maxY = MathF.Max(aUV.Y, MathF.Max(bUV.Y, cUV.Y));
+
+            float middleY = aUV.Y + bUV.Y + cUV.Y - minY - maxY;
+
+            float prevLeftX = 0;
+            float prevRightX = 0;
+
+            for (float y = MathF.Round(minY * 2048.0f) / 2048.0f; y <= middleY; y += pixelWidth)
             {
-                var left = aUV.Lerp(bUV, aCoef);
-                var right = cUV.Lerp(bUV, aCoef);
+                (float leftX, float rightX) = CalculateXBoundsForTrinangle(aUV, bUV, cUV, y);
 
-                float acStep = 0.5f / (left.DistanceTo(right) * dimension);
-
-                for (float bCoef = -acStep; bCoef <= 1.0f + acStep; bCoef += acStep)
+                for (float x = leftX - pixelWidth; x <= rightX + pixelWidth; x += pixelWidth)
                 {
-                    int x = (int)(float.Lerp(left.X, right.X, bCoef) * dimension);
-                    int y = (int)(float.Lerp(left.Y, right.Y, bCoef) * dimension);
+                    image.SetPixel((int)(x * dimension), (int)(y * dimension), color);
 
-                    image.SetPixel(x, y, color);
+                    if (x < prevLeftX || x > prevRightX)
+                        image.SetPixel((int)(x * dimension), (int)((y - pixelWidth) * dimension), color);
                 }
+
+                prevLeftX = leftX;
+                prevRightX = rightX;
+            }
+
+            for (float y = MathF.Round(maxY * 2048.0f) / 2048.0f; y >= middleY; y -= pixelWidth)
+            {
+                (float leftX, float rightX) = CalculateXBoundsForTrinangle(aUV, bUV, cUV, y);
+
+                for (float x = leftX - pixelWidth; x <= rightX + pixelWidth; x += pixelWidth)
+                {
+                    image.SetPixel((int)(x * dimension), (int)(y * dimension), color);
+
+                    if (x < prevLeftX || x > prevRightX)
+                        image.SetPixel((int)(x * dimension), (int)((y + pixelWidth) * dimension), color);
+                }
+
+                prevLeftX = leftX;
+                prevRightX = rightX;
             }
         }
 
         return image;
+    }
+
+    private (float LeftX, float RightX) CalculateXBoundsForTrinangle(Vector2 a, Vector2 b, Vector2 c, float y)
+    {
+        float leftX = 10.0f;
+        float rightX = -10.0f;
+
+        if (MathF.Max(a.Y, b.Y) >= y && MathF.Min(a.Y, b.Y) <= y)
+        {
+            float lineX = a.X + (b.X - a.X) * (y - a.Y) / (b.Y - a.Y);
+            leftX = MathF.Min(leftX, lineX);
+            rightX = MathF.Max(rightX, lineX);
+        }
+
+        if (MathF.Max(b.Y, c.Y) >= y && MathF.Min(b.Y, c.Y) <= y)
+        {
+            float lineX = b.X + (c.X - b.X) * (y - b.Y) / (c.Y - b.Y);
+            leftX = MathF.Min(leftX, lineX);
+            rightX = MathF.Max(rightX, lineX);
+        }
+
+        if (MathF.Max(a.Y, c.Y) >= y && MathF.Min(a.Y, c.Y) <= y)
+        {
+            float lineX = a.X + (c.X - a.X) * (y - a.Y) / (c.Y - a.Y);
+            leftX = MathF.Min(leftX, lineX);
+            rightX = MathF.Max(rightX, lineX);
+        }
+
+        return (leftX, rightX);
     }
 
     private void ApplyAlpha()
