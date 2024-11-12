a65014abd056d07b861daf2e5834955c8b39dee0
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-11-12 17:01:33 +0200

    Few small last tweaks

--- src/extension/atlas/atlas_unwrap.hpp
+++ src/extension/atlas/atlas_unwrap.hpp
@@ -9,7 +9,7 @@ namespace Thrive
 
 bool Unwrap(godot::ArrayMesh& mesh, float texelSize);
 
-void FinishUnwrap(
-    const godot::Ref<godot::SurfaceTool>& surfaces_tools, const godot::Ref<godot::ArrayMesh>& mesh, uint64_t surface_format);
+void FinishUnwrap(const godot::Ref<godot::SurfaceTool>& surfaces_tools, const godot::Ref<godot::ArrayMesh>& mesh,
+    uint64_t surface_format);
 
 } // namespace Thrive
