a833e05ab502c3908795d90620e697497e3020b4
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-11-15 12:47:11 +0200

    Started on the GUI parts for reordering items

---
+++ src/microbe_stage/editor/DraggableItem.cs
@@ -0,0 +1,20 @@
+﻿using Godot;
+
+/// <summary>
+///   Items in for example <see cref="GrowthOrderPicker"/> that can be dragged around by the player in the GUI
+/// </summary>
+/// <remarks>
+///   <para>
+///     TODO: needs to have support specifically added for controller manipulation of these items
+///   </para>
+/// </remarks>
+public partial class DraggableItem : Control
+{
+    public override void _Ready()
+    {
+    }
+
+    public override void _Process(double delta)
+    {
+    }
+}
---
+++ src/microbe_stage/editor/GrowthOrderPicker.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+
+/// <summary>
+///   Allows picking the growth order of things in a GUI
+/// </summary>
+public partial class GrowthOrderPicker : Control
+{
+    public override void _Ready()
+    {
+    }
+
+    public override void _Process(double delta)
+    {
+    }
+}
