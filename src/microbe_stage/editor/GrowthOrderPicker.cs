34fafc906a7c62dbc7c334805883295450ae526e
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2024-11-18 15:36:46 +0200

    Implemented growth order list order filter

--- src/microbe_stage/editor/GrowthOrderPicker.cs
+++ src/microbe_stage/editor/GrowthOrderPicker.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Godot;
 
 /// <summary>
@@ -15,12 +15,19 @@ public partial class GrowthOrderPicker : Control
     /// </summary>
     private readonly List<IPlayerReadableName> readItemOrder = new();
 
+    private readonly Comparer itemComparer;
+
 #pragma warning disable CA2213
     [Export]
     private Container buttonContainer = null!;
 
     private PackedScene draggableItemScene = null!;
 #pragma warning restore CA2213
+
+    public GrowthOrderPicker()
+    {
+        itemComparer = new Comparer(itemControls);
+    }
 
     [Signal]
     public delegate void OrderResetEventHandler();
@@ -117,16 +124,16 @@ public partial class GrowthOrderPicker : Control
     /// <returns>Items with current GUI order state applied to them</returns>
     public IEnumerable<IPlayerReadableName> ApplyOrderingToItems(IEnumerable<IPlayerReadableName> rawItems)
     {
-        // Might be some other way to do this, but the easiest way seems to just create a new list
-        var result = rawItems.ToList();
-
-        if (itemControls.Count > 0)
+        if (itemControls.Count <= 0)
         {
-            // TODO: Order based on index found for the item in itemControls
-            throw new NotImplementedException();
+            // If no existing items, can just return the raw list
+            return rawItems;
         }
 
-        return result;
+        // Need to use LINQ sort here as it is a stable sort and our sorter only does a partial ordering
+        // Apparently `Order` might not be a stable sort so for safety `OrderBy` is used as that is guaranteed
+        // according to the documentation to be stable
+        return rawItems.OrderBy(i => i, itemComparer);
     }
 
     /// <summary>
@@ -156,5 +163,36 @@ public partial class GrowthOrderPicker : Control
     {
         GUICommon.Instance.PlayButtonPressSound();
         EmitSignal(SignalName.OrderReset);
+    }
+
+    private class Comparer(List<DraggableItem> existingItems) : IComparer<IPlayerReadableName>
+    {
+        public int Compare(IPlayerReadableName? x, IPlayerReadableName? y)
+        {
+            if (ReferenceEquals(x, y))
+                return 0;
+
+            if (y is null)
+                return 1;
+
+            if (x is null)
+                return -1;
+
+            return FindIndex(x).CompareTo(FindIndex(y));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int FindIndex(IPlayerReadableName item)
+        {
+            var count = existingItems.Count;
+            for (var i = 0; i < count; ++i)
+            {
+                if (ReferenceEquals(item, existingItems[i].UserData))
+                    return i;
+            }
+
+            // We want not-found items to be last, so return max int for them rather than the default -1
+            return int.MaxValue;
+        }
     }
 }
