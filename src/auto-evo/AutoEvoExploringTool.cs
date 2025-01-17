d604a867cb34ceeba23379a7a6b37cf02281094e
Patryk26g <patryk26g@gmail.com>
2025-01-17 23:48:13 +0100

    Fix

--- src/auto-evo/AutoEvoExploringTool.cs
+++ src/auto-evo/AutoEvoExploringTool.cs
@@ -1263,8 +1263,10 @@ public partial class AutoEvoExploringTool : NodeWithInput, ISpeciesDataProvider
             {
                 MicrobeSpeciesUpgradesStatistics[upgradeName] = (
                     MicrobeSpeciesUpgradesStatistics[upgradeName].Name,
-                    microbeSpecies.Average(s => s.Organelles.Any(o => o.Upgrades?.UnlockedFeatures?.Contains(upgradeName) ?? false) ? 1 : 0),
-                    microbeSpecies.Average(s => s.Organelles.Count(o => o.Upgrades?.UnlockedFeatures?.Contains(upgradeName) ?? false)));
+                    microbeSpecies.Average(s =>
+                        s.Organelles.Any(o => o.Upgrades?.UnlockedFeatures?.Contains(upgradeName) ?? false) ? 1 : 0),
+                    microbeSpecies.Average(s =>
+                        s.Organelles.Count(o => o.Upgrades?.UnlockedFeatures?.Contains(upgradeName) ?? false)));
             }
         }
     }
