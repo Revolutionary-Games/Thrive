6b1e71171444e37c70b171fa9211aac8c8f88425
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-01-29 15:55:32 +0200

    Started with setting up the display for the heat gradients

---
+++ src/microbe_stage/HeatGradientDisplay.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+/// <summary>
+///   Displays a noise-based heat gradient on screen
+/// </summary>
+public partial class HeatGradientDisplay : TextureRect
+{
+    public override void _Ready()
+    {
+    }
+}
