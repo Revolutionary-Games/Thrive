8e2e2af8fb2864a51d66bf2a5533f54133891a47
dligr <dellpls43@gmail.com>
2025-02-08 23:46:32 +0300

    Add a tooltip to the multicellular editor's ATP balance title

--- src/microbe_stage/OrganismStatisticsPanel.cs
+++ src/microbe_stage/OrganismStatisticsPanel.cs
@@ -11,6 +11,9 @@ using Godot;
 public partial class OrganismStatisticsPanel : PanelContainer
 {
     [Export]
+    public bool IsMulticellularEditor;
+
+    [Export]
     public bool ShowHealthStat;
 
     [Export]
@@ -167,6 +170,8 @@ public partial class OrganismStatisticsPanel : PanelContainer
         atpConsumptionBar.SelectedType = SegmentedBar.Type.ATP;
 
         UpdateStatVisibility();
+        UpdateATPBalanceText();
+        RegisterTooltips();
     }
 
     public void OnTranslationsChanged()
@@ -204,17 +209,7 @@ public partial class OrganismStatisticsPanel : PanelContainer
     {
         energyBalanceInfo = energyBalance;
 
-        if (energyBalance.FinalBalance > 0)
-        {
-            atpBalanceLabel.Text = Localization.Translate("ATP_PRODUCTION");
-            atpBalanceLabel.LabelSettings = ATPBalanceNormalText;
-        }
-        else
-        {
-            atpBalanceLabel.Text = Localization.Translate("ATP_PRODUCTION") + " - " +
-                Localization.Translate("ATP_PRODUCTION_TOO_LOW");
-            atpBalanceLabel.LabelSettings = ATPBalanceNotEnoughText;
-        }
+        UpdateATPBalanceText();
 
         atpProductionLabel.Text = string.Format(CultureInfo.CurrentCulture, "{0:F1}", energyBalance.TotalProduction);
         atpConsumptionLabel.Text = string.Format(CultureInfo.CurrentCulture, "{0:F1}", energyBalance.TotalConsumption);
@@ -352,6 +347,15 @@ public partial class OrganismStatisticsPanel : PanelContainer
     {
         digestionEfficiencyLabel.RegisterToolTipForControl("digestionEfficiencyDetails", "editor");
         storageLabel.RegisterToolTipForControl("storageDetails", "editor");
+
+        if (!IsMulticellularEditor)
+        {
+            atpBalanceLabel.TooltipText = Localization.Translate("ATP_BALANCE_TOOLTIP");
+        }
+        else
+        {
+            atpBalanceLabel.TooltipText = Localization.Translate("ATP_BALANCE_TOOLTIP_MULTICELLULAR");
+        }
     }
 
     public void UpdateSize(int size)
@@ -532,6 +536,21 @@ public partial class OrganismStatisticsPanel : PanelContainer
         }
 
         EmitSignal(SignalName.OnLightLevelChanged, (int)selectedLightLevelOption);
+    }
+
+    private void UpdateATPBalanceText()
+    {
+        if (energyBalanceInfo == null || energyBalanceInfo.FinalBalance > 0)
+        {
+            atpBalanceLabel.Text = Localization.Translate("ATP_PRODUCTION");
+            atpBalanceLabel.LabelSettings = ATPBalanceNormalText;
+        }
+        else
+        {
+            atpBalanceLabel.Text = Localization.Translate("ATP_PRODUCTION") + " - " +
+                Localization.Translate("ATP_PRODUCTION_TOO_LOW");
+            atpBalanceLabel.LabelSettings = ATPBalanceNotEnoughText;
+        }
     }
 
     private void OnCompoundBalanceTypeChanged(BalanceDisplayType newType)
