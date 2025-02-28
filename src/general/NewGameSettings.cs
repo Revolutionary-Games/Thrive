7cbf46556bc942520573b62709f5a1f16f0c4f10
Joshua McKelvey <joshuakmckelvey@gmail.com>
2025-02-28 10:37:20 -0500

    Export worldSizeButton directly

--- src/general/NewGameSettings.cs
+++ src/general/NewGameSettings.cs
@@ -145,9 +145,6 @@ public partial class NewGameSettings : ControlWithInput
     public NodePath GameSeedAdvancedPath = null!;
 
     [Export]
-    public NodePath WorldSizeButtonPath = null!;
-
-    [Export]
     public NodePath IncludeMulticellularButtonPath = null!;
 
     [Export]
@@ -224,6 +221,8 @@ public partial class NewGameSettings : ControlWithInput
     private VBoxContainer dayLengthContainer = null!;
     private LineEdit gameSeed = null!;
     private LineEdit gameSeedAdvanced = null!;
+
+    [Export]
     private OptionButton worldSizeButton = null!;
 
     // Misc controls
@@ -320,7 +319,6 @@ public partial class NewGameSettings : ControlWithInput
         dayLengthReadout = GetNode<LineEdit>(DayLengthReadoutPath);
         gameSeed = GetNode<LineEdit>(GameSeedPath);
         gameSeedAdvanced = GetNode<LineEdit>(GameSeedAdvancedPath);
-        worldSizeButton = GetNode<OptionButton>(WorldSizeButtonPath);
         includeMulticellularButton = GetNode<Button>(IncludeMulticellularButtonPath);
         easterEggsButton = GetNode<Button>(EasterEggsButtonPath);
         backButton = GetNode<Button>(BackButtonPath);
@@ -544,7 +542,6 @@ public partial class NewGameSettings : ControlWithInput
                 DayLengthReadoutPath.Dispose();
                 GameSeedPath.Dispose();
                 GameSeedAdvancedPath.Dispose();
-                WorldSizeButtonPath.Dispose();
                 IncludeMulticellularButtonPath.Dispose();
                 EasterEggsButtonPath.Dispose();
                 BackButtonPath.Dispose();
