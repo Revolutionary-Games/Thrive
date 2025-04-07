803d73b06f489f61520c67c1a63a8337e8dd1659
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-04-07 14:45:51 +0300

    Committed the test runner scene
    as it might help CI

---
+++ gdunit4_testadapter/GdUnit4TestRunnerScene.cs
@@ -0,0 +1,13 @@
+﻿#pragma warning disable SA1005
+
+//!/usr/bin/env -S godot -s
+
+// ReSharper disable once CheckNamespace
+
+namespace GdUnit4.TestRunner;
+
+using Core.Runners;
+
+public partial class GdUnit4TestRunnerScene : GdUnit4TestRunnerSceneCore
+{
+}
