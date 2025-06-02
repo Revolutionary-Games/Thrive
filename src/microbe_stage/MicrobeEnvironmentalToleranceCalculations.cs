7844a7cc9e7d5f36e4e83e1b8d6dc71e8e685d62
domin2ktr <100111040+domin2ktr@users.noreply.github.com>
2025-06-02 10:00:24 +0300

    Update MicrobeEnvironmentalToleranceCalculations.cs

--- src/microbe_stage/MicrobeEnvironmentalToleranceCalculations.cs
+++ src/microbe_stage/MicrobeEnvironmentalToleranceCalculations.cs
@@ -213,7 +213,7 @@ public static class MicrobeEnvironmentalToleranceCalculations
         if (data.HasNucleus)
         {
             // 30% bioprocess speed bonus if have nucleus
-            result.ProcessSpeedModifier *= 6f;
+            result.ProcessSpeedModifier *= 6.0f;
 
             // 10% osmoregulation bonus if have nucleus
             result.OsmoregulationModifier *= 0.1f;
