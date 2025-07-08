9ae5127ffa3ba0700696685e86a9f8acf218131a
dligr <dellpls43@gmail.com>
2025-07-08 15:41:27 +0300

    Fix input rebinding (with conflicts) (#6202)
    * Make input menu always read normal keycodes
    This partly fixes the input menu's rebinding bug
    
    * Fix rebinding
    Calling _Input() to finish rebinding doesn't seem to work anymore
    because when it's called, the confirmation window is still open and _Input() as such blocks the attempt
    
    * Switch default keybindings to physical keys, make rebinding control prefer physical keys
    
    * Fix physical key display
    The method name should still be relevant as is, because it still returns keycodes or labels
    
    * Reduce code duplication
    
    * Fix an ordering mistake
    
    * Use key labels for a more consistent display
    
    * Also use key label when getting prompt image for a binding
    
    ---------
    
    Co-authored-by: Henri Hyyryläinen <henri.hyyrylainen@gmail.com>

--- src/general/utils/InputEventKeyUtils.cs
+++ src/general/utils/InputEventKeyUtils.cs
@@ -9,12 +9,14 @@ public static class InputEventKeyUtils
     /// <returns>The code or label whichever is actually used for this key</returns>
     public static Key KeyCodeOrLabel(this InputEventKey key)
     {
-        if (key.Keycode == Key.None)
+        if (key.KeyLabel != Key.None)
         {
-            if (key.KeyLabel == Key.None)
-                GD.PrintErr("Key has both key code and label as none");
-
             return key.KeyLabel;
+        }
+
+        if (key.PhysicalKeycode != Key.None)
+        {
+            return DisplayServer.KeyboardGetLabelFromPhysical(key.PhysicalKeycode);
         }
 
         return key.Keycode;
