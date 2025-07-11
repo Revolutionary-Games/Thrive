30fdb84f0832d8982dead4bb1b8919b7fc9e0dea
James Lindsay <78500760+0HyperCube@users.noreply.github.com>
2025-07-11 10:48:06 +0100

    Mucocyst activation cost up to 50% of mucocyst storage (#6174)
    * Mucocyst activation cost
    
    * Update to use a fraction of the capacity

--- simulation_parameters/Constants.cs
+++ simulation_parameters/Constants.cs
@@ -559,7 +559,10 @@ public static class Constants
     /// </summary>
     public const float MUCOCYST_SPEED_MULTIPLIER = 0.01f;
 
-    public const float MUCOCYST_MINIMUM_MUCILAGE = 1.0f;
+    /// <summary>
+    ///   What fraction of the mucilage capacity is required to activate mucocyst (slime jet upgrade)
+    /// </summary>
+    public const float MUCOCYST_ACTIVATION_MUCILAGE_FRACTION = 0.5f;
 
     /// <summary>
     ///   How much mucocyst (slime jet upgrade) drains mucilage per second
