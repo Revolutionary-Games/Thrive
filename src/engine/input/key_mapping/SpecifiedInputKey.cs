dba3a09f062658879552fd700f6d7c6f2ed4b1ca
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-07-16 10:04:10 +0300

    Updated gdUnit to latest version, C# to version 13, and fixed errors from key conversion in headless mode (#6207)
    * Updated other dependencies
    
    * Updated gdUnit4 C# packages to latest versions
    
    * Updated to C# 13
    
    * Downgraded gdUnit4.analyzers to a version that doesn't cause a warning
    
    * Updated gdUnit to latest version (5.0.5)
    
    * Ignore test adapter file in git
    
    * Fix warnings in key conversions when running unit tests
    
    also added a place that could have a player-configurable option for disabling physical key-to-label conversion
    
    * Committing the test runner again to git
    
    * Disable gdUnit4.analyzers reference
    
    * Created a single helper method for converting physical keys
    
    * Set verbosity to detailed
    
    and no stdout capture to hopefully see more in the test run output
    
    * Godot verbose option (and with stdout capture)
    
    * Specify the verbose and headless flags the other way around
    
    * Remove quotes around Godot parameters
    
    * Trying setting this flag for detailed verbosity
    
    * Remove gdUnit test runner scene from git
    
    * Made test run verbosity easily configurable and back to normal
    
    * Remove the Godot verbose flag in test mode
    
    * Automatically delete old gdunit test adapter
    
    * Split the test run to 2 phases
    
    * Adjusted some prints a bit
    
    * Slightly improve the setup instructions
    
    * Add diag.log creation
    
    * Removed test frameworks option in runsettings file
    
    * Set TreatNoTestsAsError to false
    
    * Clean up some changes and added comments

--- src/engine/input/key_mapping/SpecifiedInputKey.cs
+++ src/engine/input/key_mapping/SpecifiedInputKey.cs
@@ -419,7 +419,7 @@ public class SpecifiedInputKey : ICloneable
 
             if (Type == InputType.PhysicalKey)
             {
-                key = DisplayServer.KeyboardGetLabelFromPhysical(key);
+                key = InputEventKeyUtils.ConvertPhysicalKeycode(key);
             }
 
             // Key labels are hopefully already in a format that makes sense when translated
