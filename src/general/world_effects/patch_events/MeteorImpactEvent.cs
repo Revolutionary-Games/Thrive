9f342f5bd8c613515f5f937aea487756bf88220f
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-07-22 10:39:34 +0300

    Reworked how the meteor impact adds new compounds

--- src/general/world_effects/patch_events/MeteorImpactEvent.cs
+++ src/general/world_effects/patch_events/MeteorImpactEvent.cs
@@ -192,10 +192,15 @@ public class MeteorImpactEvent : IWorldEffect
 
         foreach (var (compoundName, levelChange) in selectedMeteor.Compounds)
         {
-            // Add 0 value for compound if it doesn't exist (e.g. for iron)
-            patch.Biome.ChangeableCompounds.TryAdd(compoundName, default);
+            bool hasCompound =
+                patch.Biome.ChangeableCompounds.TryGetValue(compoundName, out var currentCompoundLevel);
 
-            var currentCompoundLevel = patch.Biome.ChangeableCompounds[compoundName];
+            if (!hasCompound)
+            {
+                // This is adding a new compound
+                GD.Print($"Impact event is adding a new compound {compoundName} that was not present before " +
+                    $"in {patch.Name}");
+            }
 
             var definition = SimulationParameters.Instance.GetCompoundDefinition(compoundName);
 
@@ -205,7 +210,7 @@ public class MeteorImpactEvent : IWorldEffect
                 currentCompoundLevel.Density = levelChange;
 
                 // TODO: instead of hardcoding the fallback value, maybe this could look in the event template biome?
-                currentCompoundLevel.Amount = currentCompoundLevel.Amount == 0 ? 10000 : currentCompoundLevel.Amount;
+                currentCompoundLevel.Amount = currentCompoundLevel.Amount == 0 ? 125000 : currentCompoundLevel.Amount;
                 tempCompoundChanges[compoundName] = currentCompoundLevel.Density;
                 tempCloudSizes[compoundName] = currentCompoundLevel.Amount;
             }
@@ -265,10 +270,14 @@ public class MeteorImpactEvent : IWorldEffect
 
         foreach (var (compoundName, levelChange) in selectedMeteor.Compounds)
         {
-            // Add 0 value for compound if it doesn't exist (e.g. for iron)
-            patch.Biome.ChangeableCompounds.TryAdd(compoundName, default);
+            bool hasCompound =
+                patch.Biome.ChangeableCompounds.TryGetValue(compoundName, out var currentCompoundLevel);
 
-            var currentCompoundLevel = patch.Biome.ChangeableCompounds[compoundName];
+            if (!hasCompound)
+            {
+                GD.PrintErr("Did not find compound to reduce after impact event ended");
+                continue;
+            }
 
             var definition = SimulationParameters.Instance.GetCompoundDefinition(compoundName);
 
@@ -280,7 +289,7 @@ public class MeteorImpactEvent : IWorldEffect
                 tempCloudSizes[compoundName] = currentCompoundLevel.Amount;
             }
 
-            // CO2 is not reduced back to normal values
+            // CO2 (and other gases) are not reduced back to normal values
         }
 
         patch.Biome.ApplyLongTermCompoundChanges(patch.BiomeTemplate, tempCompoundChanges, tempCloudSizes);
