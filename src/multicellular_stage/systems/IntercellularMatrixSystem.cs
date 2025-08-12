f266f368e18e148b7056bd176368f9d531400947
dligr <dellpls43@gmail.com>
2025-08-12 11:04:30 +0300

    Make cell links take cell rotation into account (#6270)
    * Make cell links take cell rotation into account
    
    * Code style fix
    
    * Get `AttachedToEntity` structs but their references
    
    * Rename a variable

--- src/multicellular_stage/systems/IntercellularMatrixSystem.cs
+++ src/multicellular_stage/systems/IntercellularMatrixSystem.cs
@@ -66,19 +66,31 @@ public sealed class IntercellularMatrixSystem : AEntitySetSystem<float>
 
         Vector3 targetRelativePos;
 
+        Quaternion ourRotation;
+        Quaternion targetRotation = Quaternion.Identity;
+
         if (parentEntity == colony.Leader)
         {
-            targetRelativePos = -entity.Get<AttachedToEntity>().RelativePosition;
+            ref var ourAttachedPosition = ref entity.Get<AttachedToEntity>();
+
+            targetRelativePos = -ourAttachedPosition.RelativePosition;
+            ourRotation = ourAttachedPosition.RelativeRotation;
         }
         else
         {
-            targetRelativePos = parentEntity.Get<AttachedToEntity>().RelativePosition
-                - entity.Get<AttachedToEntity>().RelativePosition;
+            ref var ourAttachedPosition = ref entity.Get<AttachedToEntity>();
+            ref var targetAttachedPosition = ref parentEntity.Get<AttachedToEntity>();
+
+            targetRelativePos = targetAttachedPosition.RelativePosition
+                - ourAttachedPosition.RelativePosition;
+
+            ourRotation = ourAttachedPosition.RelativeRotation;
+            targetRotation = targetAttachedPosition.RelativeRotation;
         }
 
         Vector3 pointA, pointB;
         (pointA, pointB) = FindGoodConnectionPoints(ourMembrane.MembraneData,
-            targetMembrane.MembraneData, targetRelativePos);
+            targetMembrane.MembraneData, targetRelativePos, ourRotation, targetRotation);
 
         var relativePosition = pointB - pointA;
         float relativePosLength = relativePosition.Length();
@@ -106,30 +118,33 @@ public sealed class IntercellularMatrixSystem : AEntitySetSystem<float>
     }
 
     private static (Vector3 PointA, Vector3 PointB) FindGoodConnectionPoints(MembranePointData membraneA,
-        MembranePointData membraneB, Vector3 membraneBOffset)
+        MembranePointData membraneB, Vector3 membraneBOffset, Quaternion rotationA, Quaternion rotationB)
     {
-        Vector2 offset2D = new Vector2(membraneBOffset.X, membraneBOffset.Z);
-
         float min = float.MaxValue;
-        Vector2 pointA = Vector2.Zero;
-        Vector2 pointB = offset2D;
+        Vector3 pointA = Vector3.Zero;
+        Vector3 pointB = membraneBOffset;
         foreach (var a in membraneA.Vertices2D)
         {
+            var convertedA = new Vector3(a.X, 0.0f, a.Y);
+
             foreach (var b in membraneB.Vertices2D)
             {
-                float distance = a.DistanceSquaredTo(b + offset2D);
+                // First rotate the vertex by membrane B rotation
+                // Then inversely rotate it by A's rotation to get the true relative coordinates
+                var rotatedB = (rotationB * new Vector3(b.X, 0.0f, b.Y) + membraneBOffset) * rotationA;
+
+                float distance = convertedA.DistanceSquaredTo(rotatedB);
 
                 if (distance < min)
                 {
                     min = distance;
-                    pointA = a;
-                    pointB = b;
+                    pointA = convertedA;
+                    pointB = rotatedB;
                 }
             }
         }
 
-        return (new Vector3(pointA.X, 0.0f, pointA.Y),
-            new Vector3(pointB.X, 0.0f, pointB.Y) + membraneBOffset);
+        return (pointA, pointB);
     }
 
     private static void RemoveConnection(ref IntercellularMatrix intercellularMatrix)
