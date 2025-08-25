749ae51e376dbe1d61af3d12e97d817ce1515a65
CritikalH1T-pon3 <martinluca416@gmail.com>
2025-08-25 13:38:21 -0300

    Temperature miche in auto-evo penalizes sessility  (#6197)
    * TemperatureSessilityPressure progress 1
    
    although it should work like this I'm confident that there's stuff to fix
    
    * temperaturesessilitypressure 2
    
    * TemperatureSessilityPressure 3
    
    * other files changes
    
    * Update GenerateMiche.cs
    
    * temperasutesessibilitypressure 3
    
    code style alteration
    
    * Checks fix 1
    
    * Update TemperatureSessilityPressure.cs
    
    * Update TemperatureSessilityPressure.cs
    
    * Fixed some line change styling
    
    * Fixed some comments, including ones saying the opposite
    
    than what they should
    
    * Fixed missing text
    
    * Added missing BOM
    
    * Update TemperatureSessilityPressure.cs
    
    * Update TemperatureSessilityPressure.cs
    
    ---------
    
    Co-authored-by: critikal-hit <156711726+critikal-byte@users.noreply.github.com>

---
+++ src/auto-evo/selection_pressure/TemperatureSessilityPressure.cs
@@ -0,0 +1,62 @@
+﻿namespace AutoEvo;
+
+using Newtonsoft.Json;
+
+/// <summary>
+///   Selection pressure that penalizes sessile (non-motile) species in high-temperature environments.
+///   This pressure encourages species to become more mobile in hot conditions.
+/// </summary>
+[JSONDynamicTypeAllowed]
+public class TemperatureSessilityPressure : SelectionPressure
+{
+    // Needed for translation extraction
+    // ReSharper disable ArrangeObjectCreationWhenTypeEvident
+    private static readonly LocalizedString NameString = new LocalizedString("MICHE_TEMPERATURE_SESSILITY_PRESSURE");
+
+    // ReSharper restore ArrangeObjectCreationWhenTypeEvident
+
+    public TemperatureSessilityPressure(float weight) : base(weight, [
+        new ChangeBehaviorScore(ChangeBehaviorScore.BehaviorAttribute.Activity, 150.0f),
+        new UpgradeOrganelle(organelle => organelle.HasMovementComponent, new FlagellumUpgrades(1.0f)),
+    ])
+    {
+    }
+
+    [JsonIgnore]
+    public override LocalizedString Name => NameString;
+
+    /// <summary>
+    ///   Calculates the selection pressure score based on temperature and species activity.
+    ///   Higher scores are given to more active (less sessile) species in high-temperature environments.
+    /// </summary>
+    public override float Score(Species species, Patch patch, SimulationCache cache)
+    {
+        if (species is not MicrobeSpecies microbeSpecies)
+            return 0;
+
+        // Get temperature from the patch
+        if (!patch.Biome.TryGetCompound(Compound.Temperature, CompoundAmountType.Biome, out var temperatureAmount))
+            return 0.0f;
+
+        float temperature = temperatureAmount.Ambient;
+
+        // If the temperature is not high enough, no bonus
+        if (temperature <= 60.0f)
+            return 0.0f;
+
+        // Calculate score based on activity (higher activity = higher score for mobile behavior)
+        // The more active the species, the higher the score
+        float activityScore = microbeSpecies.Behaviour.Activity / Constants.MAX_SPECIES_ACTIVITY;
+        return activityScore;
+    }
+
+    /// <summary>
+    ///   This pressure doesn't provide energy to species.
+    /// </summary>
+    /// <param name="patch">The patch to evaluate</param>
+    /// <returns>Always returns 0 as this pressure doesn't provide energy</returns>
+    public override float GetEnergy(Patch patch)
+    {
+        return 0;
+    }
+}
