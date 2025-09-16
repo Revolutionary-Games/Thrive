e88cf8c6bda4389ab1a9bb4f95f4c2e7078cf549
Accidental-Explorer <alexandervanderhall1996@gmail.com>
2025-09-16 08:16:52 +0200

    Add a Miche for Endosymbiois-target Species (#6333)
    * Created a new Miche specifically for Endosymbionts based on where the host (player) species is present.
    
    * Created a new Endosymbios SelectionPressure class that is only valid for the designated endosymbiont, and provides energy based on its host species population in the patch.
    
    * Updated translations to support the Endosymbiosis Selection Pressure
    
    * Reworked to have Endosymbiosis Miches share a loop with predation Miches.
    
    * Corrected format and added UTF8 BOM.
    
    * Removed a blank line.
    
    * Updated translations

---
+++ src/auto-evo/selection_pressure/EndosymbiosisPressure.cs
@@ -0,0 +1,53 @@
+﻿namespace AutoEvo;
+
+using Newtonsoft.Json;
+
+[JSONDynamicTypeAllowed]
+public class EndosymbiosisPressure : SelectionPressure
+{
+    [JsonProperty]
+    public readonly Species Endosymbiont;
+
+    [JsonProperty]
+    public readonly Species Host;
+
+    // Needed for translation extraction
+    // ReSharper disable ArrangeObjectCreationWhenTypeEvident
+    private static readonly LocalizedString NameString =
+        new LocalizedString("MICHE_ENDOSYMBIOSIS_SELECTION_PRESSURE");
+
+    // ReSharper restore ArrangeObjectCreationWhenTypeEvident
+
+    public EndosymbiosisPressure(Species endosymbiont, Species host, float weight) : base(weight, [
+    ])
+    {
+        Endosymbiont = endosymbiont;
+        Host = host;
+    }
+
+    [JsonIgnore]
+    public override LocalizedString Name => NameString;
+
+    public override float Score(Species species, Patch patch, SimulationCache cache)
+    {
+        if (species.ID == Endosymbiont.ID)
+        {
+            return 1.0f;
+        }
+
+        return 0;
+    }
+
+    public override float GetEnergy(Patch patch)
+    {
+        if (!patch.SpeciesInPatch.TryGetValue(Host, out long population) || population <= 0)
+            return 0;
+
+        return population * Endosymbiont.GetPredationTargetSizeFactor();
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} ({Endosymbiont.FormattedName})";
+    }
+}
