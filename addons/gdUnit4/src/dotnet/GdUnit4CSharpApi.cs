c63aa6e8453e6e5b039da4c76bbe922a2a0616e8
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-09-22 14:22:36 +0300

    Updated gdUnit to 5.1.1

--- addons/gdUnit4/src/dotnet/GdUnit4CSharpApi.cs
+++ addons/gdUnit4/src/dotnet/GdUnit4CSharpApi.cs
@@ -1,4 +1,5 @@
 // Thrive customization. Disable all warnings from this third party code
+
 #pragma warning disable
 
 namespace gdUnit4.addons.gdUnit4.src.dotnet;
@@ -9,10 +10,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-
 using GdUnit4;
 using GdUnit4.Api;
-
 using Godot;
 using Godot.Collections;
 
@@ -20,130 +19,131 @@ using Godot.Collections;
 // ReSharper disable once CheckNamespace
 public partial class GdUnit4CSharpApi : GdUnit4NetApiGodotBridge
 {
-	[Signal]
-	public delegate void ExecutionCompletedEventHandler();
+    [Signal]
+    public delegate void ExecutionCompletedEventHandler();
 
-	private CancellationTokenSource? executionCts;
+    private CancellationTokenSource? executionCts;
 
-	public override void _Notification(int what)
-	{
-		if (what != NotificationPredelete)
-			return;
-		executionCts?.Dispose();
-		executionCts = null;
-	}
+    public override void _Notification(int what)
+    {
+        if (what != NotificationPredelete)
+            return;
 
-	public static bool IsApiLoaded()
-		=> true;
+        executionCts?.Dispose();
+        executionCts = null;
+    }
 
-	public static Array<Dictionary> DiscoverTests(CSharpScript sourceScript)
-	{
-		try
-		{
-			// Get the list of test case descriptors from the API
-			var testCaseDescriptors = DiscoverTestsFromScript(sourceScript);
-			// Convert each TestCaseDescriptor to a Dictionary
-			return testCaseDescriptors
-				.Select(descriptor => new Dictionary
-				{
-					["guid"] = descriptor.Id.ToString(),
-					["managed_type"] = descriptor.ManagedType,
-					["test_name"] = descriptor.ManagedMethod,
-					["source_file"] = sourceScript.ResourcePath,
-					["line_number"] = descriptor.LineNumber,
-					["attribute_index"] = descriptor.AttributeIndex,
-					["require_godot_runtime"] = descriptor.RequireRunningGodotEngine,
-					["code_file_path"] = descriptor.CodeFilePath ?? "",
-					["simple_name"] = descriptor.SimpleName,
-					["fully_qualified_name"] = descriptor.FullyQualifiedName,
-					["assembly_location"] = descriptor.AssemblyPath
-				})
-				.Aggregate(new Array<Dictionary>(), (array, dict) =>
-				{
-					array.Add(dict);
-					return array;
-				});
-		}
-		catch (Exception e)
-		{
-			GD.PrintErr($"Error discovering tests: {e.Message}\n{e.StackTrace}");
-			return new Array<Dictionary>();
-		}
-	}
+    public static bool IsApiLoaded()
+        => true;
 
-	public void ExecuteAsync(Array<Dictionary> tests, Callable listener)
-	{
-		try
-		{
-			// Cancel any ongoing execution
-			executionCts?.Cancel();
-			executionCts?.Dispose();
+    public static Array<Dictionary> DiscoverTests(CSharpScript sourceScript)
+    {
+        try
+        {
+            // Get the list of test case descriptors from the API
+            var testCaseDescriptors = DiscoverTestsFromScript(sourceScript);
 
-			// Create new cancellation token source
-			executionCts = new CancellationTokenSource();
+            // Convert each TestCaseDescriptor to a Dictionary
+            return testCaseDescriptors
+                .Select(descriptor => new Dictionary
+                {
+                    ["guid"] = descriptor.Id.ToString(),
+                    ["managed_type"] = descriptor.ManagedType,
+                    ["test_name"] = descriptor.ManagedMethod,
+                    ["source_file"] = sourceScript.ResourcePath,
+                    ["line_number"] = descriptor.LineNumber,
+                    ["attribute_index"] = descriptor.AttributeIndex,
+                    ["require_godot_runtime"] = descriptor.RequireRunningGodotEngine,
+                    ["code_file_path"] = descriptor.CodeFilePath ?? "",
+                    ["simple_name"] = descriptor.SimpleName,
+                    ["fully_qualified_name"] = descriptor.FullyQualifiedName,
+                    ["assembly_location"] = descriptor.AssemblyPath
+                })
+                .Aggregate(new Array<Dictionary>(), (array, dict) =>
+                {
+                    array.Add(dict);
+                    return array;
+                });
+        }
+        catch (Exception e)
+        {
+            GD.PrintErr($"Error discovering tests: {e.Message}\n{e.StackTrace}");
+            return new Array<Dictionary>();
+        }
+    }
 
-			var testSuiteNodes = new List<TestSuiteNode> { BuildTestSuiteNodeFrom(tests) };
-			ExecuteAsync(testSuiteNodes, listener, executionCts.Token)
-				.GetAwaiter()
-				.OnCompleted(() => EmitSignal(SignalName.ExecutionCompleted));
-		}
-		catch (Exception e)
-		{
-			GD.PrintErr($"Error executing tests: {e.Message}\n{e.StackTrace}");
-			Task.Run(() => { }).GetAwaiter().OnCompleted(() => EmitSignal(SignalName.ExecutionCompleted));
-		}
-	}
+    public void ExecuteAsync(Array<Dictionary> tests, Callable listener)
+    {
+        try
+        {
+            // Cancel any ongoing execution
+            executionCts?.Cancel();
+            executionCts?.Dispose();
 
-	public void CancelExecution()
-	{
-		try
-		{
-			executionCts?.Cancel();
-		}
-		catch (Exception e)
-		{
-			GD.PrintErr($"Error cancelling execution: {e.Message}");
-		}
-	}
+            // Create new cancellation token source
+            executionCts = new CancellationTokenSource();
 
-	// Convert a set of Tests stored as Dictionaries to TestSuiteNode
-	// all tests are assigned to a single test suit
-	internal static TestSuiteNode BuildTestSuiteNodeFrom(Array<Dictionary> tests)
-	{
-		if (tests.Count == 0)
-			throw new InvalidOperationException("Cant build 'TestSuiteNode' from an empty test set.");
+            var testSuiteNodes = new List<TestSuiteNode> { BuildTestSuiteNodeFrom(tests) };
+            ExecuteAsync(testSuiteNodes, listener, executionCts.Token)
+                .GetAwaiter()
+                .OnCompleted(() => EmitSignal(SignalName.ExecutionCompleted));
+        }
+        catch (Exception e)
+        {
+            GD.PrintErr($"Error executing tests: {e.Message}\n{e.StackTrace}");
+            Task.Run(() => { }).GetAwaiter().OnCompleted(() => EmitSignal(SignalName.ExecutionCompleted));
+        }
+    }
 
-		// Create a suite ID
-		var suiteId = Guid.NewGuid();
-		var firstTest = tests[0];
-		var managedType = firstTest["managed_type"].AsString();
-		var assemblyLocation = firstTest["assembly_location"].AsString();
-		var sourceFile = firstTest["source_file"].AsString();
+    public void CancelExecution()
+    {
+        try
+        {
+            executionCts?.Cancel();
+        }
+        catch (Exception e)
+        {
+            GD.PrintErr($"Error cancelling execution: {e.Message}");
+        }
+    }
 
-		// Create TestCaseNodes for each test in the suite
-		var testCaseNodes = tests
-			.Select(test => new TestCaseNode
-				{
-					Id = Guid.Parse(test["guid"].AsString()),
-					ParentId = suiteId,
-					ManagedMethod = test["test_name"].AsString(),
-					LineNumber = test["line_number"].AsInt32(),
-					AttributeIndex = test["attribute_index"].AsInt32(),
-					RequireRunningGodotEngine = test["require_godot_runtime"].AsBool()
-				}
-			)
-			.ToList();
+    // Convert a set of Tests stored as Dictionaries to TestSuiteNode
+    // all tests are assigned to a single test suit
+    internal static TestSuiteNode BuildTestSuiteNodeFrom(Array<Dictionary> tests)
+    {
+        if (tests.Count == 0)
+            throw new InvalidOperationException("Cant build 'TestSuiteNode' from an empty test set.");
 
-		return new TestSuiteNode
-		{
-			Id = suiteId,
-			ParentId = Guid.Empty,
-			ManagedType = managedType,
-			AssemblyPath = assemblyLocation,
-			SourceFile = sourceFile,
-			Tests = testCaseNodes
-		};
-	}
+        // Create a suite ID
+        var suiteId = Guid.NewGuid();
+        var firstTest = tests[0];
+        var managedType = firstTest["managed_type"].AsString();
+        var assemblyLocation = firstTest["assembly_location"].AsString();
+        var sourceFile = firstTest["source_file"].AsString();
+
+        // Create TestCaseNodes for each test in the suite
+        var testCaseNodes = tests
+            .Select(test => new TestCaseNode
+            {
+                Id = Guid.Parse(test["guid"].AsString()),
+                ParentId = suiteId,
+                ManagedMethod = test["test_name"].AsString(),
+                LineNumber = test["line_number"].AsInt32(),
+                AttributeIndex = test["attribute_index"].AsInt32(),
+                RequireRunningGodotEngine = test["require_godot_runtime"].AsBool()
+            })
+            .ToList();
+
+        return new TestSuiteNode
+        {
+            Id = suiteId,
+            ParentId = Guid.Empty,
+            ManagedType = managedType,
+            AssemblyPath = assemblyLocation,
+            SourceFile = sourceFile,
+            Tests = testCaseNodes
+        };
+    }
 }
 #else
 using Godot;
@@ -151,29 +151,29 @@ using Godot.Collections;
 
 public partial class GdUnit4CSharpApi : RefCounted
 {
-	[Signal]
-	public delegate void ExecutionCompletedEventHandler();
+    [Signal]
+    public delegate void ExecutionCompletedEventHandler();
 
-	public static bool IsApiLoaded()
-	{
-		GD.PushWarning("No `gdunit4.api` dependency found, check your project dependencies.");
-		return false;
-	}
+    public static bool IsApiLoaded()
+    {
+        GD.PushWarning("No `gdunit4.api` dependency found, check your project dependencies.");
+        return false;
+    }
 
 
-	public static string Version()
-		=> "Unknown";
+    public static string Version()
+        => "Unknown";
 
-	public static Array<Dictionary> DiscoverTests(CSharpScript sourceScript) => new();
+    public static Array<Dictionary> DiscoverTests(CSharpScript sourceScript) => new();
 
-	public void ExecuteAsync(Array<Dictionary> tests, Callable listener)
-	{
-	}
+    public void ExecuteAsync(Array<Dictionary> tests, Callable listener)
+    {
+    }
 
-	public static bool IsTestSuite(CSharpScript script)
-		=> false;
+    public static bool IsTestSuite(CSharpScript script)
+        => false;
 
-	public static Dictionary CreateTestSuite(string sourcePath, int lineNumber, string testSuitePath)
-		=> new();
+    public static Dictionary CreateTestSuite(string sourcePath, int lineNumber, string testSuitePath)
+        => new();
 }
 #endif
