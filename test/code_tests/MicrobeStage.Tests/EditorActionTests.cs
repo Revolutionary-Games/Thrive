6b2364b90647b9ec462007a142f276b52728cff4
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-09-26 15:20:03 +0300

    Fixed different behaviour changes combining
    causing other sliders to jump around when changing a different one

--- test/code_tests/MicrobeStage.Tests/EditorActionTests.cs
+++ test/code_tests/MicrobeStage.Tests/EditorActionTests.cs
@@ -90,4 +90,77 @@ public class EditorActionTests
         Assert.Equal(initialTolerances, castedData.OldTolerances);
         Assert.Equal(changedTolerances2, castedData.NewTolerances);
     }
+
+    [Fact]
+    public void EditorAction_SubsequentBehaviourChangesCombine()
+    {
+        var history = new EditorActionHistory<EditorAction>();
+
+        var behaviourAction1 = new BehaviourActionData(50, 1, BehaviouralValueType.Activity);
+
+        bool done = false;
+
+        history.AddAction(new SingleEditorAction<BehaviourActionData>(_ => done = true, _ => { }, behaviourAction1));
+        Assert.True(done);
+        done = false;
+
+        var behaviourAction2 = new BehaviourActionData(100, 50, BehaviouralValueType.Activity);
+
+        history.AddAction(new SingleEditorAction<BehaviourActionData>(_ => done = true, _ => { }, behaviourAction2));
+
+        Assert.True(history.CanUndo());
+        Assert.True(done);
+
+        var action = history.PopTopAction();
+        var combinedAction = Assert.IsAssignableFrom<SingleEditorAction<BehaviourActionData>>(action);
+
+        var data = combinedAction.Data.ToList();
+        Assert.Single(data);
+
+        var castedData = Assert.IsAssignableFrom<BehaviourActionData>(data[0]);
+        Assert.Equal(1, castedData.OldValue);
+        Assert.Equal(100, castedData.NewValue);
+        Assert.Equal(BehaviouralValueType.Activity, castedData.Type);
+
+        Assert.False(history.CanUndo());
+
+        // There's no easy way to verify the history is completely empty, so we misuse an API here and expect an error
+        Assert.Throws<ArgumentOutOfRangeException>(() => history.PopTopAction());
+    }
+
+    [Fact]
+    public void EditorAction_DifferentBehaviourChangeStatsDoNotCombine()
+    {
+        var history = new EditorActionHistory<EditorAction>();
+
+        var behaviourAction1 = new BehaviourActionData(50, 1, BehaviouralValueType.Activity);
+
+        bool done = false;
+
+        history.AddAction(new SingleEditorAction<BehaviourActionData>(_ => done = true, _ => { }, behaviourAction1));
+        Assert.True(done);
+        done = false;
+
+        var behaviourAction2 = new BehaviourActionData(100, 50, BehaviouralValueType.Aggression);
+
+        history.AddAction(new SingleEditorAction<BehaviourActionData>(_ => done = true, _ => { }, behaviourAction2));
+
+        Assert.True(history.CanUndo());
+        Assert.True(done);
+
+        Assert.NotNull(history.PopTopAction());
+
+        var action = history.PopTopAction();
+        var combinedAction = Assert.IsAssignableFrom<SingleEditorAction<BehaviourActionData>>(action);
+
+        var data = combinedAction.Data.ToList();
+        Assert.Single(data);
+
+        var castedData = Assert.IsAssignableFrom<BehaviourActionData>(data[0]);
+        Assert.Equal(1, castedData.OldValue);
+        Assert.Equal(50, castedData.NewValue);
+        Assert.Equal(BehaviouralValueType.Activity, castedData.Type);
+
+        Assert.False(history.CanUndo());
+    }
 }
