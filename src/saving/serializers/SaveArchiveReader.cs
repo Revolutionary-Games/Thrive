9c147d609bc04693fa8a53a61f61eaf1d1eb2dc8
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-09-30 15:52:07 +0300

    Base implementations of new save archives

---
+++ src/saving/serializers/SaveArchiveReader.cs
@@ -0,0 +1,85 @@
+﻿namespace Saving.Serializers;
+
+using System;
+using System.IO;
+using SharedBase.Archive;
+
+/// <summary>
+///   Main class for handling Thrive save reading
+/// </summary>
+public class SaveArchiveReader : ISArchiveReader
+{
+    private const int BUFFER_SIZE = 1024;
+
+    private readonly Stream stream;
+
+    private byte[]? tempBuffer;
+
+    public SaveArchiveReader(Stream stream)
+    {
+        this.stream = stream;
+    }
+
+    public byte ReadInt8()
+    {
+        return (byte)stream.ReadByte();
+    }
+
+    public uint ReadVariableLengthField32()
+    {
+        uint result = 0;
+        int shift = 0;
+
+        for (int i = 0; i < 5; i++, shift += 7)
+        {
+            var currentByte = ReadInt8();
+            result |= (uint)(currentByte & 0x7F) << shift;
+
+            if ((currentByte & 0x80) == 0)
+                return result;
+
+            if (i == 4)
+                throw new FormatException("Too many bytes in variable length field");
+        }
+
+        throw new FormatException("Variable-length field was not terminated");
+    }
+
+    public string ReadString()
+    {
+        var lengthRaw = ReadVariableLengthField32();
+
+        if (lengthRaw == 0)
+            return string.Empty;
+
+        if (lengthRaw > int.MaxValue)
+            throw new FormatException("Too long string");
+
+        int length = (int)lengthRaw;
+
+        if (length <= BUFFER_SIZE)
+        {
+            tempBuffer ??= new byte[BUFFER_SIZE];
+            ReadBytes(tempBuffer.AsSpan(0, length));
+            return ISArchiveWriter.Utf8NoSignature.GetString(tempBuffer, 0, length);
+        }
+
+        var pool = System.Buffers.ArrayPool<byte>.Shared;
+        var rented = pool.Rent(length);
+        try
+        {
+            ReadBytes(rented.AsSpan(0, length));
+            return ISArchiveWriter.Utf8NoSignature.GetString(rented, 0, length);
+        }
+        finally
+        {
+            pool.Return(rented);
+        }
+    }
+
+    public void ReadBytes(Span<byte> buffer)
+    {
+        if (stream.Read(buffer) != buffer.Length)
+            throw new EndOfStreamException();
+    }
+}
---
+++ src/saving/serializers/SaveArchiveWriter.cs
@@ -0,0 +1,41 @@
+﻿namespace Saving.Serializers;
+
+using System;
+using System.IO;
+using SharedBase.Archive;
+
+/// <summary>
+///   Main class for handling Thrive save writing
+/// </summary>
+public class SaveArchiveWriter : ISArchiveWriter
+{
+    private readonly Stream stream;
+
+    public SaveArchiveWriter(Stream stream)
+    {
+        this.stream = stream;
+
+        if (!stream.CanSeek)
+            throw new ArgumentException("Stream must be seekable");
+    }
+
+    public void Write(byte value)
+    {
+        stream.WriteByte(value);
+    }
+
+    public void Write(ReadOnlySpan<byte> value)
+    {
+        stream.Write(value);
+    }
+
+    public long GetPosition()
+    {
+        return stream.Position;
+    }
+
+    public void Seek(long position)
+    {
+        stream.Seek(position, SeekOrigin.Begin);
+    }
+}
