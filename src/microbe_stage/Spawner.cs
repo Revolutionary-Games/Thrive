4e54f743fded44211c23fb7fdc07961fe577b593
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-10-03 16:23:24 +0300

    Terrain spawning system implementation (#6364)
    * Added generation seed for terrain to patch data
    
    * Added an optimization to skip physics pos update for static
    
    bodies that cannot move after all
    
    * Added ability for spawn system to avoid terrain collisions
    
    * Added configuration types for terrain
    
    * Started working on a terrain spawning system
    
    * Some fetch and other updates for the terrain
    
    * Increased threaded run simulator time to find better runs
    
    * Added missing uid files for the new stuff
    
    * Added the terrain system to the run order
    
    * Started defining some terrain data for testing
    
    * Do a small trick to make order of methods check happy
    
    as trying to please that will just fail in a different way
    
    * Fixed spawning terrain entities
    
    * Added queue spawns to terrain and fixed it to work in general
    
    adjusted the grid size to prevent pop in
    
    * Fixed player position overlap on spawning terrain handling
    
    though for queued spawns the player position is not updated
    
    * Added an explanation for a constant
    
    * Fixed errors from Arch on player suicide
    
    * Stopped terrain z-fighting
    
    * More reasonable vents terrain configuration
    
    * Implemented group/cluster rotation options and put in one small group
    
    * Created a bigger cluster
    
    * Increased terrain grid size to make bigger arrangements possible
    
    * Added some base configurations for most patches planned to have terrain
    
    still left the vents to have stuff in it even though none of them are the planned chunk type in there

--- src/microbe_stage/Spawner.cs
+++ src/microbe_stage/Spawner.cs
@@ -40,6 +40,11 @@ public abstract class Spawner
     public bool DestroyQueued { get; set; }
 
     /// <summary>
+    ///   Name used for internal logging etc. purposes
+    /// </summary>
+    public abstract string Name { get; }
+
+    /// <summary>
     ///   Spawns the next thing. This is an enumerator to be able to control how many things to spawn per frame easily
     /// </summary>
     /// <param name="worldSimulation">The simulation to create the entity in</param>
