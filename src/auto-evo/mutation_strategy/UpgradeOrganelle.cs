51c6cabb12c50368b60f52c7608b89f1bb28d808
Accidental Explorer <alexandervanderhall1996@gmail.com>
2025-10-20 15:43:45 +0200

    Format

--- src/auto-evo/mutation_strategy/UpgradeOrganelle.cs
+++ src/auto-evo/mutation_strategy/UpgradeOrganelle.cs
@@ -29,8 +29,9 @@ public class UpgradeOrganelle : IMutationStrategy<MicrobeSpecies>
     /// </summary>
     /// <param name="criteria">Organelle requirement to apply</param>
     /// <param name="upgradeName">The name of the upgrade (from organelles.json) to apply</param>
-    /// <param name="shouldRepeat">Determines whether this mutation strategy can be used multiple times
-    ///   should be false for any upgrade that does not cost MP</param>
+    /// <param name="shouldRepeat">
+    /// Determines whether this mutation strategy can be used multiple times
+    /// should be false for any upgrade that does not cost MP</param>
     public UpgradeOrganelle(Func<OrganelleDefinition, bool> criteria, string upgradeName, bool shouldRepeat)
     {
         allOrganelles = SimulationParameters.Instance.GetAllOrganelles().Where(criteria).ToFrozenSet();
