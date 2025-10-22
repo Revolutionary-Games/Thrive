4822905888d7af8b9296260d6b3a31ee29469345
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-10-22 12:29:07 +0300

    Added new save context features to the base interface

--- src/saving/ISaveContext.cs
+++ src/saving/ISaveContext.cs
@@ -1,9 +1,22 @@
-﻿/// <summary>
-///   Read only context access to converters
+﻿using System.Collections.Generic;
+using Arch.Core;
+
+/// <summary>
+///   Read-only context access to special code running when loading archives
 /// </summary>
 public interface ISaveContext
 {
-    public SimulationParameters Simulation { get; }
+    // public SimulationParameters Simulation { get; }
 
-    public GameWorld? World { get; }
+    // public GameWorld? World { get; }
+
+    public HashSet<Entity> UnsavedEntities { get; }
+
+    public World? ProcessedEntityWorld { get; set; }
+
+    public int ActiveProcessedWorldId { get; set; }
+
+    public Dictionary<Entity, Entity> OldToNewEntityMapping { get; }
+
+    public bool SkipSavingEntity(in Entity value);
 }
