d3d8708843391141ce562c9d81f8d3c7ea480071
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-10-24 15:27:50 +0300

    Fix a bug sometimes when loading that player entity is read too early
    before it is set to a valid or invalid value

--- src/microbe_stage/MicrobeStage.cs
+++ src/microbe_stage/MicrobeStage.cs
@@ -225,6 +225,9 @@ public sealed partial class MicrobeStage : CreatureStageBase<Entity, MicrobeWorl
     {
         base._Ready();
 
+        // Set a null player until we are initialised
+        Player = Entity.Null;
+
         // Start a new game if started directly from MicrobeStage.tscn
         CurrentGame ??= GameProperties.StartNewMicrobeGame(new WorldGenerationSettings());
 
