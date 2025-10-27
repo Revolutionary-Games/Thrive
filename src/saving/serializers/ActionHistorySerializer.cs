f0efa10b787b016b6ef8ffe9df30893f77937b75
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-10-27 19:24:09 +0200

    Forgot to include these new serializers for the editor actions commit

---
+++ src/saving/serializers/ActionHistorySerializer.cs
@@ -0,0 +1,35 @@
+﻿namespace Saving.Serializers;
+
+using System;
+using System.Collections;
+using System.Reflection;
+using SharedBase.Archive;
+
+public class ActionHistorySerializer
+{
+    public const ushort SERIALIZATION_VERSION = 1;
+
+    public static void WriteToArchive(ISArchiveWriter writer, ArchiveObjectType type, object obj)
+    {
+        writer.WriteObject((IArchivable)obj);
+    }
+
+    public static object ReadFromArchive(ISArchiveReader reader, Type typeFromArchive, ushort version, int referenceId)
+    {
+        if (version is > SERIALIZATION_VERSION or <= 0)
+            throw new InvalidArchiveVersionException(version, SERIALIZATION_VERSION);
+
+        var data = reader.ReadObject<IList>();
+        int index = reader.ReadInt32();
+
+        var constructor = typeFromArchive.GetConstructor(
+            BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public,
+            [data.GetType(), typeof(int)]);
+
+        if (constructor == null)
+            throw new InvalidOperationException($"No constructor found for {typeFromArchive.Name}");
+
+        var instance = constructor.Invoke([data, index]);
+        return instance;
+    }
+}
