8627af93513ebf38ae878845d37137cbcf8ae0a4
Accidental Explorer <alexandervanderhall1996@gmail.com>
2025-11-17 00:20:55 +0100

    Balance tweak

--- simulation_parameters/Constants.cs
+++ simulation_parameters/Constants.cs
@@ -1319,7 +1319,7 @@ public static class Constants
     public const float AUTO_EVO_CHUNK_ENERGY_AMOUNT = 90000000;
     public const float AUTO_EVO_CHUNK_AMOUNT_NERF = 0.01f;
     public const float AUTO_EVO_PASSIVE_COMPOUND_COLLECTION_FRACTION = 0.1f;
-    public const float AUTO_EVO_REPRODUCTION_COMPOUND_PRODUCTION_SCORE = 2500.0f;
+    public const float AUTO_EVO_REPRODUCTION_COMPOUND_PRODUCTION_SCORE = 3000.0f;
     public const float AUTO_EVO_REPRODUCTION_COMPOUND_COST_WEAKENING_MODIFIER = 0.5f;
 
     public const float AUTO_EVO_ARTIFICIAL_UPGRADE_BONUS_SMALL = 1.5f;
