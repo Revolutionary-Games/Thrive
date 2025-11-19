a266874c14f7e65f7ae49cb97bfab88f07cf866f
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-11-19 16:39:55 +0200

    Started preparing new interfaces for multice and macro readonly species

---
+++ src/general/mutation_points/IReadOnlyMetaball.cs
@@ -0,0 +1,23 @@
+﻿using Godot;
+
+public interface IReadOnlyMetaball
+{
+    public Vector3 Position { get; }
+
+    /// <summary>
+    ///   The diameter of the metaball
+    /// </summary>
+    public float Size { get; }
+
+    public IReadOnlyMetaball? Parent { get; }
+
+    /// <summary>
+    ///   Basic rendering of the metaballs for now just uses a colour
+    /// </summary>
+    public Color Colour { get; }
+}
+
+public interface IReadonlyMacroscopicMetaball : IReadOnlyMetaball
+{
+    public IReadOnlyCellDefinition CellType { get; }
+}
---
+++ src/general/mutation_points/IReadOnlyMetaballLayout.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+
+public interface IReadOnlyMetaballLayout<T> : IReadOnlyCollection<T>
+    where T : IReadOnlyMetaball
+{
+}
