4b34a02434210aedf0599d9780a7fcaee6684c13
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2025-11-27 09:50:56 +0200

    Split out the entity valid check on its own line in DamageOnTouchSystem.cs

--- src/engine/common_systems/DamageOnTouchSystem.cs
+++ src/engine/common_systems/DamageOnTouchSystem.cs
@@ -52,8 +52,11 @@ public partial class DamageOnTouchSystem : BaseSystem<World, float>
         {
             ref var collision = ref collisions![i];
 
+            if (collision.SecondEntity == default(Entity))
+                continue;
+
             // Skip collisions with things that can't be damaged
-            if (collision.SecondEntity == default(Entity) || !collision.SecondEntity.IsAliveAndHas<Health>())
+            if (!collision.SecondEntity.IsAliveAndHas<Health>())
                 continue;
 
             // If this doesn't cause any damage, we can consider this hit here immediately a success
