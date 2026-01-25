ea9d08f96fdc2b645ddbd4dbe1204c3dc15a27ff
IdfbAn <lukazaridze4@gmail.com>
2026-01-25 23:37:29 +0400

    PatchMapDrawer.cs: Comply with JetBrains check

--- src/microbe_stage/editor/PatchMapDrawer.cs
+++ src/microbe_stage/editor/PatchMapDrawer.cs
@@ -1144,8 +1144,7 @@ public partial class PatchMapDrawer : Control
 
                 // https://github.com/Revolutionary-Games/Thrive/issues/6676
                 populationIndicatorContainer.Position =
-                    new Vector2(
-                        (Constants.PATCH_NODE_RECT_LENGTH + Constants.PATCH_REGION_BORDER_WIDTH / 4) / 4,
+                    new Vector2((Constants.PATCH_NODE_RECT_LENGTH + Constants.PATCH_REGION_BORDER_WIDTH / 4) / 4,
                         (Constants.PATCH_NODE_RECT_LENGTH + Constants.PATCH_REGION_BORDER_WIDTH / 4) / 4);
 
                 populationIndicatorContainer.AddChild(lifeIndicator);
