Revolutionary-Games/Thrive

commit 41c78b14af05320c11d8129651ea1d2d3f24d97f

Show summed colony processes in the process panel (#6709)
* Basic implementation of colony process panel
There are still some architectural problems to solve

* Introduce a new process statistics type for easier summing

* Some small fixes

* Format fixes

* Format fixes

* Slightly optimize the memory usage

* Safety checks

* Implement proper algorithms for hashing and comparing summed process stats

* Instead of conditionally renaming "Cell Processes" to "Colony Processes", just fully rename them to "Organism Processes"

* Format fixes

* Fix Process List updating too frequently
Also fix process toggling

* Add the ATP clarification
In the form of a help button that displays a popup with the said clarification. The help button is only shown if the player has a colony

* Improve speed comparison

* Make `GetPlayerProcessStatistics` not fail entirely if some colony members don't have statistics set

* Adjust some text in the multicellular stage

* Slightly improve a text

* Format fixes

* Change multicellular process panel name to "Organism processes" and make microbe panel name unique instead ("Cell processes")

* Update localizations

* Make the explanation panel smaller

* Make summed process stats' hash generator consistent
To avoid the issue where its dictionary can't find it

* Clean up unused processes

* Rename the Explanation Panel

* Switched to a data marking approach to avoiding temporary array

and needing to have an enumerator method which allocates memory internally

---------

Co-authored-by: Henri Hyyryläinen <henri.hyyrylainen@gmail.com>

src/microbe_stage/Spawners.cs
@@ -614,7 +614,7 @@ public static class SpawnHelpers
     {
         var recorder = worldSimulation.StartRecordingEntityCommands();
         return (recorder, SpawnMicrobeWithoutFinalizing(worldSimulation, spawnEnvironment, species, location,
-            aiControlled, multicellularData, recorder, out entity, multicellularSpawnState, true, random));
+            aiControlled, multicellularData, recorder, out entity, multicellularSpawnState, true, false, random));
     }
 
     public static float SpawnMicrobeWithoutFinalizing(IWorldSimulation worldSimulation,
@@ -622,7 +622,7 @@ public static class SpawnHelpers
         Vector3 location, bool aiControlled, (CellType? MulticellularCellType, int CellBodyPlanIndex) multicellularData,
         CommandBuffer recorder, out Entity entity,
         MulticellularSpawnState multicellularSpawnState = MulticellularSpawnState.Bud,
-        bool giveInitialCompounds = true, Random? random = null)
+        bool giveInitialCompounds = true, bool forceBioProcessStatistics = false, Random? random = null)
     {
         // If this method is modified, it must be ensured that CellPropertiesHelpers.ReApplyCellTypeProperties and
         // MicrobeVisualOnlySimulation microbe update methods are also up to date
@@ -700,7 +700,7 @@ public static class SpawnHelpers
 
         var bioProcesses = new BioProcesses
         {
-            ProcessStatistics = aiControlled ? null : new ProcessStatistics(),
+            ProcessStatistics = !aiControlled || forceBioProcessStatistics ? new ProcessStatistics() : null,
         };
 
         if (species is MulticellularSpecies multicellularSpecies)

src/multicellular_stage/systems/DelayedColonyOperationSystem.cs
@@ -68,7 +68,7 @@ public partial class DelayedColonyOperationSystem : BaseSystem<World, float>
         var weight = SpawnHelpers.SpawnMicrobeWithoutFinalizing(worldSimulation, spawnEnvironment, species,
             colonyPosition.Position + colonyPosition.Rotation * attachPosition.RelativePosition, true,
             (cellTemplate.ModifiableCellType, bodyPlanIndex), recorder, out var member, MulticellularSpawnState.Bud,
-            giveStartingCompounds);
+            giveStartingCompounds, colonyEntity.Has<PlayerMarker>());
 
         // Register with the spawn system to allow this entity to despawn if it gets cut off from the colony later
         // or attaching fails

commit c8f4951a204bef89b8b144949523a5e5194c7ab4

Fix a small bug caused by #6709 (microbe stage colonies were spamming errors)

src/microbe_stage/Spawners.cs
@@ -614,7 +614,7 @@ public static class SpawnHelpers
     {
         var recorder = worldSimulation.StartRecordingEntityCommands();
         return (recorder, SpawnMicrobeWithoutFinalizing(worldSimulation, spawnEnvironment, species, location,
-            aiControlled, multicellularData, recorder, out entity, multicellularSpawnState, true, false, random));
+            aiControlled, multicellularData, recorder, out entity, multicellularSpawnState, true, random));
     }
 
     public static float SpawnMicrobeWithoutFinalizing(IWorldSimulation worldSimulation,
@@ -622,7 +622,7 @@ public static class SpawnHelpers
         Vector3 location, bool aiControlled, (CellType? MulticellularCellType, int CellBodyPlanIndex) multicellularData,
         CommandBuffer recorder, out Entity entity,
         MulticellularSpawnState multicellularSpawnState = MulticellularSpawnState.Bud,
-        bool giveInitialCompounds = true, bool forceBioProcessStatistics = false, Random? random = null)
+        bool giveInitialCompounds = true, Random? random = null)
     {
         // If this method is modified, it must be ensured that CellPropertiesHelpers.ReApplyCellTypeProperties and
         // MicrobeVisualOnlySimulation microbe update methods are also up to date
@@ -700,7 +700,7 @@ public static class SpawnHelpers
 
         var bioProcesses = new BioProcesses
         {
-            ProcessStatistics = !aiControlled || forceBioProcessStatistics ? new ProcessStatistics() : null,
+            ProcessStatistics = !aiControlled ? new ProcessStatistics() : null,
         };
 
         if (species is MulticellularSpecies multicellularSpecies)

src/microbe_stage/components/MicrobeColony.cs
@@ -1,12 +1,13 @@
 ﻿namespace Components;
 
-using System;
-using System.Collections.Generic;
 using Arch.Buffer;
 using Arch.Core;
 using Arch.Core.Extensions;
 using Godot;
 using SharedBase.Archive;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 /// <summary>
 ///   Microbe colony newMember. This component is added to the colony lead cell. This contains the overall info
@@ -1132,6 +1133,11 @@ public static class MicrobeColonyHelpers
         control.QueuedSlimeSecretionTime = 0;
 
         ReportReproductionStatusOnAddToColony(addedEntity);
+
+        if (addedEntity.TryGet<BioProcesses>(out var bioProcesses))
+        {
+            addedEntity.Get<BioProcesses>().ProcessStatistics = new();
+        }
 
         var processes = addedEntity.Get<BioProcesses>().ActiveProcesses;
 

src/multicellular_stage/systems/DelayedColonyOperationSystem.cs
@@ -75,7 +75,7 @@ public partial class DelayedColonyOperationSystem : BaseSystem<World, float>
         var weight = SpawnHelpers.SpawnMicrobeWithoutFinalizing(worldSimulation, spawnEnvironment, species,
             colonyPosition.Position + colonyPosition.Rotation * attachPosition.RelativePosition, true,
             (cellTemplate.ModifiableCellType, bodyPlanIndex), recorder, out var member, MulticellularSpawnState.Bud,
-            giveStartingCompounds, colonyEntity.Has<PlayerMarker>());
+            giveStartingCompounds);
 
         // Register with the spawn system to allow this entity to despawn if it gets cut off from the colony later
         // or attaching fails