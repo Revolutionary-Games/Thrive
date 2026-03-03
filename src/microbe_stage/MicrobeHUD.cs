Revolutionary-Games/Thrive

commit 41c78b14af05320c11d8129651ea1d2d3f24d97f

Show summed colony processes in the process panel (#6709)
* Basic implementation of colony process panel
There are still some architectural problems to solve

* Introduce a new process statistics type for easier summing

* Some small fixes

* Format fixes

* Format fixes

* Slightly optimize the memory usage

* Safety checks

* Implement proper algorithms for hashing and comparing summed process stats

* Instead of conditionally renaming "Cell Processes" to "Colony Processes", just fully rename them to "Organism Processes"

* Format fixes

* Fix Process List updating too frequently
Also fix process toggling

* Add the ATP clarification
In the form of a help button that displays a popup with the said clarification. The help button is only shown if the player has a colony

* Improve speed comparison

* Make `GetPlayerProcessStatistics` not fail entirely if some colony members don't have statistics set

* Adjust some text in the multicellular stage

* Slightly improve a text

* Format fixes

* Change multicellular process panel name to "Organism processes" and make microbe panel name unique instead ("Cell processes")

* Update localizations

* Make the explanation panel smaller

* Make summed process stats' hash generator consistent
To avoid the issue where its dictionary can't find it

* Clean up unused processes

* Rename the Explanation Panel

* Switched to a data marking approach to avoiding temporary array

and needing to have an enumerator method which allocates memory internally

---------

Co-authored-by: Henri Hyyryläinen <henri.hyyrylainen@gmail.com>

src/microbe_stage/MicrobeHUD.cs
@@ -32,6 +32,14 @@ public partial class MicrobeHUD : CreatureStageHUDBase<MicrobeStage>
 
     private readonly Dictionary<CompoundDefinition, InspectedEntityLabel> hoveredCompoundControls = new();
 
+    /// <summary>
+    ///   Because of how <see cref="ChildObjectCache{TKey, TNode}"/> works, process stats instances need to remain
+    ///   consistent to reduce update frequency.
+    /// </summary>
+    private readonly Dictionary<TweakedProcess, SummedProcessStatistics> organismProcesses = new();
+
+    private readonly List<TweakedProcess> tempProcesses = new();
+
     [Export]
     private ActionButton bindingModeHotkey = null!;
 
@@ -177,6 +185,8 @@ public partial class MicrobeHUD : CreatureStageHUDBase<MicrobeStage>
             UpdateMacroscopicButton(stage.Player);
 
             UpdateHeatHelperWidget(stage.Player);
+
+            UpdateProcessPanelStatus(stage.Player);
 
             UpdateProcessPanelExternalSpeedModifier(stage.WorldSimulation.WorldTimeScale);
         }
@@ -536,9 +546,92 @@ public partial class MicrobeHUD : CreatureStageHUDBase<MicrobeStage>
         ingestedMatterBar.UpdateValue(GetPlayerUsedIngestionCapacity(), maxSize);
     }
 
-    protected override ProcessStatistics? GetPlayerProcessStatistics()
+    protected override IEnumerable<IProcessDisplayInfo>? GetPlayerProcessStatistics()
     {
-        return stage!.Player.Get<BioProcesses>().ProcessStatistics;
+        foreach (var process in organismProcesses)
+        {
+            process.Value.Clear();
+            process.Value.Marked = false;
+        }
+
+        var playerProcesses = stage!.Player.Get<BioProcesses>().ProcessStatistics?.Processes;
+
+        if (playerProcesses == null)
+        {
+            GD.PrintErr("Player process statistics are uninitialized, can't display them in the process panel");
+
+            return null;
+        }
+
+        foreach (var process in playerProcesses)
+        {
+            var display = process.Value.ComputeAverageValues();
+
+            if (!organismProcesses.TryGetValue(process.Key, out var stats))
+            {
+                stats = new SummedProcessStatistics(display);
+                organismProcesses[process.Key] = stats;
+            }
+            else
+            {
+                stats.AddProcess(display);
+            }
+
+            stats.Marked = true;
+        }
+
+        if (stage.Player.TryGet<MicrobeColony>(out var colony))
+        {
+            for (int i = 1; i < colony.ColonyMembers.Length; ++i)
+            {
+                var colonyMemberProcesses = colony.ColonyMembers[i].Get<BioProcesses>().ProcessStatistics?.Processes;
+
+                if (colonyMemberProcesses == null)
+                {
+                    GD.PrintErr(
+                        "Colony member process statistics are uninitialized, can't display them in the process panel");
+
+                    continue;
+                }
+
+                foreach (var process in colonyMemberProcesses)
+                {
+                    var display = process.Value.ComputeAverageValues();
+
+                    if (!organismProcesses.TryGetValue(process.Key, out var stats))
+                    {
+                        stats = new SummedProcessStatistics(display);
+                        organismProcesses[process.Key] = stats;
+                    }
+                    else
+                    {
+                        stats.AddProcess(display);
+                    }
+
+                    stats.Marked = true;
+                }
+            }
+        }
+
+        // Clear unmarked items
+        foreach (var entry in organismProcesses)
+        {
+            if (!entry.Value.Marked)
+                tempProcesses.Add(entry.Key);
+        }
+
+        if (tempProcesses.Count > 0)
+        {
+            foreach (var process in tempProcesses)
+            {
+                if (!organismProcesses.Remove(process))
+                    GD.PrintErr("Expected process remove failed");
+            }
+
+            tempProcesses.Clear();
+        }
+
+        return organismProcesses.Values;
     }
 
     protected override void CalculatePlayerReproductionProgress(Dictionary<Compound, float> gatheredCompounds,
@@ -820,6 +913,11 @@ public partial class MicrobeHUD : CreatureStageHUDBase<MicrobeStage>
         }
 
         UpdateColonySize(newColonySize);
+    }
+
+    private void UpdateProcessPanelStatus(Entity player)
+    {
+        processPanel.IsMicrobe = !player.Has<MicrobeColony>();
     }
 
     private void UpdateColonySizeForMacroscopic()

src/microbe_stage/Spawners.cs
@@ -614,7 +614,7 @@ public static class SpawnHelpers
     {
         var recorder = worldSimulation.StartRecordingEntityCommands();
         return (recorder, SpawnMicrobeWithoutFinalizing(worldSimulation, spawnEnvironment, species, location,
-            aiControlled, multicellularData, recorder, out entity, multicellularSpawnState, true, random));
+            aiControlled, multicellularData, recorder, out entity, multicellularSpawnState, true, false, random));
     }
 
     public static float SpawnMicrobeWithoutFinalizing(IWorldSimulation worldSimulation,
@@ -622,7 +622,7 @@ public static class SpawnHelpers
         Vector3 location, bool aiControlled, (CellType? MulticellularCellType, int CellBodyPlanIndex) multicellularData,
         CommandBuffer recorder, out Entity entity,
         MulticellularSpawnState multicellularSpawnState = MulticellularSpawnState.Bud,
-        bool giveInitialCompounds = true, Random? random = null)
+        bool giveInitialCompounds = true, bool forceBioProcessStatistics = false, Random? random = null)
     {
         // If this method is modified, it must be ensured that CellPropertiesHelpers.ReApplyCellTypeProperties and
         // MicrobeVisualOnlySimulation microbe update methods are also up to date
@@ -700,7 +700,7 @@ public static class SpawnHelpers
 
         var bioProcesses = new BioProcesses
         {
-            ProcessStatistics = aiControlled ? null : new ProcessStatistics(),
+            ProcessStatistics = !aiControlled || forceBioProcessStatistics ? new ProcessStatistics() : null,
         };
 
         if (species is MulticellularSpecies multicellularSpecies)

src/multicellular_stage/systems/DelayedColonyOperationSystem.cs
@@ -68,7 +68,7 @@ public partial class DelayedColonyOperationSystem : BaseSystem<World, float>
         var weight = SpawnHelpers.SpawnMicrobeWithoutFinalizing(worldSimulation, spawnEnvironment, species,
             colonyPosition.Position + colonyPosition.Rotation * attachPosition.RelativePosition, true,
             (cellTemplate.ModifiableCellType, bodyPlanIndex), recorder, out var member, MulticellularSpawnState.Bud,
-            giveStartingCompounds);
+            giveStartingCompounds, colonyEntity.Has<PlayerMarker>());
 
         // Register with the spawn system to allow this entity to despawn if it gets cut off from the colony later
         // or attaching fails

commit c487d3d680582b0f7503d7d88d6d9a1818eee5d2

Make newly spawned cells apply correct process statuses (#6772)
* Fix processes not merging in some cases

* Make the code set process statuses based on other cells in the colony

* Fix a small bug caused by #6709 (microbe stage colonies were spamming errors)

* Format fixes

* Rename `process1` to `memberProcess`
Because I apparently forgot to rename to something reasonable after testing this approach

* Make the code only add process stats to cells whose leader has those

* Split some code into its own method for readability
And add a `WritesToComponent`

* Make the code actually write to `BioProcesses`
Because using `TryGet` gives not a reference, but a copy

* Add a bit more error checking and reduce component lookups

as component lookups are relatively expensive operations

---------

Co-authored-by: Henri Hyyryläinen <henri.hyyrylainen@gmail.com>

src/microbe_stage/MicrobeHUD.cs
@@ -36,9 +36,9 @@ public partial class MicrobeHUD : CreatureStageHUDBase<MicrobeStage>
     ///   Because of how <see cref="ChildObjectCache{TKey, TNode}"/> works, process stats instances need to remain
     ///   consistent to reduce update frequency.
     /// </summary>
-    private readonly Dictionary<TweakedProcess, SummedProcessStatistics> organismProcesses = new();
+    private readonly Dictionary<BioProcess, SummedProcessStatistics> organismProcesses = new();
 
-    private readonly List<TweakedProcess> tempProcesses = new();
+    private readonly List<BioProcess> tempProcesses = new();
 
     [Export]
     private ActionButton bindingModeHotkey = null!;
@@ -567,10 +567,10 @@ public partial class MicrobeHUD : CreatureStageHUDBase<MicrobeStage>
         {
             var display = process.Value.ComputeAverageValues();
 
-            if (!organismProcesses.TryGetValue(process.Key, out var stats))
+            if (!organismProcesses.TryGetValue(process.Key.Process, out var stats))
             {
                 stats = new SummedProcessStatistics(display);
-                organismProcesses[process.Key] = stats;
+                organismProcesses[process.Key.Process] = stats;
             }
             else
             {
@@ -598,10 +598,10 @@ public partial class MicrobeHUD : CreatureStageHUDBase<MicrobeStage>
                 {
                     var display = process.Value.ComputeAverageValues();
 
-                    if (!organismProcesses.TryGetValue(process.Key, out var stats))
+                    if (!organismProcesses.TryGetValue(process.Key.Process, out var stats))
                     {
                         stats = new SummedProcessStatistics(display);
-                        organismProcesses[process.Key] = stats;
+                        organismProcesses[process.Key.Process] = stats;
                     }
                     else
                     {

src/microbe_stage/Spawners.cs
@@ -614,7 +614,7 @@ public static class SpawnHelpers
     {
         var recorder = worldSimulation.StartRecordingEntityCommands();
         return (recorder, SpawnMicrobeWithoutFinalizing(worldSimulation, spawnEnvironment, species, location,
-            aiControlled, multicellularData, recorder, out entity, multicellularSpawnState, true, false, random));
+            aiControlled, multicellularData, recorder, out entity, multicellularSpawnState, true, random));
     }
 
     public static float SpawnMicrobeWithoutFinalizing(IWorldSimulation worldSimulation,
@@ -622,7 +622,7 @@ public static class SpawnHelpers
         Vector3 location, bool aiControlled, (CellType? MulticellularCellType, int CellBodyPlanIndex) multicellularData,
         CommandBuffer recorder, out Entity entity,
         MulticellularSpawnState multicellularSpawnState = MulticellularSpawnState.Bud,
-        bool giveInitialCompounds = true, bool forceBioProcessStatistics = false, Random? random = null)
+        bool giveInitialCompounds = true, Random? random = null)
     {
         // If this method is modified, it must be ensured that CellPropertiesHelpers.ReApplyCellTypeProperties and
         // MicrobeVisualOnlySimulation microbe update methods are also up to date
@@ -700,7 +700,7 @@ public static class SpawnHelpers
 
         var bioProcesses = new BioProcesses
         {
-            ProcessStatistics = !aiControlled || forceBioProcessStatistics ? new ProcessStatistics() : null,
+            ProcessStatistics = !aiControlled ? new ProcessStatistics() : null,
         };
 
         if (species is MulticellularSpecies multicellularSpecies)

src/microbe_stage/components/MicrobeColony.cs
@@ -1132,6 +1132,99 @@ public static class MicrobeColonyHelpers
         control.QueuedSlimeSecretionTime = 0;
 
         ReportReproductionStatusOnAddToColony(addedEntity);
+
+        if (!addedEntity.Has<BioProcesses>())
+        {
+            GD.PrintErr("A newly added colony member has no BioProcesses set");
+            return;
+        }
+
+        if (!colony.Leader.IsAliveAndHas<BioProcesses>())
+        {
+            GD.PrintErr("Cell added to a colony with invalid leader (has no BioProcesses set)");
+            return;
+        }
+
+        ref var bioProcesses = ref addedEntity.Get<BioProcesses>();
+
+        if (colony.Leader.Get<BioProcesses>().ProcessStatistics != null)
+        {
+            // Make colony members also track processes if the leader is tracking processes
+            bioProcesses.ProcessStatistics ??= new ProcessStatistics();
+        }
+
+        ApplyColonyProcessStatuses(ref colony, addedEntity, ref bioProcesses);
+    }
+
+    private static void ApplyColonyProcessStatuses(ref MicrobeColony colony, in Entity addedEntity,
+        ref BioProcesses bioProcesses)
+    {
+        var processes = bioProcesses.ActiveProcesses;
+
+        if (processes == null)
+        {
+            GD.PrintErr(
+                "Couldn't apply process settings to a new colony member because of its ActiveProcesses being unset");
+            return;
+        }
+
+        // This remembers the last member we read processes from for slightly reduced process component reading
+        List<TweakedProcess>? memberProcesses = null;
+
+        for (int i = 0; i < processes.Count; ++i)
+        {
+            var process = processes[i];
+
+            bool enabled = true;
+            bool found = false;
+
+            // If we already know a member where we found good data, check again first in there
+            if (memberProcesses != null)
+            {
+                foreach (var memberProcess in memberProcesses)
+                {
+                    if (memberProcess.Process == process.Process)
+                    {
+                        enabled = memberProcess.SpeedMultiplier > 0.0f;
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!found)
+            {
+                foreach (var member in colony.ColonyMembers)
+                {
+                    if (member == addedEntity)
+                        continue;
+
+                    memberProcesses = member.Get<BioProcesses>().ActiveProcesses;
+
+                    if (memberProcesses == null)
+                        continue;
+
+                    foreach (var memberProcess in memberProcesses)
+                    {
+                        if (memberProcess.Process == process.Process)
+                        {
+                            enabled = memberProcess.SpeedMultiplier > 0.0f;
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (found)
+                        break;
+                }
+
+                if (!found)
+                    memberProcesses = null;
+            }
+
+            process.SpeedMultiplier = enabled ? 1.0f : 0.0f;
+            processes[i] = process;
+        }
     }
 
     /// <summary>

src/multicellular_stage/systems/DelayedColonyOperationSystem.cs
@@ -20,6 +20,7 @@ using Godot;
 [WritesToComponent(typeof(CellProperties))]
 [WritesToComponent(typeof(Physics))]
 [WritesToComponent(typeof(OrganelleContainer))]
+[WritesToComponent(typeof(BioProcesses))]
 [ReadsComponent(typeof(MulticellularSpeciesMember))]
 [ReadsComponent(typeof(WorldPosition))]
 [RunsAfter(typeof(ColonyBindingSystem))]
@@ -75,7 +76,7 @@ public partial class DelayedColonyOperationSystem : BaseSystem<World, float>
         var weight = SpawnHelpers.SpawnMicrobeWithoutFinalizing(worldSimulation, spawnEnvironment, species,
             colonyPosition.Position + colonyPosition.Rotation * attachPosition.RelativePosition, true,
             (cellTemplate.ModifiableCellType, bodyPlanIndex), recorder, out var member, MulticellularSpawnState.Bud,
-            giveStartingCompounds, colonyEntity.Has<PlayerMarker>());
+            giveStartingCompounds);
 
         // Register with the spawn system to allow this entity to despawn if it gets cut off from the colony later
         // or attaching fails