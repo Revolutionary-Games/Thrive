bb79cb839ed2f9c7b8c97679d73cf4e26195fb73
Accidental Explorer <alexandervanderhall1996@gmail.com>
2026-03-04 09:58:14 +0100

    Revert "Try reducing MAX_VARIANTS_IN_MUTATIONS for performance."
    This reverts commit cad75ae5eaa3434bf111c1fe62e3c3034cacc9f0.

--- simulation_parameters/Constants.cs
+++ simulation_parameters/Constants.cs
@@ -1216,7 +1216,7 @@ public static class Constants
 
     // Mutation Variables
     public const int MAX_VARIANTS_PER_MUTATION = 50;
-    public const int MAX_VARIANTS_IN_MUTATIONS = 10;
+    public const int MAX_VARIANTS_IN_MUTATIONS = 30;
     public const float MUTATION_PRUNING_THRESHOLD = -0.1f;
     public const float MUTATION_BACTERIA_TO_EUKARYOTE = 0.01f;
     public const float MUTATION_CREATION_RATE = 0.25f;
