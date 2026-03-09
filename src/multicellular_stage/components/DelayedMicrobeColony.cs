e6c955d36d51bff416c996e4926651cb070a023e
dligr <dellpls43@gmail.com>
2026-03-10 00:35:21 +0300

    Improve save compatibility

--- src/multicellular_stage/components/DelayedMicrobeColony.cs
+++ src/multicellular_stage/components/DelayedMicrobeColony.cs
@@ -84,7 +84,7 @@ public static class DelayedMicrobeColonyHelpers
             FinishAttachingToColony = reader.ReadObject<Entity>(),
             AttachIndex = reader.ReadInt32(),
             GrowAdditionalMembers = reader.ReadInt32(),
-            PlayAnimation = reader.ReadBool(),
+            PlayAnimation = DelayedMicrobeColony.SERIALIZATION_VERSION > 1 ? reader.ReadBool() : true,
         };
     }
 }
