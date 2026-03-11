d711659bdcae12187ad59dc2f462100ebed74ae1
Accidental Explorer <alexandervanderhall1996@gmail.com>
2026-03-11 18:21:13 +0100

    Revert "made WeightedComparedScores more bounded."
    This reverts commit a0e0eee2ac4387f7e60d3138948c2b9af8652ffa.

--- src/auto-evo/selection_pressure/SelectionPressure.cs
+++ src/auto-evo/selection_pressure/SelectionPressure.cs
@@ -42,17 +42,17 @@ public abstract class SelectionPressure : IArchivable
 
         if (oldScore == 0)
         {
-            return newScore > 0 ? Weight : 0;
+            return newScore > 0 ? (newScore * Weight * 10) : 0;
         }
 
         if (newScore > oldScore)
         {
-            return 1 - oldScore / newScore * Weight;
+            return newScore / oldScore * Weight;
         }
 
         if (oldScore > newScore)
         {
-            return -(1 - newScore / oldScore) * Weight;
+            return -(oldScore / newScore) * Weight;
         }
 
         return 0;
