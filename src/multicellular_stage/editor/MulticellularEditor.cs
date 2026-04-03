076e3994f35a1d4741eb9d41152537cfa9c3c0f8
IdfbAn <lukazaridze4@gmail.com>
2026-04-03 13:07:16 +0400

    Revert "Initial "solution" for adjacency specialization in GetPlayerDataSource()"
    This reverts commit 2b2d049f4d60509b16d8ec4c58c202d8c678f945.

--- src/multicellular_stage/editor/MulticellularEditor.cs
+++ src/multicellular_stage/editor/MulticellularEditor.cs
@@ -769,16 +769,13 @@ public partial class MulticellularEditor : EditorBase<EditorAction, MicrobeStage
         {
             var cellEnergyBalance = new EnergyBalanceInfoSimple();
 
+            // TODO: specialization from positions (GetAdjacencySpecializationBonus)
             var specialization =
                 MicrobeInternalCalculations.CalculateSpecializationBonus(cellType.ModifiableOrganelles.Organelles,
                     tempMemory1);
-            var adjacencySpecialization = editedSpecies.CalculateAverageSpecialization()
-                / editedSpecies.ModifiableCellTypes.Count;
-
-            var totalSpecialization = specialization * adjacencySpecialization;
 
             ProcessSystem.ComputeEnergyBalanceSimple(cellType.ModifiableOrganelles.Organelles, CurrentPatch.Biome,
-                in tolerances, totalSpecialization, cellType.MembraneType, Vector3.Zero, false, true,
+                in tolerances, specialization, cellType.MembraneType, Vector3.Zero, false, true,
                 CurrentGame.GameWorld.WorldSettings, CompoundAmountType.Maximum, null, cellEnergyBalance);
 
             GetBestEnergyBalanceProperties(energyBalance, cellEnergyBalance);
