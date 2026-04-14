11ab02210c169778a5260613965682509167c062
Accidental Explorer <alexandervanderhall1996@gmail.com>
2026-04-14 13:43:21 +0200

    Number tweak

--- src/auto-evo/selection_pressure/GeneralAvoidPredationSelectionPressure.cs
+++ src/auto-evo/selection_pressure/GeneralAvoidPredationSelectionPressure.cs
@@ -35,7 +35,7 @@ public class GeneralAvoidPredationSelectionPressure : SelectionPressure
         new ChangeBehaviorScore(ChangeBehaviorScore.BehaviorAttribute.Aggression, 100.0f),
         new ChangeBehaviorScore(ChangeBehaviorScore.BehaviorAttribute.Opportunism, -150.0f),
         new ChangeBehaviorScore(ChangeBehaviorScore.BehaviorAttribute.Fear, 150.0f),
-        new ChangeBehaviorScore(ChangeBehaviorScore.BehaviorAttribute.Fear, -50.0f),
+        new ChangeBehaviorScore(ChangeBehaviorScore.BehaviorAttribute.Fear, -150.0f),
         new ChangeMembraneType("double"),
         new ChangeMembraneType("cellulose"),
         new ChangeMembraneType("chitin"),
