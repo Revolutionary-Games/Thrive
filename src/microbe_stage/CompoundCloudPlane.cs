58515a655d3b70d44b47119c6ab83812b1fee2e0
Francesco <francescosollazzi37@gmail.com>
2026-05-01 17:21:26 +0200

    Removed now unused SIMD imports

--- src/microbe_stage/CompoundCloudPlane.cs
+++ src/microbe_stage/CompoundCloudPlane.cs
@@ -5,9 +5,6 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Runtime.Intrinsics;
-using System.Runtime.Intrinsics.X86;
 using System.Threading;
 using System.Threading.Tasks;
 using Godot;
