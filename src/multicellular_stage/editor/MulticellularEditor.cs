4c885d276b35428d78cb7e3c13064e2da21601aa
Accidental Explorer <alexandervanderhall1996@gmail.com>
2026-05-08 12:21:58 +0200

    Have editing the cell type in the MulticellularEditor also call to use RepositionToOrigin (which includes updating the SpecializationBonus)

--- src/multicellular_stage/editor/MulticellularEditor.cs
+++ src/multicellular_stage/editor/MulticellularEditor.cs
@@ -670,7 +670,7 @@ public partial class MulticellularEditor : EditorBase<EditorAction, MicrobeStage
         if (affectedACell)
         {
             GD.Print("Undone / redone action affected cell types");
-            cellEditorTab.OnFinishEditing(false);
+            cellEditorTab.OnFinishEditing(true);
 
             // cellEditorTab.OnEditorSpeciesSetup(EditedBaseSpecies);
             bodyPlanEditorTab.OnCellTypeEdited(cellTypeEditsHolder.GetOriginalType(selectedCellTypeToEdit));
@@ -684,7 +684,7 @@ public partial class MulticellularEditor : EditorBase<EditorAction, MicrobeStage
 
         var oldName = selectedCellTypeToEdit.CellTypeName;
 
-        cellEditorTab.OnFinishEditing(false);
+        cellEditorTab.OnFinishEditing(true);
 
         // Revert to the old name if the name is a duplicate
         if (EditedSpecies.ModifiableCellTypes.Any(c =>
