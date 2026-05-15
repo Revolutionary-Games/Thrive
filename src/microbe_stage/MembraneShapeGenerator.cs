b26dfcf1541c045f37547231f74c96b372a57b36
HexapodPhilosopher <hexapodworks@proton.me>
2026-05-15 10:49:09 +0100

    Fix normals and update textures again.

--- src/microbe_stage/MembraneShapeGenerator.cs
+++ src/microbe_stage/MembraneShapeGenerator.cs
@@ -162,7 +162,7 @@ public class MembraneShapeGenerator
                 int id = i + layer * vertexCount;
 
                 float y = 0.8f * MathF.Abs(layer - MembraneVerticalResolution) / MembraneVerticalResolution;
-                (float sin, float cos) = MathF.SinCos(uvAngleModifier * i);
+                (float sin, float cos) = MathF.SinCos((uvAngleModifier * i) - 2.3f);
 
                 Vector2 direction = (1.0f - y) * new Vector2(sin, cos) * 0.49f + new Vector2(0.5f, 0.5f);
 
