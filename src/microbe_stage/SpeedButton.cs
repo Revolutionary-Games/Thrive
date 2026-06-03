1a5285a0fa0a7b7b4031db514d5f56da52c83858
PullSL <149987958+PullSL@users.noreply.github.com>
2026-06-03 03:45:27 -0600

    Option to change alternative timescale (#7056)
    * feat: change alternative timescale
    
    * disable slowing game through alternative gamescale in favour of #7062
    
    * Fixed performance options being scrolled by default
    
    * Adjusted the text a little bit
    
    * Use more Thrive-style spacer in GUI
    
    * Revert theme change
    
    * Refactored the way the speed button was made and connected
    
    * Fixed a wrong tooltip text for alternative timescale option
    
    ---------
    
    Co-authored-by: Henri Hyyryläinen <henri.hyyrylainen@gmail.com>

---
+++ src/microbe_stage/SpeedButton.cs
@@ -0,0 +1,43 @@
+﻿using Godot;
+
+/// <summary>
+///   Handles speed button label text and colour.
+/// </summary>
+public partial class SpeedButton : TextureButton
+{
+#pragma warning disable CA2213
+    [Export]
+    private LabelSettings normalSize = null!;
+
+    [Export]
+    private LabelSettings withDecimalSize = null!;
+
+    [Export]
+    private Label label = null!;
+
+#pragma warning restore CA2213
+
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+        OnChanged(Settings.Instance.AlternativeTimescale.Value);
+        Settings.Instance.AlternativeTimescale.OnChanged += OnChanged;
+    }
+
+    public override void _ExitTree()
+    {
+        base._ExitTree();
+        Settings.Instance.AlternativeTimescale.OnChanged -= OnChanged;
+    }
+
+    public void ApplyState(bool value)
+    {
+        label.Modulate = value ? Colors.Black : Colors.White;
+    }
+
+    private void OnChanged(float value)
+    {
+        label.LabelSettings = value % 1 == 0 ? normalSize : withDecimalSize;
+        label.Text = value + "x";
+    }
+}
