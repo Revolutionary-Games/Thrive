bbedace0e37688c855b75602af1fde9f02f1a4a6
Accidental Explorer <alexandervanderhall1996@gmail.com>
2026-07-29 17:27:19 +0200

    Split up MutationsOf in RemoveOrganelle.cs

--- src/auto-evo/mutation_strategy/RemoveOrganelle.cs
+++ src/auto-evo/mutation_strategy/RemoveOrganelle.cs
@@ -50,18 +50,86 @@ public class RemoveOrganelle : IMutationStrategy<Species>
     public List<Mutant>? MutationsOf(Species baseSpecies, double mp, bool lawk,
         Random random, BiomeConditions biomeToConsider)
     {
-        if (baseSpecies is MicrobeSpecies baseMicrobeSpecies)
+        return baseSpecies switch
         {
-            if (mp < Constants.ORGANELLE_REMOVE_COST)
-                return null;
+            MicrobeSpecies microbeSpecies => MutationsOfMicrobe(microbeSpecies, mp, random),
+            MulticellularSpecies multicellularSpecies => MutationsOfMulticellular(multicellularSpecies, mp, random),
+            _ => null,
+        };
+    }
 
-            if (baseMicrobeSpecies.Organelles.Count <= 1)
-                return null;
+    private List<Mutant>? MutationsOfMicrobe(MicrobeSpecies baseSpecies, double mp, Random random)
+    {
+        if (mp < Constants.ORGANELLE_REMOVE_COST)
+            return null;
 
-            var organelles = baseMicrobeSpecies.Organelles.Where(x => Criteria(x.Definition))
+        if (baseSpecies.Organelles.Count <= 1)
+            return null;
+
+        var organelles = baseSpecies.Organelles.Where(x => Criteria(x.Definition))
+            .OrderBy(_ => random.Next()).Take(Constants.AUTO_EVO_ORGANELLE_REMOVE_ATTEMPTS);
+
+        List<Mutant>? mutated = null;
+
+        MutationWorkMemory? workMemory = null;
+
+        foreach (var organelle in organelles)
+        {
+            // The player cannot remove the nucleus, so Auto-Evo should not be able to either
+            if (organelle.Definition == Nucleus)
+                continue;
+
+            // Don't clone organelles as we want to do those ourselves
+            var newSpecies = baseSpecies.Clone(false);
+
+            workMemory ??= new MutationWorkMemory();
+
+            // Is this the best way to do this? Probably not, but this is how mutations.cs does is
+            // and the other way outright did not work
+            // This is now slightly improved - hhyyrylainen
+            var baseOrganelles = baseSpecies.Organelles.Organelles;
+            var count = baseSpecies.Organelles.Count;
+
+            for (var i = 0; i < count; ++i)
+            {
+                var parentOrganelle = baseOrganelles[i];
+
+                if (parentOrganelle == organelle)
+                    continue;
+
+                // Copy the organelle
+                var newOrganelle = parentOrganelle.Clone();
+                newSpecies.Organelles.AddIfPossible(newOrganelle, workMemory.WorkingMemory1,
+                    workMemory.WorkingMemory2);
+            }
+
+            AttachIslandHexes(newSpecies.Organelles, workMemory);
+
+            mutated ??= new List<Mutant>();
+            mutated.Add(new Mutant(newSpecies, mp - Constants.ORGANELLE_REMOVE_COST));
+        }
+
+        return mutated;
+    }
+
+    private List<Mutant>? MutationsOfMulticellular(MulticellularSpecies baseSpecies, double mp, Random random)
+    {
+        var mpCost = Constants.ORGANELLE_REMOVE_COST * Constants.MULTICELLULAR_EDITOR_COST_FACTOR;
+        if (mp < mpCost)
+            return null;
+
+        List<Mutant>? mutated = null;
+
+        var cellTypeCount = baseSpecies.CellTypes.Count;
+
+        for (var i = 0; i < cellTypeCount; ++i)
+        {
+            var baseCellType = baseSpecies.ModifiableCellTypes[i];
+            if (baseCellType.Organelles.Count <= 1)
+                continue;
+
+            var organelles = baseCellType.Organelles.Where(x => Criteria(x.Definition))
                 .OrderBy(_ => random.Next()).Take(Constants.AUTO_EVO_ORGANELLE_REMOVE_ATTEMPTS);
-
-            List<Mutant>? mutated = null;
 
             MutationWorkMemory? workMemory = null;
 
@@ -71,131 +139,68 @@ public class RemoveOrganelle : IMutationStrategy<Species>
                 if (organelle.Definition == Nucleus)
                     continue;
 
+                // The Binding Agent cannot be removed in the Multicellular Stage
+                if (organelle.Definition.HasBindingFeature)
+                    continue;
+
                 // Don't clone organelles as we want to do those ourselves
-                var newSpecies = baseMicrobeSpecies.Clone(false);
+                var newSpecies = baseSpecies.Clone(false);
+                var newCellType = newSpecies.ModifiableCellTypes[i];
+                var newCellTypeOrganelles = newCellType.ModifiableOrganelles;
 
                 workMemory ??= new MutationWorkMemory();
 
-                // Is this the best way to do this? Probably not, but this is how mutations.cs does is
-                // and the other way outright did not work
-                // This is now slightly improved - hhyyrylainen
-                var baseOrganelles = baseMicrobeSpecies.Organelles.Organelles;
-                var count = baseMicrobeSpecies.Organelles.Count;
-
-                for (var i = 0; i < count; ++i)
+                // Clone organelles for the cell types not currently targeted
+                for (var j = 0; j < cellTypeCount; ++j)
                 {
-                    var parentOrganelle = baseOrganelles[i];
+                    var clonedCellType = newSpecies.ModifiableCellTypes[j];
+
+                    if (clonedCellType == newCellType)
+                        continue;
+
+                    var parentCellTypeOrganelles =
+                        baseSpecies.ModifiableCellTypes[j].ModifiableOrganelles;
+                    var copyOrganelleCount = parentCellTypeOrganelles.Count;
+
+                    for (var k = 0; k < copyOrganelleCount; ++k)
+                    {
+                        var parentOrganelle = parentCellTypeOrganelles[k];
+
+                        if (parentOrganelle == organelle)
+                            continue;
+
+                        // Copy the organelle
+                        var copiedOrganelle = parentOrganelle.Clone();
+                        clonedCellType.ModifiableOrganelles.AddIfPossible(copiedOrganelle,
+                            workMemory.WorkingMemory1, workMemory.WorkingMemory2);
+                    }
+                }
+
+                // Clone the organelles for the targeted cell type, excluding the targeted organelle
+                // Is this the best way to do this?
+                var baseOrganelles = baseCellType.ModifiableOrganelles;
+                var organelleCount = baseCellType.Organelles.Count;
+
+                for (var j = 0; j < organelleCount; ++j)
+                {
+                    var parentOrganelle = baseOrganelles[j];
 
                     if (parentOrganelle == organelle)
                         continue;
 
                     // Copy the organelle
                     var newOrganelle = parentOrganelle.Clone();
-                    newSpecies.Organelles.AddIfPossible(newOrganelle, workMemory.WorkingMemory1,
+                    newCellTypeOrganelles.AddIfPossible(newOrganelle, workMemory.WorkingMemory1,
                         workMemory.WorkingMemory2);
                 }
 
-                AttachIslandHexes(newSpecies.Organelles, workMemory);
+                AttachIslandHexes(newCellTypeOrganelles, workMemory);
 
                 mutated ??= new List<Mutant>();
-                mutated.Add(new Mutant(newSpecies, mp - Constants.ORGANELLE_REMOVE_COST));
+                mutated.Add(new Mutant(newSpecies, mp - mpCost));
             }
-
-            return mutated;
         }
 
-        if (baseSpecies is MulticellularSpecies baseMulticellularSpecies)
-        {
-            var mpCost = Constants.ORGANELLE_REMOVE_COST * Constants.MULTICELLULAR_EDITOR_COST_FACTOR;
-            if (mp < mpCost)
-                return null;
-
-            List<Mutant>? mutated = null;
-
-            var cellTypeCount = baseMulticellularSpecies.CellTypes.Count;
-
-            for (var i = 0; i < cellTypeCount; ++i)
-            {
-                var baseCellType = baseMulticellularSpecies.ModifiableCellTypes[i];
-                if (baseCellType.Organelles.Count <= 1)
-                    continue;
-
-                var organelles = baseCellType.Organelles.Where(x => Criteria(x.Definition))
-                    .OrderBy(_ => random.Next()).Take(Constants.AUTO_EVO_ORGANELLE_REMOVE_ATTEMPTS);
-
-                MutationWorkMemory? workMemory = null;
-
-                foreach (var organelle in organelles)
-                {
-                    // The player cannot remove the nucleus, so Auto-Evo should not be able to either
-                    if (organelle.Definition == Nucleus)
-                        continue;
-
-                    // The Binding Agent cannot be removed in the Multicellular Stage
-                    if (organelle.Definition.HasBindingFeature)
-                        continue;
-
-                    // Don't clone organelles as we want to do those ourselves
-                    var newSpecies = baseMulticellularSpecies.Clone(false);
-                    var newCellType = newSpecies.ModifiableCellTypes[i];
-                    var newCellTypeOrganelles = newCellType.ModifiableOrganelles;
-
-                    workMemory ??= new MutationWorkMemory();
-
-                    // Clone organelles for the cell types not currently targeted
-                    for (var j = 0; j < cellTypeCount; ++j)
-                    {
-                        var clonedCellType = newSpecies.ModifiableCellTypes[j];
-
-                        if (clonedCellType == newCellType)
-                            continue;
-
-                        var parentCellTypeOrganelles =
-                            baseMulticellularSpecies.ModifiableCellTypes[j].ModifiableOrganelles;
-                        var copyOrganelleCount = parentCellTypeOrganelles.Count;
-
-                        for (var k = 0; k < copyOrganelleCount; ++k)
-                        {
-                            var parentOrganelle = parentCellTypeOrganelles[k];
-
-                            if (parentOrganelle == organelle)
-                                continue;
-
-                            // Copy the organelle
-                            var copiedOrganelle = parentOrganelle.Clone();
-                            clonedCellType.ModifiableOrganelles.AddIfPossible(copiedOrganelle,
-                                workMemory.WorkingMemory1, workMemory.WorkingMemory2);
-                        }
-                    }
-
-                    // Clone the organelles for the targeted cell type, excluding the targeted organelle
-                    // Is this the best way to do this?
-                    var baseOrganelles = baseCellType.ModifiableOrganelles;
-                    var organelleCount = baseCellType.Organelles.Count;
-
-                    for (var j = 0; j < organelleCount; ++j)
-                    {
-                        var parentOrganelle = baseOrganelles[j];
-
-                        if (parentOrganelle == organelle)
-                            continue;
-
-                        // Copy the organelle
-                        var newOrganelle = parentOrganelle.Clone();
-                        newCellTypeOrganelles.AddIfPossible(newOrganelle, workMemory.WorkingMemory1,
-                            workMemory.WorkingMemory2);
-                    }
-
-                    AttachIslandHexes(newCellTypeOrganelles, workMemory);
-
-                    mutated ??= new List<Mutant>();
-                    mutated.Add(new Mutant(newSpecies, mp - mpCost));
-                }
-            }
-
-            return mutated;
-        }
-
-        return null;
+        return mutated;
     }
 }
