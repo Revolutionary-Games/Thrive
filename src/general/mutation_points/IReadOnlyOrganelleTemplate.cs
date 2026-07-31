78b55146a802246838f77a71f27b5d52f2a35926
Accidental Explorer <alexandervanderhall1996@gmail.com>
2026-07-31 12:19:38 +0200

    Removed code duplication

--- src/general/mutation_points/IReadOnlyOrganelleTemplate.cs
+++ src/general/mutation_points/IReadOnlyOrganelleTemplate.cs
@@ -1,63 +1,14 @@
 ﻿public interface IReadOnlyOrganelleTemplate : IReadOnlyPositionedOrganelle
 {
-    public Compound GetActiveTargetCompound()
-    {
-        if (Definition.HasChemoreceptorComponent)
-        {
-            if (Upgrades?.CustomUpgradeData is not ChemoreceptorUpgrades chemoreceptorData)
-            {
-                return Constants.CHEMORECEPTOR_DEFAULT_COMPOUND;
-            }
+    public Compound GetActiveTargetCompound();
 
-            return chemoreceptorData.TargetCompound;
-        }
+    public Enzyme? GetActiveTargetEnzyme(string internalName);
 
-        // No other organelles are known to set up their active target compounds
-        return Compound.Invalid;
-    }
+    public float GetActiveToxicity();
 
-    public Enzyme? GetActiveTargetEnzyme(string internalName)
-    {
-        if (Definition.HasLysosomeComponent)
-        {
-            return LysosomeComponent.HasActiveEnzyme(Upgrades?.CustomUpgradeData as LysosomeUpgrades, internalName);
-        }
+    public ToxinType GetActiveToxin();
 
-        // No other organelles are known to set up their active enzymes
-        return null;
-    }
-
-    public float GetActiveToxicity()
-    {
-        if (Upgrades?.CustomUpgradeData is ToxinUpgrades toxinData)
-        {
-            return toxinData.Toxicity;
-        }
-
-        return Constants.DEFAULT_TOXICITY;
-    }
-
-    public ToxinType GetActiveToxin()
-    {
-        if (Upgrades?.CustomUpgradeData is ToxinUpgrades toxinData)
-        {
-            return toxinData.BaseType;
-        }
-
-        return ToxinType.Cytotoxin;
-    }
-
-    public Species? GetActiveTargetSpecies()
-    {
-        if (Definition.HasChemoreceptorComponent &&
-            Upgrades?.CustomUpgradeData is ChemoreceptorUpgrades chemoreceptorData)
-        {
-            return chemoreceptorData.TargetSpecies;
-        }
-
-        // No other organelles are known to set up their active target species
-        return null;
-    }
+    public Species? GetActiveTargetSpecies();
 
     public OrganelleTemplate Clone();
 }
