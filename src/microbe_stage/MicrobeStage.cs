2501ff05c464d42d5d120c572b00d870208d071b
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-08-05 09:17:11 +0300

    Added despawning of player gametes after exiting the editor (#7114)

--- src/microbe_stage/MicrobeStage.cs
+++ src/microbe_stage/MicrobeStage.cs
@@ -1104,8 +1104,8 @@ public sealed partial class MicrobeStage : CreatureStageBase<Entity, MicrobeWorl
 
         WorldSimulation.CameraFollowSystem.Disabled = false;
 
-        // When true, another cell is spawned next to the player that they scientifically speaking would have split
-        // from
+        // When true, another cell is spawned next to the player that, scientifically speaking, the player would have
+        // split from
         bool spawnAnotherCell = true;
 
         // If using sexual reproduction, do not spawn another member as we came from a gamete merge
@@ -1365,6 +1365,9 @@ public sealed partial class MicrobeStage : CreatureStageBase<Entity, MicrobeWorl
             HUD.ShowEnvironmentPanel();
             environmentPanelAutomaticallyOpened = true;
         }
+
+        // Despawn player gametes to prevent accidentally immediately triggering the editor entry again
+        DespawnPlayerGametes();
 
         if (TutorialState.Enabled && !TutorialState.ProcessPanelTutorial.Complete)
         {
@@ -2443,6 +2446,20 @@ public sealed partial class MicrobeStage : CreatureStageBase<Entity, MicrobeWorl
                 TransitionManager.Instance.AddSequence(ScreenFade.FadeType.FadeOut, 0.3f, MoveToEditor, false);
             }
         }
+    }
+
+    private void DespawnPlayerGametes()
+    {
+        OnStopGameteMerge();
+
+        WorldSimulation.EntitySystem.Query<GameteCell>(new QueryDescription().WithAll<GameteCell>(),
+            (entity, ref gamete) =>
+            {
+                if (gamete.IsPlayer)
+                {
+                    WorldSimulation.DestroyEntity(entity);
+                }
+            });
     }
 
     private void ClearResolvedTolerancesCache()
