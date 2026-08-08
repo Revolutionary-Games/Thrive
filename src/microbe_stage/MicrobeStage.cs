cc53af7cbfd66094ce8c52818427eb4a4ff3efd6
dligr <dellpls43@gmail.com>
2026-08-08 19:42:46 +0300

    Conserve colony's resources after returning from editor

--- src/microbe_stage/MicrobeStage.cs
+++ src/microbe_stage/MicrobeStage.cs
@@ -1147,6 +1147,16 @@ public sealed partial class MicrobeStage : CreatureStageBase<Entity, MicrobeWorl
 
         if (playerIsMulticellular)
         {
+            // If the player has a colony, all resources need to be transferred to the stem cell to avoid them being
+            // lost after other colony members are deleted
+            if (Player.TryGet<MicrobeColony>(out var colony))
+            {
+                foreach (var compound in MicrobeColonyHelpers.GetCompounds(ref colony).GetCompoundDictionary())
+                {
+                    Player.Get<CompoundStorage>().Compounds.Compounds[compound.Key] = compound.Value;
+                }
+            }
+
             ref var multicellularSpeciesType = ref Player.Get<MulticellularSpeciesMember>();
 
             var resolvedTolerances = MicrobeEnvironmentalToleranceCalculations.ResolveToleranceValues(
