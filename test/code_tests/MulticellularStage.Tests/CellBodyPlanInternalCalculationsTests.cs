fc8482877f78ed77b4ad386984d191464cab1b28
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-08-14 16:40:46 +0300

    Test that cell body plan buffs go in the right direction

---
+++ test/code_tests/MulticellularStage.Tests/CellBodyPlanInternalCalculationsTests.cs
@@ -0,0 +1,23 @@
+﻿namespace ThriveTest.MulticellularStage.Tests;
+
+using Xunit;
+
+public class CellBodyPlanInternalCalculationsTests
+{
+    [Fact]
+    public void CalculateFinalColonyRotation_AppliesCellCountAsPenaltyAndActomyosinAsBonus()
+    {
+        const float averageCellRotationSpeed = 2.0f;
+
+        var singleCellRotationSpeed = CellBodyPlanInternalCalculations.CalculateFinalColonyRotation(
+            averageCellRotationSpeed, 0, 1);
+        var colonyRotationSpeed = CellBodyPlanInternalCalculations.CalculateFinalColonyRotation(
+            averageCellRotationSpeed, 0, 2);
+        var rotationSpeedWithActomyosin = CellBodyPlanInternalCalculations.CalculateFinalColonyRotation(
+            averageCellRotationSpeed, 1, 1);
+
+        Assert.True(colonyRotationSpeed > singleCellRotationSpeed);
+        Assert.True(rotationSpeedWithActomyosin < singleCellRotationSpeed);
+        Assert.True(rotationSpeedWithActomyosin < colonyRotationSpeed);
+    }
+}
