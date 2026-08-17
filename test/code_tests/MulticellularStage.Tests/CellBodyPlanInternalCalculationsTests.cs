acd5c090ea0133125bce33c5c54aa29b86fec5a3
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-08-17 15:05:19 +0300

    Add an explanation comment on a test

--- test/code_tests/MulticellularStage.Tests/CellBodyPlanInternalCalculationsTests.cs
+++ test/code_tests/MulticellularStage.Tests/CellBodyPlanInternalCalculationsTests.cs
@@ -9,14 +9,17 @@ public class CellBodyPlanInternalCalculationsTests
     {
         const float averageCellRotationSpeed = 2.0f;
 
-        var singleCellRotationSpeed = CellBodyPlanInternalCalculations.CalculateFinalColonyRotation(
-            averageCellRotationSpeed, 0, 1);
-        var colonyRotationSpeed = CellBodyPlanInternalCalculations.CalculateFinalColonyRotation(
-            averageCellRotationSpeed, 0, 2);
-        var rotationSpeedWithActomyosin = CellBodyPlanInternalCalculations.CalculateFinalColonyRotation(
-            averageCellRotationSpeed, 1, 1);
+        var singleCellRotationSpeed =
+            CellBodyPlanInternalCalculations.CalculateFinalColonyRotation(averageCellRotationSpeed, 0, 1);
+        var colonyRotationSpeed =
+            CellBodyPlanInternalCalculations.CalculateFinalColonyRotation(averageCellRotationSpeed, 0, 2);
+        var rotationSpeedWithActomyosin =
+            CellBodyPlanInternalCalculations.CalculateFinalColonyRotation(averageCellRotationSpeed, 1, 1);
 
+        // Higher values are slower, so this means that colony rotation is slower than single cell rotation
         Assert.True(colonyRotationSpeed > singleCellRotationSpeed);
+
+        // But actomyosin is faster than single cell rotation and the colony rotation
         Assert.True(rotationSpeedWithActomyosin < singleCellRotationSpeed);
         Assert.True(rotationSpeedWithActomyosin < colonyRotationSpeed);
     }
