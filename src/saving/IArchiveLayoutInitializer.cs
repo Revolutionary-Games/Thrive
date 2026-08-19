93ac548d118e8727bb54b1d049afb00782ec1d18
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-08-19 16:13:57 +0300

    Fixed a rare saving bug with self-referencing chemoreceptor
    that was specifically only happening to multicellular species

---
+++ src/saving/IArchiveLayoutInitializer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+
+internal interface IArchiveLayoutInitializer
+{
+    /// <summary>
+    ///   Only call when deserializing from an archive. This overrides the list contents with the actual data.
+    /// </summary>
+    public void InitializeFromArchive(IList data, Delegate? added, Delegate? removed);
+}
