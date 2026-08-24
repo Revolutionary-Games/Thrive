95e65a89c4c7910267b31320f68ade09960b58ea
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-08-24 18:38:30 +0300

    A rework of the colony compounds distribution code

--- src/microbe_stage/ColonyCompoundBag.cs
+++ src/microbe_stage/ColonyCompoundBag.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Arch.Core;
 using Arch.Core.Extensions;
 using Components;
@@ -11,7 +12,16 @@ using Godot;
 public class ColonyCompoundBag : ICompoundStorage
 {
     private readonly object refreshListLock = new();
+
+    /// <summary>
+    ///   Used just for getting total compounds in the colony
+    /// </summary>
     private readonly Dictionary<Compound, float> summedCompoundsBuffer = new();
+
+    // These variables are for distributing compounds and are not always kept up to date
+    private readonly HashSet<Compound> usefulCompoundsBuffer = new();
+    private readonly Dictionary<Compound, float> availableCompounds = new();
+    private readonly Dictionary<Compound, float> compoundCapacities = new();
 
     private List<CompoundBag> colonyBags = new();
     private List<CompoundBag> bagBuilder = new();
@@ -73,15 +83,224 @@ public class ColonyCompoundBag : ICompoundStorage
     public void DistributeCompoundSurplus()
     {
         var bags = GetCompoundBags();
-        FillSummedCompoundsBuffer(bags);
 
-        foreach (var (compound, compoundAmount) in summedCompoundsBuffer)
+        GD.Print("Compound bag count: " + bags.Count);
+
+        usefulCompoundsBuffer.Clear();
+        availableCompounds.Clear();
+        compoundCapacities.Clear();
+
+        // Determine compounds that need sharing first
+        bool first = true;
+        foreach (var compoundBag in bags)
         {
-            var compoundDefinition = SimulationParameters.GetCompound(compound);
-            if (!TryPrepareCompoundDistribution(compound, compoundAmount, bags, compoundDefinition, out var ratio))
-                continue;
+            compoundBag.CopyUsefulToHash(usefulCompoundsBuffer, first);
+            first = false;
+        }
 
-            RedistributeCompoundAcrossBags(compound, compoundDefinition, ratio, bags);
+        // Don't share ATP
+        usefulCompoundsBuffer.Remove(Compound.ATP);
+
+        GD.Print("Useful compounds:");
+        foreach (var compound in usefulCompoundsBuffer)
+        {
+            GD.Print($" {compound}");
+        }
+
+        bool needsNonUsefulAdjustments = false;
+
+        // This is used as a sum of non-useful compounds
+        summedCompoundsBuffer.Clear();
+
+        // Then offer up the compounds
+        foreach (var bag in bags)
+        {
+            GD.Print("Next bag");
+
+            foreach (var bagCompound in bag.Compounds)
+            {
+                // Don't offer compounds to move that nobody finds useful in this colony
+                if (!usefulCompoundsBuffer.Contains(bagCompound.Key))
+                    continue;
+
+                if (bagCompound.Value <= 0)
+                    continue;
+
+                GD.Print($" Amount({bagCompound.Key}): {bagCompound.Value}");
+
+                // Has available compound, try to share with other bags
+                if (availableCompounds.TryGetValue(bagCompound.Key, out var availableAmount))
+                {
+                    availableCompounds[bagCompound.Key] = availableAmount + bagCompound.Value;
+                }
+                else
+                {
+                    availableCompounds.Add(bagCompound.Key, bagCompound.Value);
+                }
+
+                // If not useful, this will not receive any of this compound.
+                if (!bag.IsUseful(bagCompound.Key))
+                {
+                    // Store for later as we don't know yet how much of this compound other bags can take
+                    needsNonUsefulAdjustments = true;
+
+                    if (summedCompoundsBuffer.TryGetValue(bagCompound.Key, out var summedAmount))
+                    {
+                        summedCompoundsBuffer[bagCompound.Key] = summedAmount + bagCompound.Value;
+                    }
+                    else
+                    {
+                        summedCompoundsBuffer.Add(bagCompound.Key, bagCompound.Value);
+                    }
+                }
+            }
+
+            // Then capture capacities as the bag might not have every compound in it, so the above loop would miss
+            // them
+            foreach (var compound in usefulCompoundsBuffer)
+            {
+                // If not useful, don't participate in receiving this compound.
+                // The capacity check here returns 0 if the compound is not useful.
+                var bagCapacity = bag.GetCapacityForCompound(compound);
+                if (bagCapacity <= 0)
+                    continue;
+
+                GD.Print($" Capacity({compound}): {bagCapacity}");
+
+                // Report the capacity so that correct fraction of compound is received
+                if (compoundCapacities.TryGetValue(compound, out var capacity))
+                {
+                    compoundCapacities[compound] = capacity + bagCapacity;
+                }
+                else
+                {
+                    compoundCapacities.Add(compound, bagCapacity);
+                }
+            }
+        }
+
+        if (needsNonUsefulAdjustments)
+        {
+            // We want to remove these amounts to not cause infinite compounds to accumulate
+            foreach (var entry in summedCompoundsBuffer)
+            {
+                var receiverCapacity = compoundCapacities[entry.Key];
+
+                // Exclude already used space
+                foreach (var bag in bags)
+                {
+                    if (bag.IsUseful(entry.Key))
+                    {
+                        receiverCapacity -= bag.GetCompoundAmount(entry.Key);
+                    }
+                }
+
+                // Share the outgoing capacity across cells if there isn't enough space
+                var sendFactor = Math.Min(1, receiverCapacity / entry.Value);
+
+                GD.Print($"Not useful: {entry.Key}, available space: {receiverCapacity}");
+
+                // Then take as much from the sender bags as the receivers can take
+                foreach (var bag in bags)
+                {
+                    if (bag.IsUseful(entry.Key))
+                        continue;
+
+                    var amount = bag.GetCompoundAmount(entry.Key);
+                    if (amount <= 0)
+                        continue;
+
+                    // Take as much from the bag as can fit in receivers
+                    var toTake = Math.Min(receiverCapacity, amount * sendFactor);
+                    GD.Print($"Moving this amount of not useful out: {toTake}");
+
+                    // Share the outgoing capacity across cells if there isn't enough space
+                    /*if (receiverCapacity < entry.Value)
+                    {
+                        toTake *= amount / entry.Value;
+                        GD.Print($"To take adjustment to: {toTake}");
+                    }*/
+
+                    // If ran out of space, don't take anything.
+                    if (toTake > 0)
+                    {
+                        bag.TakeCompound(entry.Key, toTake);
+                        GD.Print($"Amount left in sender bag: {bag.GetCompoundAmount(entry.Key)}");
+                        receiverCapacity -= toTake;
+                    }
+
+                    // If we didn't take the full amount, then we need to reduce the number of compounds to share
+                    var couldNotShare = amount - toTake;
+                    if (couldNotShare > 0)
+                    {
+                        availableCompounds[entry.Key] -= couldNotShare;
+                        GD.Print($"Adjusted available compound down to: {availableCompounds[entry.Key]}");
+                    }
+
+                    GD.Print($"Finished one bag, capacity left: {receiverCapacity}");
+                }
+            }
+
+            summedCompoundsBuffer.Clear();
+        }
+
+        GD.Print("Compound capacities:");
+        foreach (var entry in compoundCapacities)
+        {
+            GD.Print($" {entry.Key}: {entry.Value}");
+        }
+
+        GD.Print("Compounds to share:");
+        foreach (var entry in availableCompounds)
+        {
+            GD.Print($" {entry.Key}: {entry.Value}");
+        }
+
+#if DEBUG
+        if (availableCompounds.ContainsKey(Compound.ATP))
+            throw new InvalidOperationException("ATP compound should not be shareable");
+#endif
+
+        foreach (var compound in usefulCompoundsBuffer)
+        {
+            if (!compoundCapacities.TryGetValue(compound, out var storage) || storage <= 0)
+                ReportZeroCapacityForUsefulCompoundOnce(SimulationParameters.GetCompound(compound));
+        }
+
+        // Now, we know how many shareable compounds there are and how much capacity there is for each compound.
+        // So we can give each eligible receiver their share.
+
+        foreach (var entry in availableCompounds)
+        {
+            GD.Print($"Amount of {entry.Key} to share: {entry.Value}");
+            foreach (var bag in bags)
+            {
+                var capacity = bag.GetCapacityForCompound(entry.Key);
+
+                // Don't participate if you can't receive the compound
+                if (capacity <= 0)
+                    continue;
+
+                // Other bags will grab a share according to their capacity
+                var targetLevel = availableCompounds[entry.Key] * (capacity / compoundCapacities[entry.Key]);
+
+                var difference = targetLevel - bag.GetCompoundAmount(entry.Key);
+
+                GD.Print($"Bag gets ({entry.Key}) {targetLevel} out of {entry.Value}. Adjustment: {difference}");
+
+                if (difference > 0)
+                {
+                    bag.AddCompound(entry.Key, difference);
+                }
+                else
+                {
+                    // TODO: should we check the return value here to ensure enough was taken?
+                    // We need to negate the value to get it to be positive for taking away compounds
+                    bag.TakeCompound(entry.Key, -difference);
+                }
+
+                GD.Print($" Resulting amount of {entry.Key}: {bag.GetCompoundAmount(entry.Key)}");
+            }
         }
     }
 
@@ -213,46 +432,6 @@ public class ColonyCompoundBag : ICompoundStorage
         }
     }
 
-    private bool TryPrepareCompoundDistribution(Compound compound, float compoundAmount, List<CompoundBag> bags,
-        CompoundDefinition compoundDefinition, out float ratio)
-    {
-        ratio = 0;
-
-        if (!compoundDefinition.CanBeDistributed)
-            return false;
-
-        float compoundCapacity = 0;
-        var usefulInAnyBag = false;
-
-        foreach (var bag in bags)
-        {
-            bool isUseful = bag.IsUseful(compound);
-            if (!usefulInAnyBag && isUseful)
-                usefulInAnyBag = true;
-
-            // If a bag doesn't consider a compound useful, it won't take it, thus we would calculate things
-            // incorrectly (in RedistributeCompoundAcrossBags), so capacity is not given for non-useful compounds
-            if (!isUseful)
-                continue;
-
-            compoundCapacity += bag.GetCapacityForCompound(compound);
-        }
-
-        if (!usefulInAnyBag)
-            return false;
-
-        // This is just an error print, can be removed if no more NaN issues occur.
-        // See also CompoundBag.FixNaNCompounds, which fixes NaN values after they occur
-        if (compoundCapacity == 0)
-        {
-            ReportZeroCapacityForUsefulCompoundOnce(compoundDefinition);
-            return false;
-        }
-
-        ratio = compoundAmount / compoundCapacity;
-        return true;
-    }
-
     private void ReportZeroCapacityForUsefulCompoundOnce(CompoundDefinition compoundDefinition)
     {
         if (nanIssueReported)
@@ -261,27 +440,6 @@ public class ColonyCompoundBag : ICompoundStorage
         GD.PrintErr($"Compound {compoundDefinition.Name} is set to useful but has a Capacity of zero, " +
             "https://github.com/Revolutionary-Games/Thrive/issues/3201");
         nanIssueReported = true;
-    }
-
-    private void RedistributeCompoundAcrossBags(Compound compound, CompoundDefinition compoundDefinition,
-        float ratio, List<CompoundBag> bags)
-    {
-        foreach (var bag in bags)
-        {
-            if (!bag.IsUseful(compoundDefinition))
-                continue;
-
-            var expectedAmount = ratio * bag.GetCapacityForCompound(compound);
-            var surplus = bag.GetCompoundAmount(compound) - expectedAmount;
-
-            if (surplus > 0)
-            {
-                bag.TakeCompound(compound, surplus);
-                continue;
-            }
-
-            bag.AddCompound(compound, -surplus);
-        }
     }
 
     private List<CompoundBag> GetCompoundBags()
