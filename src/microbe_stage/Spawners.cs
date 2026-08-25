ad6e8463354f362d8236aec8099940ce5ca09730
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-08-25 18:20:40 +0300

    Check actual added compounds for a problem on chunk spawn

--- src/microbe_stage/Spawners.cs
+++ src/microbe_stage/Spawners.cs
@@ -449,13 +449,13 @@ public static class SpawnHelpers
             }
 
 #if DEBUG
-            var toCheck = chunkType.Compounds.First();
+            var toCheck = compounds.Compounds.First();
 
             // ReSharper disable once CompareOfFloatsByEqualityOperator
-            if (compounds.GetCompoundAmount(toCheck.Key) != toCheck.Value.Amount)
+            if (compounds.GetCompoundAmount(toCheck.Key) != toCheck.Value)
             {
                 throw new Exception($"Chunk compound adding failed ({compounds.GetCompoundAmount(toCheck.Key)} != " +
-                    $"{toCheck.Value.Amount})");
+                    $"{toCheck.Value})");
             }
 #endif
 
