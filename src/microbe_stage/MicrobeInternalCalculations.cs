ba6f94dabc6c78bba5b2510e25759cdca2114221
ChevyLevi <49272922+ChevyLevi@users.noreply.github.com>
2026-09-09 21:49:17 +0800

    switch `Sum()` to foreach

--- src/microbe_stage/MicrobeInternalCalculations.cs
+++ src/microbe_stage/MicrobeInternalCalculations.cs
@@ -85,8 +85,14 @@ public static class MicrobeInternalCalculations
     public static float GetTotalNominalCapacity(IEnumerable<OrganelleTemplate> organelles,
         float totalSpecializationBonus)
     {
-        return organelles.Sum(o => GetNominalCapacityForOrganelle(o.Definition, o.Upgrades,
-            totalSpecializationBonus));
+        double capacity = 0;
+        foreach (var organelle in organelles)
+        {
+            capacity += GetNominalCapacityForOrganelle(organelle.Definition, organelle.Upgrades,
+                totalSpecializationBonus);
+        }
+
+        return (float)capacity;
     }
 
     public static Dictionary<Compound, float> GetTotalSpecificCapacity(IReadOnlyList<OrganelleTemplate> organelles,
