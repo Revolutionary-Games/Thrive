ea24005ead06dac36c3284bddb6a9edc7b2a2814
patryk26g <patryk26g@gmail.com>
2026-09-09 11:41:05 +0200

    Fix seconds

--- src/auto-evo/AutoEvoExploringTool.cs
+++ src/auto-evo/AutoEvoExploringTool.cs
@@ -967,7 +967,7 @@ public partial class AutoEvoExploringTool : NodeWithInput, ISpeciesDataProvider
         var (microbeSpeciesHexSizeAverage, microbeSpeciesHexSizeStandardDeviation) =
             worldsList.Select(w => w.MicrobeSpeciesAverageHexSize).CalculateAverageAndStandardDeviation();
 
-        var (totalSeconds, totalSecondsStandardDeviation) = worldsList.Select(w => w.TotalTimeUsed.Seconds)
+        var (totalSeconds, totalSecondsStandardDeviation) = worldsList.Select(w => w.TotalTimeUsed.TotalSeconds)
             .CalculateAverageAndStandardDeviation();
 
         var stringBuilder = new StringBuilder(1000);
