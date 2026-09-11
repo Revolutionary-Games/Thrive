a72e55e156d61fb454c611951763409d24ddf9eb
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-09-11 17:31:57 +0300

    Improved the reset history test

--- test/microbe_stage.tests/MicrobeEditorTests.cs
+++ test/microbe_stage.tests/MicrobeEditorTests.cs
@@ -17,10 +17,14 @@ public class MicrobeEditorTests
         var oldPlacement2 = new OrganellePlacementActionData(
             new OrganelleTemplate(new OrganelleDefinition { Hexes = [new Hex(0, 0)] }, new Hex(1, 0), 0),
             new Hex(1, 0), 0);
+        var oldPlacement3 = new OrganellePlacementActionData(
+            new OrganelleTemplate(new OrganelleDefinition { Hexes = [new Hex(0, 0)] }, new Hex(2, 0), 0),
+            new Hex(2, 0), 0);
 
         history.AddAction(new CombinedEditorAction(
             new SingleEditorAction<OrganellePlacementActionData>(_ => { }, _ => { }, oldPlacement1),
-            new SingleEditorAction<OrganellePlacementActionData>(_ => { }, _ => { }, oldPlacement2)));
+            new SingleEditorAction<OrganellePlacementActionData>(_ => { }, _ => { }, oldPlacement2),
+            new SingleEditorAction<OrganellePlacementActionData>(_ => { }, _ => { }, oldPlacement3)));
 
         var newCell = new NewMicrobeActionData(new OrganelleLayout<OrganelleTemplate>(), new MembraneType(), 0,
             Colors.White, null, null);
@@ -33,6 +37,7 @@ public class MicrobeEditorTests
 
         AssertThat(history.HexPlacedThisSession<OrganelleTemplate, CellType>(oldPlacement1.PlacedHex)).IsFalse();
         AssertThat(history.HexPlacedThisSession<OrganelleTemplate, CellType>(oldPlacement2.PlacedHex)).IsFalse();
+        AssertThat(history.HexPlacedThisSession<OrganelleTemplate, CellType>(oldPlacement3.PlacedHex)).IsFalse();
         AssertThat(history.HexPlacedThisSession<OrganelleTemplate, CellType>(newPlacement.PlacedHex)).IsTrue();
     }
 }
