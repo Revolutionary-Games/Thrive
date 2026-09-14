000b790951d236a8f6566daedefa39775be3fda5
dligr <dellpls43@gmail.com>
2026-09-15 01:01:34 +0300

    Fix a linter issue

--- src/multicellular_stage/editor/CellBodyPlanEditorComponent.Callbacks.cs
+++ src/multicellular_stage/editor/CellBodyPlanEditorComponent.Callbacks.cs
@@ -267,7 +267,7 @@ public partial class CellBodyPlanEditorComponent
     [ArchiveAllowedMethod]
     private void DoSporeCellChangeAction(SpecialCellTypeChangeActionData data)
     {
-        ChangeCellType(data.OldCellType, data.NewCellType, data.CellArchetype);
+        ChangeCellType(data.OldCellType, data.NewCellType, SpecialCellArchetype.Spore);
     }
 
     [ArchiveAllowedMethod]
@@ -278,7 +278,7 @@ public partial class CellBodyPlanEditorComponent
             CellTypeVisualsOverride?.ForgetChanges(data.NewCellType);
         }
 
-        ChangeCellType(data.NewCellType, data.OldCellType, data.CellArchetype);
+        ChangeCellType(data.NewCellType, data.OldCellType, SpecialCellArchetype.Spore);
     }
 
     [ArchiveAllowedMethod]
