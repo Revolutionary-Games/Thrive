dea0189db17959419a032eb2686176a237131807
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2026-09-17 15:10:21 +0300

    Fixed warnings from unused callback parameters
    that only exist for save compatibility reasons

--- src/multicellular_stage/editor/CellBodyPlanEditorComponent.Callbacks.cs
+++ src/multicellular_stage/editor/CellBodyPlanEditorComponent.Callbacks.cs
@@ -229,29 +229,33 @@ public partial class CellBodyPlanEditorComponent
         Editor.DirtyMutationPointsCache();
     }
 
-    // These next 6 functions are only here for save compatibility, and shouldn't be used otherwise
+    // These next 6 functions are only here for save compatibility and shouldn't be used otherwise
     [ArchiveAllowedMethod]
     private void DoGameteACellChangeAction(GameteACellTypeChangeActionData data)
     {
         // Because of the new gamete selector functionality, this intentionally does nothing
+        _ = data;
     }
 
     [ArchiveAllowedMethod]
     private void UndoGameteACellChangeAction(GameteACellTypeChangeActionData data)
     {
         // Because of the new gamete selector functionality, this intentionally does nothing
+        _ = data;
     }
 
     [ArchiveAllowedMethod]
     private void DoGameteBCellChangeAction(GameteBCellTypeChangeActionData data)
     {
         // Because of the new gamete selector functionality, this intentionally does nothing
+        _ = data;
     }
 
     [ArchiveAllowedMethod]
     private void UndoGameteBCellChangeAction(GameteBCellTypeChangeActionData data)
     {
         // Because of the new gamete selector functionality, this intentionally does nothing
+        _ = data;
     }
 
     [ArchiveAllowedMethod]
