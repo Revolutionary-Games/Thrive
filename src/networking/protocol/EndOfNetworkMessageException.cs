170fb31ac42dbf18aa90065054e68ec0c472333a
Francesco <francescosollazzi37@gmail.com>
2026-09-21 11:23:01 +0200

    EndOfNetworkMessageException moved to its own file

---
+++ src/networking/protocol/EndOfNetworkMessageException.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+/// <summary>
+///   Thrown when a network message ends before all the expected data was read
+/// </summary>
+public class EndOfNetworkMessageException() : Exception("Network message ended unexpectedly");
